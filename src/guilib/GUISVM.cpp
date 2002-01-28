5ab3f2f43a8e76dad1df1e0371dd277935365154
author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1012253787 -0000 <author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1012253787 -0000>
2002-01-28 21:36:27 +0000

    ok. svm testings seems to work with svm LIGHT. There is still the need for set_preproc I am doing this now.

--- src/guilib/GUISVM.cpp
+++ src/guilib/GUISVM.cpp
@@ -99,6 +99,35 @@ bool CGUISVM::train(char* param)
 bool CGUISVM::test(char* param)
 {
 	param=CIO::skip_spaces(param);
+	char outputname[1024];
+	char rocfname[1024];
+	FILE* outputfile=stdout;
+	FILE* rocfile=NULL;
+	int numargs=-1;
+
+	numargs=sscanf(param, "%s %s", outputname, rocfname);
+
+	if (numargs>=1)
+	{
+		outputfile=fopen(outputname, "w");
+
+		if (!outputfile)
+		{
+			CIO::message(stderr,"ERROR: could not open %s\n",outputname);
+			return false;
+		}
+
+		if (numargs==2) 
+		{
+			rocfile=fopen(rocfname, "w");
+
+			if (!rocfile)
+			{
+				CIO::message(stderr,"ERROR: could not open %s\n",rocfname);
+				return false;
+			}
+		}
+	}
 
 	CFeatures* trainfeatures=gui->guifeatures.get_train_features();
 	CFeatures* testfeatures=gui->guifeatures.get_test_features();
@@ -149,7 +178,40 @@ bool CGUISVM::test(char* param)
 
 	CIO::message("starting svm testing\n") ;
 	svm->set_C(C) ;
-	svm->svm_test(trainfeatures, testfeatures) ;
+	REAL* output= svm->svm_test(trainfeatures, testfeatures) ;
+	int total=testfeatures->get_number_of_examples();
+
+	int* label= new int[total];	
+
+	REAL* fp= new REAL[total];	
+	REAL* tp= new REAL[total];	
+
+	for (int dim=0; dim<total; dim++)
+	{
+		label[dim]= testfeatures->get_label(dim);
+
+		if (math.sign((REAL) output[dim])==label[dim])
+			fprintf(outputfile,"%+.8g (%+d)\n",(double) output[dim], label[dim]);
+		else
+			fprintf(outputfile,"%+.8g (%+d)(*)\n",(double) output[dim], label[dim]);
+	}
+
+	int possize,negsize;
+	int pointeven=math.calcroc(fp, tp, output, label, total, possize, negsize, rocfile);
+
+	double correct=possize*tp[pointeven]+(1-fp[pointeven])*negsize;
+	double fpo=fp[pointeven]*negsize;
+	double fne=(1-tp[pointeven])*possize;
+
+	printf("classified:\n");
+	printf("\tcorrect:%i\n", int (correct));
+	printf("\twrong:%i (fp:%i,fn:%i)\n", int(fpo+fne), int (fpo), int (fne));
+	printf("of %i samples (c:%f,w:%f,fp:%f,tp:%f)\n",total, correct/total, 1-correct/total, (double) fp[pointeven], (double) tp[pointeven]);
+
+	delete[] fp;
+	delete[] tp;
+	delete[] output;
+	delete[] label;
 	return true;
 }
 
