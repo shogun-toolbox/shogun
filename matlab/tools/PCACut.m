6056be58f3d744e5a8b7a1dbf077f2f455712e9a
author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1012777632 -0000 <author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1012777632 -0000>
2002-02-03 23:07:12 +0000

    *** empty log message ***

--- matlab/tools/PCACut.m
+++ matlab/tools/PCACut.m
@@ -1,24 +1,17 @@
-function [T,zeigen]=cleaner(Z, thresh) ;
+function [T,zeigen]=pcacut(Z, thresh) ;
 
+%calc mean
+mw=mean(Z,2) ;
 
-fprintf('computing PCA in %i dimensions\n', size(covz,1)) ;
-
-m=mean(Z,2) ;
-for i=1:size(Z,2)
-     Z(:,i) = Z(:,i)-m ;
-end ;
-
-covz = (Z * Z';
-covz = covz / size(Z,2);	       % rescale with sample size
-
-%d = eig(covz)
+%calc covariance matrix
+covz=cov(Z', 1);
 
 [v, d] = eig(covz);			% get the eigensystem,
                                         % negative eigenvalues have
                                         % to go ...
 d = diag(d);				% cut out diagonal
-[d,idx] = sort(d) ;
-v=v(:,idx) ;
+[d,idx] = sort(d);
+v=v(:,idx);
 dgood = find(d > thresh) ;
 
 fprintf('reducing to %i dimensions (EV:%e-%e)\n', length(dgood), min(d(dgood)), max(d(dgood))) ;
--- matlab/tools/read_feature_matrix.m
+++ matlab/tools/read_feature_matrix.m
@@ -1,4 +1,8 @@
-fname='/short/x46/sonne/svm/topfeatures.orig'
+function [matrix, labels] = read_feature_matrix(fname)
+%function [matrix, labels] = read_feature_matrix(fname)
+%
+% load feature matrix and labels
+% from genefinder save_feature_matrix output
 fid=fopen(fname, 'r');
 intlen=fread(fid, 1, 'char')
 doublelen=fread(fid, 1, 'char')
@@ -10,5 +14,5 @@ num_vec=fread(fid, 1, intstring)
 num_feat=fread(fid, 1, intstring)
 preprocd=fread(fid, 1, intstring)
 
-feature_matrix=fread(fid, [ num_feat num_vec ], doublestring);
+matrix=fread(fid, [ num_feat num_vec ], doublestring);
 labels=fread(fid, num_vec, intstring);
