38ace24df2d2b94fe9d9560c240130cd3439d643
author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1046104943 -0000 <author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1046104943 -0000>
2003-02-24 16:42:23 +0000

    starting to implement 3 'new' kernels, taking the linear kernel as code base:
    1) FixedDegreeKernel: linear kernel in n-th order alphabet
    2) WeightedDegreeKernel: linear kernel in 1...n-th order alphabet, with weighting scheme for each 'order'
    3) HistogramKernel: implimicitly computed FK from linear HMM

---
+++ src/kernel/FixedDegreeKernel.h
@@ -0,0 +1,40 @@
+#ifndef _LINEARKERNEL_H___
+#define _LINEARKERNEL_H___
+
+#include "lib/common.h"
+#include "kernel/Kernel.h"
+
+class CLinearKernel: public CKernel
+{
+ public:
+  CLinearKernel(long size, bool rescale) ;
+  ~CLinearKernel() ;
+  
+  virtual void init(CFeatures* l, CFeatures* r);
+  virtual void cleanup();
+
+  /// load and save kernel init_data
+  bool load_init(FILE* src);
+  bool save_init(FILE* dest);
+
+  bool check_features(CFeatures* f);
+
+  // return the name of a kernel
+  virtual const char* get_name() { return "Linear" ; } ;
+
+ protected:
+  /// compute kernel function for features a and b
+  /// idx_{a,b} denote the index of the feature vectors
+  /// in the corresponding feature object
+  REAL compute(long idx_a, long idx_b);
+  /*    compute_kernel*/
+
+  void init_rescale();
+  
+ protected:
+  bool rescale ;
+  double scale ;
+};
+
+#endif
+
