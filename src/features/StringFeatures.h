8ff6906307ed1cac82007e236c22a5a9cbd99f4a
author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1046364795 -0000 <author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1046364795 -0000>
2003-02-27 16:53:15 +0000

    *** empty log message ***

--- src/features/StringFeatures.h
+++ src/features/StringFeatures.h
@@ -1,32 +1,39 @@
 #ifndef _CSTRINGFEATURES__H__
 #define _CSTRINGFEATURES__H__
 
-#include "preproc/Preproc.h"
+#include "preproc/PreProc.h"
 #include "features/Features.h"
 
 class CStringFeatures: public CFeatures
 {
- public:
-  CStringFeatures(long size);
-  ~CStringFeatures() { };
-  
-  virtual EType get_feature_type() { return F_STRING ; } ;
-  
-  /** get feature vector for sample num
-      from the matrix as it is if matrix is
-      initialized, else return
-      preprocessed compute_feature_vector  
-      @param num index of feature vector
-      @param len length is returned by reference
-  */
-  char* get_feature_vector(int num, int& len, bool& free);
-  bool free_feature_vector(char* feat_vec, bool free);
-  
-protected:
-  /// compute feature vector for sample num
-  /// len is returned by reference
-  virtual char* compute_feature_vector(int num, int& len)=0;
+	public:
+		CStringFeatures(long size);
+		CStringFeatures(const CStringFeatures & orig);
 
+		virtual ~CStringFeatures();
 
+		virtual EType get_feature_type() { return F_STRING ; } ;
+
+		/** get feature vector for sample num
+		  from the matrix as it is if matrix is
+		  initialized, else return
+		  preprocessed compute_feature_vector  
+		  @param num index of feature vector
+		  @param len length is returned by reference
+		  */
+		char* get_feature_vector(int num, int& len, bool& free);
+		void free_feature_vector(CHAR* feat_vec, int num, bool free);
+
+		virtual bool preproc_feature_matrix(bool force_preprocessing=false);
+		virtual long get_num_vectors()=0 ;
+		virtual CFeatures* duplicate() const=0 ;
+		virtual bool save(FILE* dest)=0;
+
+	protected:
+		/// compute feature vector for sample num
+		/// len is returned by reference
+		virtual char* compute_feature_vector(int num, int& len)=0;
+
+		CHAR* feature_matrix;
 };
 #endif
