386d2a7a521763c04fe104d14244f644c9c77e85
author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1052919869 -0000 <author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1052919869 -0000>
2003-05-14 13:44:29 +0000

    forgot to add the files :-(

---
+++ src/features/SparseRealFeatures.h
@@ -0,0 +1,29 @@
+#ifndef _SPARSEREALFEATURES__H__
+#define _SPARSEREALFEATURES__H__
+
+#include "features/SparseFeatures.h"
+#include "lib/common.h"
+
+class CSparseRealFeatures: public CSparseFeatures<REAL>
+{
+ public:
+  CSparseRealFeatures(long size) : CSparseFeatures<REAL>(size)
+  {
+  }
+
+  CSparseRealFeatures(const CSparseRealFeatures & orig) : CSparseFeatures<REAL>(orig)
+  {
+  }
+
+  CSparseRealFeatures(char* fname) : CSparseFeatures<REAL>(fname)
+  {
+	load(fname);
+  }
+
+  virtual CFeatures* duplicate() const;
+  virtual EType get_feature_type() { return F_REAL; }
+
+  virtual bool load(char* fname);
+  virtual bool save(char* fname);
+};
+#endif
