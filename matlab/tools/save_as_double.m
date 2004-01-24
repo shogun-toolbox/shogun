eb734392775e31d035380f11a75e22c750e1b5b1
nobody <nobody@localhost>
2004-01-24 07:25:20 +0000

    This commit was manufactured by cvs2svn to create branch 'genefinder_0_1'.

--- matlab/tools/save_as_double.m
+++
@@ -1,9 +0,0 @@
-function save_as_double(L, fname) ;
-% save_as_ascii(L, fname) ;
-
-fd = fopen(fname, 'w+') ;
-for i=1:size(L,2) ;
-  fwrite(fd, L(:,i), 'double') ;
-end ;
-fclose(fd) ;
-
--- tests/type_comparison.cpp
+++
@@ -1,9 +0,0 @@
-typedef float BLA;
-
-void main()
-{
-	double a=1;
-	BLA x;
-
-	x= (BLA) a;
-}
