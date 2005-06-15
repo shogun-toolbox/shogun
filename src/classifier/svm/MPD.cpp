1b36f7fdcf29b849f19db4f097bde3250b302556
author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1118852315 -0000 <author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1118852315 -0000>
2005-06-15 16:18:35 +0000

    we now have a MPD CSVC

--- src/classifier/svm/MPD.cpp
+++ src/classifier/svm/MPD.cpp
@@ -15,15 +15,15 @@ bool CMPDSVM::train()
 {
 	CLabels* lab = CKernelMachine::get_labels();
 
-	const REAL nu=0.32;
+	//const REAL nu=0.32;
 	const REAL eps=1e-5;
 	const REAL hessreg = 1e-12;
 	const long int maxiter = 1L<<30;
-	const bool nustop=false;
-	const int k=2;
+	//const bool nustop=false;
+	//const int k=2;
 	const int n=lab->get_num_labels();
-	const REAL d = 1.0/n/nu; //NUSVC
-	//const REAL d = get_C1()/n; //CSVC
+	//const REAL d = 1.0/n/nu; //NUSVC
+	const REAL d = get_C1(); //CSVC
 	const REAL primaleps=eps;
 	const REAL dualeps=eps;
 	long int niter=0;
@@ -31,32 +31,37 @@ bool CMPDSVM::train()
 	kernel_cache = new CCache<KERNELCACHE_ELEM>(kernel->get_cache_size(), n, n);
 	REAL* alphas=new REAL[n];
 	REAL* dalphas=new REAL[n];
-	REAL* hessres=new REAL[k*n];
-	REAL* F=new REAL[k*n];
+	//REAL* hessres=new REAL[2*n];
+	REAL* hessres=new REAL[n];
+	//REAL* F=new REAL[2*n];
+	REAL* F=new REAL[n];
 
-	REAL etas[2]={0,0};
-	REAL detas[2]={0,1}; //NUSVC
-	//REAL detas[2]={0,0};   //CSVC
-	REAL hessest[2]={0,0};
-	REAL hstep[2];
+	//REAL hessest[2]={0,0};
+	//REAL hstep[2];
+	//REAL etas[2]={0,0};
+	//REAL detas[2]={0,1}; //NUSVC
+	REAL etas=0;
+	REAL detas=0;   //CSVC
+	REAL hessest=0;
+	REAL hstep;
 
-	REAL stopfac = 1;
+	const REAL stopfac = 1;
 
 	bool primalcool;
 	bool dualcool;
 
-	if (nustop)
-		etas[1] = 1;
+	//if (nustop)
+		//etas[1] = 1;
 
 	for (int i=0; i<n; i++)
 	{
 		alphas[i]=0;
 		F[i]=lab->get_label(i);
-		F[i+n]=-1;
+		//F[i+n]=-1;
 		hessres[i]=lab->get_label(i);
-		hessres[i+n]=-1;
-		dalphas[i]=F[i+n]*etas[1]; //NUSVC
-		//dalphas[i]=F[i+n]*etas[1]-1; //CSVC
+		//hessres[i+n]=-1;
+		//dalphas[i]=F[i+n]*etas[1]; //NUSVC
+		dalphas[i]=-1; //CSVC
 	}
 
 	// go ...
@@ -64,10 +69,11 @@ bool CMPDSVM::train()
 	{
 		int maxpidx=-1;
 		REAL maxpviol = -1;
-		REAL maxdviol = CMath::abs(detas[0]);
+		//REAL maxdviol = CMath::abs(detas[0]);
+		REAL maxdviol = CMath::abs(detas);
 
-		if (CMath::abs(detas[1])> maxdviol)
-			maxdviol=CMath::abs(detas[1]);
+		//if (CMath::abs(detas[1])> maxdviol)
+			//maxdviol=CMath::abs(detas[1]);
 
 		// compute kkt violations with correct sign ...
 		for (int i=0; i<n; i++)
@@ -91,10 +97,10 @@ bool CMPDSVM::train()
 			CIO::message(M_ERROR, "no violation no convergence, should not happen!\n");
 
 		// ... and evaluate stopping conditions
-		if (nustop)
-			stopfac = CMath::max(etas[1], 1e-10);    
-		else
-			stopfac = 1;
+		//if (nustop)
+			//stopfac = CMath::max(etas[1], 1e-10);    
+		//else
+			//stopfac = 1;
 
 		if (niter%10000 == 0)
 			CIO::message(M_DEBUG, "pviol:%f dviol:%f maxpidx:%d iter:%d\n", maxpviol, maxdviol, maxpidx, niter);
@@ -113,11 +119,13 @@ bool CMPDSVM::train()
 		}
 
 		// hessian updates
-		hstep[0]=-hessres[maxpidx]/(compute_H(maxpidx,maxpidx)+hessreg);
-		hstep[1]=-hessres[maxpidx+n]/(compute_H(maxpidx,maxpidx)+hessreg);
+		hstep=-hessres[maxpidx]/(compute_H(maxpidx,maxpidx)+hessreg);
+		//hstep[0]=-hessres[maxpidx]/(compute_H(maxpidx,maxpidx)+hessreg);
+		//hstep[1]=-hessres[maxpidx+n]/(compute_H(maxpidx,maxpidx)+hessreg);
 
-		hessest[0]-=F[maxpidx]*hstep[0];
-		hessest[1]-=F[maxpidx+n]*hstep[1];
+		hessest-=F[maxpidx]*hstep;
+		//hessest[0]-=F[maxpidx]*hstep[0];
+		//hessest[1]-=F[maxpidx+n]*hstep[1];
 
 		// do primal updates ..
 		REAL tmpalpha = alphas[maxpidx] - dalphas[maxpidx]/compute_H(maxpidx,maxpidx);
@@ -135,26 +143,31 @@ bool CMPDSVM::train()
 		KERNELCACHE_ELEM* h=lock_kernel_row(maxpidx);
 		for (int i=0; i<n; i++)
 		{
-			hessres[i]+=h[i]*hstep[0];
-			hessres[i+n]+=h[i]*hstep[1];
+			hessres[i]+=h[i]*hstep;
+			//hessres[i]+=h[i]*hstep[0];
+			//hessres[i+n]+=h[i]*hstep[1];
 			dalphas[i] +=h[i]*alphachange;
 		}
 		unlock_kernel_row(maxpidx);
 
-		detas[0]+=F[maxpidx]*alphachange;
-		detas[1]+=F[maxpidx+n]*alphachange;
+		detas+=F[maxpidx]*alphachange;
+		//detas[0]+=F[maxpidx]*alphachange;
+		//detas[1]+=F[maxpidx+n]*alphachange;
 
 		// if at primal minimum, do eta update ...            
 		if (primalcool)
 		{
-			REAL etachange[2] = { detas[0]/hessest[0] , detas[1]/hessest[1] };
+			//REAL etachange[2] = { detas[0]/hessest[0] , detas[1]/hessest[1] };
+			REAL etachange = detas/hessest;
 
-			etas[0]+=etachange[0];        
-			etas[1]+=etachange[1];        
+			etas+=etachange;        
+			//etas[0]+=etachange[0];        
+			//etas[1]+=etachange[1];        
 
 			// update dalphas
 			for (int i=0; i<n; i++)
-				dalphas[i]+= F[i] * etachange[0] + F[i+n] * etachange[1];
+				dalphas[i]+= F[i] * etachange;
+				//dalphas[i]+= F[i] * etachange[0] + F[i+n] * etachange[1];
 		}
 	}
 
@@ -170,14 +183,16 @@ bool CMPDSVM::train()
 	}
 
 	create_new_model(nsv);
-	set_bias(etas[0]/etas[1]);
+	//set_bias(etas[0]/etas[1]);
+	set_bias(etas);
 
 	int j=0;
 	for (int i=0; i<n; i++)
 	{
 		if (alphas[i]>0)
 		{
-			set_alpha(j, alphas[i]*lab->get_label(i)/etas[1]);
+			//set_alpha(j, alphas[i]*lab->get_label(i)/etas[1]);
+			set_alpha(j, alphas[i]*lab->get_label(i));
 			set_support_vector(j, i);
 			j++;
 		}
