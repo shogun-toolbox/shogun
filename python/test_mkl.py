14edd56a25c57b68e9540c5cc0677804873550cb
author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1121354744 -0000 <author Soeren Sonnenburg sonne@debian.org, Gunnar Raetsch Gunnar.Raetsch@tuebingen.mpg.de 1121354744 -0000>
2005-07-14 15:25:44 +0000

    +mkl python test script

---
+++ python/test_mkl.py
@@ -0,0 +1,23 @@
+import gf
+from MLab import *
+from numarray import *
+num=100;
+gf.send_command('loglevel ALL')
+labels=concatenate((-ones([1,num]), ones([1,num])),1)[0]
+features=concatenate((randn(num,2)-1,randn(num,2)+1),0)
+
+gf.send_command('c 10')
+gf.send_command('new_svm LIGHT')
+
+gf.set_labels("TRAIN", labels)
+gf.add_features("TRAIN", features)
+gf.add_features("TRAIN", features)
+gf.add_features("TRAIN", features)
+
+gf.send_command('set_kernel COMBINED ANY 100')
+gf.send_command('add_kernel 1 GAUSSIAN REAL 100 100')
+gf.send_command('add_kernel 1 GAUSSIAN REAL 100 10')
+gf.send_command('add_kernel 1 GAUSSIAN REAL 100 1')
+gf.send_command('init_kernel TRAIN')
+gf.send_command('svm_train')
+sv1=gf.get_svm();
