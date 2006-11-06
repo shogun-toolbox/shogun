86167d19aa1f0ea39e389ebc3fba30da496c0f99
Fabio De Bona <Fabio.De.Bona@tuebingen.mpg.de>
2006-11-06 16:16:21 +0000

    + added a testsuite for the shogun kernels

--- python/examples/testsuite.py
+++ python/examples/testsuite.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 
-# Kernels 
-try:
-   import unittest as ut
-   HAVE_UT = True
-except:
-   print '---=== You do not have a running unittest installation! ===---'
-   import ut
-   HAVE_UT = False
 
-import shogun.Features as f
-import shogun.Kernel as k
-import shogun.SVM as s
+# A testsuite for the Kernels 
+#
+#
+
+import unittest as ut
+
+from shogun.Features import *
+from shogun.Kernel import *
+from shogun.SVM import *
+
+from numpy import *
+from numpy.random import *
 
 class TestKernels(ut.TestCase):
    
    def setUp(self):
+      num_dat=50
+      len=70
+      acgt=array(['A','C','G','T'])
       trdat=chararray((len,2*num_dat),1,order='FORTRAN')
       trlab=concatenate((-ones(num_dat,dtype=double), ones(num_dat,dtype=double)))
       for ix in xrange(2*num_dat):
@@ -23,15 +27,31 @@ class TestKernels(ut.TestCase):
 
       trdat[10:15,trlab==1]='A'
               
-      trainfeat = CharFeatures(trdat,DNA)
-      trainlab = Labels(trlab)
+      self.trainfeat = CharFeatures(trdat,DNA)
+      self.trainlab = Labels(trlab)
+
+      simple2dData = array([[1.0,2,3,4,5,6,7,8,9,10],\
+      [0.0,0,0,0,0,0,0,0,0,0]],order='FORTRAN')
+
+      simple2dData.reshape((2,10))
+      self.real_feat = RealFeatures(simple2dData)
+
+   def testHierarchy(self):
+      assert issubclass(RealKernel,CKernel)
+
+      assert issubclass(AUCKernel,CKernel)
+      assert issubclass(GaussianKernel,CKernel)
+      assert issubclass(LinearKernel,CKernel)
+      assert issubclass(PolyKernel,CKernel)
+      
+      assert issubclass(GaussianKernel,RealKernel)
+      assert issubclass(LinearKernel,RealKernel)
+      assert issubclass(PolyKernel,RealKernel)
+
 
    def testAUCKernel(self):
-      auck = k.AUCKernel(10,None)
-      auck.init()
-      
-      
-      
+      #auck = AUCKernel(10,None)
+      #auck.init()
       pass
    
    def testCanberraWordKernel(self):
@@ -68,7 +88,9 @@ class TestKernels(ut.TestCase):
       pass
    
    def testGaussianKernel(self):
-      pass
+      gk=GaussianKernel(self.real_feat,self.real_feat, 1)
+      km = gk.get_kernel_matrix()
+      print km
       
    def testHammingWordKernel(self):
       pass
@@ -81,46 +103,74 @@ class TestKernels(ut.TestCase):
       
    def testLinearByteKernel(self):
       pass
+      
    def testLinearCharKernel(self):
       pass
+      
    def testLinearKernel(self):
-      pass
+      lk = LinearKernel(self.real_feat,self.real_feat,1)
+      lk.get_kernel_type == K_LINEAR
+      km = lk.get_kernel_matrix()
+      lk.cleanup
+      
    def testLinearWordKernel(self):
       pass
+      
    def testLocalityImprovedCharKernel(self):
       pass
+      
    def testManhattenWordKernel(self):
       pass
+      
    def testMindyGramKernel(self):
       pass
+      
    def testPolyKernel(self):
-      pass
+      pk = PolyKernel(self.real_feat,self.real_feat,1,2,True,True)
+      pk.get_kernel_type = K_POLY
+      km = pk.get_kernel_matrix()
+      pk.cleanup
+
+      
    def testPolyMatchCharKernel(self):
       pass
+
    def testPolyMatchWordKernel(self):
       pass
+      
    def testSalzbergWordKernel(self):
       pass
+      
    def testSigmoidKernel(self):
       pass
+      
    def testSimpleLocalityImprovedCharKernel(self):
       pass
+      
    def testSparseGaussianKernel(self):
       pass
+      
    def testSparseLinearKernel(self):
       pass
+      
    def testSparseNormSquaredKernel(self):
       pass
+      
    def testSparsePolyKernel(self):
       pass
+      
    def testSpectrumKernel(self):
       pass
+      
    def testWeightedDegreeCharKernel(self):
       pass
+      
    def testWeightedDegreeCharKernelPolyA(self):
       pass
+      
    def testWeightedDegreePositionCharKernel(self):
       pass
+      
    def testWordMatchKernel(self):
       pass
 
@@ -128,8 +178,4 @@ class TestKernels(ut.TestCase):
       pass
 
 if __name__ == '__main__': 
-   try:
-      unittest.main()
-   except:
-      t = TestKernels()
-      t.run()
+   ut.main()
