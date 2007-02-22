cf9d7e5a886d01a6e47b408b34678bcbdc011999
Jonas Behr <Jonas.Behr@tuebingen.mpg.de>
2007-02-22 09:44:55 +0000

    add octave test script

--- matlab/testcases/test_kernels.m
+++ matlab/testcases/test_kernels.m
@@ -1,7 +1,7 @@
 
 function y = test_kernels(filename)
-  addpath /.amd_mnt/huangho/export/kwaid0/home/jonas/shogun/trunk/src;
-  addpath /.amd_mnt/huangho/export/kwaid0/home/jonas/shogun/trunk/python/testcases/mfiles;
+  addpath('../../src');
+  addpath('../../python-modular/testcases/mfiles');
   
   if (filename(end-1:end)=='.m')
      filename = filename(1:end-2)
@@ -10,5 +10,6 @@ function y = test_kernels(filename)
      
   else
      fprintf(1,'File is not m-file')
+     y = 1;
   end
 
