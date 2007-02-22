4344f6492fdbf0f0728a6813ddd7399ea997ac45
Jonas Behr <Jonas.Behr@tuebingen.mpg.de>
2007-02-22 16:21:31 +0000

    fixed bug

--- matlab/testcases/test_kernels.m
+++ matlab/testcases/test_kernels.m
@@ -1,7 +1,7 @@
 
 function y = test_kernels(filename)
-  addpath ../../src;
-  addpath ../../python-modular/testcases/mfiles;
+  addpath ('../../src');
+  addpath ('../../python-modular/testcases/mfiles');
   
   if (filename(end-1:end)=='.m')
      filename = filename(1:end-2)
--- matlab/testcases/test_wdchar_kernel.m
+++ matlab/testcases/test_wdchar_kernel.m
@@ -1,11 +1,8 @@
-function y = test_wfchar_kernel(filename)
+function y = test_wdchar_kernel(filename)
 
   eval(filename);
 
   sg('set_features', 'TRAIN', traindat, 'DNA');
-
-
- % kname = ['set_kernel WEIGHTEDDEGREE CHAR 10 20 0 1 1 0 1 '];%, num2str(size_),' ',num2str(degree),' 1 1 '];%,inhom,' ', use_norm];
 
   sg('send_command',sprintf('set_kernel WEIGHTEDDEGREE CHAR %i',degree));
   sg('send_command', 'init_kernel TRAIN');
