8cf98ffc4bc4ca9516470af31736a1bcd94e4115
Soeren Sonnenburg <sonne@debian.org>
2007-03-12 19:21:50 +0000

    also add missing init to wds -string kernel

--- src/kernel/WeightedDegreePositionStringKernel.cpp
+++ src/kernel/WeightedDegreePositionStringKernel.cpp
@@ -61,7 +61,7 @@ CWeightedDegreePositionStringKernel::CWeightedDegreePositionStringKernel(INT siz
 		INT mkl_stepsize_)
 : CStringKernel<CHAR>(size), weights(NULL), position_weights(NULL), position_mask(NULL),
 	weights_buffer(NULL), mkl_stepsize(mkl_stepsize_), degree(d), length(0),
-	max_mismatch(max_mismatch_), seq_length(0), max_shift_vec(NULL), 
+	max_mismatch(max_mismatch_), seq_length(0), shift(NULL), max_shift_vec(NULL), 
 	sqrtdiag_lhs(NULL), sqrtdiag_rhs(NULL), initialized(false),
 	use_normalization(use_norm), tries(d), tree_initialized(false)
 {
