1ba061d4463d213bef7c31c7d94ecadb579ffff4
Jonas Behr <Jonas.Behr@tuebingen.mpg.de>
2007-04-04 14:34:55 +0000

    wds-kernel encluded

--- python-modular/testcases/test_kernels.py
+++ python-modular/testcases/test_kernels.py
@@ -1,7 +1,7 @@
-from shogun.Features import RealFeatures, CharFeatures
+from shogun.Features import RealFeatures, CharFeatures, StringCharFeatures
 from shogun.Kernel import *
 from shogun.Features import Alphabet,DNA
-from numpy import array
+from numpy import array, zeros, int32
 acc = 1e-6
 
 def test_gaussian_kernel(dict):
@@ -32,7 +32,6 @@ def test_linear_kernel(dict):
 		feat = RealFeatures(dict['traindat'])
 		kernel_fun = eval(dict['kernelname'])
 		gk=kernel_fun(feat,feat,eval(dict['bool1']))
-	
 		max1 = max(abs(dict['km_train']-gk.get_kernel_matrix()).flat)
 
 
@@ -44,7 +43,8 @@ def test_linear_kernel(dict):
 	except KeyError:
 		print 'error in m-file'
 		return False
-
+	
+	print "max1: ",max1, " max2: ", max2
 	#maximal pairwise difference must be smaler than acc
 	if max1<acc and max2<acc:
 		return True
@@ -144,6 +144,34 @@ def test_wdchar_kernel(dict):
 		k.init(feat, test_feat)
 	
 		max2 =  max(abs(dict['km_test']-k.get_kernel_matrix()).flat)
+	
+	except KeyError:
+		print 'error in m-file'
+		return False	
+
+	#maximal pairwise difference must be smaler than acc
+	if max1<acc and max2<acc:
+		return True
+
+	return False
+
+def test_wdps_kernel(dict):
+	
+	try:
+		print "traindat: ",dict['traindat'][0]
+		print "testdat: ",dict['testdat'][0]
+		stringfeat = StringCharFeatures(eval(dict['alphabet']))
+		stringfeat.set_string_features(list(dict['traindat'][0]))
+
+		stringtestfeat = StringCharFeatures(eval(dict['alphabet']))
+		stringtestfeat.set_string_features(list(dict['testdat'][0]))
+		
+		kernel_fun = eval(dict['kernelname'])
+		k= kernel_fun(stringfeat, stringfeat, dict['degree'],  zeros(dict['seqlen'], dtype=int32))
+		max1 = max(abs(dict['km_train']-k.get_kernel_matrix()).flat)
+
+		k.init(stringfeat, stringtestfeat)		
+		max2 = max(abs(dict['km_test']-k.get_kernel_matrix()).flat)
 	
 	except KeyError:
 		print 'error in m-file'
