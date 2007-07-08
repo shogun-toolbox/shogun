3c8b7270fae27840b09ac5746c3a791c1c9b51fb
Gunnar Raetsch <Gunnar.Raetsch@tuebingen.mpg.de>
2007-07-08 13:44:42 +0000

    added a missing case switch.

--- src/kernel/Kernel.cpp
+++ src/kernel/Kernel.cpp
@@ -879,6 +879,9 @@ void CKernel::list_kernel()
 		case K_SPARSENORMSQUARED:
 			SG_INFO( "K_SPARSENORMSQUARED ");
 			break;
+		case K_SPARSELINEAR:
+			SG_INFO( "K_SPARSELINEAR ");
+			break;
 		case K_COMBINED:
 			SG_INFO( "K_COMBINED ");
 			break;
