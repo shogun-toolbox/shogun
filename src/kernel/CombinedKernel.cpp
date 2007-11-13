a4bf25b5b34d30c206b1fd4fd86df59c453bbc18
Soeren Sonnenburg <sonne@debian.org>
2007-11-13 20:16:14 +0000

    fix serious bug in output computation which could occur only when a kernel supporting batch computation was used in conjunction with a kernel neither capable of batch computation and linadd AND the number of threads was > 1
    note that this may have lead to wrong svm - models on TRAINING if svm lightwas
    used and could also lead to random corruptions when computing output via
    classify

--- src/kernel/CombinedKernel.cpp
+++ src/kernel/CombinedKernel.cpp
@@ -479,7 +479,7 @@ void CCombinedKernel::emulate_compute_batch(CKernel* k, INT num_vec, INT* vec_id
 				params[t].IDX = IDX;
 				params[t].weights = weights;
 				params[t].num_suppvec = num_suppvec;
-				pthread_create(&threads[t], NULL, CCombinedKernel::compute_kernel_helper, (void*)&params[t]);
+				compute_kernel_help(&params[t]);
 
 				for (t=0; t<num_threads-1; t++)
 					pthread_join(threads[t], NULL);
