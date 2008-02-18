0e0292e8ad0ab1d46f95fdb1e0f403ee112caf1d
Soeren Sonnenburg <sonne@debian.org>
2008-02-18 19:18:01 +0000

    add missing 1,1 for fp,tp roc matlab example

--- matlab/examples/tools/calcroc.m
+++ matlab/examples/tools/calcroc.m
@@ -7,6 +7,9 @@ assert(all(size(output)==size(LTE))) ;
 hits=1-cumsum(LTE(idx)>0)/sum(LTE > 0) ;
 false_alarms=1-cumsum(LTE(idx)<0)/sum(LTE < 0) ;
 
+hits = [1 hits ];
+false_alarms = [1 false_alarms];
+
 if nargout<2,
   false_alarms = [false_alarms;hits];
 end ;
