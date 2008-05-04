b645686cbc7b3e89b91ee491da0287e15f0d1884
Soeren Sonnenburg <sonne@debian.org>
2008-05-04 05:34:35 +0000

    string features work now - adding examples for distributions :)

--- r-modular/examples/all_distribution.R
+++ r-modular/examples/all_distribution.R
@@ -1,6 +1,7 @@
 dyn.load('features/Features.so')
+dyn.load('preproc/PreProc.so')
 dyn.load('distributions/Distribution.so')
-source('lib/Library.R')
+source('preproc/PreProc.R')
 source('features/Features.R')
 source('distributions/Distribution.R')
 cacheMetaData(1)
@@ -23,6 +24,9 @@ for (i in 1:num)
 	traindata_dna[i]=paste(acgt[ceiling(4*runif(leng))], sep="", collapse="")
 	testdata_dna[i]=paste(acgt[ceiling(4*runif(leng))], sep="", collapse="")
 }
+
+traindata_dna=c(traindata_dna,recursive=TRUE)
+testdata_dna=c(testdata_dna,recursive=TRUE)
 
 cube <- list(NULL, NULL, NULL)
 numrep <- vector(mode='numeric',length=18)+100
@@ -64,7 +68,7 @@ feats$obtain_from_char(feats, charfeat, start, order, gap, reverse)
 preproc=SortWordString()
 preproc$init(preproc, feats)
 feats$add_preproc(feats, preproc)
-feats$apply_preproc()
+feats$apply_preproc(feats)
 
 histo=Histogram(feats)
 histo$train()
@@ -79,7 +83,7 @@ num_param=histo$get_num_model_parameters()
 #	end
 #end
 
-histo$get_log_likelihood()
+histo$get_log_likelihood(histo, 0)
 histo$get_log_likelihood_sample()
 
 # Linear HMM
@@ -90,16 +94,16 @@ gap=0
 reverse=FALSE
 
 charfeat=StringCharFeatures("DNA")
-charfeat$set_string_features(traindata_dna)
+charfeat$set_string_features(charfeat, traindata_dna)
 feats=StringWordFeatures(charfeat$get_alphabet())
-feats$obtain_from_char(charfeat, order-1, order, gap, reverse)
+feats$obtain_from_char(feats, charfeat, order-1, order, gap, reverse)
 preproc=SortWordString()
-preproc$init(feats)
-feats$add_preproc(preproc)
-feats$apply_preproc()
+preproc$init(preproc, feats)
+feats$add_preproc(feats, preproc)
+feats$apply_preproc(feats)
 
 hmm=LinearHMM(feats)
-hmm$train()
+hmm$train(hmm)
 
 hmm$get_transition_probs()
 
@@ -111,7 +115,7 @@ num_param=hmm$get_num_model_parameters()
 #	end
 #end
 
-hmm$get_log_likelihood()
+hmm$get_log_likelihood(hmm, 0)
 hmm$get_log_likelihood_sample()
 
 # HMM
@@ -127,15 +131,15 @@ num_examples=2
 charfeat=StringCharFeatures("CUBE")
 charfeat$set_string_features(charfeat, cubesequence)
 feats=StringWordFeatures(charfeat$get_alphabet())
-feats$obtain_from_char(charfeat, order-1, order, gap, reverse)
+feats$obtain_from_char(feats, charfeat, order-1, order, gap, reverse)
 preproc=SortWordString()
-preproc$init(feats)
-feats$add_preproc(preproc)
-feats$apply_preproc()
+preproc$init(preproc, feats)
+feats$add_preproc(feats, preproc)
+feats$apply_preproc(feats)
 
 hmm=HMM(feats, N, M, pseudo)
-hmm$train()
-hmm$baum_welch_viterbi_train(BW_NORMAL)
+hmm$train(hmm)
+hmm$baum_welch_viterbi_train(hmm, "BW_NORMAL")
 
 num_examples=feats$get_num_vectors()
 num_param=hmm$get_num_model_parameters()
@@ -154,5 +158,5 @@ best_path_state=0
 #	end
 #end
 
-hmm$get_log_likelihood()
+hmm$get_log_likelihood(hmm, 0)
 hmm$get_log_likelihood_sample()
