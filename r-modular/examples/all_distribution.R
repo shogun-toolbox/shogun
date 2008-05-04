0ce001582cabd26b6864d5b93fe6385b34dd96cc
Soeren Sonnenburg <sonne@debian.org>
2008-05-04 05:49:56 +0000

    also make the get_deriv* calls work

--- r-modular/examples/all_distribution.R
+++ r-modular/examples/all_distribution.R
@@ -77,12 +77,16 @@ histo$get_histogram()
 
 num_examples=feats$get_num_vectors()
 num_param=histo$get_num_model_parameters()
-#for i=0:(num_examples-1),
-#	for j=0:(num_param-1),
-#		histo$get_log_derivative(j, i)
-#	end
-#end
 
+# commented out as this is quite time consuming
+#derivs=matrix(0,num_param, num_examples)
+#for (i in 0:(num_examples-1))
+#{
+#	for (j in 0:(num_param-1))
+#	{
+#		derivs[j,i]=histo$get_log_derivative(histo, j, i)
+#	}
+#}
 histo$get_log_likelihood(histo, 0)
 histo$get_log_likelihood_sample()
 
@@ -109,11 +113,15 @@ hmm$get_transition_probs()
 
 num_examples=feats$get_num_vectors()
 num_param=hmm$get_num_model_parameters()
-#for i=0:(num_examples-1),
-#	for j=0:(num_param-1),
-#		histo$get_log_derivative(j, i)
-#	end
-#end
+derivs=matrix(0,num_param, num_examples)
+
+for (i in 0:(num_examples-1))
+{
+	for (j in 0:(num_param-1))
+	{
+		derivs[j,i]=hmm$get_log_derivative(hmm, j, i)
+	}
+}
 
 hmm$get_log_likelihood(hmm, 0)
 hmm$get_log_likelihood_sample()
@@ -143,20 +151,27 @@ hmm$baum_welch_viterbi_train(hmm, "BW_NORMAL")
 
 num_examples=feats$get_num_vectors()
 num_param=hmm$get_num_model_parameters()
-#for i=0:(num_examples-1),
-#	for j=0:(num_param-1),
-#		histo$get_log_derivative(j, i)
-#	end
-#end
+
+derivs=matrix(0,num_param, num_examples)
+for (i in 0:(num_examples-1))
+{
+	for (j in 0:(num_param-1))
+	{
+		derivs[j,i]=hmm$get_log_derivative(hmm, j, i)
+	}
+}
 
 best_path=0
 best_path_state=0
-#for i=0:(num_examples-1),
-#	best_path = best_path + hmm$best_path(i)
-#	for j=0:(N-1),
-#		best_path_state = best_path_state + hmm$get_best_path_state(i, j)
-#	end
-#end
+
+for (i in 0:(num_examples-1))
+{
+	best_path = best_path + hmm$best_path(hmm, i)
+	for (j in 0:(N-1))
+	{
+		best_path_state = best_path_state + hmm$get_best_path_state(hmm, i, j)
+	}
+}
 
 hmm$get_log_likelihood(hmm, 0)
 hmm$get_log_likelihood_sample()
