65a1158b0731a0465ee9880aaf5fa9f54dee7f55
Sebastian Henschel <shogun@kodeaffe.de>
2008-05-08 19:35:15 +0000

    examples: - Removed superflous example.

--- matlab_and_octave/examples/stringfeatures.m
+++
@@ -1,5 +0,0 @@
-%generate some toy data
-acgt='ACGT';
-dat={acgt([1*ones(1,10) 2*ones(1,10) 3*ones(1,10) 4*ones(1,10)])};
-sg('set_features', 'TRAIN', dat, 'DNA');
-sg('slide_window', 'TRAIN', 5, 1');
