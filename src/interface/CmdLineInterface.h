5c0b49b1d5f10dbee6b0674b97d627599baf07c6
Sebastian Henschel <shogun@kodeaffe.de>
2008-05-28 19:53:04 +0000

    interface: - Compile fix for cmdline.

--- src/interface/CmdLineInterface.h
+++ src/interface/CmdLineInterface.h
@@ -6,8 +6,8 @@
 #if defined(HAVE_CMDLINE)
 #include "interface/SGInterface.h"
 
-#define CMDLINE_COMMENT0 "#"
-#define CMDLINE_COMMENT1 "%"
+#define CMDLINE_COMMENT0 '#'
+#define CMDLINE_COMMENT1 '%'
 
 class CCmdLineInterface : public CSGInterface
 {
