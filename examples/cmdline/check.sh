fd780600d1f3f825b0082a6b50c5a32a2dd341a8
Soeren Sonnenburg <sonne@debian.org>
2008-06-15 07:13:53 +0000

    to run tests now do make make install DESTDIR=/tmp/ make tests DESTDIR=/tmp/
    (with DESTDIR optional :-)

--- examples/cmdline/check.sh
+++ examples/cmdline/check.sh
@@ -3,7 +3,7 @@
 for e in *.sg
 do
 	echo -n "running $e .."
-	if ../../src/shogun $e >/dev/null 2>&1
+	if shogun $e >/dev/null 2>&1
 	then
 		echo " OK"
 	else
--- examples/matlab_and_octave/check.sh
+++ examples/matlab_and_octave/check.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
 
-INTERFACE=`grep '^INTERFACE' ../../src/.config | cut -f 2 -d '=' | tr -d ' '`
-
-function errormsg
-{
-	echo
-	echo $1
-	echo
-	echo "not running checks...."
-	echo
-	exit 1
-}
-
-if [ "${INTERFACE}" = "matlab" ]
-then
-	errormsg "matlab is not capable of setting proper exit codes try octave!"
-fi
-
-if [ "$INTERFACE" != "octave" ]
-then
-	errormsg "octave is not the configured interface!"
-fi
-
-
 for e in *.m
 do
 	echo -n "running $e .."
-	if octave -p ../../src "$e" >/dev/null 2>&1
+	if octave "$e" >/dev/null 2>&1
 	then
 		echo " OK"
 	else
