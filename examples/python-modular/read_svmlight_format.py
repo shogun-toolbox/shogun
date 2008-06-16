30379df3defdf11154ea1806bf171dad252aa1b2
Soeren Sonnenburg <sonne@debian.org>
2008-06-16 06:47:12 +0000

    erase the created file

--- examples/python-modular/read_svmlight_format.py
+++ examples/python-modular/read_svmlight_format.py
@@ -1,4 +1,7 @@
+import os
 from shogun.Features import SparseRealFeatures
+
 f=SparseRealFeatures()
 lab=f.load_svmlight_file('../data/train_sparsereal.light')
 f.write_svmlight_file('testwrite.light', lab)
+os.unlink('testwrite.light')
