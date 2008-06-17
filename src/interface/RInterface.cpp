ab41b76575b437615c8d7b9b8f6e872c0839606a
Soeren Sonnenburg <sonne@debian.org>
2008-06-17 22:08:12 +0000

    fix index

--- src/interface/RInterface.cpp
+++ src/interface/RInterface.cpp
@@ -315,7 +315,7 @@ SEXP CRInterface::get_return_values()
 	if (m_nlhs==1)
 	{
 		SEXP arg=VECTOR_ELT(m_lhs, 0);
-		SET_VECTOR_ELT(m_lhs, m_lhs_counter, R_NilValue);
+		SET_VECTOR_ELT(m_lhs, 0, R_NilValue);
 		UNPROTECT(1);
 		return arg;
 	}
