52f6d8e82235f81f6fe5cad49b525084607a9631
Sebastian Henschel <shogun@kodeaffe.de>
2008-08-02 21:26:39 +0000

    testsuite: - Added testing for distribution in R static.

---
+++ testsuite/r/distribution.R
@@ -0,0 +1,28 @@
+distribution <- function(filename) {
+	source('util/set_features.R')
+	source('util/check_accuracy.R')
+
+	# there is some randomness involved, alas this is
+	# not working correctly in R
+	sg('init_random', init_random)
+	set.seed(init_random)
+
+	if (!set_features()) {
+		return(TRUE)
+	}
+
+	if (regexpr('HMM', name)>0) {
+		sg('new_hmm', distribution_N, distribution_M)
+		sg('bw')
+	} else {
+		print('Cannot yet train other distributions than HMM!')
+		return(TRUE)
+	}
+
+	likelihood <- abs(sg('hmm_likelihood')-distribution_likelihood)
+	print(distribution_likelihood)
+	print(sg('hmm_likelihood'))
+
+	data <- list(likelihood, 0)
+	return(check_accuracy(distribution_accuracy, 'distribution', data))
+}
--- testsuite/r/util/check_accuracy.R
+++ testsuite/r/util/check_accuracy.R
@@ -3,6 +3,8 @@ check_accuracy <- function(accuracy, type, data) {
 		print(paste('train:', data[1], ', test:', data[2], '<--- accuracy', accuracy))
 	} else if (regexpr(type, 'classifier')>0) {
 		print(paste('alphas:', data[1], ', bias:', data[2], ', sv:', data[3], ', classified:', data[4], '<--- accuracy ', accuracy))
+	} else if (regexpr(type, 'distribution')>0) {
+		print(paste('likelihood:', data[1], ', derivatives:', data[2], '<--- accuracy ', accuracy))
 	} else {
 		print(paste('Unknown accuracy type', type, '!'))
 		return(FALSE)
