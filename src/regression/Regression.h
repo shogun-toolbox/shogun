d4c9e34f841fe861b9729e34856fe91b5f1910ee
Sebastian Henschel <shogun@kodeaffe.de>
2008-10-11 05:16:08 +0000

    - Oopsie, forgot to add regression/Regression.h

---
+++ src/regression/Regression.h
@@ -0,0 +1,22 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2008 Sebastian Henschel
+ * Copyright (C) 1999-2008 Fraunhofer Institute FIRST and Max-Planck-Society
+ */
+
+#ifndef _REGRESSION_H__
+#define _REGRESSION_H__
+
+enum ERegressionType
+{
+	RT_NONE = 0,
+	RT_LIGHT = 10,
+	RT_LIBSVM = 20
+};
+
+#endif // _REGRESSION_H__
+
