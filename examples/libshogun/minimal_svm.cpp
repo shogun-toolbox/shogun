79db61cf015b047fe79bf4f7582fbf6d00d2c9bf
Soeren Sonnenburg <sonne@debian.org>
2009-02-23 08:55:01 +0000

    remove copyright from basic examples (they are included on webpage and thus only hurts..)

--- examples/libshogun/minimal_svm.cpp
+++ examples/libshogun/minimal_svm.cpp
@@ -9,37 +9,37 @@
 
 void print_message(FILE* target, const char* str)
 {
-	        fprintf(target, "%s", str);
+	fprintf(target, "%s", str);
 }
 
 int main(int argc, char** argv)
 {
 	init_shogun(&print_message);
 
-    // create some data
-    float64_t* matrix = new float64_t[6];
-    for (int32_t i=0; i<6; i++)
-        matrix[i]=i;
-    
-    // create three 2-dimensional vectors 
+	// create some data
+	float64_t* matrix = new float64_t[6];
+	for (int32_t i=0; i<6; i++)
+		matrix[i]=i;
+
+	// create three 2-dimensional vectors 
 	// shogun will now own the matrix created
-    CRealFeatures* features= new CRealFeatures();
-    features->set_feature_matrix(matrix, 2, 3);
+	CRealFeatures* features= new CRealFeatures();
+	features->set_feature_matrix(matrix, 2, 3);
 
-    // create three labels
-    CLabels* labels=new CLabels(3);
-    labels->set_label(0, -1);
-    labels->set_label(1, +1);
-    labels->set_label(2, -1);
+	// create three labels
+	CLabels* labels=new CLabels(3);
+	labels->set_label(0, -1);
+	labels->set_label(1, +1);
+	labels->set_label(2, -1);
 
-    // create gaussian kernel with cache 10MB, width 0.5
-    CGaussianKernel* kernel = new CGaussianKernel(10, 0.5);
-    kernel->init(features, features);
+	// create gaussian kernel with cache 10MB, width 0.5
+	CGaussianKernel* kernel = new CGaussianKernel(10, 0.5);
+	kernel->init(features, features);
 
 	// create libsvm with C=10 and train
-    CLibSVM* svm = new CLibSVM(10, kernel, labels);
+	CLibSVM* svm = new CLibSVM(10, kernel, labels);
 	svm->train();
-	
+
 	// classify on training examples
 	for (int32_t i=0; i<3; i++)
 		SG_SPRINT("output[%d]=%f\n", i, svm->classify_example(i));
@@ -47,6 +47,6 @@ int main(int argc, char** argv)
 	// free up memory
 	SG_UNREF(svm);
 
-    exit_shogun();
-    return 0;
+	exit_shogun();
+	return 0;
 }
