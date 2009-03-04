75ea280a62e119f446a4af718d79f747b2c028c0
Soeren Sonnenburg <sonne@debian.org>
2009-03-04 19:53:28 +0000

    make the example more fancy: plot a figure in python - matplotlib from within octave :-)

--- examples/elwms/octave_to_python.m
+++ examples/elwms/octave_to_python.m
@@ -8,7 +8,18 @@ elwms('run_python', 'pythoncode', 'print "hi"')
 elwms('run_python', 'pythoncode', 'print "hi2"')
 C=elwms('run_python', 'A',A, 'B',B, 'pythoncode', pythoncode)
 D=elwms('run_python', 'A',A+1, 'B',B*2, 'pythoncode', pythoncode)
-pythoncode=sprintf('import numpy\nresults=(A, B, [ "bla1", "bla2" ])\n');
-[A2,B2,bla]=elwms('run_python', 'A',A, 'B',B, 'pythoncode', pythoncode)
+pythoncode=sprintf("\
+\
+import numpy\n\
+x=numpy.array([[1.0,2,3],[4,5,6]])\n\
+results=(A, B, [ 'bla1', 'bla2' ], x)\n\
+\
+from pylab import *\n\
+plot(B)\n\
+show()\n\
+\
+")
+
+[A2,B2,bla,x]=elwms('run_python', 'A',A, 'B',B, 'pythoncode', pythoncode)
 sprintf('%s\n', char(bla{1}))
 sprintf('%s\n', char(bla{2}))
