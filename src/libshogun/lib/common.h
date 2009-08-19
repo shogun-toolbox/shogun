c0b499788d204c2f1a4ca7a9481da52bb764131e
Soeren Sonnenburg <sonne@debian.org>
2009-08-19 12:08:40 +0000

    a few sunos fixes.

--- src/libshogun/lib/common.h
+++ src/libshogun/lib/common.h
@@ -37,20 +37,7 @@
 */
 //@{
 
-#ifdef SUNOS
-typedef char int8_t;
-typedef short int int16_t;
-typedef int int32_t;
-typedef long int64_t;
-typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long uint64_t;
-typedef unsigned long long int uintmax_t;
-#else
 #include <stdint.h>
-#endif
-
 
 /**
  * Implementations tend to follow IEEE754
