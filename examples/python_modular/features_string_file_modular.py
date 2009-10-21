e4897ac70804aeb57d6929ecee5062d671770a17
Soeren Sonnenburg <sonne@debian.org>
2009-10-21 12:42:38 +0000

    document how to load strings from file or fasta file

--- examples/python_modular/features_string_file_modular.py
+++ examples/python_modular/features_string_file_modular.py
@@ -1,6 +1,6 @@
 from shogun.Features import StringCharFeatures, RAWBYTE
 
-# load features from file
+# load features from directory
 f=StringCharFeatures(RAWBYTE)
 f.load_from_directory(".")
 
@@ -12,3 +12,10 @@ print "str[0,0:3]", f.get_feature(0,0), f.get_feature(0,1), f.get_feature(0,2)
 print "len(str[0])", f.get_vector_length(0)
 print "str[0]", f.get_feature_vector(0)
 
+#or load features from file (one string per line)
+f.load('features_string_char_modular.py')
+print f.get_features()
+
+#or load fasta file
+#f.load_fasta('fasta.fa')
+#print f.get_features()
