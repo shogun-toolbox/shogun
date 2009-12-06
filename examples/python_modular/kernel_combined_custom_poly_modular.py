1c5d858d4457d7ba31baf6019121bca033f0aa36
Cheng Soon Ong <chengsoon.ong@inf.ethz.ch>
2009-12-06 15:56:48 +0000

    Updated CombinedKernel and CustomKernel example.

--- examples/python_modular/kernel_combined_custom_poly_modular.py
+++ examples/python_modular/kernel_combined_custom_poly_modular.py
@@ -1,34 +1,52 @@
 def combined_custom():
-	from shogun.Features import CombinedFeatures, RealFeatures, Labels
-	from shogun.Kernel import CombinedKernel, PolyKernel, CustomKernel
-	from shogun.Classifier import LibSVM
+    from shogun.Features import CombinedFeatures, RealFeatures, Labels
+    from shogun.Kernel import CombinedKernel, PolyKernel, CustomKernel
+    from shogun.Classifier import LibSVM
 
-	kernel=CombinedKernel()
-	#feats_train=CombinedFeatures()
-	
-	tfeats = RealFeatures(fm_train_real)
-	tkernel = PolyKernel(10,3)
-	tkernel.init(tfeats, tfeats)
-	K = tkernel.get_kernel_matrix()
-	kernel.append_kernel(CustomKernel(K))
-		
-	subkfeats_train = RealFeatures(fm_train_real)
-	#feats_train.append_feature_obj(subkfeats_train)
-	subkernel = PolyKernel(10,2)
-	subkernel.init(subkfeats_train, subkfeats_train)
-	kernel.append_kernel(subkernel)
+    kernel = CombinedKernel()
+    feats_train = CombinedFeatures()
+    
+    tfeats = RealFeatures(fm_train_real)
+    tkernel = PolyKernel(10,3)
+    tkernel.init(tfeats, tfeats)
+    K = tkernel.get_kernel_matrix()
+    kernel.append_kernel(CustomKernel(K))
+        
+    subkfeats_train = RealFeatures(fm_train_real)
+    feats_train.append_feature_obj(subkfeats_train)
+    subkernel = PolyKernel(10,2)
+    kernel.append_kernel(subkernel)
 
-	#kernel.init(feats_train, feats_train)
+    kernel.init(feats_train, feats_train)
+    
+    labels = Labels(fm_label_twoclass)
+    svm = LibSVM(1.0, kernel, labels)
+    svm.train()
 
-	print kernel.get_kernel_matrix()
-	
-	labels = Labels(fm_label_twoclass)
-	svm = LibSVM(1.0, kernel, labels)
-	svm.train()
+    kernel = CombinedKernel()
+    feats_pred = CombinedFeatures()
+
+    pfeats = RealFeatures(fm_test_real)
+    tkernel = PolyKernel(10,3)
+    tkernel.init(tfeats, pfeats)
+    K = tkernel.get_kernel_matrix()
+    kernel.append_kernel(CustomKernel(K))
+
+    subkfeats_test = RealFeatures(fm_test_real)
+    feats_pred.append_feature_obj(subkfeats_test)
+    subkernel = PolyKernel(10, 2)
+    kernel.append_kernel(subkernel)
+    kernel.init(feats_train, feats_pred)
+
+    svm.set_kernel(kernel)
+    svm.classify()
 
 if __name__=='__main__':
-	from tools.load import LoadMatrix
-	lm=LoadMatrix()
-	fm_train_real = lm.load_numbers('../data/fm_train_real.dat')
-	fm_label_twoclass = lm.load_labels('../data/label_train_twoclass.dat')
-	combined_custom()
+    from tools.load import LoadMatrix
+    lm=LoadMatrix()
+    fm_train_real = lm.load_numbers('../data/fm_train_real.dat')
+    fm_test_real = lm.load_numbers('../data/fm_test_real.dat')
+    fm_label_twoclass = lm.load_labels('../data/label_train_twoclass.dat')
+    fm_train_real.shape
+    fm_test_real.shape
+    combined_custom()
