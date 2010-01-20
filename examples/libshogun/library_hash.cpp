7052572f3eca108c3b767d7289e837524cb04e18
Soeren Sonnenburg <sonne@debian.org>
2010-01-20 22:03:13 +0000

    add example on how to use incremental murmur like hash

--- examples/libshogun/library_hash.cpp
+++ examples/libshogun/library_hash.cpp
@@ -14,6 +14,14 @@ int main(int argc, char** argv)
 	printf("hash(2)=%0x\n", CHash::MurmurHash2(&array[0], 2, 0xDEADBEAF));
 	printf("hash(3)=%0x\n", CHash::MurmurHash2(&array[0], 4, 0xDEADBEAF));
 
+	uint32_t h=CHash::IncrementalMurmurHash2(array[0], 0xDEADBEAF);
+	printf("inc_hash(0)=%0x\n", h);
+	h=CHash::IncrementalMurmurHash2(array[1], h);
+	printf("inc_hash(1)=%0x\n", h);
+	h=CHash::IncrementalMurmurHash2(array[2], h);
+	printf("inc_hash(2)=%0x\n", h);
+	h=CHash::IncrementalMurmurHash2(array[3], h);
+	printf("inc_hash(3)=%0x\n", h);
 	exit_shogun();
 	return 0;
 }
