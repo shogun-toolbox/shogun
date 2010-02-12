bb34d53758b1a6c056002f616a60816b5bee501f
Soeren Sonnenburg <sonne@debian.org>
2010-02-12 12:15:44 +0000

    polished example documentation script

--- examples/generate_documented.sh
+++ examples/generate_documented.sh
@@ -4,55 +4,68 @@ static_dirs="cmdline elwms matlab_and_octave python r"
 modular_dirs="octave_modular python_modular r_modular"
 lib_dirs=libshogun 
 
-for d in ${static_dirs}
-do
-	files=`ls -1 undocumented/$d | grep -v check.sh`
-	rm -rf documented/$d
-	mkdir documented/$d
+document_interface()
+{
+	iftype=$1
+	
+	if [ $iftype == "static" ]
+	then
+		if_dirs="${static_dirs}"
+		if_descr_dir="static"
 
-	for f in $files
+	elif [ $iftype == "modular" ]
+	then
+		if_dirs="${modular_dirs}"
+		if_descr_dir="modular"
+	fi
+
+	for d in ${if_dirs}
 	do
-		doc=`echo $f | cut -f 1 -d '.'`.txt
-		echo undocumented/$d/$f
-		if test -f undocumented/$d/$f
-		then
-			if test -f descriptions/static/$doc
+
+		files=`ls -1 undocumented/$d | grep -v check.sh`
+		rm -rf documented/$d
+		mkdir documented/$d
+
+
+		for f in $files
+		do
+			doc=`echo $f | cut -f 1 -d '.' | sed 's/_modular$//'`.txt
+			#echo undocumented/$d/$f
+			if test -f undocumented/$d/$f
 			then
-				cat descriptions/static/$doc undocumented/$d/$f >documented/$d/$f
-			else
-				cat undocumented/$d/$f >documented/$d/$f
+				if test -f descriptions/${if_descr_dir}/$doc
+				then
+					suffix=`echo $f | cut -f 2 -d '.'`
+
+					case "$suffix" in 
+						(py)
+							prefix="#"
+							;;
+						(sg)
+							prefix="%"
+							;;
+						(m)
+							prefix="%"
+							;;
+						(R)
+							prefix="#"
+							;;
+					esac
+					( cat descriptions/${if_descr_dir}/$doc | sed "s/^/$prefix /" ; \
+						echo; \
+						cat undocumented/$d/$f ) >documented/$d/$f
+				else
+					cat undocumented/$d/$f >documented/$d/$f
+				fi
 			fi
-		fi
+		done
+
+		test -d undocumented/$d/graphical && \
+			( mkdir documented/$d/graphical &&  \
+			cp undocumented/$d/graphical/* documented/$d/graphical/ )
 	done
+}
 
-	test -d undocumented/$d/graphical && \
-		( mkdir documented/$d/graphical &&  \
-		cp undocumented/$d/graphical/* documented/$d/graphical/ )
-done
-
-for d in ${modular_dirs}
-do
-	files=`ls -1 undocumented/$d | grep -v check.sh`
-	rm -rf documented/$d
-	mkdir documented/$d
-
-	for f in $files
-	do
-		doc=`echo $f | cut -f 1 -d '.'`.txt
-		if test -f undocumented/$d/$f
-		then
-			if test -f descriptions/modular/$doc
-			then
-				cat descriptions/modular/$doc undocumented/$d/$f >documented/$d/$f
-			else
-				cat undocumented/$d/$f >documented/$d/$f
-			fi
-		fi
-	done
-
-	test -d undocumented/$d/graphical && \
-		( mkdir documented/$d/graphical &&  \
-		cp undocumented/$d/graphical/* documented/$d/graphical/ )
-done
-
+document_interface "static"
+document_interface "modular"
 exit 0
