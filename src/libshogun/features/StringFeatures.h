b76d0441acf5119d35221fe8e8c533aa02e3eb8e
Soeren Sonnenburg <sonne@debian.org>
2010-02-16 16:34:44 +0000

    fix string feature transpose function

--- src/libshogun/features/StringFeatures.h
+++ src/libshogun/features/StringFeatures.h
@@ -459,7 +459,7 @@ template <class ST> class CStringFeatures : public CFeatures
 			int32_t num_vec;
 			T_STRING<ST>* s=get_transposed(num_feat, num_vec);
 
-			return new CStringFeatures<ST>(s, num_feat, num_vec, alphabet);
+			return new CStringFeatures<ST>(s, num_vec, num_feat, alphabet);
 		}
 
 		/** compute and return the transpose of string features matrix
