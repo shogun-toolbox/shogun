0a9436f9d486e82273c72976f722182d88b3d9ee
Christian Widmer <cwidmer@tuebingen.mpg.de>
2010-11-11 12:39:11 +0000

    fixed indentation

--- src/libshogun/classifier/LinearClassifier.cpp
+++ src/libshogun/classifier/LinearClassifier.cpp
@@ -17,9 +17,9 @@ CLinearClassifier::CLinearClassifier()
 : CClassifier(), w_dim(0), w(NULL), bias(0), features(NULL)
 {
 
-m_parameters->add_vector(&w, &w_dim, "w", "Parameter vector w.");
-m_parameters->add(&bias, "bias", "Bias b.");
-m_parameters->add((CSGSerializable**) &features, "features", "Feature object.");
+	m_parameters->add_vector(&w, &w_dim, "w", "Parameter vector w.");
+	m_parameters->add(&bias, "bias", "Bias b.");
+	m_parameters->add((CSGSerializable**) &features, "features", "Feature object.");
 
 }
 
