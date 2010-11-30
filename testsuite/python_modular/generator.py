a7ede2359179c6856506dcdda116686101b340d8
Soeren Sonnenburg <sonne@debian.org>
2010-11-30 22:39:02 +0000

    improve generator / tester

--- testsuite/python_modular/generator.py
+++ testsuite/python_modular/generator.py
@@ -4,61 +4,68 @@ import pickle
 
 example_dir = '../../examples/undocumented/python_modular'
 test_dir = '../../../testsuite/tests'
-blacklist = ("structure_dynprog_modular.py",
-            "__init__.py",
-            "serialization_svmlight_modular.py")
+blacklist = ("__init__.py", "classifier_libsvm_minimal_modular.py")
 
 def get_fname(mod_name, i):
-    return os.path.join(test_dir, mod_name + str(i) + '.txt')
+	return os.path.join(test_dir, mod_name + str(i) + '.txt')
 
 def setup_tests(tests=[]):
-    os.chdir(example_dir)
+	os.chdir(example_dir)
 
-    if not len(tests):
-        tests =  os.listdir(".")
-    else:
-        tests = [ os.path.basename(t) for t in tests ]
+	if not len(tests):
+		tests =  os.listdir(".")
+	else:
+		tests = [ os.path.basename(t) for t in tests ]
 
-    sys.path.insert(0, '.')
-    return tests
+	sys.path.insert(0, '.')
+	return tests
 
 def check_for_function(fname):
-    for l in file(fname).readlines():
-        if l.startswith("def "):
-            return True
-    return False
+	for l in file(fname).readlines():
+		if l.startswith("def "):
+			return True
+	return False
+
+def get_test_mod(t):
+	if t.endswith(".py") and not t.startswith('.') and t not in blacklist:
+		mod_name = t[:-3]
+		print mod_name,
+
+		if not check_for_function(t):
+			print " ERROR (no function)"
+			return
+
+		return __import__(mod_name), mod_name
+
+def run_test(mod, mod_name, i):
+	fname = get_fname(mod_name, i)
+	par=mod.parameter_list[i]
+	a =  getattr(mod, mod_name)(*par)
+	return a
 
 def generator(tests):
-    for t in tests:
-        if t.endswith(".py") and not t.startswith('.') and t not in blacklist:
-            mod_name = t[:-3]
-            print mod_name,
+	for t in tests:
+		try:
+			mod, mod_name = get_test_mod(t)
+		except:
+			continue
+		fname = ""
 
-            if not check_for_function(t):
-                print " ERROR (no function)"
-                continue
-
-            mod = __import__(mod_name)
-            fname = ""
-
-            try:
-                for i in xrange(len(mod.parameter_list)):
-                    fname = get_fname(mod_name, i)
-                    f = open(fname, "w")
-                    par=mod.parameter_list[i]
-                    a =  getattr(mod, mod_name)(*par)
-                    pickle.dump(a,f)
-                print " OK"
-            except Exception, e:
-                fname = get_fname(mod_name, i)
-                print " ERROR generating '%s' using '%s'" % (fname,t)
-                print e
-                continue
+		try:
+			for i in xrange(len(mod.parameter_list)):
+				fname = get_fname(mod_name, i)
+				a = run_test(mod, mod_name, i)
+				pickle.dump(a,file(fname, "w"))
+			print " OK"
+		except Exception, e:
+			print " ERROR generating '%s' using '%s'" % (fname,t)
+			print e
+			continue
 
 if __name__=='__main__':
-    from optparse import OptionParser
-    op=OptionParser()
-    op.set_usage("[<file1> <file2> ...]")
-    (opts, args)=op.parse_args()
-    tests = setup_tests(args)
-    generator(tests)
+	from optparse import OptionParser
+	op=OptionParser()
+	op.set_usage("[<file1> <file2> ...]")
+	(opts, args)=op.parse_args()
+	tests = setup_tests(args)
+	generator(tests)
--- testsuite/python_modular/tester.py
+++ testsuite/python_modular/tester.py
@@ -3,36 +3,49 @@ import os
 import filecmp
 import numpy
 
-from generator import tests, test_dir, get_fname, blacklist
+from generator import setup_tests, get_fname, blacklist, get_test_mod, run_test
 
 def compare(a, b):
-    if type(a) != type(b): return False
+	if type(a) != type(b): return False
 
-    if type(a) == numpy.ndarray: return numpy.all(a == b)
-    elif type(a) in (tuple,list):
-        if len(a) != len(b): return False
-        for obj1, obj2 in zip(a,b):
-            if type(obj1) != type(obj2): return False
-            if not compare(obj1, obj2): return False
-        return True
+	if type(a) == numpy.ndarray: return numpy.all(a == b)
+	elif type(a) in (tuple,list):
+		if len(a) != len(b): return False
+		for obj1, obj2 in zip(a,b):
+			if type(obj1) != type(obj2): return False
+			if not compare(obj1, obj2): return False
+		return True
 
-    return a == b
+	return a == b
 
-def tester():
-    for t in tests:
-        if t.endswith(".py") and not t.startswith('.') and t not in blacklist:
-            mod_name = t[:-3]
-            mod = __import__(mod_name)
-            for i in xrange(len(mod.parameter_list)):
-                fname = get_fname(mod_name, i)
-                par=mod.parameter_list[i]
-                a =  getattr(mod, mod_name)(*par)
-                b = pickle.load(file(fname))
+def tester(tests):
+	for t in tests:
+		try:
+			mod, mod_name = get_test_mod(t)
+		except:
+			continue
+		fname = ""
 
-                if compare(a,b):
-                    print t, "setting", i, "OK"
-                else:
-                    print t, "setting", i, "ERROR"
+		try:
+			for i in xrange(len(mod.parameter_list)):
+				fname = get_fname(mod_name, i)
+				a = run_test(mod, mod_name, i)
+				b = pickle.load(file(fname))
+
+				if compare(a,b):
+					print t, "setting", i, "OK"
+				else:
+					print t, "setting", i, "ERROR"
+
+		except Exception, e:
+			print " ERROR generating '%s' using '%s'" % (fname,t)
+			print e
+			continue
 
 if __name__=='__main__':
-    tester()
+	from optparse import OptionParser
+	op=OptionParser()
+	op.set_usage("[<file1> <file2> ...]")
+	(opts, args)=op.parse_args()
+	tests = setup_tests(args)
+	tester(tests)
