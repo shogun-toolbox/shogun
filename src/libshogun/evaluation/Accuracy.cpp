6d7d6306c380bf9ca3560281c00201e5c1214c4b
Sergey Lisitsyn <blackburn91@gmail.com>
2011-04-17 13:23:25 +0400

    Added CAccuracy class for evaluating classifiers accuracy

---
+++ src/libshogun/evaluation/Accuracy.cpp
@@ -0,0 +1,30 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2011 Sergey Lisitsyn
+ * Copyright (C) 2011 Berlin Institute of Technology and Max-Planck-Society
+ */
+
+#include "Accuracy.h"
+#include "features/Labels.h"
+
+using namespace shogun;
+
+float64_t CAccuracy::evaluate(CLabels* labels, CLabels* labels_valid)
+{
+	ASSERT(labels->get_num_labels() == labels_valid->get_num_labels());
+	int32_t length = labels->get_num_labels();
+	float64_t accuracy = 0.0;
+	for (int32_t i=0; i<length; i++)
+	{
+		if (labels->get_int_label(i) == labels_valid->get_int_label(i))
+		{
+			accuracy += 1.0;
+		}
+	}
+	accuracy /= length;
+	return accuracy;
+}
---
+++ src/libshogun/evaluation/Accuracy.h
@@ -0,0 +1,55 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2011 Sergey Lisitsyn
+ * Copyright (C) 2011 Berlin Institute of Technology and Max-Planck-Society
+ */
+
+#ifndef ACCURACY_H_
+#define ACCURACY_H_
+
+#include "Evaluation.h"
+#include "features/Labels.h"
+
+namespace shogun
+{
+
+/** @brief The class Accuracy
+ * used to compute accuracy of classification.
+ *
+ * Formally, for labels \f$L,R, |L|=|R|\f$ accuracy is estimated as
+ *
+ * \f[
+ * 		\frac{\sum_{i=1}^{|L|} [L_i=R_i]}{|L|}
+ * \f]
+ *
+ * Note this class is capable of evaluating both multi-class and 2-class
+ * labels.
+ *
+ */
+class CAccuracy: public CEvaluation
+{
+public:
+	/** constructor */
+	CAccuracy() : CEvaluation() {};
+
+	/** destructor */
+	virtual ~CAccuracy() {};
+
+	/** evaluate accuracy
+	 * @param labels labels for evaluating
+	 * @param labels_valid labels assumed to be correct
+	 * @return accuracy
+	 */
+	virtual float64_t evaluate(CLabels* labels, CLabels* labels_valid);
+
+	/** get name */
+	virtual inline const char* get_name() const { return "Accuracy"; }
+};
+
+}
+
+#endif /* ACCURACY_H_ */
