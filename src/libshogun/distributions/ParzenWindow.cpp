db77544e03efe3e8d53973d0589cc42925358cbe
Haipeng Wang <leleeell@gmail.com>
2011-04-17 13:28:29 +0800

    some updates on parzen window class

---
+++ src/libshogun/distributions/ParzenWindow.cpp
@@ -0,0 +1,110 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2011 Haipeng Wang
+ * Copyright (C) 2011 Berlin Institute of Technology and Max-Planck-Society
+ */
+
+#include "ParzenWindow.h"
+
+using namespace shogun;
+
+ParzenWindow::ParzenWindow()
+	: CDistribution()
+{
+	m_nVecSize = 0;
+	m_nTrainingSample = 0;
+	m_fWidth = 0.0;
+	m_ppTrainingData = NULL;
+	m_fGaussianConst = 0.0;
+}
+
+/** Since we don't store any samples in this class, so the Destructor just reset the pointer */
+ParzenWindow::~ParzenWindow()
+{
+	if(m_nVecSize * m_nTrainingSample > 0)
+	{
+		m_ppTrainingData = NULL;
+	}
+}
+
+/** Initilization: 
+ * 1) set the basic config and pass in the pointer to the training data
+ * 2) calculate the Gaussian constant.
+ */ 
+bool ParzenWindow::init(int32_t nFeaDim, int32_t nTrainPoints, float64_t fWindowWidth, float64_t** ppTrainingPoints)
+{
+	if(nFeaDim <= 0 || nTrainPoints <= 0 || fWindowWidth  <= 0 || ppTrainingPoints == NULL)
+	{
+		return false;
+	}else
+	{
+		m_nVecSize = nFeaDim;
+		m_nTrainingSample = nTrainPoints;
+		m_fWidth = fWindowWidth;
+		m_ppTrainingData = ppTrainingPoints;
+		m_fGaussianConst = pow(sqrt(DPI)*m_fWidth, m_nVecSize);
+		m_fGaussianConst = 1/m_fGaussianConst;
+		return true;
+	}
+}
+
+/** Calculate Gassuian kernel distance between two vectors. */
+float64_t ParzenWindow::GaussianProb(const float64_t* Vector1, const float64_t* Vector2)
+{
+	int32_t i = 0;
+	float64_t dis = 0;
+	for(i = 0; i <m_nVecSize; i++)
+	{
+		dis += (Vector1[i]-Vector2[i]) * (Vector1[i]-Vector2[i]);
+	}
+	dis = -1*dis/(2*m_fWidth*m_fWidth);
+	if(dis < MinExpArg)
+	{
+		dis = 0;
+	}else
+	{
+		dis = exp(dis);
+	}
+	dis = dis/m_fGaussianConst;
+	return dis;
+}
+
+/** Calculate all the densities of all the testing samples based on the training samples */
+void ParzenWindow::CalculateDensity(float64_t* const pDensity, const float64_t** ppTestingPoints, int32_t nTestingPoints, int32_t nFeaDim_Test)
+{
+	if(nFeaDim_Test != m_nVecSize)
+	{
+		SG_ERROR("Feature Dimensions for Training samples and Testing samples should be the same\n");
+	}
+	if(pDensity == NULL)
+	{
+		SG_ERROR("Density vector space should be allocated\n");
+	}
+	if(ppTestingPoints == NULL)
+	{
+		SG_ERROR("Testing feature vectors should be provided\n");
+	}
+	if(nTestingPoints == 0)
+	{
+		SG_ERROR("No Testing Samples\n");
+	}
+	if(ppTrainingData == NULL)
+	{
+		SG_ERROR("Training feature vectors should be provided\n");
+	}
+	int32_t i = 0;
+	int32_t j = 0;
+	for(i = 0; i <nTestingPoints; i++)
+	{
+		pDensity[i] = 0.0;
+		for(j = 0; j < nTrainingSample; j++)
+		{
+			pDensity[i] += GaussianProb(ppTestingPoints[i], m_ppTrainingData[j]);
+		}
+		pDensity[i] = pDensity[i]/m_nTrainingSample;
+	}
+}
---
+++ src/libshogun/distributions/ParzenWindow.h
@@ -0,0 +1,93 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2011 Haipeng Wang
+ * Copyright (C) 2011 Berlin Institute of Technology and Max-Planck-Society
+ */
+
+#ifndef __ParzenWindow_H__
+#define __ParzenWindow_H__
+
+#ifndef PI
+/** just in case PI is not defined */
+#define PI   3.14159265358979
+#endif
+
+/** 2*PI */
+#define DPI  6.28318530717959
+
+/** lowest exp() arg. If arg <  MinExpArg, exp(arg) = 0*/
+#define MinExpArg (-700.0)
+
+#include <stdio.h>
+#include <math.h>
+#include "lib/Mathematics.h"
+#include "lib/common.h"
+#include "lib/io.h"
+#include "lib/config.h"
+#include "features/Features.h"
+#include "distributions/Distribution.h"
+
+
+namespace shogun
+{
+class ParzenWindow::public CDistribution
+{
+public:
+	/** default constructor */ 
+	ParzenWindow();
+
+	/** destructor */
+	virtual ~ParzenWindow();
+
+	/** ParzenWindow Initialization
+	 *	This inilization should be run before calculating density.
+	 *	@param nFeaDim Feature dimension.
+	 *	@param nTrainPoints Number of Training Samples.
+	 *	@param fWindowWidth Window Width.
+	 *  @param ppTrainingPoints The ith pointer ppTrainingPoints[i] points to the address where the ith training sample is stored.
+	 *  @return true if successful
+	 */
+	bool init(int32_t nFeaDim, int32_t nTrainPoints, float64_t fWindowWidth, float64_t** ppTrainingPoints);
+
+	/** Calculate Gassuian kernel distance between two vectors.
+	 *	the core function is dis(x,y)=(1/pow(sqrt(2*Pi)*h,d))*(exp(-0.5/(h*h) * ||x-y||_{2}^{2})
+	 *  @return true if successful the Gassuian kernel distance;
+	 */
+	float64_t GaussianProb(const float64_t* Vector1, const float64_t* Vector2);
+	
+	/** Calculate all the densities of all the testing samples based on the training samples
+	 *  @param pDensity address where the calculated densities are stored
+	 *  @param ppTestingPoints The ith pointer ppTestingPoints[i] points to the address where the ith testing sample is stored.
+	 *  @param nTestingPoints number of testing samples
+	 *  @param nFeaDim_Test testing sample feature dimension
+	 */
+	void CalculateDensity(float64_t* const pDensity, const float64_t** ppTestingPoints, int32_t nTestingPoints, int32_t nFeaDim_Test);
+	
+protected:
+	/** Training sample feature dimension */
+	int32_t	m_nVecSize;
+
+	/** number of training samples */
+	int32_t m_nTrainingSample;
+
+	/** window width
+	 *  Since we use Gaussian kernel function as the window function, the window width is used as the kernel width.
+	 */
+	float64_t m_fWidth;
+
+	/** Pointers to the pointers of training Data
+	 *  The ith pointer ppTrainingData[i] points to the address where the ith training sample is stored.
+	 *  We don't copy the training data into this class, but just pass this pointer in.
+	 */
+	float64_t** m_ppTrainingData;
+
+	/** Guassian kernel const = 1/(pow(sqrt(2*pi)*h),d) */
+	float64_t m_fGaussianConst;
+};
+}
+
+#endif
