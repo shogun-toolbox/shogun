45fbb3ddb2efdf3a0a8a3cd96607e44811b37aa7
Soeren Sonnenburg <sonne@debian.org>
2011-04-19 11:57:57 +0200

    remove test cause and change function name to match filename

--- examples/undocumented/python_modular/classifier_gaussiannaivebayes_modular.py
+++ examples/undocumented/python_modular/classifier_gaussiannaivebayes_modular.py
@@ -4,9 +4,9 @@ traindat = lm.load_numbers('../data/fm_train_real.dat')
 testdat = lm.load_numbers('../data/fm_test_real.dat')
 label_traindat = lm.load_labels('../data/label_train_multiclass.dat')
 
-parameter_list = [[traindat,testdat,label_traindat],[traindat,testdat,label_traindat]]
+parameter_list = [[traindat,testdat,label_traindat]]
 
-def classifier_gnb_modular(fm_train_real=traindat,fm_test_real=testdat,label_train_multiclass=label_traindat):
+def classifier_gaussiannaivebayes_modular(fm_train_real=traindat,fm_test_real=testdat,label_train_multiclass=label_traindat):
 	from shogun.Features import RealFeatures, Labels
 	from shogun.Classifier import GaussianNaiveBayes
 
@@ -17,7 +17,7 @@ def classifier_gnb_modular(fm_train_real=traindat,fm_test_real=testdat,label_tra
 	gnb=GaussianNaiveBayes(feats_train, labels)
 	gnb_train = gnb.train()
 	output=gnb.classify(feats_test).get_labels()
-	return gnb,gnb_train,output
+	return gnb, gnb_train, output
 
 if __name__=='__main__':
 	print 'GaussianNaiveBayes'
