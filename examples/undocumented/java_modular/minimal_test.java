ade1c3da67476135342e441fec448dbaee0b8381
Soeren Sonnenburg <sonne@debian.org>
2011-04-26 21:29:48 +0200

    minimal java example

---
+++ examples/undocumented/java_modular/minimal_test.java
@@ -0,0 +1,12 @@
+public class runme {
+    static {
+        System.loadLibrary("Kernel");
+    }
+
+    public static void main(String argv[]) {
+        KernelJNI.init_shogun__SWIG_4();
+        GaussianKernel x = new GaussianKernel();
+        System.out.println(x.get_width());
+        KernelJNI.exit_shogun();
+    }
+}
