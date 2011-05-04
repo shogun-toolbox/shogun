e273a47f2f27878580cdb51dcefa0919e56a5f39
Baozeng Ding <sploving1@gmail.com>
2011-05-04 14:52:03 +0800

    put java classes into shogun package and add build.xml file

--- src/java_modular/shogun/examples/VectorTest.java
+++ src/java_modular/shogun/examples/VectorTest.java
@@ -1,10 +1,10 @@
-public class vector_test {
+public class VectorTest {
 	static {
 		System.loadLibrary("Features");
 	}
 
 	public static void main(String argv[]) {
-		FeaturesJNI.init_shogun__SWIG_4();
+		Features.init_shogun();
 		double[] vec = {1,2,3,4}; 
 		Labels x = new Labels(vec);
 		double[] y = new double[4];
@@ -12,6 +12,6 @@ public class vector_test {
 		for (int i = 0; i < 4; i++) {
 			System.out.println(y[i]);
 		}
-		FeaturesJNI.exit_shogun();
+		Features.exit_shogun();
 	}
 }
