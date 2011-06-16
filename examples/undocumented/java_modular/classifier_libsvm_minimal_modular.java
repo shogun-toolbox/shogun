5c71d645a57e442ae5998013ef901d41c3140bd5
Soeren Sonnenburg <sonne@debian.org>
2011-06-17 00:25:38 +0200

    polish minimal example to use jblas' signum / mean etc and simplify code slightly

--- examples/undocumented/java_modular/classifier_libsvm_minimal_modular.java
+++ examples/undocumented/java_modular/classifier_libsvm_minimal_modular.java
@@ -1,5 +1,10 @@
 import org.shogun.*;
 import org.jblas.*;
+import static org.jblas.MatrixFunctions.signum;
+import static org.jblas.DoubleMatrix.concatHorizontally;
+import static org.jblas.DoubleMatrix.ones;
+import static org.jblas.DoubleMatrix.randn;
+
 public class classifier_libsvm_minimal_modular {
 	static {
 		System.loadLibrary("Features");
@@ -7,53 +12,37 @@ public class classifier_libsvm_minimal_modular {
 		System.loadLibrary("Kernel");
 	}
 	
-	public static double mean_error(DoubleMatrix out, DoubleMatrix lab) {
-		out.assertSameLength(lab);
-		double mean=0;
-		for (int i=0; i<lab.length; i++) {
-			if (java.lang.Math.signum(out.get(i)) != java.lang.Math.signum(lab.get(i)))
-				mean++;
-		}
-		mean/=lab.length;
-		return mean;
-	}
-
-	
 	public static void main(String argv[]) {
 		Features.init_shogun_with_defaults();
+
 		int num = 1000;
 		double dist = 1.0;
 		double width = 2.1;
 		double C = 1.0;
 
-		DoubleMatrix offs=DoubleMatrix.ones(2, num).mmul(dist);
-		DoubleMatrix x = DoubleMatrix.randn(2, num).sub(offs);
-		DoubleMatrix y = DoubleMatrix.randn(2, num).add(offs);
-		DoubleMatrix traindata_real = DoubleMatrix.concatHorizontally(x, y);
+		DoubleMatrix offs=ones(2, num).mmul(dist);
+		DoubleMatrix x = randn(2, num).sub(offs);
+		DoubleMatrix y = randn(2, num).add(offs);
+		DoubleMatrix traindata_real = concatHorizontally(x, y);
 
-		DoubleMatrix m = DoubleMatrix.randn(2, num).sub(offs);
-		DoubleMatrix n = DoubleMatrix.randn(2, num).add(offs);
-		DoubleMatrix testdata_real = DoubleMatrix.concatHorizontally(m, n);
+		DoubleMatrix m = randn(2, num).sub(offs);
+		DoubleMatrix n = randn(2, num).add(offs);
+		DoubleMatrix testdata_real = concatHorizontally(m, n);
 
-		DoubleMatrix trainlab = DoubleMatrix.concatHorizontally(DoubleMatrix.ones(1, num).neg(),
-				DoubleMatrix.ones(1, num));
-		DoubleMatrix testlab = DoubleMatrix.concatHorizontally(DoubleMatrix.ones(1, num).neg(),
-				DoubleMatrix.ones(1, num));
+		DoubleMatrix o = ones(1,num);
+		DoubleMatrix trainlab = concatHorizontally(o.neg(), o);
+		DoubleMatrix testlab = concatHorizontally(o.neg(), o);
 
-		RealFeatures feats_train = new RealFeatures();
-		feats_train.set_feature_matrix(traindata_real);
-		RealFeatures feats_test = new RealFeatures();
-		feats_test.set_feature_matrix(testdata_real);
+		RealFeatures feats_train = new RealFeatures(traindata_real);
+		RealFeatures feats_test = new RealFeatures(testdata_real);
 		GaussianKernel kernel = new GaussianKernel(feats_train, feats_train, width);
 		Labels labels = new Labels(trainlab);
 		LibSVM svm = new LibSVM(C, kernel, labels);
 		svm.train();
 
-		kernel.init(feats_train, feats_test);
-		DoubleMatrix out = svm.apply().get_labels();
+		DoubleMatrix out = svm.apply(feats_test).get_labels();
 
-		double mean=mean_error(out, testlab);
-		System.out.println("Mean Error = " + mean);
+		System.out.println("Mean Error = " + signum(out).ne(testlab).mean());
 		Features.exit_shogun();
 	}
 }
