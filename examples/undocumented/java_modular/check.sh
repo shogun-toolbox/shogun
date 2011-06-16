e38049601b5a5509af8f5209d851761dee47a856
Baozeng Ding <sploving1@gmail.com>
2011-06-16 16:50:37 +0800

    add check.sh to check java examples

---
+++ examples/undocumented/java_modular/check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z "${JAVA}" ]
+then
+	JAVAC=javac
+	JAVA=java
+fi
+
+export CLASSPATH=/root/jblas/jblas-1.2.1.jar:../../../src/java_modular/shogun.jar:.
+${JAVAC} Load.java
+${JAVAC} *.java
+
+export LD_LIBRARY_PATH=../../../src/libshogun:../../../src/java_modular
+for filename in *.class
+  do
+  echo "running ${filename%.class} .."
+  ${JAVA} ${filename%.class}
+done
