2089d24a050d63a8c6a06685e06ff3b9bc3990c7
Baozeng Ding <sploving1@gmail.com>
2011-06-16 20:46:44 +0800

    fix check.sh

--- examples/undocumented/java_modular/check.sh
+++ examples/undocumented/java_modular/check.sh
@@ -7,11 +7,12 @@ fi
 
 export CLASSPATH=/usr/share/java/jblas.jar:../../../src/java_modular/shogun.jar:.
 ${JAVAC} Load.java
-${JAVAC} *.java
+${JAVAC} $(ls *.java | sed 's/Load.java//')
 
 export LD_LIBRARY_PATH=../../../src/libshogun:../../../src/java_modular
 for filename in *.class
   do
+  [ "$filename" = "Load.class" ] && continue
   echo "running ${filename%.class} .."
   ${JAVA} ${filename%.class}
 done
