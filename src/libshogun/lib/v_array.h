4c1d91de4aa7a2b434dfb12925a434e210caa309
Shashwat Lal Das <fraggerx@gmail.com>
2011-06-17 17:22:39 +0530

    Fixed license of v_array.h

--- src/libshogun/lib/v_array.h
+++ src/libshogun/lib/v_array.h
@@ -1,8 +1,17 @@
 /*
-Copyright (c) 2009 Yahoo! Inc.  All rights reserved.  The copyrights
-embodied in the content of this file are licensed under the BSD
-(revised) open source license
- */
+  Copyright (c) 2009 Yahoo! Inc.  All rights reserved.  The copyrights
+  embodied in the content of this file are licensed under the BSD
+  (revised) open source license.
+
+  Copyright (c) 2011 Berlin Institute of Technology and Max-Planck-Society.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  Shogun adjustments (w) 2011 Shashwat Lal Das
+*/
 
 #include <stdlib.h>
 
