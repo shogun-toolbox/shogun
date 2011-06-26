53b127da20c8b3563a1fe4dac0352d537e7d0e76
Sergey Lisitsyn <blackburn91@gmail.com>
2011-06-26 17:30:52 +0400

    Fixes for LandmarkIsomap

--- src/libshogun/preprocessor/LandmarkIsomap.cpp
+++ src/libshogun/preprocessor/LandmarkIsomap.cpp
@@ -22,7 +22,7 @@
 
 using namespace shogun;
 
-CLandmarkIsomap::CLandmarkIsomap() : CLandmarkMDS()
+CLandmarkIsomap::CLandmarkIsomap() : CLandmarkMDS(), m_k(3)
 {
 }
 
@@ -45,6 +45,30 @@ CCustomDistance* CLandmarkIsomap::approx_geodesic_distance(CDistance* distance)
 	float64_t* D_matrix;
 	distance->get_distance_matrix(&D_matrix,&N,&N);
 
+	float64_t* row = new float64_t[N];
+	int32_t* row_idx = new int32_t[N];
+
+	// cut-off by k
+	for (i=0; i<N; i++)
+	{
+		for (j=0; j<N; j++)
+		{
+			row[j] = D_matrix[i*N+j];
+			row_idx[j] = j;
+		}
+
+		CMath::qsort_index(row,row_idx,N);
+
+		for (j=m_k+1; j<N; j++)
+		{
+			if (i!=row_idx[j])
+				D_matrix[i*N+row_idx[j]] = CMath::ALMOST_INFTY;
+		}
+	}
+
+	delete[] row;
+	delete[] row_idx;
+
 	// floyd-warshall
 	for (k=0; k<N; k++)
 	{
@@ -55,15 +79,22 @@ CCustomDistance* CLandmarkIsomap::approx_geodesic_distance(CDistance* distance)
 		}
 	}
 
-	return new CCustomDistance(D_matrix,N,N);
+	CCustomDistance* geodesic_distance = new CCustomDistance(D_matrix,N,N);
+	delete[] D_matrix;
+
+	return geodesic_distance;
 }
 
 CSimpleFeatures<float64_t>* CLandmarkIsomap::apply_to_distance(CDistance* distance)
 {
 	ASSERT(distance);
 
+	CCustomDistance* geodesic_distance = approx_geodesic_distance(distance);
+
 	SGMatrix<float64_t> new_feature_matrix =
-			CLandmarkMDS::embed_by_distance(approx_geodesic_distance(distance));
+			CLandmarkMDS::embed_by_distance(geodesic_distance);
+
+	delete geodesic_distance;
 
 	CSimpleFeatures<float64_t>* new_features =
 			new CSimpleFeatures<float64_t>(new_feature_matrix);
@@ -75,10 +106,15 @@ CSimpleFeatures<float64_t>* CLandmarkIsomap::apply_to_distance(CDistance* distan
 SGMatrix<float64_t> CLandmarkIsomap::apply_to_feature_matrix(CFeatures* features)
 {
 	CSimpleFeatures<float64_t>* simple_features = (CSimpleFeatures<float64_t>*) features;
+
 	CDistance* distance = new CEuclidianDistance(simple_features,simple_features);
 
+	CCustomDistance* geodesic_distance = approx_geodesic_distance(distance);
+
 	SGMatrix<float64_t> new_feature_matrix =
-			CLandmarkMDS::embed_by_distance(approx_geodesic_distance(distance));
+			CLandmarkMDS::embed_by_distance(geodesic_distance);
+
+	delete geodesic_distance;
 
 	simple_features->set_feature_matrix(new_feature_matrix);
 
