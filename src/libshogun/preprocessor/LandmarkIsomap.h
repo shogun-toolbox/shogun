1114044db179ad7f5cc10e0439999c10818efc3e
Sergey Lisitsyn <blackburn91@gmail.com>
2011-06-28 00:10:10 +0400

    Restore LandmarkIsomap with not implemented tags

---
+++ src/libshogun/preprocessor/LandmarkIsomap.h
@@ -0,0 +1,98 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2011 Sergey Lisitsyn
+ * Copyright (C) 2011 Berlin Institute of Technology and Max-Planck-Society
+ */
+
+#ifndef LANDMARKISOMAP_H_
+#define LANDMARKISOMAP_H_
+#ifdef HAVE_LAPACK
+#include "preprocessor/LandmarkMDS.h"
+#include "preprocessor/SimplePreprocessor.h"
+#include "features/Features.h"
+#include "distance/Distance.h"
+#include "distance/CustomDistance.h"
+
+namespace shogun
+{
+
+class CFeatures;
+
+class CDistance;
+
+/** @brief the class LandmarkIsomap
+ *
+ */
+class CLandmarkIsomap: public CLandmarkMDS
+{
+public:
+
+	/* constructor */
+	CLandmarkIsomap();
+
+	/* destructor */
+	virtual ~CLandmarkIsomap();
+
+	/** init
+	 * @param data feature vectors for preproc
+	 */
+	virtual bool init(CFeatures* features);
+
+	/** cleanup
+	 *
+	 */
+	virtual void cleanup();
+
+	/** apply preproc to distance
+	 *
+	 */
+	virtual CSimpleFeatures<float64_t>* apply_to_distance(CDistance* distance);
+
+	/** apply preproc to feature matrix
+	 *
+	 */
+	virtual SGMatrix<float64_t> apply_to_feature_matrix(CFeatures* features);
+
+	/** apply preproc to feature vector
+	 *
+	 */
+	virtual SGVector<float64_t> apply_to_feature_vector(SGVector<float64_t> vector);
+
+	/** get name */
+	virtual inline const char* get_name() const { return "LandmarkIsomap"; };
+
+	/** get type */
+	virtual inline EPreprocessorType get_type() const { return P_LANDMARKISOMAP; };
+
+	/** setter for k parameter
+	 * @param k
+	 */
+	void inline set_k(int32_t k)
+	{
+		m_k = k;
+	}
+
+	/** getter for k parameter
+	 * @return k value
+	 */
+	int32_t inline get_k()
+	{
+		return m_k;
+	}
+
+protected:
+
+	/** k */
+	float64_t m_k;
+
+	/** approx geodesic distance */
+	CCustomDistance* approx_geodesic_distance(CDistance* distance);
+
+};
+}
+#endif /* HAVE_LAPACK */
+#endif /* LANDMARKISOMAP_H_ */
