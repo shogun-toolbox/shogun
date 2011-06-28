163498b56d6bee4f685b9412c6d84250ca1fcd1f
Shashwat Lal Das <fraggerx@gmail.com>
2011-06-28 18:09:40 +0530

    First implementation of StreamingFileFromFeatures, StreamingAsciiFile and StreamingFileFromSimpleFeatures.

---
+++ src/libshogun/lib/StreamingFileFromFeatures.cpp
@@ -0,0 +1,29 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2011 Shashwat Lal Das
+ * Copyright (C) 2011 Berlin Institute of Technology and Max-Planck-Society
+ */
+#include "lib/StreamingFileFromFeatures.h"
+
+using namespace shogun;
+
+CStreamingFileFromFeatures::CStreamingFileFromFeatures(void)
+	: CStreamingFile()
+{
+	features=NULL;
+}
+
+CStreamingFileFromFeatures::CStreamingFileFromFeatures(CFeatures* feat)
+	: CStreamingFile()
+{
+	features=NULL;
+}
+
+CStreamingFileFromFeatures::~CStreamingFileFromFeatures()
+{
+	features=NULL;
+}
