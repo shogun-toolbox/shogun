c3fdb279cc0a6670c6e56812b218344783222e95
Baozeng Ding <sploving1@gmail.com>
2011-06-30 23:54:00 +0800

    add some examples

---
+++ examples/undocumented/java_modular/kernel_comm_word_string_modular.java
@@ -0,0 +1,44 @@
+import org.shogun.*;
+import org.jblas.*;
+import static org.shogun.EAlphabet.DNA;
+public class kernel_comm_word_string_modular {
+	static {
+		System.loadLibrary("Features");
+		System.loadLibrary("Kernel");
+		System.loadLibrary("Preprocessor");
+	}
+
+	public static void main(String argv[]) {
+		Features.init_shogun_with_defaults();
+		int order = 3;
+		int gap = 0;
+		boolean reverse = false;
+		boolean use_sign = false;
+
+		String[] fm_train_dna = Load.load_dna("../data/fm_train_dna.dat");
+		String[] fm_test_dna = Load.load_dna("../data/fm_test_dna.dat");
+
+		StringCharFeatures charfeat = new StringCharFeatures(DNA);
+		charfeat.set_features(fm_train_dna);
+		StringWordFeatures feats_train = new StringWordFeatures(charfeat.get_alphabet());
+		feats_train.obtain_from_char(charfeat, order-1, order, gap, reverse);
+		SortWordString preproc = new SortWordString();
+		preproc.init(feats_train);
+		feats_train.add_preprocessor(preproc);
+		feats_train.apply_preprocessor();
+
+		StringCharFeatures charfeat_test = new StringCharFeatures(DNA);
+		charfeat_test.set_features(fm_test_dna);
+		StringWordFeatures feats_test = new StringWordFeatures(charfeat.get_alphabet());
+		feats_test.obtain_from_char(charfeat_test, order-1, order, gap, reverse);
+		feats_test.add_preprocessor(preproc);
+		feats_test.apply_preprocessor();
+
+		CommWordStringKernel kernel = new CommWordStringKernel(feats_train, feats_train, use_sign);
+
+		DoubleMatrix km_train = kernel.get_kernel_matrix();
+		kernel.init(feats_train, feats_test);
+		DoubleMatrix km_test = kernel.get_kernel_matrix();
+		Features.exit_shogun();
+	}
+}
