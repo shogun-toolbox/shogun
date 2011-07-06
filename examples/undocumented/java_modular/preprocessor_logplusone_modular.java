e04e91c70dc6b20fa8501161436bb47847929e1e
Baozeng Ding <sploving1@gmail.com>
2011-07-06 23:33:38 +0800

    add some preprocessor examples

---
+++ examples/undocumented/java_modular/preprocessor_logplusone_modular.java
@@ -0,0 +1,40 @@
+import org.shogun.*;
+import org.jblas.*;
+public class preprocessor_logplusone_modular {
+	static {
+		System.loadLibrary("Features");
+		System.loadLibrary("Kernel");
+		System.loadLibrary("Preprocessor");
+	}
+
+	public static void main(String argv[]) {
+		Features.init_shogun_with_defaults();
+		double width = 1.4;
+		int size_cache = 10;
+
+		DoubleMatrix traindata_real = Load.load_numbers("../data/fm_train_real.dat");
+		DoubleMatrix testdata_real = Load.load_numbers("../data/fm_test_real.dat");
+
+		RealFeatures feats_train = new RealFeatures(traindata_real);
+		RealFeatures feats_test = new RealFeatures(testdata_real);
+
+		LogPlusOne preproc = new LogPlusOne();
+		preproc.init(feats_train);
+		feats_train.add_preprocessor(preproc);
+		feats_train.apply_preprocessor();
+		feats_test.add_preprocessor(preproc);
+		feats_test.apply_preprocessor();
+
+	
+		Chi2Kernel kernel = new Chi2Kernel(feats_train, feats_train, width, size_cache);
+
+		DoubleMatrix km_train = kernel.get_kernel_matrix();
+		kernel.init(feats_train, feats_test);
+		DoubleMatrix km_test = kernel.get_kernel_matrix();
+
+		System.out.println(km_train.toString());
+		System.out.println(km_test.toString());
+
+		Features.exit_shogun();
+	}
+}
