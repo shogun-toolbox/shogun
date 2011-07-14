d5988ef301459e0b7e3e84f47b24e5fdcc2be5ac
Heiko Strathmann <heiko.strathmann@gmail.com>
2011-07-14 21:45:27 +0200

    Squashed commit of the following:
    commit 116d110c03a9a4c394ecfbf52f11bb4eaac0d31e
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 21:40:53 2011 +0200
    
        -removed wrong SG_REF
        -usage of SGVector constructor to create array
    
    commit 477f42508a5f3f78cf8f4f3f3d4013745e58193c
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 21:31:03 2011 +0200
    
        added classes
    
    commit b0399426af55d015a25104ce148b8f13beec6850
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 21:30:34 2011 +0200
    
        applied interface changes due to SGVector changes
    
    commit 614b7fb3a93834cbde3977432941dc775609b5d5
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 21:30:12 2011 +0200
    
        -generell cleanup
        -usage of CDynamicObjectArray and CDynamicArray instead of DynArray
        -SGVector cleanup
    
    commit 2dc5f3625ee1dcd864bf45fa4dbd0928fd6dd996
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 21:29:06 2011 +0200
    
        added shuffle() method
    
    commit c6acd061a3ec4f5fc595e9d12d6f3c3740fa14d9
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 21:28:57 2011 +0200
    
        added comment
    
    commit da48e781312037a8660eb01d5f4d4f47f9ff3d1b
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 17:06:38 2011 +0200
    
        Revert "made get_evaluation_direction() const"
    
        This reverts commit da627efed746f371a9d33bf2799670a072523f40.
    
    commit 321aeefe86b4f9808055c29cc2889b171b2e9eee
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 17:02:49 2011 +0200
    
        made get_evaluation_direction() const
    
    commit c89a51a6269df4f37b6ce01671457da24a7b0b8e
    Author: Heiko Strathmann <heiko.strathmann@gmail.com>
    Date:   Thu Jul 14 16:50:36 2011 +0200
    
        made has_children() method const

--- src/libshogun/evaluation/SplittingStrategy.cpp
+++ src/libshogun/evaluation/SplittingStrategy.cpp
@@ -23,44 +23,40 @@ CSplittingStrategy::CSplittingStrategy(CLabels* labels, int32_t num_subsets) :
 {
 	/* construct all arrays */
 	for (index_t i=0; i<num_subsets; ++i)
-		m_subset_indices.append_element(new DynArray<index_t> ());
+		m_subset_indices.append_element(new CDynamicArray<index_t> ());
 
 	SG_REF(m_labels);
 }
 
 CSplittingStrategy::~CSplittingStrategy()
 {
-	/* delete all created arrays */
-	for (index_t i=0; i<m_subset_indices.get_num_elements(); ++i)
-		delete m_subset_indices[i];
-
 	SG_UNREF(m_labels);
 }
 
-void CSplittingStrategy::generate_subset_indices(index_t subset_idx,
-		SGVector<index_t>& result)
+SGVector<index_t> CSplittingStrategy::generate_subset_indices(index_t subset_idx)
 {
 	/* construct SGVector copy from index vector */
-	DynArray<index_t>* to_copy=m_subset_indices.get_element_safe(subset_idx);
+	CDynamicArray<index_t>* to_copy=m_subset_indices.get_element_safe(
+			subset_idx);
 
 	index_t num_elements=to_copy->get_num_elements();
-
-	/* fill result vector */
-	result.vector=new index_t[num_elements];
-	result.vlen=num_elements;
+	SGVector<index_t> result(num_elements, true);
 
 	/* copy data */
 	memcpy(result.vector, to_copy->get_array(), sizeof(index_t)*num_elements);
+
+	SG_UNREF(to_copy);
+
+	return result;
 }
 
-void CSplittingStrategy::generate_subset_inverse(index_t subset_idx,
-		SGVector<index_t>& result)
+SGVector<index_t> CSplittingStrategy::generate_subset_inverse(index_t subset_idx)
 {
-	DynArray<index_t>* to_invert=m_subset_indices.get_element_safe(subset_idx);
+	CDynamicArray<index_t>* to_invert=m_subset_indices.get_element_safe(
+			subset_idx);
 
-	/* fill result vector */
-	result.vlen=m_labels->get_num_labels()-to_invert->get_num_elements();
-	result.vector=new index_t[result.vlen];
+	SGVector<index_t> result(
+			m_labels->get_num_labels()-to_invert->get_num_elements(), true);
 
 	index_t index=0;
 	for (index_t i=0; i<m_labels->get_num_labels(); ++i)
@@ -69,4 +65,8 @@ void CSplittingStrategy::generate_subset_inverse(index_t subset_idx,
 		if (to_invert->find_element(i)==-1)
 			result.vector[index++]=i;
 	}
+
+	SG_UNREF(to_invert);
+
+	return result;
 }
