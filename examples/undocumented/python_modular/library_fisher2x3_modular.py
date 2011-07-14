140b13900e64a5fe51bda2391ff29c1dd679e612
Soeren Sonnenburg <sonne@debian.org>
2011-07-14 09:02:58 +0200

    add example for fisher test

---
+++ examples/undocumented/python_modular/library_fisher2x3_modular.py
@@ -0,0 +1,18 @@
+from numpy import *
+from shogun.Library import *
+
+x=array([[20.0,15,15],[10,20,20]])
+y=array([[21,21,18],[19,19,22]])
+z=array([[15,27,18],[32,5,23]])
+
+
+parameter_list = [[x,concatenate((x,y,z),1)],[y,concatenate((y,y,x),1)]]
+
+def library_fisher2x3_modular(table, tables):
+	pval=Math_fishers_exact_test_for_2x3_table(table)
+	pvals=Math_fishers_exact_test_for_multiple_2x3_tables(tables)
+	return (pval,pvals)
+
+if __name__=='__main__':
+	print 'Fisher 2x3'
+	library_fisher2x3_modular(*parameter_list[0])
