a615b6ecf839e9e7e603be1da76f082451b7991a
Baozeng Ding <sploving1@gmail.com>
2011-07-15 11:15:58 +0800

    add remaining kernel examples

---
+++ examples/undocumented/java_modular/kernel_linear_byte_modular.java
@@ -0,0 +1,32 @@
+import org.shogun.*;
+import org.jblas.*;
+public class kernel_linear_byte_modular {
+	static {
+		System.loadLibrary("Features");
+		System.loadLibrary("Kernel");
+	}
+
+	public static void main(String argv[]) {
+		Features.init_shogun_with_defaults();
+		double scale = 1.2;
+
+		DoubleMatrix traindata_real = Load.load_numbers("../data/fm_train_byte.dat");
+		DoubleMatrix testdata_real = Load.load_numbers("../data/fm_test_byte.dat");
+
+		ByteFeatures feats_train = new ByteFeatures(traindata_real);
+		ByteFeatures feats_test = new ByteFeatures(testdata_real);
+
+		LinearKernel kernel = new LinearKernel(feats_train, feats_test);
+		kernel.set_normalizer(new AvgDiagKernelNormalizer(scale));
+		kernel.init(feats_train, feats_train);
+
+		DoubleMatrix km_train = kernel.get_kernel_matrix();
+		kernel.init(feats_train, feats_test);
+		DoubleMatrix km_test = kernel.get_kernel_matrix();
+
+		System.out.println(km_train.toString());
+		System.out.println(km_test.toString());
+
+		Features.exit_shogun();
+	}
+}
