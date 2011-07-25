d2f13fb17d3f9998af1a175dfd4e2bea4544fb3d
Soeren Sonnenburg <sonne@debian.org>
2011-07-25 17:28:48 +0200

    add example to just serialize matrix

---
+++ examples/undocumented/python_modular/serialization_matrix_modular.py
@@ -0,0 +1,21 @@
+from modshogun import *
+from numpy import array
+
+parameter_list=[[[[1.0,2,3],[4,5,6]]]]
+
+def serialization_matrix_modular(m):
+	feats=RealFeatures(array(m))
+	#feats.io.set_loglevel(0)
+	fstream = SerializableAsciiFile("foo.asc", "w")
+	feats.save_serializable(fstream)
+
+	l=Labels(array([1.0,2,3]))
+	fstream = SerializableAsciiFile("foo2.asc", "w")
+	l.save_serializable(fstream)
+
+	os.unlink("foo.asc")
+	os.unlink("foo2.asc")
+
+if __name__=='__main__':
+	print 'Serialization Matrix Modular'
+	serialization_matrix_modular(*parameter_list[0])
