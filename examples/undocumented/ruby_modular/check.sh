620037ed381d27a99b8dc2ce304b85fe58a0159d
Soeren Sonnenburg <sonne@debian.org>
2011-07-29 00:42:10 +0200

    add single file test

--- examples/undocumented/ruby_modular/check.sh
+++ examples/undocumented/ruby_modular/check.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-for i in *.rb;
-do
-	ruby -I../../../src/interfaces/ruby_modular $i
-done
+if test -z "$1"
+then
+	for i in *.rb;
+	do
+		echo -n $i
+
+		if ruby -I../../../src/interfaces/ruby_modular $i >/dev/null 2>&1
+		then
+			echo " OK"
+		else
+			echo " FAIL"
+		fi
+	done
+else
+	ruby -I../../../src/interfaces/ruby_modular $1
+fi
