cb88253f5fa09236ec651bc74564c8bf42ac3c5f
Heiko Strathmann <heiko.strathmann@gmail.com>
2011-07-29 16:43:29 +0200

    -added init method -registered parameters -usage of CMath::sqrt instead of ::sqrt -replaced float64_t* R variable by SGVector<float64_t>

--- src/shogun/clustering/KMeans.h
+++ src/shogun/clustering/KMeans.h
@@ -113,7 +113,7 @@ class CKMeans : public CDistanceMachine
 		 */
 		SGVector<float64_t> get_radiuses()
 		{
-			return SGVector<float64_t>(R,k);
+			return R;
 		}
 
 		/** get centers
@@ -175,6 +175,9 @@ class CKMeans : public CDistanceMachine
 		 */
 		virtual bool train_machine(CFeatures* data=NULL);
 
+	private:
+		void init();
+
 	protected:
 		/// maximum number of iterations
 		int32_t max_iter;
@@ -186,7 +189,7 @@ class CKMeans : public CDistanceMachine
 		int32_t dimensions;
 
 		/// radi of the clusters (size k)
-		float64_t* R;
+		SGVector<float64_t> R;
 		
 		/// centers of the clusters (size dimensions x k)
 		float64_t* mus;
