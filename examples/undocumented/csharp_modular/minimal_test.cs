035f3cbd443ae6eba756fb7cc609305ad90be90c
Soeren Sonnenburg <sonne@debian.org>
2011-07-29 13:30:13 +0200

    fix minimal_test example to work with modshogun

--- examples/undocumented/csharp_modular/minimal_test.cs
+++ examples/undocumented/csharp_modular/minimal_test.cs
@@ -4,7 +4,7 @@ public class HelloWorld
 {
 	public static void Main(string[] args)
 	{
-		Library.init_shogun_with_defaults();
+		modshogun.init_shogun_with_defaults();
 		GaussianKernel k = new GaussianKernel();
 		Console.WriteLine(k.get_width());
 	}
