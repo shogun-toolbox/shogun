16c813f9bde9b66ddd70931bdf51b132f1cb4b85
Heiko Strathmann <heiko.strathmann@gmail.com>
2011-08-05 17:34:21 +0200

    added documentation

--- src/shogun/lib/StringMap.h
+++ src/shogun/lib/StringMap.h
@@ -16,15 +16,22 @@
 namespace shogun
 {
 
+/** @brief Implements key-value pairs that are put into a CStringMap
+ * Note that strings are copied in constructor.
+ */
 class CStringMapElement : public CSGObject
 {
 public:
+	/** constructor */
 	CStringMapElement();
 
+	/** constructor. Takes key and value string and stores a copy of it */
 	CStringMapElement(const char* key, const char* value);
 
+	/** destructor */
 	virtual ~CStringMapElement();
 
+	/** @return name of the SG_SERIALIZABLE */
 	inline virtual const char* get_name() const { return "StringMapElement"; }
 
 private:
@@ -37,16 +44,38 @@ public:
 
 template<class T> class CDynamicObjectArray;
 
+/** @brief implements a simple map for c-strings.
+ * Note that these are copied when put to the map. When getting an element,
+ * these copies are returned.
+ *
+ * As for now the implemenation is simple: O(n) for get, O(1) for put.
+ * This may be done by some kind of tree to get logarithmic costs.
+ * Beware of putting large amounts of elements in this map! Slow.
+ */
 class CStringMap : public CSGObject
 {
 public:
+	/** constructor */
 	CStringMap();
 
+	/** destructor */
 	virtual ~CStringMap();
 
+	/** puts a key-value pair into the map
+	 *
+	 * @param key key of the element
+	 * @param value value of the element
+	 */
 	void put(const char* key, const char* value);
+
+	/** gets a value of a provided key
+	 *
+	 * @param key key of the element to get
+	 * @return value of the specified key, NULL if there is no such
+	 */
 	const char* get(const char* key) const;
 
+	/** @return name of the SG_SERIALIZABLE */
 	inline virtual const char* get_name() const { return "StringMap"; }
 
 protected:
