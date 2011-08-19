92a2edbe1cd91800519c6364d607139d5e099038
Sergey Lisitsyn <blackburn91@gmail.com>
2011-08-20 01:01:37 +0400

    Updated kPCA ruby example

--- examples/undocumented/ruby_modular/preprocessor_kernelpca_modular.rb
+++ examples/undocumented/ruby_modular/preprocessor_kernelpca_modular.rb
@@ -8,13 +8,13 @@ data = LoadMatrix.load_numbers('../data/fm_train_real.dat')
 
 parameter_list = [[data, 0.01, 1.0], [data, 0.05, 2.0]]
 
-def preprocessor_kernelpcacut_modular(data, threshold, width)
+def preprocessor_kernelpca_modular(data, threshold, width)
 	
 	features = RealFeatures(data)
 	
 	kernel = GaussianKernel(features,features,width)
 		
-	preprocessor = KernelPCACut(kernel,threshold)
+	preprocessor = KernelPCA(kernel)
 	preprocessor.init(features)
 	preprocessor.apply_to_feature_matrix(features)
 
@@ -24,8 +24,8 @@ def preprocessor_kernelpcacut_modular(data, threshold, width)
 
 end
 if __FILE__ == $0
-	print 'KernelPCACut'
-	preprocessor_kernelpcacut_modular(*parameter_list[0])
+	print 'KernelPCA'
+	preprocessor_kernelpca_modular(*parameter_list[0])
 
 
 end
