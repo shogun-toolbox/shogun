d88e533d8c34392ae9a4f62df3acb7a3e070fc4a
Sergey Lisitsyn <blackburn91@gmail.com>
2011-08-20 00:59:55 +0400

    Updated kPCA python modular example

--- examples/undocumented/python_modular/preprocessor_kernelpca_modular.py
+++ examples/undocumented/python_modular/preprocessor_kernelpca_modular.py
@@ -5,16 +5,16 @@ data = lm.load_numbers('../data/fm_train_real.dat')
 
 parameter_list = [[data, 0.01, 1.0], [data, 0.05, 2.0]]
 
-def preprocessor_kernelpcacut_modular(data, threshold, width):
+def preprocessor_kernelpca_modular(data, threshold, width):
 	from shogun.Features import RealFeatures
-	from shogun.Preprocessor import KernelPCACut
+	from shogun.Preprocessor import KernelPCA
 	from shogun.Kernel import GaussianKernel
 	
 	features = RealFeatures(data)
 	
 	kernel = GaussianKernel(features,features,width)
 		
-	preprocessor = KernelPCACut(kernel,threshold)
+	preprocessor = KernelPCA(kernel)
 	preprocessor.init(features)
 	preprocessor.apply_to_feature_matrix(features)
 
@@ -22,6 +22,6 @@ def preprocessor_kernelpcacut_modular(data, threshold, width):
 
 
 if __name__=='__main__':
-	print 'KernelPCACut'
-	preprocessor_kernelpcacut_modular(*parameter_list[0])
+	print 'KernelPCA'
+	preprocessor_kernelpca_modular(*parameter_list[0])
 
