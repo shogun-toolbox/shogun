a6bf023b79a68115f03e84db88ea592a86a18918
Shashwat Lal Das <fraggerx@gmail.com>
2011-08-26 16:50:19 +0530

    Commented StreamingStringFeatures example better.

--- examples/undocumented/libshogun/streaming_stringfeatures.cpp
+++ examples/undocumented/libshogun/streaming_stringfeatures.cpp
@@ -1,3 +1,5 @@
+// This example simply demonstrates the use/working of StreamingStringFeatures
+
 #include <shogun/io/StreamingAsciiFile.h>
 #include <shogun/features/StreamingStringFeatures.h>
 
@@ -18,11 +20,15 @@ int main(int argc, char **argv)
 {
 	init_shogun_with_defaults();
 
+	// Create a StreamingAsciiFile from our input file
 	CStreamingAsciiFile* file = new CStreamingAsciiFile("../data/fm_train_dna.dat");
 
+	// This file contains unlabelled data, so the second arg is `false'.
 	CStreamingStringFeatures<char>* feat = new CStreamingStringFeatures<char>(file, false, 1024);
+	// Alphabet to use is DNA
 	feat->use_alphabet(DNA);
 
+	// Loop over all examples and simply display each example
 	feat->start_parser();
 	while (feat->get_next_example())
 	{
@@ -32,12 +38,12 @@ int main(int argc, char **argv)
 	}
 	feat->end_parser();
 
+	// Get the alphabet and display the histogram
 	CAlphabet* alpha = feat->get_alphabet();
-
 	printf("\nThe histogram is:\n");
 	alpha->print_histogram();
-
 	SG_UNREF(alpha);
+
 	SG_UNREF(feat);
 	SG_UNREF(file);
 
