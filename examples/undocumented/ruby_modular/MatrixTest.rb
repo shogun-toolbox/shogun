fbb28f974180bf62e3b4524fa2acf575d216970b
Soeren Sonnenburg <sonne@debian.org>
2011-09-02 20:14:26 +0200

    removal of useless ruby examples

--- examples/undocumented/ruby_modular/MatrixTest.rb
+++
@@ -1,9 +0,0 @@
-require 'modshogun'
-require 'shogun_helpers'
-require 'narray'
-require 'pp'
-x = Modshogun::RealFeatures.new
-y = [[1, 2], [3, 4], [5, 6]]
-x.set_feature_matrix(y)
-z = x.get_feature_matrix()
-pp(z)
--- examples/undocumented/ruby_modular/minimal_test.rb
+++
@@ -1,4 +0,0 @@
-require 'modshogun'
-require 'shogun_helpers'
-gk=Modshogun::GaussianKernel.new()
-puts gk.get_width()
