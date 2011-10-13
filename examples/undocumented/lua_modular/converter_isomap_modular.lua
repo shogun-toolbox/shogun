5cec6855af1417a7ef4ddb414a429a7a36cf5e55
Sergey Lisitsyn <blackburn91@gmail.com>
2011-10-14 02:57:09 +0400

    Updated lua isomap example

--- examples/undocumented/lua_modular/converter_isomap_modular.lua
+++ examples/undocumented/lua_modular/converter_isomap_modular.lua
@@ -5,18 +5,18 @@ data = load_numbers('../data/fm_train_real.dat')
 
 parameter_list = {{data}}
 
-function preprocessor_isomap_modular(data)
+function converter_isomap_modular(data)
 	features = modshogun.RealFeatures(data)
 		
-	preprocessor = modshogun.Isomap()
-	preprocessor:set_target_dim(1)
-	preprocessor:apply_to_feature_matrix(features)
+	converter = modshogun.Isomap()
+	converter:set_target_dim(1)
+	converter:apply(features)
 
 	return features
 end
 
 if debug.getinfo(3) == nill then
 	print 'Isomap'
-	preprocessor_isomap_modular(unpack(parameter_list[1]))
+	converter_isomap_modular(unpack(parameter_list[1]))
 end
 
