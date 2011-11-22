8c760f5944ed109743d034931d23ba5ace9ba17a
Soeren Sonnenburg <sonne@debian.org>
2011-11-22 07:57:22 +0100

    fix mkl example

--- examples/undocumented/libshogun/mkl_memleak_test.cpp
+++ examples/undocumented/libshogun/mkl_memleak_test.cpp
@@ -1,5 +1,8 @@
 #include <shogun/base/init.h>
+#include <shogun/classifier/mkl/MKLClassification.h>
 #include <shogun/regression/svr/MKLRegression.h>
+#include <shogun/regression/svr/LibSVR.h>
+#include <shogun/regression/svr/SVRLight.h>
 #include <shogun/features/Labels.h>
 #include <shogun/lib/DataType.h>
 #include <shogun/kernel/CustomKernel.h>
@@ -12,13 +15,21 @@ using namespace shogun;
 
 int main(int argc, char** argv)
 {
-	init_shogun();
+	init_shogun_with_defaults();
 
 	CMKLRegression* regressor = new CMKLRegression();
+	//CMKLClassification* regressor = new CMKLClassification();
+	regressor->set_mkl_norm(2.0);
+	//regressor->set_solver_type(ST_GLPK);
+	//CSVRLight* regressor = new CSVRLight();
+	//CLibSVR* regressor = new CLibSVR();
+	SG_REF(regressor);
+	//regressor->parallel->set_num_threads(1);
 
 	SGVector<float64_t> vec(N);
-	for (int i=0; i<N; i++) vec.vector[i] = i>>3;
+	for (int i=0; i<N; i++) vec.vector[i] = (i%2) ? (1.0) : (-1.0);
 	CLabels* labels = new CLabels(vec);
+	SG_REF(labels);
 
 	SGMatrix<float64_t> matrix1(N,N);
 	SGMatrix<float64_t> matrix2(N,N);
@@ -28,6 +39,12 @@ int main(int argc, char** argv)
 		{
 			matrix1.matrix[i*N+j] = (i-j)*(i-j)/(1000.0*1000.0);
 			matrix2.matrix[i*N+j] = (i-j)*(i-j)/(1000.0*1000.0);
+
+			if (i==j)
+			{
+				matrix1.matrix[i*N+j]+=100;
+				matrix2.matrix[i*N+j]+=100;
+			}
 		}
 
 	CCustomKernel* kernel1 = new CCustomKernel(matrix1);
@@ -37,6 +54,7 @@ int main(int argc, char** argv)
 	matrix2.destroy_matrix();
 
 	CCombinedKernel* ckernel = new CCombinedKernel();
+	SG_REF(ckernel);
 	ckernel->append_kernel(kernel1);
 	ckernel->append_kernel(kernel2);
 
