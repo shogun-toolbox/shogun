21324b9536cbd0f81909f4d820f6f6ea4d7d043d
Sergey Lisitsyn <blackburn91@gmail.com>
2011-12-06 01:12:52 +0400

    Faces application to dimreduction, added word embedding example

---
+++ applications/dimreduction/words_embedding.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Written (W) 2011 Sergey Lisitsyn
+# Copyright (C) 2011 Sergey Lisitsyn
+
+from numpy import *
+from pylab import *
+from modshogun import *
+import random
+import difflib
+
+def word_kernel(words):
+	N = len(words)
+	dist_matrix = zeros([N,N])
+	for i in range(N):
+		for j in range(i,N):
+			s = difflib.SequenceMatcher(None,words[i],words[j])
+			dist_matrix[i,j] = s.ratio()
+	dist_matrix = 0.5*(dist_matrix+dist_matrix.T) 
+	return CustomKernel(dist_matrix)
+	
+print 'loading'
+words = []
+f = open("../../data/toy/words.dat")
+for line in f:
+	words.append(line[:-1])
+f.close()
+print 'loaded'
+
+converter = KernelLocallyLinearEmbedding()
+converter.set_k(10)
+converter.set_target_dim(2)
+converter.parallel.set_num_threads(1)
+embedding = converter.embed_kernel(word_kernel(words[:200]))
+embedding_matrix = embedding.get_feature_matrix()
+fig = figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(embedding_matrix[0,:],embedding_matrix[1,:],alpha=0.0)
+
+for i,word in enumerate(words[:100]):
+	text(embedding_matrix[0,i],embedding_matrix[1,i],word,fontsize=8)
+axis('off')
+show()
+
