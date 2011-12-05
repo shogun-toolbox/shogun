050aad2035e68c367dda509e7bd97f5708433e2f
Sergey Lisitsyn <blackburn91@gmail.com>
2011-12-06 01:41:02 +0400

    Added testing info for MDS into python example

--- examples/undocumented/python_modular/converter_multidimensionalscaling_modular.py
+++ examples/undocumented/python_modular/converter_multidimensionalscaling_modular.py
@@ -1,4 +1,5 @@
 from tools.load import LoadMatrix
+import numpy
 
 lm=LoadMatrix()
 data = lm.load_numbers('../data/fm_train_real.dat')
@@ -8,16 +9,25 @@ parameter_list = [[data]]
 def converter_multidimensionalscaling_modular(data):
 	from shogun.Features import RealFeatures
 	from shogun.Converter import MultidimensionalScaling
+	from shogun.Distance import EuclidianDistance
 	
 	features = RealFeatures(data)
 		
+	distance_before = EuclidianDistance()
+	distance_before.init(features,features)
+
 	converter = MultidimensionalScaling()
-	converter.set_target_dim(1)
+	converter.set_target_dim(2)
 	converter.set_landmark(False)
-	converter.apply(features)
+	embedding =converter.apply(features)
 
-	return features
+	distance_after = EuclidianDistance()
+	distance_after.init(embedding,embedding)
 
+	distance_matrix_after = distance_after.get_distance_matrix()
+	distance_matrix_before = distance_before.get_distance_matrix()
+
+	return numpy.linalg.norm(distance_matrix_after-distance_matrix_before)/numpy.linalg.norm(distance_matrix_before)
 
 if __name__=='__main__':
 	print 'MultidimensionalScaling'
