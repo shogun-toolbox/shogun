633147d99d21d251d7ceb06c771ea1855677e7cf
Soeren Sonnenburg <sonne@debian.org>
2011-12-05 20:17:45 +0100

    fix library and evaluation test

--- examples/undocumented/python_modular/library_time.py
+++ examples/undocumented/python_modular/library_time.py
@@ -1,12 +1,19 @@
 import time
 from modshogun import Time
 
+parameter_list = [[1]]
+def library_time(sleep_secs):
+	# measure wall clock time difference
+	t=Time()
+	time.sleep(1)
+	diff=t.cur_time_diff()
 
-# measure wall clock time difference
-t=Time()
-time.sleep(1)
-diff=t.cur_time_diff()
+	# measure CPU time required
+	cpu_diff=t.cur_runtime_diff_sec()
 
-# measure CPU time required
+	# return results as integers to enable testing
+	return round(diff),round(cpu_diff)
 
-cpu_diff=t.cur_runtime_diff_sec()
+if __name__=='__main__':
+	print 'Time'
+	library_time(*parameter_list[0])
