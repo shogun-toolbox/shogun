2ed91ccd4dd6619a5242d3893dd13224389c8054
Heiko Strathmann <heiko.strathmann@gmail.com>
2012-01-25 00:08:01 +0000

    added me as author

--- src/shogun/base/SGObject.cpp
+++ src/shogun/base/SGObject.cpp
@@ -5,6 +5,7 @@
  * (at your option) any later version.
  *
  * Written (W) 2008-2009 Soeren Sonnenburg
+ * Written (W) 2011-2012 Heiko Strathmann
  * Copyright (C) 2008-2009 Fraunhofer Institute FIRST and Max Planck Society
  */
 
