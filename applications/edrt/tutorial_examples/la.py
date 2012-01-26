2f45918f73e56ed25412184a2db472b23dbc9e40
Sergey Lisitsyn <blackburn91@gmail.com>
2012-01-27 03:44:56 +0400

    Added current version of the EDRT tutorial

---
+++ applications/edrt/tutorial_examples/la.py
@@ -0,0 +1,33 @@
+import modshogun as sg
+import data
+import numpy as np
+
+# load data
+feature_matrix = data.swissroll()
+# create features instance
+features = sg.RealFeatures(feature_matrix)
+
+# create Laplacian Eigenmaps converter instance
+converter = sg.LaplacianEigenmaps()
+
+# set target dimensionality
+converter.set_target_dim(2)
+# set number of neighbors
+converter.set_k(20)
+# set tau multiplier 
+converter.set_tau(1.0)
+
+# compute embedding with Laplacian Eigenmaps method
+embedding = converter.embed(features)
+
+# compute cosine distance matrix 'manually'
+N = features.get_num_vectors()
+distance_matrix = np.zeros((N,N))
+for i in range(N):
+	for j in range(N):
+		distance_matrix[i,j] = \
+		  np.linalg.norm(feature_matrix[:,i]-feature_matrix[:,j],2)
+# create custom distance instance
+distance = sg.CustomDistance(distance_matrix)
+# construct embedding based on created distance
+converter.embed_distance(distance)
---
+++ applications/edrt/tutorial_examples/lltsa.py
@@ -0,0 +1,22 @@
+import modshogun as sg
+import dataloader as loader
+
+# load data
+feature_matrix = loader.load('mnist.mat')
+# create features instance
+features = sg.RealFeatures(feature_matrix)
+
+# create Linear Local Tangent Space Alignment converter instance
+converter = sg.LinearLocalTangentSpaceAlignment()
+
+# set target dimensionality
+converter.set_target_dim(2)
+# set number of neighbors 
+converter.set_k(10)
+# set number of threads
+converter.parallel.set_num_threads(2)
+# set nullspace shift (optional)
+converter.set_nullspace_shift(-1e-6)
+
+# compute embedding with Linear Local Tangent Space Alignment method
+embedding = converter.embed(features)
