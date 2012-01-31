53ecdb2b25ef140511ac4bac33cb32da45b0a005
Heiko Strathmann <heiko.strathmann@gmail.com>
2012-01-31 12:32:19 +0000

    corrections

--- examples/undocumented/python_modular/evaluation_cross_validation_classification.py
+++ examples/undocumented/python_modular/evaluation_cross_validation_classification.py
@@ -21,53 +21,55 @@ label_traindat=concatenate((-ones(num_vectors), ones(num_vectors)));
 parameter_list = [[traindat,label_traindat]]
 
 def modelselection_grid_search_linear_modular(traindat=traindat, label_traindat=label_traindat):
-from shogun.Evaluation import CrossValidation, CrossValidationResult
-from shogun.Evaluation import ContingencyTableEvaluation, ACCURACY
-from shogun.Evaluation import StratifiedCrossValidationSplitting
-from shogun.ModelSelection import GridSearchModelSelection
-from shogun.ModelSelection import ModelSelectionParameters, R_EXP
-from shogun.ModelSelection import ParameterCombination
-from shogun.Features import Labels
-from shogun.Features import RealFeatures
-from shogun.Classifier import LibLinear, L2R_L2LOSS_SVC
+    from shogun.Evaluation import CrossValidation, CrossValidationResult
+    from shogun.Evaluation import ContingencyTableEvaluation, ACCURACY
+    from shogun.Evaluation import StratifiedCrossValidationSplitting
+    from shogun.ModelSelection import GridSearchModelSelection
+    from shogun.ModelSelection import ModelSelectionParameters, R_EXP
+    from shogun.ModelSelection import ParameterCombination
+    from shogun.Features import Labels
+    from shogun.Features import RealFeatures
+    from shogun.Classifier import LibLinear, L2R_L2LOSS_SVC
 
-# build parameter tree to select C1 and C2 
-param_tree_root=ModelSelectionParameters()
-c1=ModelSelectionParameters("C1");
-param_tree_root.append_child(c1)
-c1.build_values(-2.0, 2.0, R_EXP);
+    # build parameter tree to select C1 and C2 
+    param_tree_root=ModelSelectionParameters()
+    c1=ModelSelectionParameters("C1");
+    param_tree_root.append_child(c1)
+    c1.build_values(-2.0, 2.0, R_EXP);
 
-c2=ModelSelectionParameters("C2");
-param_tree_root.append_child(c2);
-c2.build_values(-2.0, 2.0, R_EXP);
+    c2=ModelSelectionParameters("C2");
+    param_tree_root.append_child(c2);
+    c2.build_values(-2.0, 2.0, R_EXP);
 
-# training data
-features=RealFeatures(traindat)
-labels=Labels(label_traindat)
+    # training data
+    features=RealFeatures(traindat)
+    labels=Labels(label_traindat)
 
-# classifier
-classifier=LibLinear(L2R_L2LOSS_SVC)
+    # classifier
+    classifier=LibLinear(L2R_L2LOSS_SVC)
 
-# splitting strategy for 5 fold cross-validation
-splitting_strategy=CrossValidationSplitting(labels, 5)
+    # splitting strategy for 5 fold cross-validation (for classification its better
+    # to use "StratifiedCrossValidation", but the standard
+    # "StratifiedCrossValidationSplitting" is also available
+    splitting_strategy=StratifiedCrossValidationSplitting(labels, 5)
 
-# evaluation method
-evaluation_criterium=ContingencyTableEvaluation(ACCURACY)
+    # evaluation method
+    evaluation_criterium=ContingencyTableEvaluation(ACCURACY)
 
-# cross-validation instance
-cross_validation=CrossValidation(classifier, features, labels,
-	splitting_strategy, evaluation_criterium)
+    # cross-validation instance
+    cross_validation=CrossValidation(classifier, features, labels,
+	    splitting_strategy, evaluation_criterium)
 	
-# (optional) repeat x-val 10 times
-cross_validation.set_num_runs(10)
+    # (optional) repeat x-val 10 times
+    cross_validation.set_num_runs(10)
 
-# (optional) request 95% confidence intervals for results (not actually needed
-# for this toy example)
-cross_validation.set_conf_int_alpha(0.05)
+    # (optional) request 95% confidence intervals for results (not actually needed
+    # for this toy example)
+    cross_validation.set_conf_int_alpha(0.05)
 	
-# perform cross-validation and print results
-result=cross_validation.evaluate()
-result.print_result()
+    # perform cross-validation and print results
+    result=cross_validation.evaluate()
+    result.print_result()
 
 if __name__=='__main__':
 	print 'Evaluation CrossValidation'
