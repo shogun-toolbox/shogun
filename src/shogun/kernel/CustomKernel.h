1d96f816c8d8d83dca75ec445e8c6890054b4af5
Heiko Strathmann <heiko.strathmann@gmail.com>
2012-02-07 22:04:47 +0000

    added my name in copyright

--- src/shogun/kernel/CustomKernel.h
+++ src/shogun/kernel/CustomKernel.h
@@ -5,6 +5,7 @@
  * (at your option) any later version.
  *
  * Written (W) 1999-2009 Soeren Sonnenburg
+ * Written (W) 2011-2012 Heiko Strathmann
  * Copyright (C) 1999-2009 Fraunhofer Institute FIRST and Max-Planck-Society
  */
 
