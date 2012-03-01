8ec5d0e2c9e2034b8c78795aa41ddb1122b467be
iglesias <fernando.iglesiasg@gmail.com>
2012-03-01 12:50:53 +0100

    ~ mahalanobis example python_modular

--- examples/undocumented/python_modular/distance_mahalanobis_modular.py
+++ examples/undocumented/python_modular/distance_mahalanobis_modular.py
@@ -1,15 +1,15 @@
 '''
 This example shows how to use the class MahalanobisDistance to compute the 
-distance from a feature vector to a distribution of feature vectors.
+distance from a feature vector to another vector of the same distributiona or the 
+distance from a vector to a distribution of feature vectors.
 
 The program first loads toy data and creates objects of the class RealFeatures
 to create an instance of the class MahalanobisDistance. Later it computes and 
 prints on the screen the Mahalanobis distance from each of the feature vectors 
 in feats_test to the set of features feats_train.
 
-Note that only the second argument is relevant in the method distance of the 
-class MahalanobisDistance. The first one is not used and is simply kept to 
-respect class hierarchy.
+Note that if both distributions are equal thenonly the second argument is relevant 
+in the method distance of the class MahalanobisDistance.
 
 If you compare the results given by this program with other software, please 
 keep in mind that the covariance matrix of a group of features in shogun is
@@ -30,14 +30,14 @@ def distance_mahalanobis_modular (fm_train_real = traindat, fm_test_real = testd
 	from shogun.Features import RealFeatures
 	from shogun.Distance import MahalanobisDistance
 
-        feats_train = RealFeatures(fm_train_real)
+	feats_train = RealFeatures(fm_train_real)
 	feats_test  = RealFeatures(fm_test_real)
 
-	distance = MahalanobisDistance(feats_train, feats_test)
-
+	distance = MahalanobisDistance(feats_test, feats_train)
 	for i in range(feats_test.get_num_vectors()):
-		dm = distance.distance(0, i)
-		print dm
+		for j in range(feats_train.get_num_vectors()):
+			dm = distance.distance(i, j)
+			print dm
 
 if __name__=='__main__':
 	print 'MahalanobisDistance'
