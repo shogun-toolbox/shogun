c3aec3f2a4254559efdbe6e657b2407d28abaef9
Soeren Sonnenburg <sonne@debian.org>
2012-03-09 10:46:58 +0100

    if max_train_time == 0 - disable time limit for ocas

--- src/shogun/classifier/svm/libocas.cpp
+++ src/shogun/classifier/svm/libocas.cpp
@@ -898,7 +898,10 @@ ocas_return_value_T svm_ocas_solver_difC(
     if( ocas.Q_P - ocas.Q_D <= TolRel*LIBOCAS_ABS(ocas.Q_P)) ocas.exitflag = 1; 
     if( ocas.Q_P - ocas.Q_D <= TolAbs) ocas.exitflag = 2; 
     if( ocas.Q_P <= QPBound) ocas.exitflag = 3; 
-    if( ocas.ocas_time >= MaxTime) ocas.exitflag = 4; 
+	if (MaxTime>0)
+	{
+		if( ocas.ocas_time >= MaxTime) ocas.exitflag = 4; 
+	}
     if(ocas.nCutPlanes >= BufSize) ocas.exitflag = -1;
          
   } /* end of the main loop */
