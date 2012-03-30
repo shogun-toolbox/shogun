dc87cb55fea12eb94a52628e29d53e7196819db7
Sergey Lisitsyn <blackburn91@gmail.com>
2012-03-30 21:27:54 +0400

    Libedrt next iteration, LTSA and dif maps

--- src/shogun/converter/LocallyLinearEmbedding.cpp
+++ src/shogun/converter/LocallyLinearEmbedding.cpp
@@ -12,7 +12,6 @@
 #include <shogun/lib/config.h>
 #ifdef HAVE_LAPACK
 #include <shogun/converter/EmbeddingConverter.h>
-#include <shogun/converter/libedrt.h>
 #include <shogun/mathematics/arpack.h>
 #include <shogun/kernel/LinearKernel.h>
 #include <shogun/mathematics/lapack.h>
@@ -44,6 +43,11 @@ CLocallyLinearEmbedding::CLocallyLinearEmbedding() :
 #else
 	m_use_arpack = false;
 #endif
+#ifdef HAVE_SUPERLU
+	m_use_superlu = true;
+#else
+	m_use_superlu = false;
+#endif
 	init();
 }
 
@@ -59,7 +63,9 @@ void CLocallyLinearEmbedding::init()
 	m_parameters->add(&m_reconstruction_shift, "reconstruction_shift", 
 	                  "shift used to regularize reconstruction step");
 	m_parameters->add(&m_use_arpack, "use_arpack",
-	                  "whether arpack is being used or not");
+	                  "whether arpack should be used or not");
+	m_parameters->add(&m_use_superlu, "use_superlu",
+	                  "whether superlu should be used or not");
 }
 
 
@@ -119,8 +125,26 @@ float64_t CLocallyLinearEmbedding::get_reconstruction_shift() const
 	return m_reconstruction_shift;
 }
 
+void CLocallyLinearEmbedding::set_use_superlu(bool use_superlu)
+{
+#ifndef HAVE_SUPERLU
+	if (use_superlu)
+		SG_ERROR("No SUPERLU available in this configuration");
+#endif 
+	m_use_superlu = use_superlu;
+}
+
+bool CLocallyLinearEmbedding::get_use_superlu() const
+{
+	return m_use_superlu;
+}
+
 void CLocallyLinearEmbedding::set_use_arpack(bool use_arpack)
 {
+#ifndef HAVE_ARPACK
+	if (use_arpack)
+		SG_ERROR("No ARPACK available in this configuration");
+#endif 
 	m_use_arpack = use_arpack;
 }
 
@@ -138,127 +162,43 @@ CFeatures* CLocallyLinearEmbedding::apply(CFeatures* features)
 {
 	ASSERT(features);
 	// check features
-	if (!(features->get_feature_class()==C_SIMPLE &&
-	      features->get_feature_type()==F_DREAL))
-	{
-		SG_ERROR("Given features are not of SimpleRealFeatures type.\n");
-	}
-	// shorthand for simplefeatures
-	CSimpleFeatures<float64_t>* simple_features = (CSimpleFeatures<float64_t>*) features;
-	SG_REF(features);
+	CDotFeatures* dot_features = (CDotFeatures*)features;
+	ASSERT(dot_features);
+	SG_REF(dot_features);
 
 	// get and check number of vectors
-	int32_t N = simple_features->get_num_vectors();
+	int32_t N = dot_features->get_num_vectors();
 	if (m_k>=N)
 		SG_ERROR("Number of neighbors (%d) should be less than number of objects (%d).\n",
 		         m_k, N);
 
-	// compute distance matrix
-	//
-	CKernel* kernel = new CLinearKernel(simple_features,simple_features);
+	CKernel* kernel = new CLinearKernel(dot_features,dot_features);
 	CKernel* custom_kernel = new CCustomKernel(kernel);
 
 	float64_t* new_features = NULL;
 
 	edrt_options_t options;
-	options.method = KERNEL_LOCALLY_LINEAR_EMBEDDING;
+	options.method = get_edrt_method();
 	options.use_arpack = m_use_arpack;
+	options.use_superlu = m_use_superlu;
+	options.klle_reconstruction_shift = m_reconstruction_shift;
+	options.nullspace_shift = m_nullspace_shift;
 
-	edrt_embedding(options,
-	               m_target_dim,N,2,m_k,NULL,
-	               &compute_kernel,NULL,NULL,(void*)custom_kernel,
+	edrt_embedding(options, m_target_dim, N, 0, m_k, NULL,
+	               &compute_kernel, NULL, NULL, (void*)kernel,
 	               &new_features);
 
 	SG_UNREF(custom_kernel);
 	SG_UNREF(kernel);
 
-	SG_UNREF(features);
+	SG_UNREF(dot_features);
 	return (CFeatures*)(new CSimpleFeatures<float64_t>(SGMatrix<float64_t>(new_features,m_target_dim,N)));
 }
 
-float64_t CLocallyLinearEmbedding::compute_kernel(int32_t i, int32_t j, void* user_data)
+float64_t CLocallyLinearEmbedding::compute_kernel(int32_t i, int32_t j, const void* user_data)
 {
 	return ((CKernel*)user_data)->kernel(i,j);
 }
 
-int32_t CLocallyLinearEmbedding::estimate_k(CSimpleFeatures<float64_t>* simple_features, SGMatrix<int32_t> neighborhood_matrix)
-{
-	int32_t right = m_max_k;
-	int32_t left = m_k;
-	int32_t left_third;
-	int32_t right_third;
-	ASSERT(right>=left);
-	if (right==left) return left;
-	int32_t dim;
-	int32_t N;
-	float64_t* feature_matrix= simple_features->get_feature_matrix(dim,N);
-	float64_t* z_matrix = SG_MALLOC(float64_t,right*dim);
-	float64_t* covariance_matrix = SG_MALLOC(float64_t,right*right);
-	float64_t* resid_vector = SG_MALLOC(float64_t, right);
-	float64_t* id_vector = SG_MALLOC(float64_t, right);
-	while (right-left>2)
-	{
-		left_third = (left*2+right)/3;
-		right_third = (right*2+left)/3;
-		float64_t left_val = compute_reconstruction_error(left_third,dim,N,feature_matrix,z_matrix,
-		                                                  covariance_matrix,resid_vector,
-		                                                  id_vector,neighborhood_matrix);
-		float64_t right_val = compute_reconstruction_error(right_third,dim,N,feature_matrix,z_matrix,
-		                                                   covariance_matrix,resid_vector,
-		                                                   id_vector,neighborhood_matrix);
-		if (left_val<right_val)
-			right = right_third;
-		else 
-			left = left_third;
-	}
-	SG_FREE(z_matrix);
-	SG_FREE(covariance_matrix);
-	SG_FREE(resid_vector);
-	SG_FREE(id_vector);
-	return right;
-}
 
-float64_t CLocallyLinearEmbedding::compute_reconstruction_error(int32_t k, int dim, int N, float64_t* feature_matrix,
-                                                                float64_t* z_matrix, float64_t* covariance_matrix,
-                                                                float64_t* resid_vector, float64_t* id_vector,
-                                                                SGMatrix<int32_t> neighborhood_matrix)
-{
-	// todo parse params
-	int32_t i,j;
-	float64_t total_residual_norm = 0.0;
-	for (i=CMath::random(0,20); i<N; i+=N/20)
-	{
-		for (j=0; j<k; j++)
-		{
-			cblas_dcopy(dim,feature_matrix+neighborhood_matrix[i*m_k+j]*dim,1,z_matrix+j*dim,1);   
-			cblas_daxpy(dim,-1.0,feature_matrix+i*dim,1,z_matrix+j*dim,1);
-		}
-		cblas_dgemm(CblasColMajor,CblasTrans,CblasNoTrans,
-			    k,k,dim,
-			    1.0,z_matrix,dim,
-			    z_matrix,dim,
-			    0.0,covariance_matrix,k);
-		for (j=0; j<k; j++)
-		{
-			resid_vector[j] = 1.0;
-			id_vector[j] = 1.0;
-		}
-		if (k>dim)
-		{
-			float64_t trace = 0.0;
-			for (j=0; j<k; j++)
-				trace += covariance_matrix[j*k+j];
-			for (j=0; j<m_k; j++)
-				covariance_matrix[j*k+j] += m_reconstruction_shift*trace;
-		}
-		clapack_dposv(CblasColMajor,CblasLower,k,1,covariance_matrix,k,id_vector,k);
-		float64_t norming=0.0;
-		for (j=0; j<k; j++)
-			norming += id_vector[j];
-		cblas_dscal(k,-1.0/norming,id_vector,1);
-		cblas_dsymv(CblasColMajor,CblasLower,k,-1.0,covariance_matrix,k,id_vector,1,1.0,resid_vector,1);
-		total_residual_norm += cblas_dnrm2(k,resid_vector,1);
-	}
-	return total_residual_norm/k;
-}
 #endif /* HAVE_LAPACK */
--- src/shogun/converter/libedrt.h
+++ src/shogun/converter/libedrt.h
@@ -17,33 +17,6 @@ enum edrt_method_t
 	MULTIDIMENSIONAL_SCALING
 };
 
-struct DISTANCE_COVERTREE_POINT
-{
-public:
-
-	DISTANCE_COVERTREE_POINT(int index, double (*distance_f)(int, int, void*), void* user_d)
-	{
-		point_index = index;
-		distance_function = distance_f;
-		user_data = user_d;
-		kii = distance_function(point_index, point_index, user_data);
-	}
-
-	inline double distance(const DISTANCE_COVERTREE_POINT& p) const
-	{
-		return kii+p.kii-2.0*distance_function(point_index, p.point_index, user_data); 
-	}
-	inline bool operator==(const DISTANCE_COVERTREE_POINT& p) const
-	{ 
-		return (p.point_index==point_index); 
-	}
-
-	int point_index;
-	double kii;
-	double (*distance_function)(int, int, void*);
-	void* user_data;
-};
-
 struct edrt_options_t
 {
 	edrt_options_t()
@@ -52,11 +25,24 @@ struct edrt_options_t
 		num_threads = 1;
 		use_arpack = true;
 		use_superlu = true;
+		klle_reconstruction_shift = 1e-3;
+		diffusion_maps_t = 1;
+		nullspace_shift = 1e-9;
 	}
+	// EDRT method
 	edrt_method_t method;
+	// number of threads
 	int num_threads;
+	// true if ARPACK should be used
 	bool use_arpack;
+	// true if SuperLU should be used
 	bool use_superlu;
+	// kernel LLE reconstruction shift
+	double klle_reconstruction_shift;
+	// diffusion maps t
+	int diffusion_maps_t;
+	// nullspace regularization shift
+	double nullspace_shift;
 };
 
 int edrt_embedding(
@@ -65,34 +51,11 @@ int edrt_embedding(
 		const int N, /* number of vectors to embed */
 		const int dimension, /* dimension of feature vectors */
 		const int k, /* number of neighbors */
-		double (*distance)(int, int, void*), /* distance function between feature vectors */
-		double (*kernel)(int, int, void*), /* kernel function between feature vectors */
-		double* (*access_feature_vector)(int, void*), /* feature vector access */
-		void (*free_feature_vector)(int, void*), /* free feature vector */
-		void* user_data, /* user data for callbacks */
+		double (*distance)(int, int, const void*), /* distance function between feature vectors */
+		double (*kernel)(int, int, const void*), /* kernel function between feature vectors */
+		double* (*access_feature_vector)(int, const void*), /* feature vector access */
+		void (*free_feature_vector)(int, const void*), /* free feature vector */
+		const void* user_data, /* user data for callbacks */
 		double **output /* output */);
-
-double* klle_weight_matrix(
-		int* neighborhood_matrix, /* indices of neighbors for each vector*/
-		int N, /* number of vectors to embed */
-		int k, /* number of neighbors */
-		int matrix_k, /* actual k for neighborhood matrix */
-		int num_threads, /* number of threads */
-		double (*kernel)(int, int, void*), /* kernel function between feature vectors */
-		void* user_data /* user data for callbacks */);
-
-void* klle_weight_matrix_thread(void* parameter_map);
-
-double* eigendecomposition_embedding(
-		double* weight_matrix, 
-		int N, 
-		int target_dimension, 
-		bool use_arpack);
-
-int* kernel_neighbors_matrix(
-		int N, 
-		int k, 
-		double (*kernel)(int, int, void*),
-		void* user_data);
 
 #endif /* libedrt_h_ */
