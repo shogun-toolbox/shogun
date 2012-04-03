e7172c6255fb9a09b13b999cdc8af0f4b0c596af
Harshit Syal <harshit.syal@nsitonline.in>
2012-04-03 13:29:52 +0530

    Made some indentation and witespace changes

--- src/shogun/classifier/svm/NewtonSVM.h
+++ src/shogun/classifier/svm/NewtonSVM.h
@@ -47,42 +47,46 @@ class CNewtonSVM : public CLinearMachine
 		 */
 		inline void set_C(float64_t c) { C=c; }
 
+		/** get epsilon
+		 *  @return epsilon
+		 */
+		inline float64_t get_epsilon() { return epsilon; }
+
+		/**
+		 * set epsilon
+	         * @param epsilon constant epsilon
+		 */
+		inline void set_epsilon(float64_t e) { epsilon=e; }
+
 		/** get C
-		 * 
-		 * @return C
+		 *  @return C
 		 */
 		inline float64_t get_C() { return C; }
 
 		
 		/** set if bias shall be enabled
-		 *
-		 * @param enable_bias if bias shall be enabled
+		 *  @param enable_bias if bias shall be enabled
 		 */
 		inline void set_bias_enabled(bool enable_bias) { use_bias=enable_bias; }
 
 		/** get if bias is enabled
-		 *
-		 * @return if bias is enabled
+		 *  @return if bias is enabled
 		 */
 		inline bool get_bias_enabled() { return use_bias; }
 
 		/** set num_iter
-		 *
-		 * @return num_iter 
+		 *  @return num_iter 
 		 */
 		inline int32_t get_num_iter() {return num_iter;}
 
 		/** set iter
-		 *
-		 * @param num_iter number of iterations
+		 *  @param num_iter number of iterations
 		 */
 		inline void set_num_iter(int32_t iter) { num_iter=iter; }
 		
 		/** @return object name */
 		inline virtual const char* get_name() const { return "NewtonSVM"; }
 	
-
-
 	protected:
 		/** train SVM classifier
 		 *
@@ -97,9 +101,10 @@ class CNewtonSVM : public CLinearMachine
 		void obj_fun_linear(float64_t* weights,float64_t* out,float64_t* obj,int32_t* sv,int32_t* numsv,float64_t* grad);
 
 		void line_search_linear(float64_t* weights,float64_t* d,float64_t* out,float64_t* tx);
+
 	protected:
 		/** lambda=1/C */
-		float64_t lambda,C;
+		float64_t lambda,C,epsilon;
 		float64_t prec;
 		int32_t x_n,x_d,num_iter;
 		/** if bias is used */
