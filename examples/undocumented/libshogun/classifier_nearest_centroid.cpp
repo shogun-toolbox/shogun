69cca92afe98d82d05005767a7a3108f2a72514c
Philippe Tillet <Phil.Tillet@gmail.com>
2012-04-08 20:17:18 +0200

    * Fixed the example include and comments * Added GPL Header * Removed apply() * For loop style changed * Now construct matrix before constructing features * Fixed memory leaks

--- examples/undocumented/libshogun/classifier_nearest_centroid.cpp
+++ examples/undocumented/libshogun/classifier_nearest_centroid.cpp
@@ -1,8 +1,5 @@
 #include <shogun/features/Labels.h>
-#include <shogun/io/AsciiFile.h>
-#include <shogun/io/SGIO.h>
 #include <shogun/features/SimpleFeatures.h>
-#include <shogun/machine/LinearMulticlassMachine.h>
 #include <shogun/distance/EuclidianDistance.h>
 #include <shogun/classifier/NearestCentroid.h>
 #include <shogun/base/init.h>
@@ -18,29 +15,30 @@ int main(){
 	init_shogun(&print_message);
 	index_t num_vec=4;
 	index_t num_feat=2;
-	index_t num_class=4;
+	index_t num_class=2;
 
 	// create some data
 	SGMatrix<float64_t> matrix(num_feat, num_vec);
 	CMath::range_fill_vector(matrix.matrix, num_feat*num_vec);
-	// create vectors
-	// shogun will now own the matrix created
+	
+	// Create features ; shogun will now own the matrix created
 	CSimpleFeatures<float64_t>* features=new CSimpleFeatures<float64_t>(matrix);
 
 	CMath::display_matrix(matrix.matrix,num_feat,num_vec);
-	// create three labels
+	
+	//Create labels
 	CLabels* labels=new CLabels(num_vec);
 	for (index_t i=0; i<num_vec; ++i)
 		labels->set_label(i, i%num_class);
 
-	// create gaussian kernel with cache 10MB, width 0.5
+	//Create Euclidian Distance
 	CEuclidianDistance* distance = new CEuclidianDistance(features,features);
 
-	// create libsvm with C=10 and train
+	//Create Nearest Centroid
 	CNearestCentroid* nearest_centroid = new CNearestCentroid(distance, labels);
 	nearest_centroid->train();
-// 
-// 	// classify on training examples
+ 
+//  	classify on training examples
 	CLabels* output=nearest_centroid->apply();
 	CMath::display_vector(output->get_labels().vector, output->get_num_labels(),
 			"batch output");
--- src/shogun/classifier/NearestCentroid.cpp
+++ src/shogun/classifier/NearestCentroid.cpp
@@ -1,35 +1,17 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Philippe Tillet
+ */
+
 #include "NearestCentroid.h"
 
 namespace shogun{
 	
-	CLabels* CNearestCentroid::apply()
-	{
-		ASSERT(distance);
-		ASSERT(distance->get_num_vec_rhs());
-		int32_t n_centroids=m_centroids->get_num_vectors();
-		ASSERT(m_centroids->get_num_vectors()>0);
-		
-		int32_t num_lab=distance->get_num_vec_rhs();
-		CLabels* output=new CLabels(num_lab);
-		for(int32_t idx = 0 ; idx<num_lab ; ++idx){
-			output->set_label(idx,CDistanceMachine::apply(idx));
-		}
-		return output;
-	}
-
-	CLabels* CNearestCentroid::apply(CFeatures* data)
-	{
-		distance->replace_rhs(data);
-		return apply();
-	}
-
-	float64_t CNearestCentroid::apply(int32_t vec_idx)
-	{
-		return CDistanceMachine::apply(vec_idx);
-	}
-
-
-	CNearestCentroid::CNearestCentroid() : CDistanceMachine()
+  	CNearestCentroid::CNearestCentroid() : CDistanceMachine()
 	{
 		init();
 	}
@@ -43,9 +25,18 @@ namespace shogun{
 		set_labels(trainlab);
 	}
 
+	CNearestCentroid::~CNearestCentroid()
+	{
+		if(m_is_trained)
+			distance->remove_lhs();
+		else
+			delete m_centroids;
+	}
+
 	void CNearestCentroid::init()
 	{
 		m_shrinking=0;
+		m_is_trained=false;
 		set_store_model_features(true);
 		m_centroids = new CSimpleFeatures<float64_t>();
 	}
@@ -73,37 +64,48 @@ namespace shogun{
 		int32_t num_vectors = data->get_num_vectors();
 		int32_t num_classes = m_labels->get_num_classes();
 		int32_t num_feats = ((CSimpleFeatures<float64_t>*)data)->get_num_features();
-		m_centroids->free_feature_matrix();
 		float64_t* centroids = SG_MALLOC(float64_t, num_feats*num_classes);
+		for(int32_t i=0 ; i < num_feats*num_classes ; i++)
+		{
+			centroids[i]=0;
+		}
 		m_centroids->set_num_features(num_feats);
 		m_centroids->set_num_vectors(num_classes);
-		m_centroids->set_feature_matrix(centroids,num_feats,num_classes);
 		
 		int64_t* num_per_class = new int64_t[num_classes];
-		for(int32_t i=0 ; i<num_classes ; ++i){
+		for(int32_t i=0 ; i<num_classes ; i++)
+		{
 			num_per_class[i]=0;
 		}
 		
-		for(int32_t idx=0 ; idx<num_vectors ; ++idx){
+		for(int32_t idx=0 ; idx<num_vectors ; idx++)
+		{
 			int32_t target_len, current_len;
 			bool target_free, current_free;
 			int32_t current_class = m_labels->get_label(idx);
-			float64_t* target = m_centroids->get_feature_vector(current_class,target_len,target_free);
+			float64_t* target = centroids + num_feats*current_class;
 			float64_t* current = ((CSimpleFeatures<float64_t>*)data)->get_feature_vector(idx,current_len,current_free);
 			CMath::add(target,1.0,target,1.0,current,current_len);
-			++num_per_class[current_class];
+			num_per_class[current_class]++;
 		}
 
-		
-		for(int32_t i=0 ; i<num_classes ; ++i){
-			int32_t target_len;
-			bool target_free;
-			float64_t* target = m_centroids->get_feature_vector(i,target_len,target_free);
-			CMath::scale_vector(1.0/(float64_t)num_per_class[i],target,target_len);
+
+		for(int32_t i=0 ; i<num_classes ; i++)
+		{
+			float64_t* target = centroids + num_feats*i;
+			CMath::scale_vector(1.0/(float64_t)num_per_class[i],target,num_feats);
 		}
+				
+		m_centroids->free_feature_matrix();
+		m_centroids->set_feature_matrix(centroids,num_feats,num_classes);
+
 		
 		distance->init(m_centroids,distance->get_rhs());
-
+		
+		m_is_trained=true;
+		
+		delete [] num_per_class;
+		
 		return true;
 	}
 
