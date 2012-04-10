35efeb26662e405b7be48b0e1062ab9e30bd7ca5
pluskid <pluskid@gmail.com>
2012-04-10 21:30:14 +0800

    Remove some debug code.

--- examples/undocumented/python_modular/regression_least_squares_modular.py
+++ examples/undocumented/python_modular/regression_least_squares_modular.py
@@ -1,19 +1,17 @@
 ###########################################################################
 # kernel ridge regression
 ###########################################################################
-from numpy import array, load
+from numpy import array
 from numpy.random import seed, rand
 from tools.load import LoadMatrix
 lm=LoadMatrix()
 
-# traindat = lm.load_numbers('../data/fm_train_real.dat')
-# testdat = lm.load_numbers('../data/fm_test_real.dat')
-# label_traindat = lm.load_labels('../data/label_train_twoclass.dat')
-traindat = load('/home/pluskid/work/gsoc/sandbox/cover-tree/train_matrix_prewitt.npy')
-testdat = load('/home/pluskid/work/gsoc/sandbox/cover-tree/test_matrix_prewitt.npy')
-label_traindat = load('/home/pluskid/work/gsoc/sandbox/cover-tree/train_labels_prewitt.npy')
+traindat = lm.load_numbers('../data/fm_train_real.dat')
+testdat = lm.load_numbers('../data/fm_test_real.dat')
+label_traindat = lm.load_labels('../data/label_train_twoclass.dat')
 
-parameter_list = [[traindat[:,1:1000],testdat[:,1:1000],label_traindat[1:1000]]]
+
+parameter_list = [[traindat,testdat,label_traindat]]
 
 def regression_least_squares_modular (fm_train=traindat,fm_test=testdat,label_train=label_traindat,tau=1e-6):
 
