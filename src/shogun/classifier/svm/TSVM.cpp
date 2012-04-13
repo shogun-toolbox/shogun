ebb640a4c274abb1d77e57986eb7f84b38263320
Harshit Syal <harshit.syal@nsitonline.in>
2012-04-13 18:05:46 +0530

    Added Tsvm.h/.cpp

---
+++ src/shogun/classifier/svm/TSVM.cpp
@@ -0,0 +1,120 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2006-2009 Soeren Sonnenburg
+ * Copyright (C) 2006-2009 Fraunhofer Institute FIRST and Max-Planck-Society
+ */
+
+#include <shogun/classifier/svm/TSVM.h>
+#include <shogun/features/Labels.h>
+#include <shogun/mathematics/Math.h>
+#include <shogun/classifier/svm/ssl.h>
+#include <shogun/machine/LinearMachine.h>
+#include <shogun/features/DotFeatures.h>
+#include <shogun/features/Labels.h>
+
+using namespace shogun;
+
+CTSVM::CTSVM()
+: CLinearMachine(), C1(1), C2(1), epsilon(1e-6), use_bias(true)
+{
+}
+
+CTSVM::CTSVM(
+	float64_t C, CDotFeatures* traindat, CLabels* trainlab)
+: CLinearMachine(), C1(C), C2(C), epsilon(1e-6), use_bias(true)
+{
+	set_features(traindat);
+	set_labels(trainlab);
+}
+
+
+CTSVM::~CTSVM()
+{
+}
+
+bool CTSVM::train_machine(CFeatures* data)
+{
+	ASSERT(m_labels);
+
+	if (data)
+	{
+		if (!data->has_property(FP_DOT))
+			SG_ERROR("Specified features are not of type CDotFeatures\n");
+		set_features((CDotFeatures*) data);
+	}
+
+	ASSERT(features);
+
+	SGVector<float64_t> train_labels=m_labels->get_labels();
+	int32_t num_feat=features->get_dim_feature_space();
+	int32_t num_vec=features->get_num_vectors();
+
+	ASSERT(num_vec==train_labels.vlen);
+	SG_FREE(w);
+
+	struct options Options;
+	struct data Data;
+	struct vector_double Weights;
+	struct vector_double Outputs;
+
+	Data.m=num_vec;
+	Data.Y=train_labels.vector;
+	Data.u=0;
+	for(int32_t i=0;i<num_vec;i++)
+	{	
+		if(Data.Y[i]==0)
+			Data.u++;
+	}
+	Data.l=num_vec-Data.u;
+	Data.n=num_feat+1;
+	Data.nz=num_feat+1;
+	Data.features=features;
+	Data.C = SG_MALLOC(float64_t, Data.l);
+
+	Options.algo = TSVM;
+	//Options.lambda=1/(2*get_C1());
+	//Options.lambda_u=1/(2*get_C1());
+	Options.lambda=1/(get_C1());
+	Options.lambda_u=1/(get_C1());
+	
+	Options.S=10000;
+	Options.R=0.5;
+	Options.epsilon = get_epsilon();
+	Options.cgitermax=10000;
+	Options.mfnitermax=50;
+	Options.Cp = get_C2()/get_C1();
+	Options.Cn = 1;
+
+	if (use_bias)
+		Options.bias=1.0;
+	else
+		Options.bias=0.0;
+
+	for (int32_t i=0;i<num_vec;i++)
+	{
+		if(train_labels.vector[i]>0)
+			Data.C[i]=Options.Cp;
+		else
+			Data.C[i]=Options.Cn;
+	}
+	
+	ssl_train(&Data, &Options, &Weights, &Outputs);
+
+	ASSERT(Weights.vec && Weights.d==num_feat+1);
+
+	float64_t sgn=train_labels.vector[0];
+	for (int32_t i=0; i<num_feat+1; i++)
+		Weights.vec[i]*=sgn;
+
+	set_w(SGVector<float64_t>(Weights.vec, num_feat));
+	set_bias(Weights.vec[num_feat]);
+
+	SG_FREE(Data.C);
+	SG_FREE(Outputs.vec);
+	train_labels.free_vector();
+	return true;
+}
---
+++ src/shogun/classifier/svm/TSVM.h
@@ -0,0 +1,115 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2006-2009 Soeren Sonnenburg
+ * Copyright (C) 2006-2009 Fraunhofer Institute FIRST and Max-Planck-Society
+ */
+
+#ifndef _TSVM_H___
+#define _TSVM_H___
+
+#include <shogun/lib/common.h>
+#include <shogun/machine/LinearMachine.h>
+#include <shogun/features/DotFeatures.h>
+#include <shogun/features/Labels.h>
+
+namespace shogun
+{
+/** @brief class TSVM */
+class CTSVM : public CLinearMachine
+{
+	public:
+		/** default constructor */
+		CTSVM();
+
+		/** constructor
+		 *
+		 * @param C constant C
+		 * @param traindat training features
+		 * @param trainlab labels for features
+		 */
+		CTSVM(
+			float64_t C, CDotFeatures* traindat,
+			CLabels* trainlab);
+		virtual ~CTSVM();
+
+		/** get classifier type
+		 *
+		 * @return classifier type TSVM
+		 */
+		virtual inline EClassifierType get_classifier_type() { return CT_TSVM; }
+
+		/** set C
+		 *
+		 * @param c_neg new C constant for negatively labeled examples
+		 * @param c_pos new C constant for positively labeled examples
+		 *
+		 */
+		inline void set_C(float64_t c_neg, float64_t c_pos) { C1=c_neg; C2=c_pos; }
+
+		/** get C1
+		 *
+		 * @return C1
+		 */
+		inline float64_t get_C1() { return C1; }
+
+		/** get C2
+		 *
+		 * @return C2
+		 */
+		inline float64_t get_C2() { return C2; }
+
+		/** set if bias shall be enabled
+		 *
+		 * @param enable_bias if bias shall be enabled
+		 */
+		inline void set_bias_enabled(bool enable_bias) { use_bias=enable_bias; }
+
+		/** get if bias is enabled
+		 *
+		 * @return if bias is enabled
+		 */
+		inline bool get_bias_enabled() { return use_bias; }
+
+		/** set epsilon
+		 *
+		 * @param eps new epsilon
+		 */
+		inline void set_epsilon(float64_t eps) { epsilon=eps; }
+
+		/** get epsilon
+		 *
+		 * @return epsilon
+		 */
+		inline float64_t get_epsilon() { return epsilon; }
+
+		/** @return object name */
+		inline virtual const char* get_name() const { return "TSVM"; }
+
+	protected:
+		/** train SVM classifier
+		 *
+		 * @param data training data (parameter can be avoided if distance or
+		 * kernel-based classifiers are used and distance/kernels are
+		 * initialized with train data)
+		 *
+		 * @return whether training was successful
+		 */
+		virtual bool train_machine(CFeatures* data=NULL);
+
+	protected:
+		/** C1 */
+		float64_t C1;
+		/** C2 */
+		float64_t C2;
+		/** epsilon */
+		float64_t epsilon;
+
+		/** if bias is used */
+		bool use_bias;
+};
+}
+#endif
