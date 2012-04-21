6562df4e4a15c9e9e8f449edd524e590197a129f
Heiko Strathmann <heiko.strathmann@gmail.com>
2012-04-21 14:14:40 +0100

    Changed subset system to use stacks of subsets. These are the basic classes needed for this

---
+++ src/shogun/features/SubsetStack.cpp
@@ -0,0 +1,112 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Heiko Strathmann
+ */
+
+#include <shogun/features/SubsetStack.h>
+#include <shogun/io/SGIO.h>
+#include <shogun/base/Parameter.h>
+
+using namespace shogun;
+
+CSubsetStack::CSubsetStack()
+{
+	init();
+}
+
+CSubsetStack::~CSubsetStack()
+{
+	SG_UNREF(m_active_subsets_stack);
+	SG_UNREF(m_active_subset);
+}
+
+void CSubsetStack::remove_all_subsets()
+{
+	/* delete all active subsets, backwards due to DynArray implementation */
+	for (index_t i=m_active_subsets_stack->get_num_elements()-1; i>=0; --i)
+		m_active_subsets_stack->delete_element(i);
+
+	SG_UNREF(m_active_subset);
+}
+
+void CSubsetStack::init()
+{
+	SG_ADD((CSGObject**)&m_active_subset, "active_subset",
+			"Currently active subset", MS_NOT_AVAILABLE);
+	SG_ADD((CSGObject**)&m_active_subsets_stack, "active_subsets_stack",
+			"Stack of active subsets", MS_NOT_AVAILABLE);
+
+	m_active_subset=NULL;
+	m_active_subsets_stack=new CDynamicObjectArray<CSubset>();
+	SG_REF(m_active_subsets_stack);
+}
+
+void CSubsetStack::add_subset(SGVector<index_t> subset)
+{
+	/* active subset will be changed anyway, no unref */
+	SG_UNREF(m_active_subset);
+
+	/* two cases: stack is empty/stack is not empty */
+	if (m_active_subsets_stack->get_num_elements())
+	{
+		/* if there are alreay subsets, we need to map given one through
+		 * existing ones */
+
+		/* get latest current subset */
+		CSubset* latest=m_active_subsets_stack->get_last_element();
+
+		SGVector<index_t> new_active_subset=SGVector<index_t>(subset.vlen, true);
+
+		/* using the latest current subset, transform all indices by the latest
+		 * added subset (dynamic programming greets you) */
+		for (index_t i=0; i<latest->get_size(); ++i)
+			new_active_subset.vector[i]=latest->m_subset_idx[subset.vector[i]];
+
+		/* replace active subset */
+		m_active_subset=new CSubset(new_active_subset);
+		SG_REF(m_active_subset);
+
+		SG_UNREF(latest);
+	}
+	else
+	{
+		/* just use plain given subset since there is nothing to map */
+		m_active_subset=new CSubset(subset);
+		SG_REF(m_active_subset);
+	}
+
+	/* add current active subset on stack of active subsets in any case */
+	m_active_subsets_stack->append_element(m_active_subset);
+}
+
+void CSubsetStack::remove_subset()
+{
+	index_t num_subsets=m_active_subsets_stack->get_num_elements();
+	if (num_subsets)
+	{
+		SG_UNREF(m_active_subset);
+
+		/* if there are subsets left on stack, set the next one as active and
+		 * delete last one */
+		if (num_subsets>1)
+		{
+			/* delete last element on stack */
+			index_t last_idx=m_active_subsets_stack->get_num_elements()-1;
+			m_active_subsets_stack->delete_element(last_idx);
+
+			/* use new last element on stack as active subset */
+			m_active_subset=m_active_subsets_stack->get_element(last_idx-1);
+		}
+
+		/* otherwise, active subset is just empty */
+	}
+	else
+	{
+		SG_WARNING("%s::remove_subset() was called but there is no subset set."
+				"\n", get_name());
+	}
+}
---
+++ src/shogun/features/SubsetStack.h
@@ -0,0 +1,98 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Heiko Strathmann
+ */
+
+#ifndef __SUBSETSTACK_H_
+#define __SUBSETSTACK_H_
+
+#include <shogun/base/SGObject.h>
+#include <shogun/mathematics/Math.h>
+#include <shogun/lib/DynamicObjectArray.h>
+#include <shogun/features/Subset.h>
+
+
+namespace shogun
+{
+
+/** @brief class to add subset support to another class. A CSubsetStackStack instance
+ * should be added and wrapper methods to all interfaces should be added.
+ *
+ * The subsets are organized as a stack. One can add arbritary many index sets
+ * which always refer to the current subset (identity if no element in stack)
+ * This way, one can define "subsets of subsets". Use the index conversion
+ * method to get original indices.
+ *
+ * Internally, a stack of active subsets is saved. Each time an index set is
+ * added, a new element will be put on stack, using the old element to get
+ * mappig. On  removal, the last element on stack will be removed. This is done
+ * for computational convenience.
+ */
+class CSubsetStack: public CSGObject
+{
+public:
+	/** Constructor. Creates empty subset stack
+	 *
+	 * @param original_size number of elements in the underlying data
+	 */
+	CSubsetStack();
+
+	/** destructor */
+	virtual ~CSubsetStack();
+
+	/** @return name of the SGSerializable */
+	inline const char* get_name() const { return "SubsetStack"; }
+
+	/** adds an index set to the current subset
+	 * @param subset index subset to add
+	 * */
+	virtual void add_subset(SGVector<index_t> subset);
+
+	/** removes the last added index set */
+	virtual void remove_subset();
+
+	/** removes all subsets, leaving this subset being the pure identity */
+	virtual void remove_all_subsets();
+
+	/** @return size of active subset, -1 if there is no such */
+	inline index_t get_size() {
+		return has_subsets() ? m_active_subset->get_size() : -1;
+	}
+
+	/** @return true iff no subset was added */
+	inline virtual bool has_subsets() const
+	{
+		return m_active_subsets_stack->get_num_elements();
+	}
+
+	/** returns the corresponding real index of a subset index
+	 * Maps through all added subsets in stack.
+	 *
+	 * @ return array index of the provided subset index
+	 */
+	inline index_t subset_idx_conversion(index_t idx) const
+	{
+		return m_active_subset ? m_active_subset->m_subset_idx.vector[idx] : idx;
+	}
+
+private:
+	/** registers and initializes parameters */
+	void init();
+
+private:
+	/** stack of active subsets. All active subsets are stored to avoid
+	 * recomputing them when subsets are removed. There is always the identity
+	 * subset as first element in here (only internal visible, has_subsets()
+	 * returns false if only this identity is present) */
+	CDynamicObjectArray<CSubset>* m_active_subsets_stack;
+
+	/** active index subset. Last element on stack for quick access */
+	CSubset* m_active_subset;
+};
+
+}
+#endif /* __SUBSETSTACK_H_ */
