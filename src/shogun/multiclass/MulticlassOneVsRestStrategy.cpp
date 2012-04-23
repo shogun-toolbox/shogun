8745033e86cdd6002368b1014307b8a8ede43957
Chiyuan Zhang <pluskid@gmail.com>
2012-04-23 16:07:57 +0800

    Move different MC Strategies into separate files.

---
+++ src/shogun/multiclass/MulticlassOneVsRestStrategy.cpp
@@ -0,0 +1,46 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Chiyuan Zhang
+ * Copyright (C) 2012 Chiyuan Zhang
+ */
+
+#include <shogun/multiclass/MulticlassOneVsRestStrategy.h>
+
+using namespace shogun;
+
+CMulticlassOneVsRestStrategy::CMulticlassOneVsRestStrategy()
+	:CMulticlassStrategy(), m_num_machines(0), m_rejection_strategy(NULL)
+{
+}
+
+CMulticlassOneVsRestStrategy::CMulticlassOneVsRestStrategy(CRejectionStrategy *rejection_strategy)
+	:CMulticlassStrategy(), m_num_machines(0), m_rejection_strategy(rejection_strategy)
+{
+}
+
+SGVector<int32_t> CMulticlassOneVsRestStrategy::train_prepare_next()
+{
+	CMulticlassStrategy::train_prepare_next();
+
+	for (int32_t i=0; i < m_orig_labels->get_num_labels(); ++i)
+	{
+		if (m_orig_labels->get_int_label(i)==m_train_iter)
+			m_train_labels->set_label(i, +1.0);
+		else
+			m_train_labels->set_label(i, -1.0);
+	}
+
+	return SGVector<int32_t>();
+}
+
+int32_t CMulticlassOneVsRestStrategy::decide_label(const SGVector<float64_t> &outputs, int32_t num_classes)
+{
+	if (m_rejection_strategy && m_rejection_strategy->reject(outputs))
+		return CLabels::REJECTION_LABEL;
+
+	return CMath::arg_max(outputs.vector, 1, outputs.vlen);
+}
--- src/shogun/multiclass/MulticlassStrategy.h
+++ src/shogun/multiclass/MulticlassStrategy.h
@@ -1,3 +1,13 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Chiyuan Zhang
+ * Copyright (C) 2012 Chiyuan Zhang
+ */
+
 #ifndef MULTICLASSSTRATEGY_H__
 #define MULTICLASSSTRATEGY_H__
 
@@ -35,16 +45,7 @@ public:
 	virtual EMulticlassStrategy get_strategy_type()=0;
 
 	/** start training */
-	virtual void train_start(CLabels *orig_labels, CLabels *train_labels)
-	{
-		if (m_train_labels != NULL)
-			SG_ERROR("Stop the previous training task before starting a new one!");
-		SG_REF(train_labels);
-		m_train_labels=train_labels;
-		SG_REF(orig_labels);
-		m_orig_labels=orig_labels;
-		m_train_iter=0;
-	}
+	virtual void train_start(CLabels *orig_labels, CLabels *train_labels);
 
 	/** has more training phase */
 	virtual bool train_has_more()=0;
@@ -52,18 +53,10 @@ public:
 	/** prepare for the next training phase.
 	 * @return The subset that should be applied. Return NULL when no subset is needed.
 	 */
-	virtual SGVector<int32_t> train_prepare_next()
-	{
-		m_train_iter++;
-		return NULL;
-	}
+	virtual SGVector<int32_t> train_prepare_next();
 
 	/** finish training, release resources */
-	virtual void train_stop()
-	{
-		SG_UNREF(m_train_labels);
-		SG_UNREF(m_orig_labels);
-	}
+	virtual void train_stop();
 
 	/** decide the final label.
 	 * @param outputs a vector of output from each machine (in that order)
@@ -72,153 +65,14 @@ public:
 	virtual int32_t decide_label(const SGVector<float64_t> &outputs, int32_t num_classes)=0;
 
 	/** get number of machines used in this strategy.
+	 * @param num_classes number of classes in this problem
 	 */
-	virtual int32_t get_num_machines()=0;
+	virtual int32_t get_num_machines(int32_t num_classes)=0;
 
 protected:
 	CLabels *m_train_labels;
 	CLabels *m_orig_labels;
 	int32_t m_train_iter;
-};
-
-class CMulticlassOneVsRestStrategy: public CMulticlassStrategy
-{
-public:
-	/** constructor */
-	CMulticlassOneVsRestStrategy();
-
-	/** constructor with rejection strategy */
-	CMulticlassOneVsRestStrategy(CRejectionStrategy *rejection_strategy);
-
-	/** destructor */
-	virtual ~CMulticlassOneVsRestStrategy() {}
-
-	/** get rejection strategy */
-	CRejectionStrategy *get_rejection_strategy()
-	{
-		SG_REF(m_rejection_strategy);
-		return m_rejection_strategy;
-	}
-
-	/** set rejection strategy */
-	void set_rejection_strategy(CRejectionStrategy *rejection_strategy)
-	{
-		SG_REF(rejection_strategy);
-		SG_UNREF(m_rejection_strategy);
-		m_rejection_strategy = rejection_strategy;
-	}
-
-	/** start training */
-	virtual void train_start(CLabels *orig_labels, CLabels *train_labels)
-	{
-		CMulticlassStrategy::train_start(orig_labels, train_labels);
-		m_num_machines=m_orig_labels->get_num_classes();
-	}
-
-	/** has more training phase */
-	virtual bool train_has_more()
-	{
-		return m_train_iter < m_num_machines;
-	}
-
-	/** prepare for the next training phase.
-	 * @return NULL, since no subset is needed in one-vs-rest strategy
-	 */ 
-	virtual SGVector<int32_t> train_prepare_next();
-
-	/** decide the final label.
-	 * @param outputs a vector of output from each machine (in that order)
-	 * @param num_classes number of classes
-	 */
-	virtual int32_t decide_label(const SGVector<float64_t> &outputs, int32_t num_classes);
-
-	/** get number of machines used in this strategy.
-	 * one-vs-rest strategy use one machine for each of the classes.
-	 */
-	virtual int32_t get_num_machines()
-	{
-		return m_num_machines;
-	}
-
-	/** get strategy type */
-	virtual EMulticlassStrategy get_strategy_type()
-	{
-		return ONE_VS_REST_STRATEGY;
-	}
-
-	/** get name */
-	virtual const char* get_name() const
-	{
-		return "MulticlassOneVsRestStrategy";
-	};
-
-protected:
-	int32_t m_num_machines;
-	CRejectionStrategy *m_rejection_strategy;
-};
-
-class CMulticlassOneVsOneStrategy: public CMulticlassStrategy
-{
-public:
-	/** constructor */
-	CMulticlassOneVsOneStrategy();
-
-	/** destructor */
-	virtual ~CMulticlassOneVsOneStrategy() {}
-
-	/** start training */
-	virtual void train_start(CLabels *orig_labels, CLabels *train_labels)
-	{
-		CMulticlassStrategy::train_start(orig_labels, train_labels);
-		m_num_classes = m_orig_labels->get_num_classes();
-		m_num_machines=m_num_classes*(m_num_classes-1)/2;
-
-		m_train_pair_idx_1 = 0;
-		m_train_pair_idx_2 = 1;
-	}
-
-	/** has more training phase */
-	virtual bool train_has_more()
-	{
-		return m_train_iter < m_num_machines;
-	}
-
-	/** prepare for the next training phase.
-	 * @return the subset that should be applied before training.
-	 */
-	virtual SGVector<int32_t> train_prepare_next();
-
-	/** decide the final label.
-	 * @param outputs a vector of output from each machine (in that order)
-	 * @param num_classes number of classes
-	 */
-	virtual int32_t decide_label(const SGVector<float64_t> &outputs, int32_t num_classes);
-
-	/** get number of machines used in this strategy.
-	 * one-vs-one strategy use one machine for each pair of classes.
-	 */
-	virtual int32_t get_num_machines()
-	{
-		return m_num_machines;
-	}
-
-	/** get strategy type */
-	virtual EMulticlassStrategy get_strategy_type()
-	{
-		return ONE_VS_ONE_STRATEGY;
-	}
-
-	/** get name */
-	virtual const char* get_name() const
-	{
-		return "MulticlassOneVsOneStrategy";
-	};
-
-protected:
-	int32_t m_num_machines;
-	int32_t m_num_classes;
-	int32_t m_train_pair_idx_1;
-	int32_t m_train_pair_idx_2;
 };
 
 } // namespace shogun
