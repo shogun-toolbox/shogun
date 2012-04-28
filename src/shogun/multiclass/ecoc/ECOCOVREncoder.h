290fcd83249bb376718d1d07f538d53d7b3d12eb
Chiyuan Zhang <pluskid@gmail.com>
2012-04-28 22:58:24 +0800

    Added ECOC Encoder.

---
+++ src/shogun/multiclass/ecoc/ECOCOVREncoder.h
@@ -0,0 +1,42 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Chiyuan Zhang
+ * Copyright (C) 2012 Chiyuan Zhang
+ */
+
+#ifndef ECOCOVRENCODER_H__
+#define ECOCOVRENCODER_H__
+
+#include <shogun/multiclass/ecoc/ECOCEncoder.h>
+
+namespace shogun
+{
+
+class CECOCOVREncoder: public CECOCEncoder
+{
+public:
+    /** constructor */
+    CECOCOVREncoder() {}
+
+    /** destructor */
+    virtual ~CECOCOVREncoder() {}
+
+    /** get name */
+    virtual const char* get_name()
+    {
+        return "ECOCOVREncoder";
+    }
+
+    /** init codebook.
+     * @param num_classes number of classes in this problem
+     */
+    virtual SGMatrix<int32_t> create_codebook(int32_t num_classes);
+};
+
+}
+
+#endif /* end of include guard: ECOCOVRENCODER_H__ */
