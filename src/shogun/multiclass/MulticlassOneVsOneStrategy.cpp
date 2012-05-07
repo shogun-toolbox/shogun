401ab302d40e5330b87ff88784706662e8237efd
Sergey Lisitsyn <blackburn91@gmail.com>
2012-05-07 15:49:38 +0400

    Fixed wrong method call

--- src/shogun/multiclass/MulticlassOneVsOneStrategy.cpp
+++ src/shogun/multiclass/MulticlassOneVsOneStrategy.cpp
@@ -60,7 +60,7 @@ SGVector<int32_t> CMulticlassOneVsOneStrategy::train_prepare_next()
 		m_train_pair_idx_2=m_train_pair_idx_1+1;
 	}
 
-	subset.resize(tot);
+	subset.resize_vector(tot);
 	return subset;
 }
 
