263905114acc670dde415787db2e4389f2e04109
Chiyuan Zhang <pluskid@gmail.com>
2012-05-07 21:58:56 +0800

    A new method for ECOC Random Sparse to avoid invalid code

--- src/shogun/multiclass/ecoc/ECOCRandomDenseEncoder.h
+++ src/shogun/multiclass/ecoc/ECOCRandomDenseEncoder.h
@@ -11,12 +11,12 @@
 #ifndef ECOCRANDOMDENSEENCODER_H__
 #define ECOCRANDOMDENSEENCODER_H__
 
-#include <shogun/multiclass/ecoc/ECOCRandomSparseEncoder.h>
+#include <shogun/multiclass/ecoc/ECOCEncoder.h>
 
 namespace shogun
 {
 
-class CECOCRandomDenseEncoder: public CECOCRandomSparseEncoder
+class CECOCRandomDenseEncoder: public CECOCEncoder
 {
 public:
     /** constructor
@@ -27,25 +27,23 @@ public:
      *
      * @see get_default_code_length
      */
-    CECOCRandomDenseEncoder(int32_t maxiter=1000, int32_t codelen=0, float64_t pposone=0.5, float64_t pnegone=0.5)
-        :CECOCRandomSparseEncoder(maxiter, codelen, 0, pposone, pnegone)
-    {
-    }
+    CECOCRandomDenseEncoder(int32_t maxiter=10000, int32_t codelen=0, float64_t pposone=0.5);
 
     /** destructor */
     virtual ~CECOCRandomDenseEncoder() {}
 
     /** set probability
      * @param pposone probability of +1
-     * @param pnegone probability of -1
      */
-    void set_probability(float64_t pposone, float64_t pnegone)
-    {
-        CECOCRandomSparseEncoder::set_probability(0, pposone, pnegone);
-    }
+    void set_probability(float64_t pposone);
 
     /** get name */
     virtual const char* get_name() const { return "ECOCRandomDenseEncoder"; }
+
+    /** init codebook.
+     * @param num_classes number of classes in this problem
+     */
+    virtual SGMatrix<int32_t> create_codebook(int32_t num_classes);
 
     /** get default code length
      * @param num_classes number of classes
@@ -56,10 +54,29 @@ public:
      *   E. Allwein, R. Schapire, and Y. Singer. Reducing multiclass to binary: A unifying approach
      *   for margin classifiers. Journal of Machine Learning Research, 1:113-141, 2002.
      */
-    virtual int32_t get_default_code_length(int32_t num_classes) const
+    int32_t get_default_code_length(int32_t num_classes) const
     {
         return static_cast<int32_t>(CMath::round(10 * CMath::log(num_classes)));
     }
+
+protected:
+    int32_t   m_maxiter; // max number of iterations
+    int32_t   m_codelen; // code length
+    float64_t m_pposone; // probability of +1
+
+private:
+    /** ensure probability sum to one
+     * @param pposone probability of +1
+     */
+    bool check_probability(float64_t pposone)
+    {
+        if (pposone >= 0.999 || pposone <= 0.0001)
+            return false;
+        return true;
+    }
+
+    /** init parameters */
+    void init();
 };
 
 } // namespace shogun
