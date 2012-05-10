39b064ee36de98cd2296f69638b03cf1ec0f2528
Soeren Sonnenburg <sonne@debian.org>
2012-05-10 21:47:06 +0200

    fix crasher in mkl / svmlight

--- src/shogun/classifier/svm/SVMLight.cpp
+++ src/shogun/classifier/svm/SVMLight.cpp
@@ -1654,9 +1654,6 @@ void CSVMLight::update_linear_component_mkl_linadd(
 	// restore old weights
 	kernel->set_subkernel_weights(SGVector<float64_t>(w_backup,num_weights));
 
-	SG_FREE(w_backup);
-	SG_FREE(w1);
-
 	call_mkl_callback(a, label, lin);
 }
 
