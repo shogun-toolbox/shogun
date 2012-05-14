58f6bd9c3d472c74d63509c0f0e72f8711dac3cd
iglesias <fernando.iglesiasg@gmail.com>
2012-05-14 16:01:04 +0200

    + hierarchy for SOMachines and preliminary implementations

---
+++ src/shogun/machine/LinearStructuredOutputMachine.h
@@ -0,0 +1,69 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Fernando José Iglesias García
+ * Copyright (C) 2012 Fernando José Iglesias García
+ */
+
+#ifndef _LINEARSTRUCTUREDOUTPUTMACHINE_H__
+#define _LINEARSTRUCTUREDOUTPUTMACHINE_H__
+
+#include <shogun/features/Features.h>
+#include <shogun/machine/StructuredOutputMachine.h>
+
+namespace shogun
+{
+
+/** TODO doc */
+class CLinearStructuredOutputMachine : public CStructuredOutputMachine
+{
+	public:
+		/** default constructor  */
+		CLinearStructuredOutputMachine();
+
+		/** standard constructor
+		 *
+		 * @param model structured model with application specific functions
+		 * @param loss structured loss function
+		 * @param labels structured labels
+		 * @param features features
+		 */
+		CLinearStructuredOutputMachine(CStructuredModel* model, CStructuredLoss* loss, CStructuredLabels* labs, CFeatures* features);
+
+		/** destructor */
+		virtual ~CLinearStructuredOutputMachine();
+
+		/** set features
+		 *
+		 * @param f features
+		 */
+		void set_features(CFeatures* f);
+
+		/** get features
+		 *
+		 * @return features
+		 */
+		CFeatures* get_features() const;
+
+		/** @return object name */
+		inline virtual const char* get_name() const 
+		{ 
+			return "LinearStructuredOutputMachine"; 
+		}
+
+	private:
+		/** register parameters */
+		void register_parameters();
+
+	protected:
+		/** feature vectors */
+		CFeatures* m_features;
+
+}; /* class CLinearStructuredOutputMachine */
+
+} /* namespace shogun */
+
+#endif /* _LINEARSTRUCTUREDOUTPUTMACHINE_H__ */
