cfc1eca854cd9b48101c94b46516ebb6c1368216
iglesias <fernando.iglesiasg@gmail.com>
2012-05-14 16:01:04 +0200

    + hierarchy for SOMachines and preliminary implementations

--- src/shogun/machine/StructuredOutputMachine.h
+++ src/shogun/machine/StructuredOutputMachine.h
@@ -11,6 +11,9 @@
 #ifndef _STRUCTUREDOUTPUTMACHINE_H__
 #define _STRUCTUREDOUTPUTMACHINE_H__
 
+#include <shogun/features/StructuredLabels.h>
+#include <shogun/lib/StructuredData.h>
+#include <shogun/machine/Machine.h>
 #include <shogun/so/StructuredLoss.h>
 #include <shogun/so/StructuredModel.h>
 
@@ -18,17 +21,61 @@ namespace shogun
 {
 
 /** TODO doc */
-class CStructuredOutputMachine : CMachine
+class CStructuredOutputMachine : public CMachine
 {
-	protected:
-		/** feature vectors */
-		CFeatures* m_features;
+	public:
+		/** deafult constructor */
+		CStructuredOutputMachine();
 
+		/** standard constructor
+		 *
+		 * @param model structured model with application specific functions
+		 * @param loss structured loss function
+		 * @param labels structured labels
+		 */
+		CStructuredOutputMachine(CStructuredModel* model, CStructuredLoss* loss, CStructuredLabels* labs);
+
+		/** destructor */
+		virtual ~CStructuredOutputMachine();
+
+		/** set labels
+		 *
+		 * @param lab labels
+		 */
+		virtual void set_labels(CStructuredLabels* labs);
+
+		/** @return object name */
+		inline virtual const char* get_name() const 
+		{ 
+			return "StructuredOutputMachine"; 
+		}
+
+		/** apply machine to the currently set features
+		 *
+		 * @return output 'labels'
+		 */
+		/* TODO change this to StructuredLabels when hierarchy fixed */
+		virtual CLabels* apply();
+
+		/** apply machine to data
+		 *
+		 * @param data (test)data to be classified
+		 * @return classified labels
+		 */
+		/* TODO change this to StructuredLabels when hierarchy fixed */
+		virtual CLabels* apply(CFeatures* data);
+
+	private:
+		/** register parameters */
+		void register_parameters();
+
+	protected:
 		/** the model that contains the application dependent modules */
 		CStructuredModel* m_model;
 
 		/** the general loss function */
 		CStructuredLoss* m_loss;
+
 
 }; /* class CStructuredOutputMachine */
 
