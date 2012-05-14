3b3ce93da1ff6a4802ee2d8e69965475428aa123
iglesias <fernando.iglesiasg@gmail.com>
2012-05-14 13:52:39 +0200

    + initial SO interface

---
+++ src/shogun/so/StructuredLoss.cpp
@@ -0,0 +1,21 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Fernando José Iglesias García
+ * Copyright (C) 2012 Fernando José Iglesias García
+ */
+
+#include <shogun/so/StructuredLoss.h>
+
+using namespace shogun;
+
+CStructuredLoss::CStructuredLoss()
+{
+}
+
+CStructuredLoss::~CStructuredLoss()
+{
+}
---
+++ src/shogun/so/StructuredModel.cpp
@@ -0,0 +1,47 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Fernando José Iglesias García
+ * Copyright (C) 2012 Fernando José Iglesias García
+ */
+
+#include <shogun/so/StructuredModel.h>
+
+using namespace shogun;
+
+CStructuredModel::CStructuredModel()
+{
+}
+
+CStructuredModel::~CStructuredModel()
+{
+}
+
+/* TODO */
+void CStructuredModel::init()
+{
+}
+
+/* TODO */
+int32_t CStructuredModel::get_dim()
+{
+}
+
+CFeatures* CStructuredModel::compute_combined_feature(int32_t feat_idx, int32_t lab_idx)
+{
+	return m_compute_combined_feature(m_features, m_labels, feat_idx, lab_idx);
+}
+
+CStructuredData CStructuredModel::argmax(SGVector< float64_t > w, int32_t feat_idx)
+{
+	return m_argmax(m_features, m_labels, w, feat_idx);
+}
+
+
+float64_t CStructuredModel::compute_delta_loss(CStructuredLabels* labels, CStructuredData ypred, int32_t ytrue_id)
+{
+	return m_compute_delta_loss(labels, ypred, ytrue_id);
+}
