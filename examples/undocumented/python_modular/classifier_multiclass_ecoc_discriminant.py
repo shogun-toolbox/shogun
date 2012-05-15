88ce66e36167bebc59075ed20eab26d433357a59
Chiyuan Zhang <pluskid@gmail.com>
2012-05-15 21:03:26 +0800

    refactor data preparation of multiclass python_modular.

--- examples/undocumented/python_modular/classifier_multiclass_ecoc_discriminant.py
+++ examples/undocumented/python_modular/classifier_multiclass_ecoc_discriminant.py
@@ -1,23 +1,6 @@
-from os.path import exists
-from tools.load import LoadMatrix
-lm=LoadMatrix()
+import classifier_multiclass_shared
 
-if exists('../data/../mldata/uci-20070111-optdigits.mat'):
-    from scipy.io import loadmat
-
-    mat = loadmat('../data/../mldata/uci-20070111-optdigits.mat')['int0'].astype(float)
-    X = mat[:-1,:]
-    Y = mat[-1,:]
-    isplit = X.shape[1]/2
-    traindat = X[:,:isplit]
-    label_traindat = Y[:isplit]
-    testdat = X[:, isplit:]
-    label_testdat = Y[isplit:]
-else:
-    traindat = lm.load_numbers('../data/fm_train_real.dat')
-    testdat  = lm.load_numbers('../data/fm_test_real.dat')
-    label_traindat = lm.load_labels('../data/label_train_multiclass.dat')
-    label_testdat = None
+[traindat, label_traindat, testdat, label_testdat] = classifier_multiclass_shared.prepare_data(False)
 
 parameter_list = [[traindat,testdat,label_traindat,label_testdat,2.1,1,1e-5],[traindat,testdat,label_traindat,label_testdat,2.2,1,1e-5]]
 
