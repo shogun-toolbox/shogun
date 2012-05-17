2e96c0fd6f83d2cb0f964d7a5bebbf869478315b
iglesias <fernando.iglesiasg@gmail.com>
2012-05-18 01:33:01 +0200

    * minor indent fix in SGMatrix

--- src/shogun/lib/SGMatrix.h
+++ src/shogun/lib/SGMatrix.h
@@ -60,14 +60,14 @@ template<class T> class SGMatrix : public SGReferencedData
 			return &matrix[col*num_rows];
 		}
 
-        /** operator overload for matrix read only access
-         * @param i_row
-         * @param i_col
-         */
-        inline const T& operator()(index_t i_row, index_t i_col) const
-        {
-            return matrix[i_col*num_rows + i_row];
-        }
+		/** operator overload for matrix read only access
+		 * @param i_row
+		 * @param i_col
+		 */
+		inline const T& operator()(index_t i_row, index_t i_col) const
+		{
+		    return matrix[i_col*num_rows + i_row];
+		}
 
 		/** operator overload for matrix read only access
 		 * @param index to access
@@ -77,14 +77,14 @@ template<class T> class SGMatrix : public SGReferencedData
 			return matrix[index];
 		}
 
-        /** operator overload for matrix r/w access
-         * @param i_row
-         * @param i_col
-         */
-        inline T& operator()(index_t i_row, index_t i_col)
-        {
-            return matrix[i_col*num_rows + i_row];
-        }
+		/** operator overload for matrix r/w access
+		 * @param i_row
+		 * @param i_col
+		 */
+		inline T& operator()(index_t i_row, index_t i_col)
+		{
+		    return matrix[i_col*num_rows + i_row];
+		}
 
 		/** operator overload for matrix r/w access
 		 * @param index to access
