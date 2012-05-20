869399d001b766085fea72e1f560d0e0ec4217b7
Chiyuan Zhang <pluskid@gmail.com>
2012-05-20 21:13:01 +0800

    Added Margin Loss Decoder

---
+++ src/shogun/multiclass/ecoc/ECOCLLBDecoder.cpp
@@ -0,0 +1,11 @@
+#include <shogun/multiclass/ecoc/ECOCLLBDecoder.h>
+
+using namespace shogun;
+
+float64_t CECOCLLBDecoder::compute_distance(SGVector<float64_t> outputs, const int32_t *code)
+{
+    float64_t loss = 0;
+    for (int32_t i=0; i < outputs.vlen; ++i)
+        loss += outputs[i]*code[i];
+    return -loss;
+}
