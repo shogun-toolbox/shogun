60b2900a1660386c3528083fba240216a8452fd4
Soeren Sonnenburg <sonne@debian.org>
2012-05-21 20:58:17 +0200

    fix libsvm py example

--- examples/undocumented/python_modular/classifier_libsvm_modular.py
+++ examples/undocumented/python_modular/classifier_libsvm_modular.py
@@ -8,7 +8,7 @@ label_traindat = lm.load_labels('../data/label_train_twoclass.dat')
 parameter_list = [[traindat,testdat,label_traindat,2.1,1,1e-5],[traindat,testdat,label_traindat,2.2,1,1e-5]]
 
 def classifier_libsvm_modular (fm_train_real=traindat,fm_test_real=testdat,label_train_twoclass=label_traindat,width=2.1,C=1,epsilon=1e-5):
-	from shogun.Features import RealFeatures, Labels
+	from shogun.Features import RealFeatures, BinaryLabels
 	from shogun.Kernel import GaussianKernel
 	from shogun.Classifier import LibSVM
 
@@ -16,7 +16,7 @@ def classifier_libsvm_modular (fm_train_real=traindat,fm_test_real=testdat,label
 	feats_test=RealFeatures(fm_test_real)
 	
 	kernel=GaussianKernel(feats_train, feats_train, width)
-	labels=Labels(label_train_twoclass)
+	labels=BinaryLabels(label_train_twoclass)
 
 	svm=LibSVM(C, kernel, labels)
 	svm.set_epsilon(epsilon)
@@ -27,6 +27,7 @@ def classifier_libsvm_modular (fm_train_real=traindat,fm_test_real=testdat,label
 	supportvectors = sv_idx=svm.get_support_vectors()
 	alphas=svm.get_alphas()
 	predictions = svm.apply()
+	print predictions.get_labels()
 	return predictions, svm, predictions.get_labels()
 
 if __name__=='__main__':
