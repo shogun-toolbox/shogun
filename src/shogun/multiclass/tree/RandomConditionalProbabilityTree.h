e8f8a95b7a51ba3fef7da170e559b7efaac7d5f5
Chiyuan Zhang <pluskid@gmail.com>
2012-06-03 12:02:09 +0800

    Added a simple base class for multiclass machine.

---
+++ src/shogun/multiclass/tree/RandomConditionalProbabilityTree.h
@@ -0,0 +1,39 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Chiyuan Zhang
+ * Copyright (C) 2012 Chiyuan Zhang
+ */
+
+#ifndef RANDOMCONDITIONALPROBABILITYTREE_H__
+#define RANDOMCONDITIONALPROBABILITYTREE_H__
+
+#include <shogun/multiclass/tree/ConditionalProbabilityTree.h>
+
+namespace shogun
+{
+
+class CRandomConditionalProbabilityTree: public CConditionalProbabilityTree
+{
+public:
+    /** constructor */
+	CRandomConditionalProbabilityTree() {}
+
+    /** destructor */
+	virtual ~CRandomConditionalProbabilityTree() {}
+
+    /** get name */
+    virtual const char* get_name() const { return "RandomConditionalProbabilityTree"; }
+
+private:
+	/** to prevent compile error of class_list.cpp */
+	virtual void __placeholder__()=0;
+};
+
+} /* shogun */ 
+
+#endif /* end of include guard: RANDOMCONDITIONALPROBABILITYTREE_H__ */
+
