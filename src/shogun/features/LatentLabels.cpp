7b4eefd84de098993933f77f3c79850434d8a4a2
Viktor Gal <viktor.gal@maeth.com>
2012-06-05 21:44:21 +0200

    LatentLabels implementation

--- src/shogun/features/LatentLabels.cpp
+++ src/shogun/features/LatentLabels.cpp
@@ -12,14 +12,79 @@
 
 using namespace shogun;
 
-CLatentLabels::CLatentLabels ()
+CLatentData::CLatentData ()
 {
 
+}
+
+CLatentData::~CLatentData ()
+{
+
+}
+
+CLatentLabels::CLatentLabels ()
+  : CLabels ()
+{
+  init ();
+}
+
+CLatentLabels::CLatentLabels (int32_t num_labels)
+  : CLabels (num_labels)
+{
+  init ();
+  m_labels = new CDynamicObjectArray<CLatentData> (num_labels);
+  SG_REF (m_labels);
 }
 
 CLatentLabels::~CLatentLabels ()
 {
-
+  SG_UNREF (m_labels);
 }
 
+void CLatentLabels::init ()
+{
+  SG_ADD((CSGObject**) &m_labels, "m_labels", "The labels", MS_NOT_AVAILABLE);
+  m_labels = NULL;
+}
+
+CDynamicObjectArray<CLatentData>* CLatentLabels::get_labels () const
+{
+  SG_REF (m_labels);
+  return m_labels;
+}
+
+CLatentData* CLatentLabels::get_label (int32_t idx) const
+{
+  ASSERT (m_labels != NULL);
+  if (idx < 0 || idx >= get_num_labels())
+    SG_ERROR("Out of index!\n");
+
+  return (CLatentData*) m_labels->get_element (idx);
+}
+
+void CLatentLabels::add_label (CLatentData* label)
+{
+  ASSERT (m_labels != NULL);
+  m_labels->push_back (label);
+}
+
+bool CLatentLabels::set_label (int32_t idx, CLatentData* label)
+{
+  if (idx < get_num_labels ())
+  {
+    return m_labels->insert_element (label, idx);
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int32_t CLatentLabels::get_num_labels() const
+{
+  if (m_labels == NULL)
+    return 0;
+  else
+    return m_labels->get_num_elements ();
+}
 
--- src/shogun/features/LatentLabels.h
+++ src/shogun/features/LatentLabels.h
@@ -12,16 +12,51 @@
 #define __LATENTLABELS_H__
 
 #include <shogun/features/Labels.h>
+#include <shogun/lib/DynamicObjectArray.h>
 
 namespace shogun
 {
+  class CLatentData : public CSGObject
+  {
+    public:
+      CLatentData ();
+
+      virtual ~CLatentData ();
+
+      virtual const char* get_name() const { return "LatentData"; }
+  };
+
   class CLatentLabels : public CLabels
   {
     public:
       CLatentLabels ();
 
+      CLatentLabels (int32_t num_labels);
+
       virtual ~CLatentLabels ();
+
+      CDynamicObjectArray<CLatentData>* get_labels() const;
+
+      CLatentData* get_label (int32_t idx) const;
+
+      void add_label (CLatentData* label);
+
+      using CLabels::set_label;
+
+      bool set_label (int32_t idx, CLatentData* label);
+
+      int32_t get_num_labels() const;
+
+      virtual const char* get_name() const { return "LatentLabels"; }
+
+    protected:
+      /** the vector of labels */
+      CDynamicObjectArray<CLatentData>* m_labels;
+
+    private:
+      void init ();
   };
 }
 
 #endif /* __LATENTLABELS_H__ */
+
