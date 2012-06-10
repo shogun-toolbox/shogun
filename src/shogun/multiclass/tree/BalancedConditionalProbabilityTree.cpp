8f891b2c286d00c3c688d6c4aa2ea3494565fae7
Chiyuan Zhang <pluskid@gmail.com>
2012-06-10 20:13:43 +0800

    Balanced CPT construction.

---
+++ src/shogun/multiclass/tree/BalancedConditionalProbabilityTree.cpp
@@ -0,0 +1,54 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Chiyuan Zhang
+ * Copyright (C) 2012 Chiyuan Zhang
+ */
+
+#include <shogun/multiclass/tree/BalancedConditionalProbabilityTree.h>
+
+using namespace shogun;
+
+CBalancedConditionalProbabilityTree::CBalancedConditionalProbabilityTree()
+	:m_alpha(0.6)
+{
+	SG_ADD(&m_alpha, "m_alpha", "Trade-off parameter of tree balance", MS_NOT_AVAILABLE);
+}
+
+void CBalancedConditionalProbabilityTree::set_alpha(float64_t alpha)
+{
+	if (alpha < 0 || alpha > 1)
+		SG_ERROR("expect 0 <= alpha <= 1, but got %g\n", alpha);
+	m_alpha = alpha;
+}
+
+bool CBalancedConditionalProbabilityTree::which_subtree(node_t *node, SGVector<float32_t> ex)
+{
+	float64_t pred = predict_node(ex, node);
+	float64_t depth_left = tree_depth(node->left());
+	float64_t depth_right = tree_depth(node->right());
+
+	float64_t cnt_left = CMath::pow(2.0, depth_left);
+	float64_t cnt_right = CMath::pow(2.0, depth_right);
+
+	float64_t obj_val = (1-m_alpha) * 2 * (pred-0.5) + m_alpha * CMath::log2(cnt_left/cnt_right);
+
+	if (obj_val > 0)
+		return false; // go right
+	return true; // go left
+}
+
+int32_t CBalancedConditionalProbabilityTree::tree_depth(node_t *node)
+{
+	int32_t depth = 0;
+	while (node != NULL)
+	{
+		depth++;
+		node = node->left();
+	}
+
+	return depth;
+}
---
+++ src/shogun/multiclass/tree/BalancedConditionalProbabilityTree.h
@@ -0,0 +1,65 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012 Chiyuan Zhang
+ * Copyright (C) 2012 Chiyuan Zhang
+ */
+
+#ifndef BALANCEDCONDITIONALPROBABILITYTREE_H__
+#define BALANCEDCONDITIONALPROBABILITYTREE_H__
+
+#include <shogun/multiclass/tree/ConditionalProbabilityTree.h>
+
+namespace shogun
+{
+
+/**
+ * Balanced Conditional Probability Tree.
+ *
+ * The tree is constructed to trade-off the existing regressor's prediction
+ * and the balance (depth) of the tree. The parameter alpha in [0,1]
+ * control the trade-off.
+ *
+ * * when alpha = 1, best efforts are made to ensure the tree is balanced
+ * * when alpha = 0, the balance of tree is complete ignored
+ *
+ */
+class CBalancedConditionalProbabilityTree: public CConditionalProbabilityTree
+{
+public:
+    /** constructor */
+	CBalancedConditionalProbabilityTree();
+
+    /** destructor */
+	virtual ~CBalancedConditionalProbabilityTree() {}
+
+    /** get name */
+    virtual const char* get_name() const { return "BalancedConditionalProbabilityTree"; }
+
+	/** set alpha */
+	void set_alpha(float64_t alpha);
+
+	/** get alpha */
+	float64_t get_alpha() const { return m_alpha; }
+
+protected:
+	/** decide which subtree to go, when training the tree structure.
+	 * @param node the node being decided
+	 * @param ex the example being decided
+	 * @return true if should go left, false otherwise
+	 */
+	virtual bool which_subtree(node_t *node, SGVector<float32_t> ex);
+
+private:
+	int32_t tree_depth(node_t *node);
+
+	float64_t m_alpha; ///< trade-off parameter for tree balance
+};
+
+} /* shogun */ 
+
+#endif /* end of include guard: BALANCEDCONDITIONALPROBABILITYTREE_H__ */
+
