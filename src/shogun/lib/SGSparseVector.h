59068d620c29a44bcff2894fd1e7e772aa7265e1
Christian Widmer <cwidmer@tuebingen.mpg.de>
2012-06-18 21:36:39 +0200

    extended SGSparseVector to make items accessible with operator[] at the cost of increased memory requirements. Adapted typemap for python_modular. Made use of this extension in MTL DCD solver.

--- src/shogun/lib/SGSparseVector.h
+++ src/shogun/lib/SGSparseVector.h
@@ -5,6 +5,7 @@
  * (at your option) any later version.
  *
  * Written (W) 2012 Fernando José Iglesias García
+ * Written (W) 2012 Christian Widmer
  * Written (W) 2010,2012 Soeren Sonnenburg
  * Copyright (C) 2010 Berlin Institute of Technology
  * Copyright (C) 2012 Soeren Sonnenburg
@@ -15,9 +16,12 @@
 
 #include <shogun/lib/config.h>
 #include <shogun/lib/DataType.h>
+#include <map>
+#include <iostream>
 
 namespace shogun
 {
+
 
 /** @brief template class SGSparseVectorEntry */
 template <class T> struct SGSparseVectorEntry
@@ -40,7 +44,10 @@ public:
 	SGSparseVector(SGSparseVectorEntry<T>* feats, index_t num_entries,
 			index_t index, bool free_v=false) :
 			num_feat_entries(num_entries), features(feats),
-			do_free(free_v) {}
+			do_free(free_v)
+	{
+		create_idx_map();
+	}
 
 	/** constructor to create new vector in memory */
 	SGSparseVector(index_t num_entries, index_t index, bool free_v=false) :
@@ -52,7 +59,10 @@ public:
 	/** copy constructor */
 	SGSparseVector(const SGSparseVector& orig) :
 			num_feat_entries(orig.num_feat_entries),
-			features(orig.features), do_free(orig.do_free) {}
+			features(orig.features), do_free(orig.do_free)
+	{
+		create_idx_map();
+	}
 
 	/** free vector */
 	void free_vector()
@@ -60,6 +70,7 @@ public:
 		if (do_free)
 			SG_FREE(features);
 
+		dense_to_sparse_idx.clear();
 		features=NULL;
 		do_free=false;
 		num_feat_entries=0;
@@ -72,6 +83,56 @@ public:
 		free_vector();
 	}
 
+	/** create mapping from dense idx to sparse idx */
+	//TODO a call to this was added to type map for python modular ONLY, needs to be added to others
+	void create_idx_map()
+	{
+		dense_to_sparse_idx.clear();
+		for (int32_t i=0; i!=num_feat_entries; i++)
+		{
+			dense_to_sparse_idx[features[i].feat_index] = i;
+		}
+	}
+
+	/** operator overload for vector read only access
+	 *
+	 * @param index dimension to access
+	 *
+	 */
+	inline const T& operator[](index_t index) const
+	{
+		// lookup complexity is O(log n)
+		std::map<index_t, index_t>::const_iterator it = dense_to_sparse_idx.find(index);
+
+		if (it != dense_to_sparse_idx.end())
+		{
+			// use mapping for lookup
+			return features[it->second].entry;
+		} else {
+			return zero;
+		}
+	}
+
+		
+	/** TODO: operator overload for vector r/w access
+	 *
+	 * @param index dimension to access
+	 *
+	inline T& operator[](index_t index)
+	{
+		return dense_to_sparse_idx[index];
+		// lookup complexity is O(log n)
+		typename std::map<index_t, T>::iterator it = dense_to_sparse_idx.find(index);
+
+		if (it != dense_to_sparse_idx.end())
+		{
+			return it->second;
+		} else {
+			return dense_to_sparse_idx.insert(index, 0);
+		}
+	}
+		*/
+
 public:
 	/** number of feature entries */
 	index_t num_feat_entries;
@@ -81,6 +142,25 @@ public:
 
 	/** whether vector needs to be freed */
 	bool do_free;
+
+protected:	
+	/** store mapping of indices */
+	std::map<index_t, index_t> dense_to_sparse_idx;
+
+	/** zero element */
+	static const T zero;
+
 };
+
+// inititalize static member in template class
+template <typename T>
+const T SGSparseVector<T>::zero = T(0);
+
 }
+
+/*
+template<class T>
+const T typename SGSparseVector<T>::zero = 0;
+*/
+
 #endif // __SGSPARSEVECTOR_H__
