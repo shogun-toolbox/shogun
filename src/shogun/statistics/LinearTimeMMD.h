c3d7a15a34baae7ac6780129d345cc0376e28d24
Heiko Strathmann <heiko.strathmann@gmail.com>
2012-06-21 18:57:32 +0100

    added documentation

--- src/shogun/statistics/LinearTimeMMD.h
+++ src/shogun/statistics/LinearTimeMMD.h
@@ -18,6 +18,17 @@ namespace shogun
 
 class CFeatures;
 
+/** @brief This class implements the linear time Maximum Mean Statistic as
+ * described in
+ * ﻿Gretton, A., Borgwardt, K. M., Rasch, M. J., Schölkopf, B., & Smola, A. (2012).
+ * A Kernel Two-Sample Test. Journal of Machine Learning Research, 13, 671-721.
+ *
+ * Along with the statistic comes a method to compute a p-value based on a
+ * Gaussian approximation of the null-distribution which is also possible in
+ * linear time and constant space. Bootstrapping, of course, is also possible.
+ *
+ * To choose, use set_p_value_method(MMD1_GAUSSIAN). *
+ */
 class CLinearTimeMMD: public CKernelTwoSampleTestStatistic
 {
 public:
@@ -26,7 +37,22 @@ public:
 
 	virtual ~CLinearTimeMMD();
 
+	/** Computes the squared linear time MMD for the current data. his is an
+	 * unbiased estimate
+	 *
+	 * @return squared linear time MMD
+	 */
 	virtual float64_t compute_statistic();
+
+	/** Computes the p-value for a given statistic. The method for computing
+	 * the p-value can be set via set_p_value_method() method. Since the null-
+	 * distribution is normal, a Gaussian approximation is available along with
+	 * bootstrapping
+	 *
+	 * @param statistic statistic to compute the p-value for
+	 *
+	 * @return p-value of the given statistic
+	 */
 	virtual float64_t compute_p_value(float64_t statistic);
 
 	/** computes a linear time estimate of the variance of the squared linear
