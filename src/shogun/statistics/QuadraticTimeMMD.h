10aed36fade4e4b5b8c6799bfbc38cd45ff6b5cd
Heiko Strathmann <heiko.strathmann@gmail.com>
2012-06-28 17:50:26 +0100

    added convienience constructor for appending features

--- src/shogun/statistics/QuadraticTimeMMD.h
+++ src/shogun/statistics/QuadraticTimeMMD.h
@@ -60,7 +60,31 @@ class CQuadraticTimeMMD : public CKernelTwoSampleTestStatistic
 {
 	public:
 		CQuadraticTimeMMD();
+
+		/** Constructor
+		 *
+		 * @param p_and_q feature data. Is assumed to contain samples from both
+		 * p and q. First all samples from p, then from index q_start all
+		 * samples from q
+		 *
+		 * @param kernel kernel to use
+		 * @param p_and_q samples from p and q, appended
+		 * @param q_start index of first sample of q
+		 */
 		CQuadraticTimeMMD(CKernel* kernel, CFeatures* p_and_q, index_t q_start);
+
+		/** Constructor.
+		 * This is a convienience constructor which copies both features to one
+		 * element and then calls the other constructor. Needs twice the memory
+		 * for a short time
+		 *
+		 * @param kernel kernel for MMD
+		 * @param p samples from distribution p, will be copied and NOT
+		 * SG_REF'ed
+		 * @param q samples from distribution q, will be copied and NOT
+		 * SG_REF'ed
+		 */
+		CQuadraticTimeMMD(CKernel* kernel, CFeatures* p, CFeatures* q);
 
 		virtual ~CQuadraticTimeMMD();
 
