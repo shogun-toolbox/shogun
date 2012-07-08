53590193f8eb6d66155c3ee0c4ffefed45c2bd45
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2012-07-09 02:33:21 +0400

    Made LibSVM use openmp

--- src/shogun/lib/external/shogun_libsvm.cpp
+++ src/shogun/lib/external/shogun_libsvm.cpp
@@ -51,9 +51,7 @@
 #include <string.h>
 #include <stdarg.h>
 
-#ifdef HAVE_PTHREAD
-#include <pthread.h>
-#endif
+#include <omp.h>
 
 namespace shogun
 {
@@ -244,93 +242,23 @@ public:
 		if(x_square) CMath::swap(x_square[i],x_square[j]);
 	}
 
-	static void* compute_Q_parallel_helper(void* p)
-	{
-		Q_THREAD_PARAM* params= (Q_THREAD_PARAM*) p;
-		int32_t i=params->i;
-		int32_t start=params->start;
-		int32_t end=params->end;
-		float64_t* y=params->y;
-		Qfloat* data=params->data;
-		const LibSVMKernel* q=params->q;
-
-		if (y) // two class
-		{
-			for(int32_t j=start;j<end;j++)
-				data[j] = (Qfloat) y[i]*y[j]*q->kernel_function(i,j);
-		}
-		else // one class, eps svr
-		{
-			for(int32_t j=start;j<end;j++)
-				data[j] = (Qfloat) q->kernel_function(i,j);
-		}
-
-		return NULL;
-	}
-
 	void compute_Q_parallel(Qfloat* data, float64_t* lab, int32_t i, int32_t start, int32_t len) const
 	{
 		int32_t num_threads=sg_parallel->get_num_threads();
-		if (num_threads < 2)
+		int32_t j;
+		if (lab) // two class
 		{
-			Q_THREAD_PARAM params;
-			params.i=i;
-			params.start=start;
-			params.end=len;
-			params.y=lab;
-			params.data=data;
-			params.q=this;
-			compute_Q_parallel_helper((void*) &params);
+#pragma omp parallel private(j) num_threads(num_threads)
+#pragma omp for schedule(dynamic) nowait
+			for(j=start;j<len;j++)
+				data[j] = (Qfloat) lab[i]*lab[j]*this->kernel_function(i,j);
 		}
-		else
+		else // one class, eps svr
 		{
-#ifdef HAVE_PTHREAD
-			int32_t total_num=(len-start);
-			pthread_t* threads = SG_MALLOC(pthread_t, num_threads-1);
-			Q_THREAD_PARAM* params = SG_MALLOC(Q_THREAD_PARAM, num_threads);
-			int32_t step= total_num/num_threads;
-
-			int32_t t;
-
-			num_threads--;
-			for (t=0; t<num_threads; t++)
-			{
-				params[t].i=i;
-				params[t].start=t*step;
-				params[t].end=(t+1)*step;
-				params[t].y=lab;
-				params[t].data=data;
-				params[t].q=this;
-
-				int code=pthread_create(&threads[t], NULL,
-						compute_Q_parallel_helper, (void*)&params[t]);
-
-				if (code != 0)
-				{
-					SG_SWARNING("Thread creation failed (thread %d of %d) "
-							"with error:'%s'\n",t, num_threads, strerror(code));
-					num_threads=t;
-					break;
-				}
-			}
-
-			params[t].i=i;
-			params[t].start=t*step;
-			params[t].end=len;
-			params[t].y=lab;
-			params[t].data=data;
-			params[t].q=this;
-			compute_Q_parallel_helper(&params[t]);
-
-			for (t=0; t<num_threads; t++)
-			{
-				if (pthread_join(threads[t], NULL) != 0)
-					SG_SWARNING("pthread_join of thread %d/%d failed\n", t, num_threads);
-			}
-
-			SG_FREE(params);
-			SG_FREE(threads);
-#endif /* HAVE_PTHREAD */
+#pragma omp parallel private(j) num_threads(num_threads)
+#pragma omp for schedule(dynamic) nowait
+			for(j=start;j<len;j++)
+				data[j] = (Qfloat) this->kernel_function(i,j);
 		}
 	}
 
