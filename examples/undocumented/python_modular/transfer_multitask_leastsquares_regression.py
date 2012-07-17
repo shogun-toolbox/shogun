197ebdf427b449c742f226a1a3cd640e606ced8a
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2012-07-17 23:56:01 +0400

    Added multitask python examples

--- examples/undocumented/python_modular/transfer_multitask_leastsquares_regression.py
+++ examples/undocumented/python_modular/transfer_multitask_leastsquares_regression.py
@@ -9,7 +9,7 @@ label_traindat = lm.load_labels('../data/label_train_twoclass.dat')
 
 parameter_list = [[traindat,testdat,label_traindat]]
 
-def transfer_multitask_group_regression(fm_train=traindat,fm_test=testdat,label_train=label_traindat):
+def transfer_multitask_leastsquares_regression(fm_train=traindat,fm_test=testdat,label_train=label_traindat):
 
 	from modshogun import RegressionLabels, RealFeatures, IndexBlock, IndexBlockGroup, MultitaskLSRegression
 
@@ -24,11 +24,13 @@ def transfer_multitask_group_regression(fm_train=traindat,fm_test=testdat,label_
 	task_group.add_block(task_two)
 
 	mtlsr = MultitaskLSRegression(0.1,features,labels,task_group)
+	mtlsr.set_regularization(1) # use regularization ratio
+	mtlsr.set_tolerance(1e-2) # use 1e-2 tolerance
 	mtlsr.train()
 	mtlsr.set_current_task(0)
 	out = mtlsr.apply_regression().get_labels()
 	return out
 
 if __name__=='__main__':
-	print('TransferMultitaskGroupRegression')
-	transfer_multitask_group_regression(*parameter_list[0])
+	print('TransferMultitaskLeastSquaresRegression')
+	transfer_multitask_leastsquares_regression(*parameter_list[0])
---
+++ examples/undocumented/python_modular/transfer_multitask_trace_logistic_regression.py
@@ -0,0 +1,37 @@
+from numpy import array,hstack
+from numpy.random import seed, rand
+from tools.load import LoadMatrix
+lm=LoadMatrix()
+
+traindat = lm.load_numbers('../data/fm_train_real.dat')
+testdat = lm.load_numbers('../data/fm_test_real.dat')
+label_traindat = lm.load_labels('../data/label_train_twoclass.dat')
+
+parameter_list = [[traindat,testdat,label_traindat]]
+
+def transfer_multitask_trace_logistic_regression(fm_train=traindat,fm_test=testdat,label_train=label_traindat):
+
+	from modshogun import BinaryLabels, RealFeatures, IndexBlock, IndexBlockGroup, MultitaskTraceLogisticRegression
+
+	features = RealFeatures(hstack((traindat,traindat)))
+	labels = BinaryLabels(hstack((label_train,label_train)))
+
+	n_vectors = features.get_num_vectors()
+	task_one = IndexBlock(0,n_vectors/2)
+	task_two = IndexBlock(n_vectors/2,n_vectors)
+	task_group = IndexBlockGroup()
+	task_group.add_block(task_one)
+	task_group.add_block(task_two)
+
+	mtlr = MultitaskTraceLogisticRegression(0.1,features,labels,task_group)
+	mtlr.set_tolerance(1e-2) # use 1e-2 tolerance
+	mtlr.set_max_iter(10)
+	mtlr.train()
+	mtlr.set_current_task(0)
+	out = mtlr.apply_regression().get_labels()
+
+	return out
+
+if __name__=='__main__':
+	print('TransferMultitaskTraceLogisticRegression')
+	transfer_multitask_trace_logistic_regression(*parameter_list[0])
