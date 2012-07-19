fa6ce12e94e30928305d7cc0d6eae938e238aa94
Soeren Sonnenburg <sonne@debian.org>
2012-07-19 22:38:09 +0200

    pass COMP_CPP through for compiling libshogun examples

--- examples/undocumented/libshogun/check.sh
+++ examples/undocumented/libshogun/check.sh
@@ -7,21 +7,22 @@ test -n "$LIB_PATH" && LIB_PATH="LIB_PATH=${LIB_PATH}"
 test -n "$INC_PATH" && INC_PATH="INC_PATH=${INC_PATH}"
 test -n "$LIBS" && LIBS="LIBS=${LIBS}"
 test -n "$INCLUDES" && INCLUDES="INCLUDES=${INCLUDES}"
+test -n "$CC" && CC="CC=${CC}"
 test -z "$MAKE" && MAKE="make"
 
 for e in `$MAKE print_targets | grep -v ^make`
 do
 	echo -n "running $e .."
-	if $MAKE "$e" "$LIBRARY_PATH" "$LIB_PATH" "$INC_PATH" "$LIBS" "$INCLUDES" >/dev/null 2>&1 && "./$e" >/dev/null 2>&1
+	if $MAKE "$e" $CC $LIBRARY_PATH $LIB_PATH $INC_PATH $LIBS $INCLUDES >/dev/null 2>&1 && "./$e" >/dev/null 2>&1
 	then
 		echo " OK"
 	else
 		echo " ERROR"
 		echo "================================================================================" >>error.log
-		echo "error in $MAKE $e $LIBRARY_PATH $LIB_PATH $INC_PATH $LIBS" >>error.log
+		echo "error in $MAKE $e $CC $LIBRARY_PATH $LIB_PATH $INC_PATH $LIBS" >>error.log
 		echo "error in ./$e" >>error.log 2>&1
 		echo "================================================================================" >>error.log
-		$MAKE "$e" "$LIBRARY_PATH" "$LIB_PATH" "$INC_PATH" "$LIBS" >>error.log 2>&1 && "./$e" >>error.log
+		$MAKE "$e" $CC $LIBRARY_PATH $LIB_PATH $INC_PATH $LIBS >>error.log 2>&1 && "./$e" >>error.log
 		echo "================================================================================" >>error.log
 		echo >>error.log
 		echo >>error.log
