644ef806e4300cd02b35eefe167b0a4bcf734c9e
Heiko Strathmann <heiko.strathmann@gmail.com>
2012-07-31 18:25:24 +0100

    added bootstrapping for precomputed kernel matrices

--- src/shogun/statistics/KernelTwoSampleTestStatistic.cpp
+++ src/shogun/statistics/KernelTwoSampleTestStatistic.cpp
@@ -10,6 +10,7 @@
 #include <shogun/statistics/KernelTwoSampleTestStatistic.h>
 #include <shogun/features/Features.h>
 #include <shogun/kernel/Kernel.h>
+#include <shogun/kernel/CustomKernel.h>
 
 using namespace shogun;
 
@@ -48,4 +49,58 @@ void CKernelTwoSampleTestStatistic::init()
 	SG_ADD((CSGObject**)&m_kernel, "kernel", "Kernel for two sample test",
 			MS_AVAILABLE);
 	m_kernel=NULL;
+}
+
+SGVector<float64_t> CKernelTwoSampleTestStatistic::bootstrap_null()
+{
+	/* compute bootstrap statistics for null distribution */
+	SGVector<float64_t> results(m_bootstrap_iterations);
+
+	/* memory for index permutations, (would slow down loop) */
+	SGVector<index_t> ind_permutation(m_p_and_q->get_num_vectors());
+	ind_permutation.range_fill();
+
+	/* check if kernel is a custom kernel. In that case, changing features is
+	 * not what we want but just subsetting the kernel itself */
+	CCustomKernel* custom_kernel;
+	if (m_kernel->get_kernel_type()==K_CUSTOM)
+	{
+		custom_kernel=(CCustomKernel*)m_kernel;
+		custom_kernel->add_row_subset(ind_permutation);
+		custom_kernel->add_col_subset(ind_permutation);
+	}
+	else
+	{
+		custom_kernel=NULL;
+		m_p_and_q->add_subset(ind_permutation);
+	}
+
+
+	for (index_t i=0; i<m_bootstrap_iterations; ++i)
+	{
+		/* idea: merge features of p and q, shuffle, and compute statistic.
+		 * This is done using subsets here */
+
+		/* create index permutation and add as subset. This will mix samples
+		 * from p and q */
+		SGVector<int32_t>::permute_vector(ind_permutation);
+
+		/* compute statistic for this permutation of mixed samples */
+		results[i]=compute_statistic();
+	}
+
+	/* clean up */
+	if (custom_kernel)
+	{
+		custom_kernel->remove_row_subset();
+		custom_kernel->remove_col_subset();
+	}
+	else
+	{
+		custom_kernel=NULL;
+		m_p_and_q->remove_subset();
+	}
+
+	/* clean up and return */
+	return results;
 }
