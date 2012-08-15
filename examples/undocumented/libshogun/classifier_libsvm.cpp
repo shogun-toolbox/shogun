62d929c6ed3a4a8b4fb59ee5c58c2ab8c116c267
Heiko Strathmann <heiko.strathmann@gmail.com>
2012-08-15 18:41:12 +0100

    cleaned up example a bit

--- examples/undocumented/libshogun/classifier_libsvm.cpp
+++ examples/undocumented/libshogun/classifier_libsvm.cpp
@@ -5,99 +5,105 @@
  * (at your option) any later version.
  *
  * Written (W) 2008-2009 Soeren Sonnenburg
+ * Written (W) 2012 Heiko Strathmann
  * Copyright (C) 2008-2009 Fraunhofer Institute FIRST and Max Planck Society
  */
 #include <shogun/kernel/GaussianKernel.h>
 #include <shogun/labels/BinaryLabels.h>
 #include <shogun/features/DenseFeatures.h>
 #include <shogun/classifier/svm/LibSVM.h>
-#include <shogun/mathematics/Math.h>
-#include <shogun/lib/common.h>
-#include <shogun/base/init.h>
-
-#include <stdlib.h>
-#include <stdio.h>
 
 using namespace shogun;
 
-#define NUM 100
-#define DIMS 2
-#define DIST 0.5
-
-float64_t* lab;
-SGMatrix<float64_t> feat;
-
-void gen_rand_data()
+void gen_rand_data(SGVector<float64_t> lab, SGMatrix<float64_t> feat,
+		float64_t dist)
 {
-	lab=SG_MALLOC(float64_t, NUM);
-	feat=SGMatrix<float64_t>(DIMS, NUM);
+	index_t dims=feat.num_rows;
+	index_t num=lab.vlen;
 
-	for (int32_t i=0; i<NUM; i++)
+	for (int32_t i=0; i<num; i++)
 	{
-		if (i<NUM/2)
+		if (i<num/2)
 		{
 			lab[i]=-1.0;
 
-			for (int32_t j=0; j<DIMS; j++)
-				feat.matrix[i*DIMS+j]=CMath::random(0.0,1.0)+DIST;
+			for (int32_t j=0; j<dims; j++)
+				feat(j, i)=CMath::random(0.0, 1.0)+dist;
 		}
 		else
 		{
 			lab[i]=1.0;
 
-			for (int32_t j=0; j<DIMS; j++)
-				feat.matrix[i*DIMS+j]=CMath::random(0.0,1.0)-DIST;
+			for (int32_t j=0; j<dims; j++)
+				feat(j, i)=CMath::random(0.0, 1.0)-dist;
 		}
 	}
-	SGVector<float64_t>::display_vector(lab,NUM);
-	SGMatrix<float64_t>::display_matrix(feat.matrix,DIMS, NUM);
+	lab.display_vector("lab");
+	feat.display_matrix("feat");
 }
 
-int main()
+void test_libsvm()
 {
-
 	const int32_t feature_cache=0;
 	const int32_t kernel_cache=0;
 	const float64_t rbf_width=10;
 	const float64_t svm_C=10;
 	const float64_t svm_eps=0.001;
 
-	init_shogun();
+	index_t num=100;
+	index_t dims=2;
+	float64_t dist=0.5;
 
-	gen_rand_data();
+	SGVector<float64_t> lab(num);
+	SGMatrix<float64_t> feat(dims, num);
+
+	gen_rand_data(lab, feat, dist);
 
 	// create train labels
-	CLabels* labels=new CBinaryLabels(SGVector<float64_t>(lab, NUM));
+	CLabels* labels=new CBinaryLabels(lab);
 
 	// create train features
-	CDenseFeatures<float64_t>* features = new CDenseFeatures<float64_t>(feature_cache);
+	CDenseFeatures<float64_t>* features=new CDenseFeatures<float64_t>(
+			feature_cache);
 	SG_REF(features);
 	features->set_feature_matrix(feat);
 
 	// create gaussian kernel
-	CGaussianKernel* kernel = new CGaussianKernel(kernel_cache, rbf_width);
+	CGaussianKernel* kernel=new CGaussianKernel(kernel_cache, rbf_width);
 	SG_REF(kernel);
 	kernel->init(features, features);
 
 	// create svm via libsvm and train
-	CLibSVM* svm = new CLibSVM(svm_C, kernel, labels);
+	CLibSVM* svm=new CLibSVM(svm_C, kernel, labels);
 	SG_REF(svm);
 	svm->set_epsilon(svm_eps);
 	svm->train();
 
-	printf("num_sv:%d b:%f\n", svm->get_num_support_vectors(), svm->get_bias());
+	SG_SPRINT("num_sv:%d b:%f\n", svm->get_num_support_vectors(),
+			svm->get_bias());
 
 	// classify + display output
 	CBinaryLabels* out_labels=CBinaryLabels::obtain_from_generic(svm->apply());
 
-	for (int32_t i=0; i<NUM; i++)
-		printf("out[%d]=%f (%f)\n", i, out_labels->get_label(i), out_labels->get_confidence(i));
+	for (int32_t i=0; i<num; i++)
+	{
+		SG_SPRINT("out[%d]=%f (%f)\n", i, out_labels->get_label(i),
+				out_labels->get_confidence(i));
+	}
 
 	SG_UNREF(out_labels);
 	SG_UNREF(kernel);
 	SG_UNREF(features);
 	SG_UNREF(svm);
+}
+
+int main()
+{
+	init_shogun();
+
+	test_libsvm();
 
 	exit_shogun();
 	return 0;
 }
+
