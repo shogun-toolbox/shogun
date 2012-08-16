d3f9d7525c250adeb6d45a9be889a29cd2116962
Evgeniy Andreev <gsomix@gmail.com>
2012-08-16 16:53:26 +0400

    added design of zero-copy methods

--- examples/undocumented/python_modular/features_dense_protocols_modular.py
+++ examples/undocumented/python_modular/features_dense_protocols_modular.py
@@ -9,7 +9,7 @@ data=[[1,2,3],[4,5,6],[7,8,9],[-1,-2,-3]]
 
 parameter_list = [[data]]
 
-def features_dense_real_modular(in_data=data):
+def features_dense_protocols_modular(in_data=data):
 	m_real=array(in_data, dtype=float64, order='F')
 	f_real=RealFeatures(m_real)
 
@@ -78,5 +78,5 @@ def features_dense_real_modular(in_data=data):
 	return f_real[:,0]
 
 if __name__=='__main__':
-	print('dense_real')
-	features_dense_real_modular(*parameter_list[0])
+	print('dense_protocols')
+	features_dense_protocols_modular(*parameter_list[0])
