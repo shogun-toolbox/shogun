479d86a8da8b93e7042c5d77547a92309bc484c3
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2012-08-26 02:58:53 +0400

    Added confidence matrix computation abilities to CV MC storage and made everything optional

--- src/shogun/evaluation/CrossValidationMulticlassStorage.cpp
+++ src/shogun/evaluation/CrossValidationMulticlassStorage.cpp
@@ -10,22 +10,44 @@
 #include <shogun/evaluation/CrossValidationMulticlassStorage.h>
 #include <shogun/evaluation/ROCEvaluation.h>
 #include <shogun/evaluation/PRCEvaluation.h>
+#include <shogun/evaluation/MulticlassAccuracy.h>
 
 using namespace shogun;
 
 void CCrossValidationMulticlassStorage::post_init()
 {
-	SG_DEBUG("Allocating %d ROC graphs\n", m_num_folds*m_num_runs*m_num_classes);
-	m_fold_ROC_graphs = SG_MALLOC(SGMatrix<float64_t>, m_num_folds*m_num_runs*m_num_classes);
-	for (int32_t i=0; i<m_num_folds*m_num_runs*m_num_classes; i++)
-		new (&m_fold_ROC_graphs[i]) SGMatrix<float64_t>();
-	
-	SG_DEBUG("Allocating %d PRC graphs\n", m_num_folds*m_num_runs*m_num_classes);
-	m_fold_PRC_graphs = SG_MALLOC(SGMatrix<float64_t>, m_num_folds*m_num_runs*m_num_classes);
-	for (int32_t i=0; i<m_num_folds*m_num_runs*m_num_classes; i++)
-		new (&m_fold_PRC_graphs[i]) SGMatrix<float64_t>();
+	if (m_initialized)
+		SG_ERROR("CrossValidationMulticlassStorage was already initialized once\n");
 
-	m_evaluations_results = SGVector<float64_t>(m_num_folds*m_num_runs*m_num_classes*m_binary_evaluations->get_num_elements());
+	if (m_compute_ROC)
+	{
+		SG_DEBUG("Allocating %d ROC graphs\n", m_num_folds*m_num_runs*m_num_classes);
+		m_fold_ROC_graphs = SG_MALLOC(SGMatrix<float64_t>, m_num_folds*m_num_runs*m_num_classes);
+		for (int32_t i=0; i<m_num_folds*m_num_runs*m_num_classes; i++)
+			new (&m_fold_ROC_graphs[i]) SGMatrix<float64_t>();
+	}
+
+	if (m_compute_PRC)
+	{
+		SG_DEBUG("Allocating %d PRC graphs\n", m_num_folds*m_num_runs*m_num_classes);
+		m_fold_PRC_graphs = SG_MALLOC(SGMatrix<float64_t>, m_num_folds*m_num_runs*m_num_classes);
+		for (int32_t i=0; i<m_num_folds*m_num_runs*m_num_classes; i++)
+			new (&m_fold_PRC_graphs[i]) SGMatrix<float64_t>();
+	}
+
+	if (m_binary_evaluations->get_num_elements())
+		m_evaluations_results = SGVector<float64_t>(m_num_folds*m_num_runs*m_num_classes*m_binary_evaluations->get_num_elements());
+
+	m_accuracies = SGVector<float64_t>(m_num_folds*m_num_runs);
+
+	if (m_compute_conf_matrices)
+	{
+		m_conf_matrices = SG_MALLOC(SGMatrix<int32_t>, m_num_folds*m_num_runs);
+		for (int32_t i=0; i<m_num_folds*m_num_runs; i++)
+			new (&m_conf_matrices[i]) SGMatrix<int32_t>();
+	}
+
+	m_initialized = true;
 }
 
 void CCrossValidationMulticlassStorage::init_expose_labels(CLabels* labels)
@@ -44,12 +66,18 @@ void CCrossValidationMulticlassStorage::post_update_results()
 		SG_DEBUG("Computing ROC for run %d fold %d class %d", m_current_run_index, m_current_fold_index, c);
 		CBinaryLabels* pred_labels_binary = m_pred_labels->get_binary_for_class(c);
 		CBinaryLabels* true_labels_binary = m_true_labels->get_binary_for_class(c);
-		eval_ROC.evaluate(pred_labels_binary, true_labels_binary);
-		m_fold_ROC_graphs[m_current_run_index*m_num_folds*m_num_classes+m_current_fold_index*m_num_classes+c] = 
-			eval_ROC.get_ROC();
-		eval_PRC.evaluate(pred_labels_binary, true_labels_binary);
-		m_fold_PRC_graphs[m_current_run_index*m_num_folds*m_num_classes+m_current_fold_index*m_num_classes+c] = 
-			eval_PRC.get_PRC();
+		if (m_compute_ROC)
+		{
+			eval_ROC.evaluate(pred_labels_binary, true_labels_binary);
+			m_fold_ROC_graphs[m_current_run_index*m_num_folds*m_num_classes+m_current_fold_index*m_num_classes+c] = 
+				eval_ROC.get_ROC();
+		}
+		if (m_compute_PRC)
+		{
+			eval_PRC.evaluate(pred_labels_binary, true_labels_binary);
+			m_fold_PRC_graphs[m_current_run_index*m_num_folds*m_num_classes+m_current_fold_index*m_num_classes+c] = 
+				eval_PRC.get_PRC();
+		}
 		
 		for (int32_t i=0; i<n_evals; i++)
 		{
@@ -61,6 +89,14 @@ void CCrossValidationMulticlassStorage::post_update_results()
 
 		SG_UNREF(pred_labels_binary);
 		SG_UNREF(true_labels_binary);
+	}
+	CMulticlassAccuracy accuracy;
+
+	m_accuracies[m_current_run_index*m_num_folds+m_current_fold_index] = accuracy.evaluate(m_pred_labels, m_true_labels);
+	
+	if (m_compute_conf_matrices)
+	{
+		m_conf_matrices[m_current_run_index*m_num_folds+m_current_fold_index] = CMulticlassAccuracy::get_confusion_matrix(m_pred_labels, m_true_labels);
 	}
 }
 
