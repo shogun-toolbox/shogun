47ed83f930cb437c0356154a911fd9763ee7b661
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2012-09-02 11:17:46 +0400

    Fixed position of eigen3 guard in student t likelihood class

--- src/shogun/regression/gp/StudentsTLikelihood.cpp
+++ src/shogun/regression/gp/StudentsTLikelihood.cpp
@@ -11,9 +11,9 @@
  *
  */
 
-#ifdef HAVE_EIGEN3
 
 #include <shogun/regression/gp/StudentsTLikelihood.h>
+#ifdef HAVE_EIGEN3
 #include <shogun/modelselection/ParameterCombination.h>
 #include <shogun/mathematics/Statistics.h>
 #include <shogun/base/Parameter.h>
