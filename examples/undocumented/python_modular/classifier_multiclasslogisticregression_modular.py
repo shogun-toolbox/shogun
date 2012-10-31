276a2ff8a85cadb935f5740603d45b19ef2d116d
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2012-11-01 01:57:28 +0400

    Made multiclass logistic regression python example safe when shogun is compiled without eigen3

--- examples/undocumented/python_modular/classifier_multiclasslogisticregression_modular.py
+++ examples/undocumented/python_modular/classifier_multiclasslogisticregression_modular.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 from tools.multiclass_shared import prepare_data
 
+try:
+	from shogun.Classifier import MulticlassLogisticRegression
+except ImportError:
+	print "recompile shogun with Eigen3 support"
+	import sys
+	sys.exit(0)
+
+
 [traindat, label_traindat, testdat, label_testdat] = prepare_data(False)
 
 parameter_list = [[traindat,testdat,label_traindat,label_testdat,2.1,1e-5],[traindat,testdat,label_traindat,label_testdat,2.2,1e-5]]
 
 def classifier_multiclasslogisticregression_modular (fm_train_real=traindat,fm_test_real=testdat,label_train_multiclass=label_traindat,label_test_multiclass=label_testdat,z=1,epsilon=1e-5):
 	from shogun.Features import RealFeatures, MulticlassLabels
-	from shogun.Classifier import MulticlassLogisticRegression
 
 	feats_train=RealFeatures(fm_train_real)
 	feats_test=RealFeatures(fm_test_real)
