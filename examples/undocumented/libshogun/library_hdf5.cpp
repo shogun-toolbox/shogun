1a41f826a1b8a13f9e4aa86d7e11d9202dc851dd
Soeren Sonnenburg <sonne@debian.org>
2012-11-28 20:56:55 +0100

    test for HDF5 in example

--- examples/undocumented/libshogun/library_hdf5.cpp
+++ examples/undocumented/libshogun/library_hdf5.cpp
@@ -10,6 +10,7 @@ using namespace shogun;
 int main(int argc, char** argv)
 {
 	init_shogun_with_defaults();
+#ifdef HAVE_HDF5
 	CHDF5File* hdf = new CHDF5File((char*) "../data/australian.libsvm.h5",'r', "/data/data");
 	float64_t* mat;
 	int32_t num_feat;
@@ -17,6 +18,7 @@ int main(int argc, char** argv)
 	hdf->get_matrix(mat, num_feat, num_vec);
 
 	SGMatrix<float64_t>::display_matrix(mat, num_feat, num_vec);
+#endif
 
 	exit_shogun();
 	return 0;
