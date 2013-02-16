e667b9e60e4a1619d867a51e28f66290043d9d5f
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2013-02-17 03:31:50 +0400

    More safety for parameter dictionary routine. Related to #880

--- src/shogun/base/SGObject.cpp
+++ src/shogun/base/SGObject.cpp
@@ -1210,7 +1210,7 @@ void CSGObject::build_parameter_dictionary(CMap<TParameter*, CSGObject*>& dict)
 
 			dict.add(p, this);
 
-			if (p->m_datatype.m_ptype == PT_SGOBJECT)
+			if ((p->m_datatype.m_ptype == PT_SGOBJECT) && (p->m_datatype.m_ctype == CT_SCALAR) && (*(CSGObject**)(p->m_parameter) != NULL))
 			{
 				CSGObject* child =
 						*((CSGObject**)(p->m_parameter));
