22d91cec0b6b7c125e00c3254f89b23445951797
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-02-28 19:26:36 +0000

    added a unit test that checks whether bootstrapping statistics are the same for a custom kernel and a normal one

--- tests/unit/statistics/QuadraticTimeMMD_unittest.cc
+++ tests/unit/statistics/QuadraticTimeMMD_unittest.cc
@@ -14,6 +14,7 @@
 #include <shogun/kernel/CustomKernel.h>
 #include <shogun/features/DenseFeatures.h>
 #include <shogun/features/streaming/StreamingDenseFeatures.h>
+#include <shogun/features/streaming/generators/MeanShiftDataGenerator.h>
 #include <shogun/mathematics/Statistics.h>
 #include <gtest/gtest.h>
 
@@ -191,4 +192,131 @@ TEST(QuadraticTimeMMD,test_quadratic_mmd_precomputed_kernel)
 	SG_UNREF(features_p);
 	SG_UNREF(features_q);
 	SG_UNREF(p_and_q);
+}
+
+TEST(QuadraticTimeMMD,custom_kernel_vs_normal_kernel)
+{
+	/* number of examples kept low in order to make things fast */
+	index_t m=20;
+	index_t dim=2;
+	float64_t difference=0.5;
+
+	/* streaming data generator for mean shift distributions */
+	CMeanShiftDataGenerator* gen_p=new CMeanShiftDataGenerator(0, dim);
+	CMeanShiftDataGenerator* gen_q=new CMeanShiftDataGenerator(difference, dim);
+
+	/* stream some data from generator */
+	CFeatures* feat_p=gen_p->get_streamed_features(m);
+	CFeatures* feat_q=gen_q->get_streamed_features(m);
+
+	/* set kernel a-priori. usually one would do some kernel selection. See
+	 * other examples for this. */
+	float64_t width=10;
+	CGaussianKernel* kernel=new CGaussianKernel(10, width);
+
+	/* create quadratic time mmd instance. Note that this constructor
+	 * copies p and q and does not reference them */
+	CQuadraticTimeMMD* mmd=new CQuadraticTimeMMD(kernel, feat_p, feat_q);
+
+	/* perform test: compute p-value and test if null-hypothesis is rejected for
+	 * a test level of 0.05 */
+	float64_t alpha=0.05;
+
+	mmd->set_null_approximation_method(BOOTSTRAP);
+	mmd->set_bootstrap_iterations(3);
+
+	/* compute tpye I and II error using normal and precomputed kernel */
+	index_t num_trials=3;
+	SGVector<float64_t> type_I_errors(num_trials);
+	SGVector<float64_t> type_II_errors(num_trials);
+	SGVector<float64_t> type_I_mmds(num_trials);
+	SGVector<float64_t> type_I_threshs(num_trials);
+	SGVector<float64_t> type_II_mmds(num_trials);
+	SGVector<float64_t> type_II_threshs(num_trials);
+
+	SGVector<index_t> inds(2*m);
+	inds.range_fill();
+	CFeatures* p_and_q=mmd->get_p_and_q();
+
+	/* use fixed seed */
+	CMath::init_random(1);
+	for (index_t i=0; i<num_trials; ++i)
+	{
+		/* this effectively means that p=q - rejecting is tpye I error */
+		inds.permute();
+		p_and_q->add_subset(inds);
+		type_I_mmds[i]=mmd->compute_statistic();
+		type_I_threshs[i]=mmd->compute_threshold(alpha);
+		type_I_errors[i]=type_I_mmds[i]<type_I_threshs[i];
+		p_and_q->remove_subset();
+
+		type_II_mmds[i]=mmd->compute_statistic();
+		type_II_threshs[i]=mmd->compute_threshold(alpha);
+		type_II_errors[i]=type_II_mmds[i]<type_II_threshs[i];
+	}
+	SG_UNREF(p_and_q);
+
+	//SG_SPRINT("normal kernel\n");
+	//type_I_errors.display_vector("type_I_errors");
+	//type_II_errors.display_vector("type_II_errors");
+	//type_I_mmds.display_vector("type_I_mmds");
+	//type_I_threshs.display_vector("type_I_threshs");
+	//type_II_mmds.display_vector("type_II_mmds");
+	//type_II_threshs.display_vector("type_II_threshs");
+
+	/* same thing with precomputed kernel */
+	SGVector<float64_t> type_I_errors_pre(num_trials);
+	SGVector<float64_t> type_II_errors_pre(num_trials);
+	SGVector<float64_t> type_I_mmds_pre(num_trials);
+	SGVector<float64_t> type_I_threshs_pre(num_trials);
+	SGVector<float64_t> type_II_mmds_pre(num_trials);
+	SGVector<float64_t> type_II_threshs_pre(num_trials);
+	kernel->init(p_and_q, p_and_q);
+	CCustomKernel* precomputed=new CCustomKernel(kernel);
+	mmd->set_kernel(precomputed);
+	inds.range_fill();
+	CMath::init_random(1);
+	for (index_t i=0; i<num_trials; ++i)
+	{
+		/* this effectively means that p=q - rejecting is tpye I error */
+		inds.permute();
+		precomputed->add_row_subset(inds);
+		precomputed->add_col_subset(inds);
+		type_I_mmds_pre[i]=mmd->compute_statistic();
+		type_I_threshs_pre[i]=mmd->compute_threshold(alpha);
+		type_I_errors_pre[i]=type_I_mmds_pre[i]<type_I_threshs_pre[i];
+		precomputed->remove_row_subset();
+		precomputed->remove_col_subset();
+
+		type_II_mmds_pre[i]=mmd->compute_statistic();
+		type_II_threshs_pre[i]=mmd->compute_threshold(alpha);
+		type_II_errors_pre[i]=type_II_mmds_pre[i]<type_II_threshs_pre[i];
+	}
+
+	//SG_SPRINT("precomputed kernel\n");
+	//type_I_errors_pre.display_vector("type_I_errors");
+	//type_II_errors_pre.display_vector("type_II_errors");
+	//type_I_mmds_pre.display_vector("type_I_mmds");
+	//type_I_threshs_pre.display_vector("type_I_threshs");
+	//type_II_mmds_pre.display_vector("type_II_mmds");
+	//type_II_threshs_pre.display_vector("type_II_threshs");
+
+	for (index_t i=0; i<num_trials; ++i)
+	{
+		ASSERT(CMath::abs(type_I_errors[i]-type_I_errors_pre[i])<10E-15);
+		ASSERT(CMath::abs(type_II_errors_pre[i]-type_II_errors_pre[i])<10E-15);
+		ASSERT(CMath::abs(type_I_mmds_pre[i]-type_I_mmds_pre[i])<10E-15);
+		ASSERT(CMath::abs(type_I_threshs_pre[i]-type_I_threshs_pre[i])<10E-15);
+		ASSERT(CMath::abs(type_II_mmds_pre[i]-type_II_mmds_pre[i])<10E-15);
+		ASSERT(CMath::abs(type_II_threshs_pre[i]-type_II_threshs_pre[i])<10E-15);
+	}
+
+	/* clean up */
+	SG_UNREF(mmd);
+	SG_UNREF(gen_p);
+	SG_UNREF(gen_q);
+
+	/* convienience constructor of MMD was used, these were not referenced */
+	SG_UNREF(feat_p);
+	SG_UNREF(feat_q);
 }
