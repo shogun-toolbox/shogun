b6c25aa6ddcc3dd07f1d94f2970fb331bc9a8702
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2013-03-12 19:36:11 +0400

    An attempt to get MDS example make more sense

--- examples/undocumented/python_modular/converter_multidimensionalscaling_modular.py
+++ examples/undocumented/python_modular/converter_multidimensionalscaling_modular.py
@@ -21,7 +21,7 @@ def converter_multidimensionalscaling_modular (data):
 		converter = MultidimensionalScaling()
 		converter.set_target_dim(2)
 		converter.set_landmark(False)
-		embedding =converter.apply(features)
+		embedding = converter.apply(features)
 
 		distance_after = EuclideanDistance()
 		distance_after.init(embedding,embedding)
@@ -29,7 +29,7 @@ def converter_multidimensionalscaling_modular (data):
 		distance_matrix_after = distance_after.get_distance_matrix()
 		distance_matrix_before = distance_before.get_distance_matrix()
 
-		return numpy.linalg.norm(distance_matrix_after-distance_matrix_before)/numpy.linalg.norm(distance_matrix_before)
+		return numpy.linalg.norm(distance_matrix_after-distance_matrix_before)/numpy.linalg.norm(distance_matrix_before) < 1e-6
 	except ImportError:
 		print('No Eigen3 available')
 
