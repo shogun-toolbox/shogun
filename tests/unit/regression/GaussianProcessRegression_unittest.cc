68783f6c203314e7fe8a9d2ac0bdbb0670628cb2
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-03-13 15:44:55 +0000

    added a unit test for larger test than training data

--- tests/unit/regression/GaussianProcessRegression_unittest.cc
+++ tests/unit/regression/GaussianProcessRegression_unittest.cc
@@ -68,6 +68,74 @@ TEST(GaussianProcessRegression,apply_regression)
 	SG_UNREF(gpr);
 }
 
+TEST(GaussianProcessRegression,apply_regression_larger_test)
+{
+	/* create some easy regression data: 1d noisy sine wave */
+	index_t n=3;
+	index_t n_test=5;
+
+	SGMatrix<float64_t> X(1, n);
+	SGMatrix<float64_t> X_test(1, n_test);
+	SGVector<float64_t> Y(n);
+
+	X[0]=0;
+	X[1]=1.1;
+	X[2]=2.2;
+
+	X_test[0]=0.3;
+	X_test[1]=1.3;
+	X_test[2]=2.5;
+	X_test[2]=2.7;
+	X_test[2]=3.1;
+
+
+	for (index_t i=0; i<n; ++i)
+	{
+		Y[i]=CMath::sin(X(0, i));
+	}
+
+	/* shogun representation */
+	CDenseFeatures<float64_t>* feat_train=new CDenseFeatures<float64_t>(X);
+	CDenseFeatures<float64_t>* feat_test=new CDenseFeatures<float64_t>(X_test);
+	CRegressionLabels* label_train=new CRegressionLabels(Y);
+
+	/* specity GPR with exact inference */
+	float64_t sigma=1;
+	float64_t shogun_sigma=sigma*sigma*2;
+	CGaussianKernel* kernel=new CGaussianKernel(10, shogun_sigma);
+	CZeroMean* mean=new CZeroMean();
+	CGaussianLikelihood* lik=new CGaussianLikelihood();
+	lik->set_sigma(1);
+	CExactInferenceMethod* inf=new CExactInferenceMethod(kernel, feat_train,
+			mean, label_train, lik);
+	CGaussianProcessRegression* gpr=new CGaussianProcessRegression(inf,
+			feat_train, label_train);
+
+	/* perform inference */
+	gpr->set_return_type(CGaussianProcessRegression::GP_RETURN_MEANS);
+	CRegressionLabels* predictions=gpr->apply_regression(feat_test);
+
+	/* do some checks against gpml toolbox*/
+
+	/* m =
+
+   0.221198406887592
+   0.537437461176145
+   0.431605035301329
+   0.373048041692408
+   0.253688340068952 */
+	SGVector<float64_t> prediction_vector=predictions->get_labels();
+//	EXPECT_LE(CMath::abs(prediction_vector[0]-0.221198406887592), 10E-15);
+//	EXPECT_LE(CMath::abs(prediction_vector[1]-0.537437461176145), 10E-15);
+//	EXPECT_LE(CMath::abs(prediction_vector[2]-0.431605035301329), 10E-15);
+//	EXPECT_LE(CMath::abs(prediction_vector[2]-0.373048041692408), 10E-15);
+//	EXPECT_LE(CMath::abs(prediction_vector[2]-0.253688340068952), 10E-15);
+
+
+	SG_UNREF(predictions);
+	SG_UNREF(gpr);
+}
+
 TEST(GaussianProcessRegression, get_mean_vector)
 {
 	/* create some easy regression data: 1d noisy sine wave */
