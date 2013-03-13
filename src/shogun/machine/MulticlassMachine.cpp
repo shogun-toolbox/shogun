c80ac5e045d35c0d37d857e7b3dcec5a926cd7e2
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2013-03-14 01:20:38 +0400

    Fixed MC machine confidences allocation

--- src/shogun/machine/MulticlassMachine.cpp
+++ src/shogun/machine/MulticlassMachine.cpp
@@ -112,7 +112,7 @@ CMulticlassLabels* CMulticlassMachine::apply_multiclass(CFeatures* data)
 			SG_ERROR("num_machines = %d, did you train your machine?", num_machines)
 
 		CMulticlassLabels* result=new CMulticlassLabels(num_vectors);
-		result->allocate_confidences_for(m_multiclass_strategy->get_num_classes());
+		result->allocate_confidences_for(num_machines);
 
 		CBinaryLabels** outputs=SG_MALLOC(CBinaryLabels*, num_machines);
 
