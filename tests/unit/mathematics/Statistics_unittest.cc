892ffdb9fec0d23b077a9acbde7ac39905f33ff3
lambday <heavensdevil6909@gmail.com>
2013-03-29 20:34:17 +0530

    Modified the unit test for log_det

--- tests/unit/mathematics/Statistics_unittest.cc
+++ tests/unit/mathematics/Statistics_unittest.cc
@@ -10,33 +10,34 @@
 using namespace shogun;
 using namespace Eigen;
 
-TEST(Statistics,log_det)
+// TEST 1
+TEST(Statistics, log_det_test_1)
 {
-	/* TEST 1 */
-	/* create a small test matrix, positive definite, symmetric */
+	// create a small test matrix, symmetric positive definite
 	index_t size = 3;
 	SGMatrix<float64_t> m(size, size);
 	
-	/* initialize the matrix */
+	// initialize the matrix
 	m(0, 0) =   4; m(0, 1) =  12; m(0, 2) = -16;
 	m(1, 0) =  12; m(1, 1) =  37; m(1, 2) = -43;
 	m(2, 0) = -16; m(2, 1) = -43; m(2, 2) =  98;
 	
 	/* the cholesky decomposition gives m = L.L', where
 	 * L = [(2, 0, 0), (6, 1, 0), (-8, 5, 3)].
-	 * 2 * (log(2) + log(1) + log(3)) = 3.58352
+	 * 2 * (log(2) + log(1) + log(3)) = 3.58351893846
 	 */
-	EXPECT_FLOAT_EQ(CStatistics::log_det(m), 3.58352);
+	Map<MatrixXd> M(m.matrix, m.num_rows, m.num_cols);
+	EXPECT_NEAR(CStatistics::log_det(m), log(M.determinant()), 1E-10);
 
-	// TEST 2
-	// create random positive definite symmetric matrix
-	size = 100;
-	MatrixXd A(size, size);	// random matrix
-        MatrixXd M(size, size);
+}
 
-	// to make all the values between [0, 1] (is it really needed?)
-	A = MatrixXd::Random(size,size) * 0.5 + MatrixXd::Constant(size,size,0.5);
-	M = A * A.transpose();	// positive definite matrix
+// TEST 2
+TEST(Statistics, log_det_test_2)
+{
+	// create a fixed symmetric positive definite matrix
+	index_t size = 100;
+	VectorXd A = VectorXd::LinSpaced(size, 1, size);
+	MatrixXd M = A * A.transpose() + MatrixXd::Identity(size, size);
 
 	// copy the matrix to a SGMatrix to pass it to log_det
 	SGMatrix<float64_t> K(size,size);
@@ -47,7 +48,7 @@ TEST(Statistics,log_det)
 	}
 	
 	// check if log_det is equal to log(det(M))
-	EXPECT_FLOAT_EQ(CStatistics::log_det(K), log(M.determinant()));
+	EXPECT_NEAR(CStatistics::log_det(K), 12.731839097176634, 1E-10);
 
 }
 
