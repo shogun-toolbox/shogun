f8dba512516a336baf9b1369949443b8b4cea3d6
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2013-04-06 22:50:07 +0400

    Fixed minimal version of Eigen3 that supports sparse matrices

--- src/shogun/mathematics/eigen3.h
+++ src/shogun/mathematics/eigen3.h
@@ -16,7 +16,7 @@
 	//#define EIGEN_RUNTIME_NO_MALLOC
 	#include <Eigen/Eigen>
 	#include <Eigen/Dense>
-	#if EIGEN_VERSION_AT_LEAST(3,1,0)
+	#if EIGEN_VERSION_AT_LEAST(3,0,93)
 		#include <Eigen/Sparse>
 	#else
 		#define EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
