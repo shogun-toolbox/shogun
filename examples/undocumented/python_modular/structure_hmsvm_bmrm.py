10d45851cfd2926035a506c700987373535822ed
iglesias <fernando.iglesiasg@gmail.com>
2013-04-15 01:22:19 +0200

    Rename HMSVMLabels to SequenceLabels. Because these labels can be used in other StructuredModels apart from the HMSVMModel.

--- examples/undocumented/python_modular/structure_hmsvm_bmrm.py
+++ examples/undocumented/python_modular/structure_hmsvm_bmrm.py
@@ -11,7 +11,7 @@ parameter_list=[[data_dict]]
 def structure_hmsvm_bmrm (m_data_dict=data_dict):
 	from shogun.Features   import RealMatrixFeatures
 	from shogun.Loss       import HingeLoss
-	from shogun.Structure  import HMSVMLabels, HMSVMModel, Sequence, TwoStateModel, SMT_TWO_STATE
+	from shogun.Structure  import SequenceLabels, HMSVMModel, Sequence, TwoStateModel, SMT_TWO_STATE
 	from shogun.Evaluation import StructuredAccuracy
 	from shogun.Structure  import DualLibQPBMSOSVM
 
@@ -20,7 +20,7 @@ def structure_hmsvm_bmrm (m_data_dict=data_dict):
 	idxs = numpy.nonzero(labels_array == -1)
 	labels_array[idxs] = 0
 
-	labels = HMSVMLabels(labels_array, 250, 500, 2)
+	labels = SequenceLabels(labels_array, 250, 500, 2)
 	features = RealMatrixFeatures(m_data_dict['signal'].astype(float), 250, 500)
 
 	loss = HingeLoss()
