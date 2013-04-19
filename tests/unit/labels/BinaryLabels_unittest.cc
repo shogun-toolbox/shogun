7f31c48370ed1781aabea83e951c3069206051e7
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-04-19 13:31:14 +0100

    added unit test for sigmoid fitting

---
+++ tests/unit/labels/BinaryLabels_unittest.cc
@@ -0,0 +1,33 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2012-2013 Heiko Strathmann
+ */
+
+#include <shogun/base/init.h>
+#include <shogun/labels/BinaryLabels.h>
+#include <gtest/gtest.h>
+
+using namespace shogun;
+
+TEST(BinaryLabels,scores_to_probabilities)
+{
+	CBinaryLabels* labels=new CBinaryLabels(10);
+	labels->set_values(SGVector<float64_t>(labels->get_num_labels()));
+
+	for (index_t i=0; i<labels->get_num_labels(); ++i)
+		labels->set_value(i%2==0 ? 1 : -1, i);
+
+	labels->get_values().display_vector("scores");
+	labels->scores_to_probabilities();
+
+	/* only two probabilities will be the result. Results from implementation that
+	 * comes with the original paper, see BinaryLabels documentation */
+	EXPECT_NEAR(labels->get_value(0), 0.8571428439385661, 10E-15);
+	EXPECT_NEAR(labels->get_value(1), 0.14285715606143384, 10E-15);
+
+	SG_UNREF(labels);
+}
