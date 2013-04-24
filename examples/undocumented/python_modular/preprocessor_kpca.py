97449d74d6db3b474507fe64119f23691109fa6d
Deepak <deerishi@gmail.com>
2013-04-25 00:08:04 +0530

    Update preprocessor_kpca.py

--- examples/undocumented/python_modular/preprocessor_kpca.py
+++ examples/undocumented/python_modular/preprocessor_kpca.py
@@ -7,11 +7,8 @@ row_vector=2
 data=cir.generate_data(number_of_points_for_circle1,number_of_points_for_circle2,row_vector)
 d=zeros((row_vector,number_of_points_for_circle1))
 d2=zeros((row_vector,number_of_points_for_circle2)) 
-for i in range(0,row_vector):
-  d[i][:]=data[i][0:number_of_points_for_circle1]
-	d2[i][:]=data[i][number_of_points_for_circle1:(number_of_points_for_circle1+number_of_points_for_circle2)]	
-
-
+d=[data[i][0:number_of_points_for_circle1] for i in range(0,row_vector)]
+d2=[data[i][number_of_points_for_circle1:(number_of_points_for_circle1+number_of_points_for_circle2)] for i in range(0,row_vector)]	
 parameter_list = [[data,0.01,1.0], [data,0.05,2.0]]
 def preprocessor_kernelpca_modular (data, threshold, width):
 	
@@ -28,9 +25,8 @@ def preprocessor_kernelpca_modular (data, threshold, width):
 	lx0=len(X2)
 	modified_d1=zeros((lx0,number_of_points_for_circle1))
 	modified_d2=zeros((lx0,number_of_points_for_circle2))
-	for i in range(lx0):
-		modified_d1[i][:]=X2[i][0:number_of_points_for_circle1]
-		modified_d2[i][:]=X2[i][number_of_points_for_circle1:(number_of_points_for_circle1+number_of_points_for_circle2)]	
+	modified_d1=[X2[i][0:number_of_points_for_circle1] for i in range(lx0)]
+	modified_d2=[X2[i][number_of_points_for_circle1:(number_of_points_for_circle1+number_of_points_for_circle2)] for i in range(lx0)]
 	return features
 
 if __name__=='__main__':
