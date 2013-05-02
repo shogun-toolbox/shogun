31da88aac065398f6aaf0ad0a0ff897fbb72c151
Viktor Gal <viktor.gal@maeth.com>
2013-05-02 15:51:11 +0200

    Create class for PRNG wrapper class for SFMT

---
+++ src/shogun/mathematics/Random.cpp
@@ -0,0 +1,126 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2013 Viktor Gal
+ * Copyright (C) 2013 Viktor Gal
+ */
+
+#include <shogun/mathematics/Random.h>
+
+using namespace shogun;
+
+CRandom::CRandom()
+ : m_seed(12345)
+{
+	init();
+	register_params();
+}
+
+CRandom::CRandom(uint32_t seed)
+ : m_seed(seed)
+{
+	init();
+	register_params();
+}
+
+CRandom::~CRandom()
+{
+	PTHREAD_LOCK_DESTROY(&m_state_lock);
+	SG_FREE(m_sfmt);
+}
+
+void CRandom::seed(uint32_t seed)
+{
+	m_seed = seed;
+	reinit(PRNG_32);
+}
+
+void CRandom::init()
+{
+	m_sfmt = SG_MALLOC(sfmt_t, 1);
+	PTHREAD_LOCK_INIT(&m_state_lock);
+	reinit(PRNG_32);
+}
+
+uint32_t CRandom::random_32()
+{
+	if (m_state != PRNG_32)
+		reinit(PRNG_32);
+
+	return sfmt_genrand_uint32(m_sfmt);
+}
+
+uint64_t CRandom::random_64()
+{
+	if (m_state != PRNG_64)
+		reinit(PRNG_64);
+
+	return sfmt_genrand_uint64(m_sfmt);
+}
+
+void CRandom::fill_array_32(uint32_t* array, int size)
+{
+	if (m_state != PRNG_32)
+		reinit(PRNG_32);
+
+	sfmt_fill_array32(m_sfmt, array, size);
+}
+
+void CRandom::fill_array_64(uint64_t* array, int size)
+{
+	if (m_state != PRNG_64)
+		reinit(PRNG_64);
+
+	sfmt_fill_array64(m_sfmt, array, size);
+}
+
+float64_t CRandom::random_close()
+{
+	if (m_state != PRNG_32)
+		reinit(PRNG_32);
+
+	return sfmt_genrand_real1(m_sfmt);
+}
+
+float64_t CRandom::random_open()
+{
+	if (m_state != PRNG_32)
+		reinit(PRNG_32);
+
+	return sfmt_genrand_real3(m_sfmt);
+}
+
+float64_t CRandom::random_half_open()
+{
+	if (m_state != PRNG_32)
+		reinit(PRNG_32);
+
+	return sfmt_genrand_real2(m_sfmt);
+}
+
+float64_t CRandom::random_float_res53()
+{
+	if (m_state != PRNG_64)
+		reinit(PRNG_64);
+
+	return sfmt_genrand_res53(m_sfmt);
+}
+
+void CRandom::reinit(PRNG_STATE state)
+{
+	PTHREAD_LOCK(&m_state_lock);
+	sfmt_init_gen_rand(m_sfmt, m_seed);
+	m_state = state;
+	PTHREAD_UNLOCK(&m_state_lock);
+}
+
+void CRandom::register_params()
+{
+	//SG_ADD(&m_seed, "seed", "Seed for PRNG", MS_NOT_AVAILABLE);
+	//SG_ADD(&m_sfmt, "sfmt", "SFMT struct for PRNG", MS_NOT_AVAILABLE);
+	//SG_ADD(&m_state, "state", "state of the PRNG", MS_NOT_AVAILABLE);
+	//SG_ADD(&m_state_lock, "state_lock", "state lock", MS_NOT_AVAILABLE);
+}
---
+++ src/shogun/mathematics/Random.h
@@ -0,0 +1,94 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2013 Viktor Gal
+ * Copyright (C) 2013 Viktor Gal
+ */
+
+#ifndef __RANDOM_H__
+#define __RANDOM_H__
+
+#include <shogun/base/SGObject.h>
+
+#include <shogun/lib/external/SFMT/SFMT.h>
+
+namespace shogun
+{
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS	
+	enum PRNG_STATE
+	{
+		PRNG_32 = 1,
+		PRNG_64 = 2
+	};
+#endif
+
+	/** @breif: PRNG */
+	class CRandom : public CSGObject
+	{
+		public:
+			/** default ctor */
+			CRandom();
+
+			/** ctor
+			 * @param seed the seed for the PRNG
+			 */
+			CRandom(uint32_t seed);
+
+			/** dtor */
+			virtual ~CRandom();
+
+			/** set seed
+			 *
+			 * @param seed seed for PRNG
+			 */
+			void seed(uint32_t seed);
+
+			uint32_t random_32();
+			
+			uint64_t random_64();
+
+			void fill_array_32(uint32_t* array, int size);
+
+			void fill_array_64(uint64_t* array, int size);
+
+			float64_t random_close();
+			float64_t random_open();
+			float64_t random_half_open();
+			float64_t random_float_res53();
+
+			virtual const char* get_name() const { return "Random"; } 
+
+		private:
+			/** register parameters */
+			void register_params();
+
+			/** initialise the object */
+			void init();
+
+			/** reinit for new state
+			 * 
+			 * @param state PRNG_32 or PRNG_64
+			 */
+			void reinit(PRNG_STATE state);
+
+		private:
+			/** seed */
+			uint32_t m_seed;
+
+			/** SFMT struct */
+			sfmt_t* m_sfmt;
+
+			/** PRNG state */
+			PRNG_STATE m_state;
+
+			/** state lock */
+			PTHREAD_LOCK_T m_state_lock;
+
+	};
+}
+
+#endif /* __RANDOM_H__ */
