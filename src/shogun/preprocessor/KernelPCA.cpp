ae09bc7e73e4b39a585b74cdfca25b6dbef0a4f4
itsuper7 <itsuper7@gmail.com>
2013-05-08 21:23:56 +0800

    indent code to shogun style

--- src/shogun/preprocessor/KernelPCA.cpp
+++ src/shogun/preprocessor/KernelPCA.cpp
@@ -86,22 +86,21 @@ bool CKernelPCA::init(CFeatures* features)
 		float64_t s = SGVector<float64_t>::sum(bias_tmp, n)/n;
 		SGVector<float64_t>::add_scalar(-s, bias_tmp, n);
                 
-                shogun::TAPKEE_PARAMETERS_FOR_SHOGUN parameters;
-                parameters.target_dimension = m_target_dim;
-                parameters.kernel = m_kernel;
-                parameters.method = shogun::SHOGUN_KERNEL_PCA;
-                CDenseFeatures<float64_t>*embedding = tapkee_embed( parameters );
-
-                m_kernel->cleanup();
+        	shogun::TAPKEE_PARAMETERS_FOR_SHOGUN parameters;
+       		parameters.target_dimension = m_target_dim;
+        	parameters.kernel = m_kernel;
+        	parameters.method = shogun::SHOGUN_KERNEL_PCA;
+        	CDenseFeatures<float64_t>*embedding = tapkee_embed( parameters );
+		m_kernel->cleanup();
                
 		m_transformation_matrix = embedding->get_feature_matrix();
                  embedding->free_features();
-                delete embedding;
+		delete embedding;
 		kernel_matrix = SGMatrix<float64_t>();
 		m_bias_vector = SGVector<float64_t>(m_target_dim);
 		SGVector<float64_t>::fill_vector(m_bias_vector.vector, m_bias_vector.vlen, 0.0);
 		cblas_dgemv(CblasColMajor, CblasNoTrans,
-				m_target_dim, n, 1.0, m_transformation_matrix.matrix, m_target_dim, bias_tmp, 1, 0.0, m_bias_vector.vector, 1);
+				m_target_dim, n, 1.0, m_transformation_matrix.matrix, m_target_dim, 					bias_tmp, 1, 0.0, m_bias_vector.vector, 1);
 
 		SG_FREE(bias_tmp);
                 
@@ -114,40 +113,34 @@ bool CKernelPCA::init(CFeatures* features)
 
 void CKernelPCA::fill_projected_data(float64_t *target_array, CFeatures* features)
 {
-       m_kernel->init(features, m_init_features);
-       int32_t i,j,k;
-       int32_t num_vectors = ((CDenseFeatures<float64_t>*) features)->get_num_vectors();
-       int32_t n = m_transformation_matrix.num_cols;
-         
-       for (i=0; i<num_vectors; i++)
-       { 
-         for (j=0; j<m_target_dim; j++)
-           target_array[i*m_target_dim+j] = m_bias_vector.vector[j];
-         for (j=0; j<n; j++)
-         {
-           float64_t kij = m_kernel->kernel(i,j);
-           for(k=0; k<m_target_dim; k++)
-             target_array[i*m_target_dim + k] +=
-                 kij*m_transformation_matrix.matrix[j*m_target_dim + k];
-             
-           
-         }
-       }
-       
-        m_kernel->cleanup();
+	m_kernel->init(features, m_init_features);
+	int32_t i,j,k;
+	int32_t num_vectors = ((CDenseFeatures<float64_t>*) features)->get_num_vectors();
+	int32_t n = m_transformation_matrix.num_cols;
+	for (i=0; i<num_vectors; i++)
+	{ 
+        	for (j=0; j<m_target_dim; j++)
+         		target_array[i*m_target_dim+j] = m_bias_vector.vector[j];
+         	for (j=0; j<n; j++)
+         	{
+         		float64_t kij = m_kernel->kernel(i,j);
+         		for(k=0; k<m_target_dim; k++)
+        			target_array[i*m_target_dim + k] +=
+                                    kij*m_transformation_matrix.matrix[j*m_target_dim + k];           
+         	}
+       }       
+       m_kernel->cleanup();
       
 }
 
 SGMatrix<float64_t> CKernelPCA::apply_to_feature_matrix(CFeatures* features)
 {
 	ASSERT(m_initialized)
-        CDenseFeatures<float64_t>* simple_features = (CDenseFeatures<float64_t>*)features;
-        int32_t num_vectors = simple_features->get_num_vectors();
+	CDenseFeatures<float64_t>* simple_features = (CDenseFeatures<float64_t>*)features;
+	int32_t num_vectors = simple_features->get_num_vectors();
 	float64_t* new_feature_matrix = SG_MALLOC(float64_t, m_target_dim*num_vectors);
-        
         fill_projected_data(new_feature_matrix, features);
-        
-	simple_features->set_feature_matrix(SGMatrix<float64_t>(new_feature_matrix,m_target_dim,num_vectors));
+        simple_features->set_feature_matrix(SGMatrix<float64_t>(new_feature_matrix,m_target_dim,num_vectors));
 	return ((CDenseFeatures<float64_t>*)features)->get_feature_matrix();
 }
 
@@ -155,10 +148,10 @@ SGVector<float64_t> CKernelPCA::apply_to_feature_vector(SGVector<float64_t> vect
 {
 	ASSERT(m_initialized)
 
-        float64_t* new_feature_vector = SG_MALLOC(float64_t, m_target_dim*1);
-        fill_projected_data(new_feature_vector, new CDenseFeatures<float64_t>(SGMatrix<float64_t>(vector.vector,vector.vlen,1)));
+	float64_t* new_feature_vector = SG_MALLOC(float64_t, m_target_dim*1);
+	fill_projected_data(new_feature_vector, new CDenseFeatures<float64_t>(SGMatrix<float64_t>(vector.vector,vector.vlen,1)));
 	SGVector<float64_t> result = SGVector<float64_t>(new_feature_vector ,m_target_dim);
-        return result;
+	return result;
 }
 
 CDenseFeatures<float64_t>* CKernelPCA::apply_to_string_features(CFeatures* features)
@@ -166,8 +159,8 @@ CDenseFeatures<float64_t>* CKernelPCA::apply_to_string_features(CFeatures* featu
 	ASSERT(m_initialized)
 
 	int32_t num_vectors = features->get_num_vectors();
-        float64_t* new_feature_matrix = SG_MALLOC(float64_t, m_target_dim*num_vectors);
-        fill_projected_data(new_feature_matrix, features);
+	float64_t* new_feature_matrix = SG_MALLOC(float64_t, m_target_dim*num_vectors);
+	fill_projected_data(new_feature_matrix, features);
 	return new CDenseFeatures<float64_t>(SGMatrix<float64_t>(new_feature_matrix,m_target_dim,num_vectors));
 }
 
