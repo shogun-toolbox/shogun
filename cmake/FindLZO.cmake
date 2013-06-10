dc96a97a03cff63c4e0d50616b4e219dbeeed963
Viktor Gal <viktor.gal@maeth.com>
2013-06-10 17:24:47 +0200

    Fix FindLZO.cmake Detecting liblzo on darwin was not working and the linking flags were not exported properly

--- cmake/FindLZO.cmake
+++ cmake/FindLZO.cmake
@@ -6,9 +6,9 @@
 
 find_path(LZO_INCLUDE_DIR NAMES lzo/lzoconf.h)
 
-find_library(LZO_LIB NAMES liblzo2.so)
+find_library(LZO_LIBS NAMES lzo2)
 
-if (LZO_LIB)
+if (LZO_LIBS)
   if (LZO_INCLUDE_DIR)
     set(LZO_FOUND TRUE)
   else ()
@@ -20,7 +20,7 @@ endif ()
 
 if (LZO_FOUND)
   if (NOT LZO_FIND_QUIETLY)
-    message(STATUS "Lzo Library ${LZO_LIB}")
+    message(STATUS "Lzo Library ${LZO_LIBS}")
     message(STATUS "Lzo Include Found in ${LZO_INCLUDE_DIR}")
   endif ()
 else ()
