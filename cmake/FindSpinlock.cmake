464949313b4a02bd22c2ade6c8141d3f4b61892d
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2013-06-12 21:33:47 +0400

    Made spinlocks not found if pthreads are not detected

--- cmake/FindSpinlock.cmake
+++ cmake/FindSpinlock.cmake
@@ -13,4 +13,8 @@ IF (CMAKE_USE_PTHREADS_INIT)
 		MESSAGE(STATUS "Spinlock support not found")
 		SET(SPINLOCK_FOUND FALSE)
 	ENDIF (HAVE_SPINLOCK)
+ELSE ()
+		MESSAGE(STATUS "Spinlock support not found due to no pthreads available")
+		SET(SPINLOCK_FOUND FALSE)
 ENDIF ()
+
