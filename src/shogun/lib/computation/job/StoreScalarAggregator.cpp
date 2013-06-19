cacb172b79d832629472226492c592a3f9c5961d
lambday <heavensdevil6909@gmail.com>
2013-06-19 22:35:38 +0530

    few base classes and implementations for the log-det framework added

---
+++ src/shogun/lib/computation/job/StoreScalarAggregator.cpp
@@ -0,0 +1,78 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Written (W) 2013 Soumyajit De
+ */
+
+#include <shogun/lib/common.h>
+#include <shogun/lib/computation/job/ScalarResult.h>
+#include <shogun/lib/computation/job/StoreScalarAggregator.h>
+
+namespace shogun
+{
+template<class T>
+CStoreScalarAggregator<T>::CStoreScalarAggregator()
+	: CJobResultAggregator(), m_aggregate(static_cast<T>(0))
+	{
+		SG_GCDEBUG("%s created (%p)\n", this->get_name(), this)
+	}
+
+template<class T>
+CStoreScalarAggregator<T>::~CStoreScalarAggregator()
+	{
+		SG_GCDEBUG("%s destroyed (%p)\n", this->get_name(), this)
+	}
+
+template<class T>
+void CStoreScalarAggregator<T>::submit_result(CJobResult* result)
+	{
+		SG_GCDEBUG("Entering ...\n")
+
+		// check for proper typecast
+		// DEBUG COMMENT : the following does NOT increase refcount of result
+		CScalarResult<T>* new_result=dynamic_cast<CScalarResult<T>*>(result);
+		if (!new_result)
+			SG_ERROR("result is not of CScalarResult type!\n");
+		// aggregate it with previous
+		m_aggregate+=new_result->get_result();
+
+		SG_GCDEBUG("Leaving ...\n")
+	}
+
+template<>
+void CStoreScalarAggregator<bool>::submit_result(CJobResult* result)
+	{
+		SG_NOTIMPLEMENTED
+	}
+
+template<>
+void CStoreScalarAggregator<char>::submit_result(CJobResult* result)
+	{
+		SG_NOTIMPLEMENTED
+	}
+
+template<class T>
+void CStoreScalarAggregator<T>::finalize()
+	{
+		m_result=(CJobResult*)(new CScalarResult<T>(m_aggregate));
+		SG_REF(m_result);
+	}
+
+template class CStoreScalarAggregator<bool>;
+template class CStoreScalarAggregator<char>;
+template class CStoreScalarAggregator<int8_t>;
+template class CStoreScalarAggregator<uint8_t>;
+template class CStoreScalarAggregator<int16_t>;
+template class CStoreScalarAggregator<uint16_t>;
+template class CStoreScalarAggregator<int32_t>;
+template class CStoreScalarAggregator<uint32_t>;
+template class CStoreScalarAggregator<int64_t>;
+template class CStoreScalarAggregator<uint64_t>;
+template class CStoreScalarAggregator<float32_t>;
+template class CStoreScalarAggregator<float64_t>;
+template class CStoreScalarAggregator<floatmax_t>;
+template class CStoreScalarAggregator<complex64_t>;
+}
