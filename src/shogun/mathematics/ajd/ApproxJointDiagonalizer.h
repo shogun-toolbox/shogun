ffb662eb4d8572b73e6ccd64aa3d2066db27f6dd
Kevin <kevinhughes27@gmail.com>
2013-07-02 16:20:54 -0400

    added base class for AJD and an example of a derived class header

---
+++ src/shogun/mathematics/ajd/ApproxJointDiagonalizer.h
@@ -0,0 +1,71 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2013 Kevin Hughes
+ *
+ * Thanks to Andreas Ziehe and Cedric Gouy-Pailler
+ */
+
+#ifndef APPROXJOINTDIAGONALIZER_H_
+#define APPROXJOINTDIAGONALIZER_H_
+
+#include <shogun/lib/common.h>
+#include <shogun/base/SGObject.h>
+#include <shogun/lib/SGMatrix.h>
+#include <shogun/lib/SGNDArray.h>
+
+#include <limits>
+
+namespace shogun
+{
+
+/** @brief Class ApproxJointDiagonalizer defines an 
+ * Approximate Joint Diagonalizer (AJD) interface.
+ * 
+ * AJD finds the matrix V that best diagonalizes
+ * a set {C1 ... Ck} of real valued symmetric
+ * NxN matrices - V*C*VT
+ */
+class CApproxJointDiagonalizer : public CSGObject
+{
+	public:
+	
+		/** constructor */
+		CApproxJointDiagonalizer() : CSGObject()
+		{
+		};
+
+		/** destructor */
+		virtual ~CApproxJointDiagonalizer()
+		{
+		}
+	
+		/** Computes the matrix V that best diagonalizes C 
+		 * @param C, the set of matrices to be diagonalized
+		 * @param V0, an extimate of the matrix V
+		 * @param eps, machine epsilon or desired epsilon
+		 * @param itermax, maximum number of iterations
+		 * @return V, the matrix the best diagonalizes C 
+		 */
+		virtual SGMatrix<float64_t> compute(SGNDArray<float64_t> &C,
+						   SGMatrix<float64_t> *V0=NULL,
+						   double eps=std::numeric_limits<double>::epsilon(),
+						   int itermax=200) = 0;
+
+		/** return the matrix V that best diagonalizes C */
+		SGMatrix<float64_t> V()
+		{
+			return m_V;
+		}
+
+	protected:
+	
+		/** the matrix V that best diagonalizes C */	
+		SGMatrix<float64_t> m_V;
+
+};
+}
+#endif //APPROXJOINTDIAGONALIZER_H_ 
---
+++ src/shogun/mathematics/ajd/JADiag.h
@@ -0,0 +1,72 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2013 Kevin Hughes
+ *
+ * Thanks to Andreas Ziehe and Cedric Gouy-Pailler
+ */
+
+#ifndef JADIAG_H_
+#define JADIAG_H_
+
+#include <shogun/lib/common.h>
+#include <shogun/base/SGObject.h>
+#include <shogun/lib/SGMatrix.h>
+#include <shogun/lib/SGNDArray.h>
+
+#include <limits>
+
+namespace shogun
+{
+
+/** @brief Class JADiag
+ *
+ * An Approximate Joint Diagonalizer (AJD) Implementation
+ */
+class CJADiag : public CApproxJointDiagonalizer
+{
+	public:
+	
+		/** constructor */
+		CJADiag()
+		{
+		};
+
+		/** destructor */
+		virtual ~CJADiag()
+		{
+		}
+	
+		/** Computes the matrix V that best diagonalizes C 
+		 * @param C, the set of matrices to be diagonalized
+		 * @param V0, an extimate of the matrix V
+		 * @param eps, machine epsilon or desired epsilon
+		 * @param itermax, maximum number of iterations
+		 * @return V, the matrix the best diagonalizes C 
+		 */
+		static SGMatrix<float64_t> diagonalize(SGNDArray<float64_t> &C,
+       							SGMatrix<float64_t> *V0=NULL,
+							double eps=std::numeric_limits<double>::epsilon(),
+							int itermax=200));
+
+		/** Computes the matrix V that best diagonalizes C 
+		 * @param C, the set of matrices to be diagonalized
+		 * @param V0, an extimate of the matrix V
+		 * @param eps, machine epsilon or desired epsilon
+		 * @param itermax, maximum number of iterations
+		 * @return V, the matrix the best diagonalizes C 
+		 */
+		virtual SGMatrix<float64_t> compute(SGNDArray<float64_t> &C,
+						   SGMatrix<float64_t> *V0=NULL,
+						   double eps=std::numeric_limits<double>::epsilon(),
+						   int itermax=200)
+		{
+			m_V = diagonalize(C,V0,eps,itermax);
+			return m_V;	
+		}
+};
+}
+#endif //JADIAG_H_ 
