41c0b5f3aa1a8a1a8ebdaa29733cbc2eb540fdbc
hushell <hushell@hushell-U510.(none)>
2013-07-08 13:46:56 -0700

    Factor Graph

---
+++ src/shogun/structure/Factor.cpp
@@ -0,0 +1,280 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2013 Shell Hu 
+ * Copyright (C) 2013 Shell Hu 
+ */
+
+#include <shogun/structure/Factor.h>
+#include <shogun/base/Parameter.h>
+
+using namespace shogun;
+
+CFactor::CFactor()
+	: m_factor_type(NULL), m_data_source(NULL) 
+{
+	SG_UNSTABLE("CFactor::CFactor()", "\n");
+	register_parameters();
+	m_is_data_dep = false;
+
+	SG_REF(m_factor_type);
+	SG_REF(m_data_source);
+}
+
+CFactor::CFactor(CTableFactorType* ftype,
+	SGVector<int32_t> var_index,
+	SGVector<float64_t> data)
+	: m_factor_type(ftype), m_var_index(var_index), 
+	m_data_source(NULL), m_data(data), m_is_data_dep(true)
+{
+	ASSERT(ftype != NULL);
+	ASSERT(ftype->get_cardinalities().size() == m_var_index.size());
+
+	register_parameters();
+
+	if (m_data.size() == 0)
+		m_is_data_dep = false;
+
+	if (ftype->is_table() && m_is_data_dep)
+		m_energies.resize_vector(ftype->get_prodcardinalities());
+
+	SG_REF(m_factor_type);
+	SG_REF(m_data_source);
+}
+
+CFactor::CFactor(CTableFactorType* ftype,
+	SGVector<int32_t> var_index,
+	SGSparseVector<float64_t> data_sparse)
+	: m_factor_type(ftype), m_var_index(var_index), 
+	m_data_source(NULL), m_data_sparse(data_sparse), m_is_data_dep(true) 
+{
+	ASSERT(ftype != NULL);
+	ASSERT(ftype->get_cardinalities().size() == m_var_index.size());
+
+	register_parameters();
+
+	if (m_data_sparse.num_feat_entries == 0)
+		m_is_data_dep = false;
+
+	if (ftype->is_table() && m_is_data_dep)
+		m_energies.resize_vector(ftype->get_prodcardinalities());
+
+	SG_REF(m_factor_type);
+	SG_REF(m_data_source);
+}
+
+CFactor::CFactor(CTableFactorType* ftype,
+	SGVector<int32_t> var_index,
+	CFactorDataSource* data_source)
+	: m_factor_type(ftype), m_var_index(var_index), 
+	m_data_source(data_source), m_is_data_dep(true) 
+{
+	ASSERT(ftype != NULL);
+	ASSERT(ftype->get_cardinalities().size() == m_var_index.size());
+	ASSERT(data_source != NULL);
+
+	register_parameters();
+
+	if (ftype->is_table())
+		m_energies.resize_vector(ftype->get_prodcardinalities());
+
+	SG_REF(m_factor_type);
+	SG_REF(m_data_source);
+}
+
+CFactor::~CFactor()
+{
+	SG_UNREF(m_factor_type);
+	SG_UNREF(m_data_source);
+}
+
+const CTableFactorType* CFactor::get_factor_type() const 
+{
+	SG_REF(m_factor_type);
+	return m_factor_type;
+}
+
+void CFactor::set_factor_type(CTableFactorType* ftype)
+{
+	m_factor_type = ftype;
+	SG_REF(m_factor_type);
+}
+
+const SGVector<int32_t> CFactor::get_variables() const 
+{
+	return m_var_index;
+}
+
+void CFactor::set_variables(SGVector<int32_t> vars)
+{
+	m_var_index = vars.clone();
+}
+
+const SGVector<int32_t> CFactor::get_cardinalities() const 
+{
+	return m_factor_type->get_cardinalities();
+}
+
+const SGVector<float64_t> CFactor::get_data() const 
+{
+	if (m_data_source != NULL)
+		return m_data_source->data();
+
+	return m_data;
+}
+
+const SGSparseVector<float64_t> CFactor::get_data_sparse() const 
+{
+	if (m_data_source != NULL)
+		return m_data_source->data_sparse();
+
+	return m_data_sparse;
+}
+
+void CFactor::set_data(SGVector<float64_t> data_dense)
+{
+	m_data = data_dense.clone();	
+	m_is_data_dep = true;
+}
+
+void CFactor::set_data_sparse(SGSparseVectorEntry<float64_t>* data_sparse, 
+	int32_t dlen)
+{
+	m_data_sparse = SGSparseVector<float64_t>(data_sparse, dlen);
+	m_is_data_dep = true;
+}
+
+bool CFactor::is_data_dependent() const
+{
+	return m_is_data_dep;
+}
+
+const SGVector<float64_t> CFactor::get_energies() const 
+{
+	if (is_data_dependent() == false && m_energies.size() == 0) 
+	{
+		const SGVector<float64_t> ft_energies = m_factor_type->get_w();
+		ASSERT(ft_energies.size() == m_factor_type->get_prodcardinalities());
+		return ft_energies;
+	}
+	return m_energies;
+}
+
+SGVector<float64_t> CFactor::get_energies() 
+{
+	if (is_data_dependent() == false && m_energies.size() == 0) 
+	{
+		SGVector<float64_t> ft_energies 
+			= const_cast<CTableFactorType*>(m_factor_type)->get_w();
+		ASSERT(ft_energies.size() == m_factor_type->get_prodcardinalities());
+		return ft_energies;
+	}
+	return m_energies;
+}
+
+float64_t CFactor::evaluate_energy(const SGVector<int32_t> state) const 
+{
+	int32_t index = m_factor_type->index_from_universe_assignment(state, m_var_index);
+	return get_energies()[index]; // note for data indep, we get m_w not m_energies
+}
+
+void CFactor::compute_energies() 
+{
+	if (is_data_dependent() == false) 
+		return;
+
+	// For some factor types the size of the energy table is determined only
+	// after an initialization step from training data.
+	if (m_energies.size() == 0)
+		m_energies.resize_vector(m_factor_type->get_prodcardinalities());
+
+	const SGVector<float64_t> H = get_data();
+	const SGSparseVector<float64_t> H_sparse = get_data_sparse();
+
+	if (H_sparse.num_feat_entries == 0) 
+		m_factor_type->compute_energies(H, m_energies);
+	else 
+		m_factor_type->compute_energies(H_sparse, m_energies);
+}
+
+void CFactor::compute_gradients(
+	const SGVector<float64_t> marginals,
+	SGVector<float64_t>& parameter_gradient, 
+	float64_t mult) const 
+{
+	const SGVector<float64_t> H = get_data();
+	const SGSparseVector<float64_t> H_sparse = get_data_sparse();
+
+	if (H_sparse.num_feat_entries == 0) 
+		m_factor_type->compute_gradients(H, marginals, parameter_gradient, mult);
+	else 
+		m_factor_type->compute_gradients(H_sparse, marginals, parameter_gradient, mult);
+}
+
+void CFactor::register_parameters()
+{
+	SG_ADD((CSGObject**)&m_factor_type, "m_type_name", "Factor type name", MS_NOT_AVAILABLE);
+	SG_ADD(&m_var_index, "m_var_index", "Factor variable index", MS_NOT_AVAILABLE);
+	SG_ADD(&m_energies, "m_energies", "Factor energies", MS_NOT_AVAILABLE);
+	SG_ADD((CSGObject**)&m_data_source, "m_data_source", "Factor data source", MS_NOT_AVAILABLE);
+	SG_ADD(&m_data, "m_data", "Factor data", MS_NOT_AVAILABLE);
+	SG_ADD(&m_data_sparse, "m_data_sparse", "Sparse factor data", MS_NOT_AVAILABLE);
+	SG_ADD(&m_is_data_dep, "m_is_data_dep", "Factor is data dependent or not", MS_NOT_AVAILABLE);
+}
+
+CFactorDataSource::CFactorDataSource() 
+{
+	register_parameters();
+}
+
+CFactorDataSource::CFactorDataSource(const SGVector<float64_t> dense)
+	: m_dense(dense) 
+{
+	register_parameters();
+}
+
+CFactorDataSource::CFactorDataSource(const SGSparseVector<float64_t> sparse)
+	: m_sparse(sparse)
+{
+	register_parameters();
+}
+
+CFactorDataSource::~CFactorDataSource() 
+{
+}
+
+bool CFactorDataSource::is_sparse() const 
+{
+	return (m_dense.size() == 0);
+}
+
+const SGVector<float64_t> CFactorDataSource::data() const 
+{
+	return m_dense;
+}
+
+const SGSparseVector<float64_t> CFactorDataSource::data_sparse() const 
+{
+	return m_sparse;
+}
+
+void CFactorDataSource::set_data(SGVector<float64_t> dense)
+{
+	m_dense = dense.clone();	
+}
+
+void CFactorDataSource::set_data_sparse(SGSparseVectorEntry<float64_t>* sparse, 
+	int32_t dlen)
+{
+	m_sparse = SGSparseVector<float64_t>(sparse, dlen);
+}
+
+void CFactorDataSource::register_parameters()
+{
+	SG_ADD(&m_dense, "m_dense", "Shared data", MS_NOT_AVAILABLE);
+	SG_ADD(&m_sparse, "m_sparse", "Shared sparse data", MS_NOT_AVAILABLE);
+}
+
---
+++ src/shogun/structure/FactorType.h
@@ -0,0 +1,220 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2013 Shell Hu 
+ * Copyright (C) 2013 Shell Hu 
+ */
+
+#ifndef __FACTOR_TYPE_H__
+#define __FACTOR_TYPE_H__
+
+#include <shogun/base/SGObject.h>
+#include <shogun/lib/SGVector.h>
+#include <shogun/lib/SGString.h>
+
+namespace shogun
+{
+
+/* @brief Class CFactorType defines the way of factor parameterization
+ */
+class CFactorType : public CSGObject
+{
+public:
+	/** default constructor is prohibitted */
+	CFactorType();
+
+	/** Constructor
+	 * if w.size() == 0
+	 *   data_size = prod_card, i.e. product of cardinalities
+	 * else if w.size() != 0, i.e. parameterized factor type
+	 *   data_size = w.size() / prod_card, ensure divisible
+	 *   Note w.size() == prod_card may be data indep case
+	 *
+	 * @param id used for query in FactorGraphModel
+	 * @param card cardinalities of variables in the clique
+	 * @param w factor parameters
+	 */
+	CFactorType(const SGString<char> id, const SGVector<int32_t> card,
+		const SGVector<float64_t> w = SGVector<float64_t>());
+
+	/** deconstructor */
+	virtual ~CFactorType();
+
+	/** @return name of class */
+	virtual const char* get_name() const { return "FactorType"; }
+
+	/** @return get factor type id */
+	virtual const char* get_type_id() const;
+
+	/** set factor type id
+	 *
+	 * @param tid_str a string 
+	 * @param length of tid_str
+	 */
+	virtual void set_type_id(char* tid_str, int32_t slen);
+
+	/** @return get factor parameters */
+	virtual SGVector<float64_t> get_w();
+
+	/** @return get factor parameters */
+	virtual const SGVector<float64_t> get_w() const;
+
+	/** set parameters
+	 *
+	 * @param w factor parameters
+	 */
+	void set_w(SGVector<float64_t> w);
+
+	/** @return dimension of the factor parameter vector */
+	virtual int32_t get_w_dim() const;
+
+	/** @return cardinalities of the variables */
+	virtual const SGVector<int32_t> get_cardinalities() const;
+
+	/** set cardinalities 
+	 *
+	 * @param cards cardinalities of variables
+	 */
+	virtual void set_cardinalities(SGVector<int32_t> cards);
+
+	/** @return product of cardinalities */
+	virtual int32_t get_prodcardinalities() const;
+
+	/** parameters mapping to the global model parameters
+	 *
+	 * @param map indices in model parameter vector
+	 */
+	virtual void set_params_mapping(SGVector<int32_t> map);
+
+	/** @return is it a table factor type */
+	virtual bool is_table() const { return false; }
+
+protected:
+	/** initialize m_prod_card and m_cumprod_cards */
+	void init_card();
+
+private:
+	void register_parameters();
+
+protected:
+	SGString<char> m_type_id;
+
+	/** variable information */
+	SGVector<int32_t> m_cards;
+	SGVector<int32_t> m_cumprod_cards;
+	int32_t m_prod_card;
+
+	/** data information */
+	int32_t m_data_size;
+
+	/** factor paramters */
+	SGVector<float64_t> m_w;
+	/** indices to model parameters of FactorGraphModel */
+	SGVector<int32_t> m_map_params;
+};
+
+/* @brief Class CTableFactorType the way that store assignments of variables
+ * and energies in a table or a multi-array
+ */
+class CTableFactorType : public CFactorType
+{
+public:
+	/** default constructor */
+	CTableFactorType();
+
+	/** constructor
+	 *
+	 * @param id used for query in FactorGraphModel
+	 * @param card cardinalities of variables in the clique
+	 * @param w factor parameters
+	 */
+	CTableFactorType(const SGString<char> id, const SGVector<int32_t> card,
+		const SGVector<float64_t> w = SGVector<float64_t>());
+
+	/** deconstructor */
+	virtual ~CTableFactorType();
+
+	/** @return class name */
+	virtual const char* get_name() const { return "TableFactorType"; }
+
+	/** @return this is the table implementation but memory inefficient */
+	virtual bool is_table() const { return true; }
+
+	/** infer variable state from a given index of energy table
+	 * 
+	 * @param ei The linear index. 
+	 * @param var_index The variable index of this factor:
+	 * @return variable state 
+	 */
+	int32_t state_from_index(int32_t ei, int32_t var_index) const;
+
+	/** get variable assignment from absolute energy index
+	 *
+	 * @param ei energy index
+	 * @return variable assignment
+	 */
+	SGVector<int32_t> assignment_from_index(int32_t ei) const;
+
+	/** energy index from a given assignment
+	 *
+	 * @param assig variable assignments
+	 * @return energy index
+	 */
+	int32_t index_from_assignment(const SGVector<int32_t> assig) const;
+
+	/** energy index in the table of a factor given an universe assignment
+	 *
+	 * @param assig variable assignments
+	 * @param variable indices of that particular factor
+	 * @return energy index
+	 */
+	int32_t index_from_universe_assignment(const SGVector<int32_t> assig,
+		const SGVector<int32_t> var_index) const;
+
+	/** compute energy values from parameters for a specific factor.
+	 *
+	 * @param factor_data dense factor data vector 
+	 * @param energies forwarded energy table
+	 */
+	virtual void compute_energies(const SGVector<float64_t> factor_data,
+		SGVector<float64_t>& energies) const;
+
+	/** Compute energy values from parameters for a specific factor.
+	 *
+	 * @param factor_data sparse factor data 
+	 * @param energies forwarded energy table
+	 */
+	virtual void compute_energies(const SGSparseVector<float64_t> factor_data_sparse,
+		SGVector<float64_t>& energies) const;
+
+	/** compute parameter gradient from marginals and factor data
+	 *
+	 * @param factor_data dense factor data 
+	 * @param marginals marginal distribution of the factor
+	 * @param parameter_gradient gradient of factor parameters
+	 * @param mult multiplier 
+	 */
+	virtual void compute_gradients(const SGVector<float64_t> factor_data,
+		const SGVector<float64_t> marginals,
+		SGVector<float64_t>& parameter_gradient, double mult) const;
+
+	/** compute parameter gradient from marginals and factor data
+	 *
+	 * @param factor_data sparse factor data 
+	 * @param marginals marginal distribution of the factor
+	 * @param parameter_gradient gradient of factor parameters
+	 * @param mult multiplier
+	 */
+	virtual void compute_gradients(const SGSparseVector<float64_t> factor_data_sparse,
+		const SGVector<float64_t> marginals,
+		SGVector<float64_t>& parameter_gradient, double mult) const;
+
+};
+
+}
+
+#endif
+
