2fe7f0c9de71174d44fb5ac54f560353182b36be
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-07-09 20:58:45 +0200

    [WIP] LMNN top-level implementation with not implemented auxiliary methods

--- src/shogun/metric/LMNN.cpp
+++ src/shogun/metric/LMNN.cpp
@@ -11,7 +11,9 @@
 #ifdef HAVE_EIGEN3
 
 #include <shogun/metric/LMNN.h>
-#include <Eigen/Dense>
+#include <shogun/mathematics/Math.h>
+#include <shogun/multiclass/KNN.h>
+#include <shogun/distance/EuclideanDistance.h>
 
 using namespace shogun;
 using namespace Eigen;
@@ -21,12 +23,13 @@ CLMNN::CLMNN()
 	init();
 }
 
-CLMNN::CLMNN(CFeatures* features, CMulticlassLabels* labels)
+CLMNN::CLMNN(CFeatures* features, CMulticlassLabels* labels, uint32_t k)
 {
 	init();
 
 	m_features = features;
-	m_labels   = labels;
+	m_labels = labels;
+	m_k = k;
 
 	SG_REF(m_features)
 	SG_REF(m_labels)
@@ -43,9 +46,77 @@ const char* CLMNN::get_name() const
 	return "LMNN";
 }
 
-//TODO
 void CLMNN::train(SGMatrix<float64_t> init_transform)
 {
+	/// Check training data
+	REQUIRE(m_labels->get_num_labels()==m_features->get_num_vectors(),
+			"The number of feature vectors must be equal to the number of labels\n")
+
+	/// Initializations
+
+	// Linear transform L, the Mahalanobis distance is L^T*L
+	MatrixXd L = Map<MatrixXd>(init_transform.matrix, init_transform.num_rows,
+			init_transform.num_cols);
+	// Iteration counter
+	uint32_t iter = 0;
+	// Previous active set of impostors, empty at first
+	ImpostorsSetType prev_impostors;
+	// Compute target or genuine neighbours
+	SGMatrix<int32_t> target_nn = find_target_nn();
+	// Compute outer products between all pairs of feature vectors
+	OuterProductsMatrixType outer_products = compute_outer_products();
+	// Initialize (sub-)gradient
+	MatrixXd gradient = sum_outer_products(outer_products, target_nn);
+	// Stop criterion
+	bool stop = false;
+	// Value of the objective function at every iteration
+	SGVector<float64_t> obj(m_maxiter);
+	// The step size is modified depending on how the objective changes, leave the
+	// step size member unchanged and use a local one
+	float64_t stepsize = m_stepsize;
+
+	/// Main loop
+	while (not stop && iter < m_maxiter)
+	{
+		SG_PROGRESS(iter, 0, m_maxiter)
+
+		//FIXME add approximate computation of impostors set
+		// Find current set of impostors
+		ImpostorsSetType cur_impostors = find_impostors(L, target_nn);
+
+		// (Sub-) gradient computation
+		update_gradient(gradient, outer_products, cur_impostors, prev_impostors);
+		// Take gradient step and project onto the positive semi-definite cone
+		gradient_step(L, gradient, stepsize);
+
+		// Compute objective
+		obj[iter] = compute_objective(L, outer_products, target_nn, cur_impostors);
+
+		// Correct step size
+		if (iter > 0)
+		{
+			// Difference between current and previous objective
+			float64_t delta = obj[iter] - obj[iter-1];
+
+			if (delta > 0)
+			{
+				// The objective has increased, we have probably jumped over the optimum,
+				// thus, decrease the step size
+				stepsize *= 0.5;
+			}
+			else
+			{
+				// The objective has decreased, we are in the right direction,
+				// increase the step size
+				stepsize *= 1.01;
+			}
+		}
+
+		// Update iteration counter
+		iter = iter + 1;
+		// Update previous set of impostors
+		prev_impostors = cur_impostors;
+	}
 }
 
 SGMatrix<float64_t> CLMNN::get_linear_transform() const
@@ -59,8 +130,8 @@ CCustomMahalanobisDistance* CLMNN::get_distance() const
 
 	// Put the linear transform L in Eigen to perform the matrix multiplication
 	// L is not copied to another region of memory
-	Map<const MatrixXd> map_linear_transform(m_linear_transform.matrix, m_linear_transform.num_rows,
-			m_linear_transform.num_cols);
+	Map<const MatrixXd> map_linear_transform(m_linear_transform.matrix,
+			m_linear_transform.num_rows, m_linear_transform.num_cols);
 	// TODO exploit that M is symmetric
 	MatrixXd M = map_linear_transform.transpose()*map_linear_transform;
 	// TODO avoid copying
@@ -78,14 +149,93 @@ CCustomMahalanobisDistance* CLMNN::get_distance() const
 	return distance;
 }
 
+float64_t CLMNN::get_regularization() const
+{
+	return m_regularization;
+}
+
+void CLMNN::set_regularization(const float64_t regularization)
+{
+	m_regularization = regularization;
+}
+
+float64_t CLMNN::get_stepsize() const
+{
+	return m_stepsize;
+}
+
+void CLMNN::set_stepsize(const float64_t stepsize)
+{
+	m_stepsize = stepsize;
+}
+
+uint32_t CLMNN::get_maxiter() const
+{
+	return m_maxiter;
+}
+
+void CLMNN::set_maxiter(const uint32_t maxiter)
+{
+	m_maxiter = maxiter;
+}
+
 void CLMNN::init()
 {
-	SG_ADD(&m_linear_transform, "m_linear_transform", "Linear transform in matrix form", MS_NOT_AVAILABLE)
+	SG_ADD(&m_linear_transform, "m_linear_transform", "Linear transform in matrix form",
+			MS_NOT_AVAILABLE)
 	SG_ADD((CSGObject**) &m_features, "m_features", "Training features", MS_NOT_AVAILABLE)
 	SG_ADD((CSGObject**) &m_labels, "m_labels", "Training labels", MS_NOT_AVAILABLE)
+	SG_ADD(&m_k, "m_k", "Number of target neighbours per example", MS_NOT_AVAILABLE)
+	SG_ADD(&m_regularization, "m_regularization", "Regularization", MS_AVAILABLE)
+	SG_ADD(&m_stepsize, "m_stepsize", "Step size in gradient descent", MS_NOT_AVAILABLE)
+	SG_ADD(&m_maxiter, "m_maxiter", "Maximum number of iterations", MS_NOT_AVAILABLE)
 
 	m_features = NULL;
-	m_labels   = NULL;
+	m_labels = NULL;
+	m_k = 1;
+	m_regularization = 0.5;
+	m_stepsize = 1e-07;
+	m_maxiter = 1000;
+}
+
+//TODO
+SGMatrix<int32_t> CLMNN::find_target_nn() const
+{
+	return SGMatrix<int32_t>(m_k, m_features->get_num_vectors());
+}
+
+//TODO
+OuterProductsMatrixType CLMNN::compute_outer_products() const
+{
+	return OuterProductsMatrixType();
+}
+
+//TODO
+MatrixXd CLMNN::sum_outer_products(const OuterProductsMatrixType& C, const SGMatrix<int32_t> idxs) const
+{
+	return VectorXd();
+}
+
+//TODO
+ImpostorsSetType CLMNN::find_impostors(const MatrixXd& L, const SGMatrix<int32_t> target_nn) const
+{
+	return ImpostorsSetType();
+}
+
+//TODO
+void CLMNN::update_gradient(MatrixXd& G, const OuterProductsMatrixType& C, const ImpostorsSetType& Nc, const ImpostorsSetType& Np) const
+{
+}
+
+//TODO
+void CLMNN::gradient_step(MatrixXd& L, const MatrixXd& G, float64_t stepsize) const
+{
+}
+
+//TODO
+float64_t CLMNN::compute_objective(const MatrixXd& L, const OuterProductsMatrixType& C, const SGMatrix<int32_t> target_nn, const ImpostorsSetType& Nc) const
+{
+	return 0.0;
 }
 
 #endif /* HAVE_EIGEN3 */
