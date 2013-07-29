5733a65137c48909c95272fbbc6df57af5a1bb67
Soeren Sonnenburg <sonne@debian.org>
2013-07-29 23:21:25 +0200

    define _USE_MATH_DEFINES to get non-standard constant M_PI

--- src/shogun/mathematics/Math.h
+++ src/shogun/mathematics/Math.h
@@ -25,6 +25,10 @@
 #include <shogun/base/Parallel.h>
 #include <shogun/mathematics/Random.h>
 
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+
 #include <math.h>
 #include <stdio.h>
 #include <float.h>
