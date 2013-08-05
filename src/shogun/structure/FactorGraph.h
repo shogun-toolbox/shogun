8adb4f01d7f4d6d4b66e1f1f59286bb357990d5c
hushell <hushell@hushell-U510.(none)>
2013-08-05 16:31:07 -0700

    FactorGraph structure analysis

--- src/shogun/structure/FactorGraph.h
+++ src/shogun/structure/FactorGraph.h
@@ -19,10 +19,12 @@
 
 namespace shogun {
 
-/* @brief Class CFactorGraph a factor graph is a structured input in general 
+/** @brief Class CFactorGraph a factor graph is a structured input in general 
  */
 class CFactorGraph : public CFeatures
 {
+	class CDisjointSet;
+
 public:
 	CFactorGraph();
 
@@ -44,21 +46,6 @@ public:
 	/** @return class name */
 	virtual const char* get_name() const { return "FactorGraph"; }
 
-	/** @return all the factors */
-	CDynamicObjectArray* get_factors() const;
-
-	/** @return cardinalities */
-	SGVector<int32_t> get_cardinalities() const;
-
-	/** set cardinalities 
-	 *
-	 * @param cards cardinalities of all variables
-	 */
-	void set_cardinalities(SGVector<int32_t> cards);
-
-	/** @return all the shared data */
-	CDynamicObjectArray* get_factor_data_sources() const;
-
 	/** add a factor 
 	 *
 	 * @param factor a factor pointer
@@ -70,6 +57,21 @@ public:
 	 * @param datasource a factor data source 
 	 */
 	void add_data_source(CFactorDataSource* datasource);
+
+	/** @return all the factors */
+	CDynamicObjectArray* get_factors() const;
+
+	/** @return all the shared data */
+	CDynamicObjectArray* get_factor_data_sources() const;
+
+	/** @return cardinalities */
+	SGVector<int32_t> get_cardinalities() const;
+
+	/** set cardinalities 
+	 *
+	 * @param cards cardinalities of all variables
+	 */
+	void set_cardinalities(SGVector<int32_t> cards);
 
 	/** compute energy tables in the factor graph */ 
 	void compute_energies();
@@ -98,17 +100,153 @@ public:
 	/** @return number of factors */
 	virtual int32_t get_num_vectors() const;
 
-private:
+	/** @return disjoint set */
+	CDisjointSet* get_disjoint_set() const;
 
+	/** @return number of edges */
+	int32_t get_num_edges() const;
+
+	/** connect graph nodes by performing union-find algorithm */
+	void connect_components();
+
+	/** @return is acyclic graph or not */
+	bool is_acyclic_graph() const;
+
+	/** @return is connected graph or not */
+	bool is_connected_graph() const;
+
+	/** @return is tree graph or not */
+	bool is_tree_graph() const;
+
+private:
+	/** register parameters */
 	void register_parameters();
 
-private:
+	/** initialization */
+	void init();
 
+protected:
+	// TODO: FactorNode, VariableNode, such that they have IDs
+
+	/** cardinalities */
 	SGVector<int32_t> m_cardinalities;
 
+	/** added factors */
 	CDynamicObjectArray* m_factors;
 
+	/** added data sources */
 	CDynamicObjectArray* m_datasources;
+
+	/** disjoint set */
+	CDisjointSet* m_dset;
+
+	/** if has circle in the graph */
+	bool m_has_cycle;
+
+	/** number of edges */
+	int32_t m_num_edges;
+	
+private:
+	/** @brief Class CDisjointSet data structure for linking graph nodes
+	 * It's easy to identify connected graph, acyclic graph, roots of forest etc.
+	 * please refer to http://en.wikipedia.org/wiki/Disjoint-set_data_structure
+	 */
+	class CDisjointSet : public CSGObject
+	{
+	public:
+		/** default constructor */
+		CDisjointSet();
+	
+		/** constructor 
+		 *
+		 * @param num_elements number of initial disjoint elements
+		 */
+		CDisjointSet(int32_t num_elements);
+	
+		/** destructor */
+		~CDisjointSet() { }
+	
+		/** @return class name */
+		virtual const char* get_name() const { return "DisjointSet"; }
+	
+		/** initialize internal data structures */
+		void make_sets();
+	
+		/** find root of the set containing x with path compression 
+		 *
+		 * @param x queried element 
+		 * @return the root
+		 */
+		int32_t find_set(int32_t x);
+	
+		/** link two roots, higher ranked root will be new root
+		 * 
+		 * @param xroot root of the set containing x 
+		 * @param yroot root of the set containing y 
+		 * @return new root 
+		 */
+		int32_t link_set(int32_t xroot, int32_t yroot);
+	
+		/** link the roots of two sets containing x and y respectively 
+		 * and return if they were linked 
+		 *
+		 * @param x first element to be linked
+		 * @param y second element to be linked
+		 * @return if x and y were in the same set
+		 */
+		bool union_set(int32_t x, int32_t y);
+	
+		/** if element x and element y is in the same set
+		 *
+		 * @param x first element
+		 * @param y second element
+		 * @return if x and y are in the same set
+		 */
+		bool is_same_set(int32_t x, int32_t y);
+	
+		/** give each disjoint set a label
+		 *
+		 * @param out_labels label for each element
+		 * @return number of unique labels
+		 */
+		int32_t get_unique_labeling(SGVector<int32_t> out_labels);
+	
+		/** get number of sets
+		 *
+		 * @return number of sets
+		 */
+		int32_t get_num_sets();
+	
+		/** if union-find is performed
+		 *
+		 * @return is connected
+		 */
+		bool get_connected();
+	
+		/** set connection flag after union-find
+		 *
+		 * @param is_connected boolean variable
+		 */
+		void set_connected(bool is_connected);
+	
+	private:
+		/** register parameters */
+		void register_parameters();
+	
+	private:
+		/** number of elements */
+		int32_t m_num_elements;
+	
+		/** parent array */
+		SGVector<int32_t> m_parent;
+	
+		/** rank array */
+		SGVector<int32_t> m_rank;
+	
+		/** connection flag */
+		bool m_is_connected;
+	};
+
 };
 
 }
