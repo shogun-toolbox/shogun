6340a2e2da2f4577996539e190b26921626749b5
Viktor Gal <viktor.gal@maeth.com>
2013-08-06 10:00:26 +0200

    Fix Eigen3 version detection remove gcc+python_modular builds from travis matrix

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -15,21 +15,7 @@
 # Copyright (c) 2009 Benoit Jacob <jacob.benoit.1@gmail.com>
 # Redistribution and use is allowed according to the terms of the 2-clause BSD license.
 
-if(NOT Eigen_FIND_VERSION)
-  if(NOT Eigen_FIND_VERSION_MAJOR)
-    set(Eigen_FIND_VERSION_MAJOR 3)
-  endif(NOT Eigen_FIND_VERSION_MAJOR)
-  if(NOT Eigen_FIND_VERSION_MINOR)
-    set(Eigen_FIND_VERSION_MINOR 0)
-  endif(NOT Eigen_FIND_VERSION_MINOR)
-  if(NOT Eigen_FIND_VERSION_PATCH)
-    set(Eigen_FIND_VERSION_PATCH 0)
-  endif(NOT Eigen_FIND_VERSION_PATCH)
-
-  set(Eigen_FIND_VERSION "${Eigen_FIND_VERSION_MAJOR}.${Eigen_FIND_VERSION_MINOR}.${Eigen_FIND_VERSION_PATCH}")
-endif(NOT Eigen_FIND_VERSION)
-
-macro(_eigen3_check_version)
+macro(_eigen3_get_version)
   file(READ "${EIGEN_INCLUDE_DIR}/Eigen/src/Core/util/Macros.h" _eigen3_version_header)
 
   string(REGEX MATCH "define[ \t]+EIGEN_WORLD_VERSION[ \t]+([0-9]+)" _eigen3_world_version_match "${_eigen3_version_header}")
@@ -40,43 +26,26 @@ macro(_eigen3_check_version)
   set(EIGEN_MINOR_VERSION "${CMAKE_MATCH_1}")
 
   set(EIGEN_VERSION ${EIGEN_WORLD_VERSION}.${EIGEN_MAJOR_VERSION}.${EIGEN_MINOR_VERSION})
-  if(${EIGEN_VERSION} VERSION_LESS ${Eigen_FIND_VERSION})
-    set(EIGEN_VERSION_OK FALSE)
-  else(${EIGEN_VERSION} VERSION_LESS ${Eigen_FIND_VERSION})
-    set(EIGEN_VERSION_OK TRUE)
-  endif(${EIGEN_VERSION} VERSION_LESS ${Eigen_FIND_VERSION})
+ 
+endmacro(_eigen3_get_version)
 
-  if(NOT EIGEN_VERSION_OK)
+find_path(EIGEN_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
+    PATHS
+    ${CMAKE_INSTALL_PREFIX}/include
+    PATH_SUFFIXES eigen3 eigen
+  )
 
-    message(STATUS "Eigen version ${EIGEN_VERSION} found in ${EIGEN_INCLUDE_DIR}, "
-                   "but at least version ${Eigen_FIND_VERSION} is required")
-  endif(NOT EIGEN_VERSION_OK)
-endmacro(_eigen3_check_version)
+if(EIGEN_INCLUDE_DIR)
+  _eigen3_get_version()
+endif(EIGEN_INCLUDE_DIR)
 
-if (EIGEN_INCLUDE_DIRS)
-
-  # in cache already
-  _eigen3_check_version()
-  set(EIGEN_FOUND ${EIGEN_VERSION_OK})
-
+include(FindPackageHandleStandardArgs)
+if ( CMAKE_VERSION LESS 2.8.3 )
+  find_package_handle_standard_args (Eigen3 DEFAULT_MSG EIGEN_INCLUDE_DIR VERSION_VAR EIGEN_VERSION)
 else ()
+  find_package_handle_standard_args (Eigen3 REQUIRED_VARS EIGEN_INCLUDE_DIR VERSION_VAR EIGEN_VERSION)
+endif ()
 
-  find_path(EIGEN_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
-      PATHS
-      ${CMAKE_INSTALL_PREFIX}/include
-      ${KDE4_INCLUDE_DIR}
-      PATH_SUFFIXES eigen3 eigen
-    )
-
-  if(EIGEN_INCLUDE_DIR)
-    _eigen3_check_version()
-  endif(EIGEN_INCLUDE_DIR)
-
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(Eigen DEFAULT_MSG EIGEN_INCLUDE_DIR EIGEN_VERSION_OK)
-
-  mark_as_advanced(EIGEN_INCLUDE_DIR)
-  SET(EIGEN_INCLUDE_DIRS ${EIGEN_INCLUDE_DIR} CACHE PATH "The Eigen include path.")
-
-endif()
+mark_as_advanced(EIGEN_INCLUDE_DIR)
+SET(EIGEN_INCLUDE_DIRS ${EIGEN_INCLUDE_DIR} CACHE PATH "The Eigen include path.")
 
