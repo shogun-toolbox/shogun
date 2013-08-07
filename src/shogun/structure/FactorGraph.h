59029952decda87d6d83fc4bf0c20722bc346a6c
hushell <hushell@hushell-U510.(none)>
2013-08-07 04:34:49 -0700

    MAP Inference for factor graph

--- src/shogun/structure/FactorGraph.h
+++ src/shogun/structure/FactorGraph.h
@@ -16,14 +16,15 @@
 #include <shogun/features/Features.h>
 #include <shogun/structure/Factor.h>
 #include <shogun/structure/FactorGraphLabels.h>
+#include <shogun/structure/DisjointSet.h>
 
-namespace shogun {
+namespace shogun 
+{
 
 /** @brief Class CFactorGraph a factor graph is a structured input in general 
  */
 class CFactorGraph : public CFeatures
 {
-	class CDisjointSet;
 
 public:
 	CFactorGraph();
@@ -106,7 +107,10 @@ public:
 	/** @return number of edges */
 	int32_t get_num_edges() const;
 
-	/** connect graph nodes by performing union-find algorithm */
+	/** connect graph nodes by performing union-find algorithm 
+	 * NOTE: make sure call this func before performing inference 
+	 * and other functions related to structure analysis
+	 */
 	void connect_components();
 
 	/** @return is acyclic graph or not */
@@ -146,107 +150,6 @@ protected:
 	/** number of edges */
 	int32_t m_num_edges;
 	
-private:
-	/** @brief Class CDisjointSet data structure for linking graph nodes
-	 * It's easy to identify connected graph, acyclic graph, roots of forest etc.
-	 * please refer to http://en.wikipedia.org/wiki/Disjoint-set_data_structure
-	 */
-	class CDisjointSet : public CSGObject
-	{
-	public:
-		/** default constructor */
-		CDisjointSet();
-	
-		/** constructor 
-		 *
-		 * @param num_elements number of initial disjoint elements
-		 */
-		CDisjointSet(int32_t num_elements);
-	
-		/** destructor */
-		~CDisjointSet() { }
-	
-		/** @return class name */
-		virtual const char* get_name() const { return "DisjointSet"; }
-	
-		/** initialize internal data structures */
-		void make_sets();
-	
-		/** find root of the set containing x with path compression 
-		 *
-		 * @param x queried element 
-		 * @return the root
-		 */
-		int32_t find_set(int32_t x);
-	
-		/** link two roots, higher ranked root will be new root
-		 * 
-		 * @param xroot root of the set containing x 
-		 * @param yroot root of the set containing y 
-		 * @return new root 
-		 */
-		int32_t link_set(int32_t xroot, int32_t yroot);
-	
-		/** link the roots of two sets containing x and y respectively 
-		 * and return if they were linked 
-		 *
-		 * @param x first element to be linked
-		 * @param y second element to be linked
-		 * @return if x and y were in the same set
-		 */
-		bool union_set(int32_t x, int32_t y);
-	
-		/** if element x and element y is in the same set
-		 *
-		 * @param x first element
-		 * @param y second element
-		 * @return if x and y are in the same set
-		 */
-		bool is_same_set(int32_t x, int32_t y);
-	
-		/** give each disjoint set a label
-		 *
-		 * @param out_labels label for each element
-		 * @return number of unique labels
-		 */
-		int32_t get_unique_labeling(SGVector<int32_t> out_labels);
-	
-		/** get number of sets
-		 *
-		 * @return number of sets
-		 */
-		int32_t get_num_sets();
-	
-		/** if union-find is performed
-		 *
-		 * @return is connected
-		 */
-		bool get_connected();
-	
-		/** set connection flag after union-find
-		 *
-		 * @param is_connected boolean variable
-		 */
-		void set_connected(bool is_connected);
-	
-	private:
-		/** register parameters */
-		void register_parameters();
-	
-	private:
-		/** number of elements */
-		int32_t m_num_elements;
-	
-		/** parent array */
-		SGVector<int32_t> m_parent;
-	
-		/** rank array */
-		SGVector<int32_t> m_rank;
-	
-		/** connection flag */
-		bool m_is_connected;
-	};
-
 };
 
 }
