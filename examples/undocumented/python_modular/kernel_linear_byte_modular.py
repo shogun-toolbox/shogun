a8013bea1b520120fba90da8ded86b9991993c4c
Viktor Gal <viktor.gal@maeth.com>
2013-08-09 01:33:13 +0200

    Switch kernel_linear_byte_modular.py example to use CSVFile reader

--- examples/undocumented/python_modular/kernel_linear_byte_modular.py
+++ examples/undocumented/python_modular/kernel_linear_byte_modular.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 from numpy import ubyte
-lm=LoadMatrix()
 
-traindat = ubyte(lm.load_numbers('../data/fm_train_byte.dat'))
-testdat = ubyte(lm.load_numbers('../data/fm_test_byte.dat'))
+traindat = '../data/fm_train_byte.dat'
+testdat = '../data/fm_test_byte.dat'
 
 parameter_list=[[traindat,testdat],[traindat,testdat]]
 
 def kernel_linear_byte_modular (fm_train_byte=traindat,fm_test_byte=testdat):
-	from shogun.Kernel import LinearKernel
-	from shogun.Features import ByteFeatures
+	from modshogun import LinearKernel, ByteFeatures, CSVFile
 
-	feats_train=ByteFeatures(fm_train_byte)
-	feats_test=ByteFeatures(fm_test_byte)
+	feats_train=ByteFeatures(CSVFile(fm_train_byte))
+	feats_test=ByteFeatures(CSVFile(fm_test_byte))
 
 	kernel=LinearKernel(feats_train, feats_train)
 	km_train=kernel.get_kernel_matrix()
