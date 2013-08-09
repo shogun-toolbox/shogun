5c83ab49016e75a4c2a407a33f73ab77fa250404
Soeren Sonnenburg <sonne@debian.org>
2013-08-10 00:15:38 +0200

    convert distances to CSVFile

--- examples/undocumented/python_modular/distance_chisquare_modular.py
+++ examples/undocumented/python_modular/distance_chisquare_modular.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
-from tools.load import LoadMatrix
-lm=LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
-testdat = lm.load_numbers('../data/fm_test_real.dat')
+traindat = '../data/fm_train_real.dat'
+testdat = '../data/fm_test_real.dat'
 
 parameter_list = [[traindat,testdat,],[traindat,testdat]]
 
-def distance_chisquare_modular (fm_train_real=traindat,fm_test_real=testdat):
-
-	from shogun.Features import RealFeatures
-	from shogun.Distance import ChiSquareDistance
-	feats_train=RealFeatures(fm_train_real)
-	feats_test=RealFeatures(fm_test_real)
+def distance_chisquare_modular (train_fname=traindat,test_fname=testdat):
+	from modshogun import RealFeatures, ChiSquareDistance, CSVFile
+	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_test=RealFeatures(CSVFile(test_fname))
 
 	distance=ChiSquareDistance(feats_train, feats_train)
-
 	dm_train=distance.get_distance_matrix()
+
 	distance.init(feats_train, feats_test)
 	dm_test=distance.get_distance_matrix()
 	return distance,dm_train,dm_test
