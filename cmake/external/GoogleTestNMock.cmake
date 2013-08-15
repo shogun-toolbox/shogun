30f1b5b67aa61104ff32cb3127a1c1986e402acb
Viktor Gal <viktor.gal@maeth.com>
2013-08-15 11:18:38 +0200

    Move GoogleMock source directory to third_party disable svn checkout of GoogleMock if the right revision is already checked out into the source dir

--- cmake/external/GoogleTestNMock.cmake
+++ cmake/external/GoogleTestNMock.cmake
@@ -1,21 +1,39 @@
 MergeCFLAGS()
 GetCompilers()
 
+SET (GMOCK_REVISION 443)
 include(ExternalProject)
 ExternalProject_Add(
 	GoogleMock
 	SVN_REPOSITORY http://googlemock.googlecode.com/svn/trunk
-	SVN_REVISION -r 443
+	SVN_REVISION -r ${GMOCK_REVISION}
 	UPDATE_COMMAND ""
     TIMEOUT 10
 	PREFIX ${CMAKE_BINARY_DIR}/GoogleMock
 	DOWNLOAD_DIR ${THIRD_PARTY_DIR}/GoogleMock
+	SOURCE_DIR ${THIRD_PARTY_DIR}/GoogleMock
 	INSTALL_COMMAND ""
 	CMAKE_ARGS 	-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${THIRD_PARTY_DIR}/libs/gmock
 		-DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${THIRD_PARTY_DIR}/libs/gmock
 		-DCMAKE_CXX_FLAGS:STRING=${MERGED_CXX_FLAGS}${CMAKE_DEFINITIONS}
 		-DCMAKE_C_COMPILER:STRING=${C_COMPILER}
 		-DCMAKE_CXX_COMPILER:STRING=${CXX_COMPILER}
+)
+
+ExternalProject_Get_Property(GoogleMock STAMP_DIR)
+ExternalProject_Get_Property(GoogleMock SOURCE_DIR)
+
+ExternalProject_Add_Step(
+	GoogleMock check_revision
+	COMMAND ${CMAKE_COMMAND} -D STAMP_DIR=${STAMP_DIR}
+                     		 -D SRC_DIR=${SOURCE_DIR}
+                     		 -D SVN_EXEC=${Subversion_SVN_EXECUTABLE}
+                     		 -D REVISION=${GMOCK_REVISION}
+                     		 -D PROJECT_NAME=GoogleMock
+                     		 -P ${CMAKE_MODULE_PATH}/CheckSVNRevision.cmake
+	COMMENT "Checking Google Mock checked out revision"
+	DEPENDERS download
+	DEPENDEES mkdir
 )
 
 UNSET(C_COMPILER)
