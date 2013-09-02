4c689ad4bfbb79b7b05374e74d2d8c8d16aba004
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2013-09-03 00:20:02 +0200

    Update LMNN and KNN Python modular examples to use CSVFile.

--- examples/undocumented/python_modular/metric_lmnn_modular.py
+++ examples/undocumented/python_modular/metric_lmnn_modular.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 
-from tools.load import LoadMatrix
+traindat = '../data/fm_train_real.dat'
+testdat = '../data/fm_test_real.dat'
+label_traindat = '../data/label_train_multiclass.dat'
 
-# load training data and test features
-lm=LoadMatrix()
-traindat = lm.load_numbers('../data/fm_train_real.dat')
-testdat = lm.load_numbers('../data/fm_test_real.dat')
-label_traindat = lm.load_labels('../data/label_train_multiclass.dat')
+parameter_list = [[traindat,testdat,label_traindat,3]]
 
-parameter_list = [[traindat,testdat,label_traindat,3],[traindat,testdat,label_traindat,3]]
-
-def metric_lmnn_modular(fm_train_real=traindat,fm_test_real=testdat,label_train_multiclass=label_traindat,k=3):
-	from modshogun import RealFeatures,MulticlassLabels,LMNN,KNN
+def metric_lmnn_modular(train_fname=traindat,test_fname=testdat,label_train_fname=label_traindat,k=3):
+	from modshogun import RealFeatures,MulticlassLabels,LMNN,KNN,CSVFile
 
 	# wrap features and labels into Shogun objects
-	feats_train=RealFeatures(fm_train_real)
-	feats_test=RealFeatures(fm_test_real)
-	labels=MulticlassLabels(label_train_multiclass)
+	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_test=RealFeatures(CSVFile(test_fname))
+	labels=MulticlassLabels(CSVFile(label_train_fname))
 
 	# LMNN
 	lmnn=LMNN(feats_train,labels,k)
