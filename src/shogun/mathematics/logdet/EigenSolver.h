c54bd1d48331bebf818717ec5f83552af353f197
lambday <heavensdevil6909@gmail.com>
2013-09-05 23:08:10 +0530

    precomputed eigenvalue set added in eigensolver (log-det)

--- src/shogun/mathematics/logdet/EigenSolver.h
+++ src/shogun/mathematics/logdet/EigenSolver.h
@@ -56,10 +56,24 @@ public:
 	 */
 	virtual void compute() = 0;
 
+	/** sets the min eigelvalue of a real valued self-adjoint linear operator */
+	void set_min_eigenvalue(float64_t min_eigenvalue)
+	{
+		m_min_eigenvalue=min_eigenvalue;
+		m_is_computed_min=true;
+	}
+
 	/** @return min eigenvalue of a real valued self-adjoint linear operator */
 	const float64_t get_min_eigenvalue() const
 	{
 		return m_min_eigenvalue;
+	}
+
+	/** sets the max eigelvalue of a real valued self-adjoint linear operator */
+	void set_max_eigenvalue(float64_t max_eigenvalue)
+	{
+		m_max_eigenvalue=max_eigenvalue;
+		m_is_computed_max=true;
 	}
 
 	/** @return max eigenvalue of a real valued self-adjoint linear operator */
@@ -83,6 +97,12 @@ protected:
 	/** the linear solver whose eigenvalues have to be found */
 	CLinearOperator<float64_t>* m_linear_operator;
 
+	/** flag that denotes that the minimum eigenvalue is already computed */
+	bool m_is_computed_min;
+
+	/** flag that denotes that the maximum eigenvalue is already computed */
+	bool m_is_computed_max;
+
 private:
 	/** initialize with default values and register params */
 	void init()
@@ -90,6 +110,8 @@ private:
 		m_min_eigenvalue=0.0;
 		m_max_eigenvalue=0.0;
 		m_linear_operator=NULL;
+		m_is_computed_min=false;
+		m_is_computed_max=false;
 	
 		SG_ADD(&m_min_eigenvalue, "min_eigenvalue",
 			"Minimum eigenvalue of a real valued self-adjoint linear operator",
@@ -102,6 +124,15 @@ private:
 		SG_ADD((CSGObject**)&m_linear_operator, "linear_operator",
 			"Self-adjoint linear operator",
 			MS_NOT_AVAILABLE);
+
+		SG_ADD(&m_is_computed_min, "is_computed_min",
+			"Flag denoting that the minimum eigenvalue has already been computed",
+			MS_NOT_AVAILABLE);
+
+		SG_ADD(&m_max_eigenvalue, "is_computed_max",
+			"Flag denoting that the maximum eigenvalue has already been computed",
+			MS_NOT_AVAILABLE);
+
 	}
 };
 
