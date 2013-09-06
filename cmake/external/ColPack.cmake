85b5000ed2ce4d0e04dbc89c4c4e2bd556e45671
Viktor Gal <viktor.gal@maeth.com>
2013-09-06 10:53:54 +0200

    Little fix for external ColPack.cmake issue #1529

--- cmake/external/ColPack.cmake
+++ cmake/external/ColPack.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(
 	DOWNLOAD_DIR ${THIRD_PARTY_DIR}/COLPACK
 	URL http://cscapes.cs.purdue.edu/download/ColPack/ColPack-1.0.9.tar.gz
 	URL_MD5 54ae2daacd00a0a278d2e4fa94bba81b
-	CONFIGURE_COMMAND rm <SOURCE_DIR>/config.status && <SOURCE_DIR>/configure --includedir=${THIRD_PARTY_DIR}/include/ColPack --libdir=${THIRD_PARTY_DIR}/libs/ColPack --disable-shared --with-pic
+	CONFIGURE_COMMAND <SOURCE_DIR>/configure --includedir=${THIRD_PARTY_DIR}/include/ColPack --libdir=${THIRD_PARTY_DIR}/libs/ColPack --disable-shared --with-pic
 	)
 
 SET(COLPACK_INCLUDE_DIRS ${THIRD_PARTY_DIR}/include/ColPack)
