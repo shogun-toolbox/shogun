49803e099263d6ba7ae48aca6a2008db3d0efad5
Viktor Gal <viktor.gal@maeth.com>
2013-09-07 09:14:22 +0200

    Fix issue #1523 Add ARPREC detection and bundle option to cmake build

---
+++ cmake/FindARPREC.cmake
@@ -0,0 +1,13 @@
+
+find_path(ARPREC_INCLUDE_DIR arprec/mp_real.h HINTS $ENV{ARPREC_DIR}/include)
+find_library(ARPREC_LIBRARIES arprec HINTS $ENV{ARPREC_DIR}/lib)
+
+# handle REQUIRED and QUIET options
+include (FindPackageHandleStandardArgs)
+if (CMAKE_VERSION LESS 2.8.3)
+  find_package_handle_standard_args (ARPREC DEFAULT_MSG ARPREC_LIBRARIES ARPREC_INCLUDE_DIR)
+else ()
+  find_package_handle_standard_args (ColPack REQUIRED_VARS ARPREC_LIBRARIES ARPREC_INCLUDE_DIR)
+endif ()
+
+mark_as_advanced(ARPREC_INCLUDE_DIR ARPREC_LIBRARIES)
