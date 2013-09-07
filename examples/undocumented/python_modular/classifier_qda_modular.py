shogun-toolbox/shogun

commit 03c02571f146a1f83c7cd675a1731cfc34da28b4

convert more examples to CSVFile

examples/undocumented/python_modular/classifier_qda_modular.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
-from tools.load import LoadMatrix
-lm = LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
-testdat = lm.load_numbers('../data/fm_test_real.dat')
-label_traindat = lm.load_labels('../data/label_train_multiclass.dat')
+traindat = '../data/fm_train_real.dat'
+testdat = '../data/fm_test_real.dat'
+label_traindat = '../data/label_train_multiclass.dat'
 
 parameter_list = [[traindat, testdat, label_traindat, 1e-4, False], \
 		  [traindat, testdat, label_traindat, 1e-4, True]]
 
-def classifier_qda_modular (fm_train_real=traindat, fm_test_real=testdat, label_train_twoclass=label_traindat, tolerance=1e-4, store_covs=False):
+def classifier_qda_modular (train_fname=traindat, test_fname=testdat, label_fname=label_traindat, tolerance=1e-4, store_covs=False):
 	from modshogun import RealFeatures, MulticlassLabels
-	from modshogun import QDA
+	from modshogun import QDA, CSVFile
 
 	feats_train = RealFeatures(fm_train_real)
 	feats_test  = RealFeatures(fm_test_real)
 
-	labels = MulticlassLabels(label_train_twoclass)
+	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_test=RealFeatures(CSVFile(test_fname))
+	labels=BinaryLabels(CSVFile(label_fname))
 
 	qda = QDA(feats_train, labels, tolerance, store_covs)
 	qda.train()

examples/undocumented/python_modular/classifier_svmsgd_modular.py
@@ -1,38 +1,27 @@
 #!/usr/bin/env python
-from tools.load import LoadMatrix
-lm=LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
-testdat = lm.load_numbers('../data/fm_test_real.dat')
-label_traindat = lm.load_labels('../data/label_train_twoclass.dat')
+traindat = '../data/fm_train_real.dat'
+testdat = '../data/fm_test_real.dat'
+label_traindat = '../data/label_train_twoclass.dat'
 
 parameter_list = [[traindat,testdat,label_traindat,0.9,1,6],[traindat,testdat,label_traindat,0.8,1,5]]
 
-def classifier_svmsgd_modular (fm_train_real=traindat,fm_test_real=testdat,label_train_twoclass=label_traindat,C=0.9,num_threads=1,num_iter=5):
-
+def classifier_svmsgd_modular (train_fname=traindat,test_fname=testdat,label_fname=label_traindat,C=0.9,num_threads=1,num_iter=5):
 	from modshogun import RealFeatures, SparseRealFeatures, BinaryLabels
-	from modshogun import SVMSGD
+	from modshogun import SVMSGD, CSVFile
 
-	realfeat=RealFeatures(fm_train_real)
-	feats_train=SparseRealFeatures()
-	feats_train.obtain_from_simple(realfeat)
-	realfeat=RealFeatures(fm_test_real)
-	feats_test=SparseRealFeatures()
-	feats_test.obtain_from_simple(realfeat)
-
-	labels=BinaryLabels(label_train_twoclass)
+	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_test=RealFeatures(CSVFile(test_fname))
+	labels=BinaryLabels(CSVFile(label_fname))
 
 	svm=SVMSGD(C, feats_train, labels)
 	svm.set_epochs(num_iter)
 	#svm.io.set_loglevel(0)
 	svm.train()
 
-	svm.set_features(feats_test)
-	svm.apply().get_labels()
-	predictions = svm.apply()
+	bias=svm.get_bias()
+	w=w.get_w()
+	predictions = svm.apply(feats_test)
 	return predictions, svm, predictions.get_labels()
-
-
 
 if __name__=='__main__':
 	print('SVMSGD')

examples/undocumented/python_modular/clustering_hierarchical_modular.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
-from tools.load import LoadMatrix
-lm=LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
+traindat = '../data/fm_train_real.dat'
 
 parameter_list = [[traindat,3],[traindat,4]]
 
 def clustering_hierarchical_modular (fm_train=traindat,merges=3):
+	from modshogun import EuclideanDistance, RealFeatures, Hierarchical, CSVFile
 
-	from modshogun import EuclideanDistance
-	from modshogun import RealFeatures
-	from modshogun import Hierarchical
-
-
-	feats_train=RealFeatures(fm_train)
+	feats_train=RealFeatures(CSVFile(train_fname))
 	distance=EuclideanDistance(feats_train, feats_train)
 
 	hierarchical=Hierarchical(merges, distance)

examples/undocumented/python_modular/clustering_kmeans_modular.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python
-##!/usr/bin/env python
-#"""
-#Explicit examples on how to use clustering
-#"""
-from tools.load import LoadMatrix
-lm=LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
+traindat = '../data/fm_train_real.dat'
 
 parameter_list = [[traindat,3],[traindat,4]]
 
 def clustering_kmeans_modular (fm_train=traindat,k=3):
-
-	from modshogun import EuclideanDistance
-	from modshogun import RealFeatures
-	from modshogun import KMeans
-	from modshogun import Math_init_random
+	from modshogun import EuclideanDistance, RealFeatures, KMeans, Math_init_random, CSVFile
 	Math_init_random(17)
 
-	feats_train=RealFeatures(fm_train)
+	feats_train=RealFeatures(CSVFile(train_fname))
 	distance=EuclideanDistance(feats_train, feats_train)
 
 	kmeans=KMeans(k, distance)

examples/undocumented/python_modular/distance_euclidean_modular.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
-from tools.load import LoadMatrix
-lm=LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
-testdat = lm.load_numbers('../data/fm_test_real.dat')
+traindat = '../data/fm_train_real.dat'
+testdat = '../data/fm_test_real.dat'
 
 parameter_list = [[traindat,traindat],[traindat,testdat]]
 
-def distance_euclidean_modular(fm_train_real=traindat,fm_test_real=testdat):
+def distance_euclidean_modular(train_fname=traindat,test_fname=testdat):
 
-	from modshogun import RealFeatures
-	from modshogun import EuclideanDistance
+	from modshogun import RealFeatures, EuclideanDistance
 
-	feats_train=RealFeatures(fm_train_real)
-	feats_test=RealFeatures(fm_test_real)
+	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_test=RealFeatures(CSVFile(test_fname))
 
 	distance=EuclideanDistance(feats_train, feats_train)
 

examples/undocumented/python_modular/classifier_lda_modular.py
@@ -16,10 +16,9 @@ def classifier_lda_modular (train_fname=traindat,test_fname=testdat,label_fname=
 	lda=LDA(gamma, feats_train, labels)
 	lda.train()
 
-	lda.get_bias()
-	lda.get_w()
-	lda.set_features(feats_test)
-	predictions = lda.apply().get_labels()
+	bias=lda.get_bias()
+	w=lda.get_w()
+	predictions = lda.apply(feats_test).get_labels()
 	return lda,predictions
 
 if __name__=='__main__':

examples/undocumented/python_modular/classifier_svmlin_modular.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
-from tools.load import LoadMatrix
-lm=LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
-testdat = lm.load_numbers('../data/fm_test_real.dat')
-label_traindat = lm.load_labels('../data/label_train_twoclass.dat')
+traindat = '../data/fm_train_real.dat'
+testdat = '../data/fm_test_real.dat'
+label_traindat = '../data/label_train_twoclass.dat'
 
 parameter_list = [[traindat,testdat,label_traindat,0.9,1e-5,1],[traindat,testdat,label_traindat,0.8,1e-5,1]]
 
-def classifier_svmlin_modular (fm_train_real=traindat,fm_test_real=testdat,label_train_twoclass=label_traindat,C=0.9,epsilon=1e-5,num_threads=1):
+def classifier_svmlin_modular (train_fname=traindat,test_fname=testdat,label_fname=label_traindat,C=0.9,epsilon=1e-5,num_threads=1):
 	from modshogun import RealFeatures, SparseRealFeatures, BinaryLabels
-	from modshogun import SVMLin
+	from modshogun import SVMLin, CSVFile
 
-	realfeat=RealFeatures(fm_train_real)
-	feats_train=SparseRealFeatures()
-	feats_train.obtain_from_simple(realfeat)
-	realfeat=RealFeatures(fm_test_real)
-	feats_test=SparseRealFeatures()
-	feats_test.obtain_from_simple(realfeat)
-
-	labels=BinaryLabels(label_train_twoclass)
+	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_test=RealFeatures(CSVFile(test_fname))
+	labels=BinaryLabels(CSVFile(label_fname))
 
 	svm=SVMLin(C, feats_train, labels)
 	svm.set_epsilon(epsilon)
@@ -27,13 +19,10 @@ def classifier_svmlin_modular (fm_train_real=traindat,fm_test_real=testdat,label
 	svm.set_bias_enabled(True)
 	svm.train()
 
-	svm.set_features(feats_test)
-	svm.get_bias()
-	svm.get_w()
-	svm.apply().get_labels()
-	predictions = svm.apply()
+	bias=svm.get_bias()
+	w=svm.get_w()
+	predictions = svm.apply(feats_test)
 	return predictions, svm, predictions.get_labels()
-
 
 if __name__=='__main__':
 	print('SVMLin')

examples/undocumented/python_modular/classifier_svmocas_modular.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python
-from tools.load import LoadMatrix
-lm=LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
-testdat = lm.load_numbers('../data/fm_test_real.dat')
-label_traindat = lm.load_labels('../data/label_train_twoclass.dat')
+traindat = '../data/fm_train_real.dat'
+testdat = '../data/fm_test_real.dat'
+label_traindat = '../data/label_train_twoclass.dat'
 
 parameter_list = [[traindat,testdat,label_traindat,0.9,1e-5,1],[traindat,testdat,label_traindat,0.8,1e-5,1]]
 
-def classifier_svmocas_modular (fm_train_real=traindat,fm_test_real=testdat,label_train_twoclass=label_traindat,C=0.9,epsilon=1e-5,num_threads=1):
+def classifier_svmocas_modular (train_fname=traindat,test_fname=testdat,label_fname=label_traindat,C=0.9,epsilon=1e-5,num_threads=1):
+	from modshogun import RealFeatures, BinaryLabels
+	from modshogun import SVMOcas, CSVFile
 
-	from modshogun import RealFeatures, SparseRealFeatures, BinaryLabels
-	from modshogun import SVMOcas
-
-	realfeat=RealFeatures(fm_train_real)
-	feats_train=SparseRealFeatures()
-	feats_train.obtain_from_simple(realfeat)
-	realfeat=RealFeatures(fm_test_real)
-	feats_test=SparseRealFeatures()
-	feats_test.obtain_from_simple(realfeat)
-
-	labels=BinaryLabels(label_train_twoclass)
+	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_test=RealFeatures(CSVFile(test_fname))
+	labels=BinaryLabels(CSVFile(label_fname))
 
 	svm=SVMOcas(C, feats_train, labels)
 	svm.set_epsilon(epsilon)
@@ -28,9 +19,9 @@ def classifier_svmocas_modular (fm_train_real=traindat,fm_test_real=testdat,labe
 	svm.set_bias_enabled(False)
 	svm.train()
 
-	svm.set_features(feats_test)
-	svm.apply().get_labels()
-	predictions = svm.apply()
+	bias=svm.get_bias()
+	w=svm.get_w()
+	predictions = svm.apply(feats_test)
 	return predictions, svm, predictions.get_labels()
 
 if __name__=='__main__':

commit 976c052fda479555b8c5496c6db3db4e21d3bad8

Fix bugs in python modular examples introduced by 03c02571f1

examples/undocumented/python_modular/classifier_qda_modular.py
@@ -10,12 +10,9 @@ def classifier_qda_modular (train_fname=traindat, test_fname=testdat, label_fnam
 	from modshogun import RealFeatures, MulticlassLabels
 	from modshogun import QDA, CSVFile
 
-	feats_train = RealFeatures(fm_train_real)
-	feats_test  = RealFeatures(fm_test_real)
-
 	feats_train=RealFeatures(CSVFile(train_fname))
 	feats_test=RealFeatures(CSVFile(test_fname))
-	labels=BinaryLabels(CSVFile(label_fname))
+	labels=MulticlassLabels(CSVFile(label_fname))
 
 	qda = QDA(feats_train, labels, tolerance, store_covs)
 	qda.train()

examples/undocumented/python_modular/classifier_svmsgd_modular.py
@@ -19,7 +19,7 @@ def classifier_svmsgd_modular (train_fname=traindat,test_fname=testdat,label_fna
 	svm.train()
 
 	bias=svm.get_bias()
-	w=w.get_w()
+	w=svm.get_w()
 	predictions = svm.apply(feats_test)
 	return predictions, svm, predictions.get_labels()
 

examples/undocumented/python_modular/clustering_hierarchical_modular.py
@@ -6,7 +6,7 @@ parameter_list = [[traindat,3],[traindat,4]]
 def clustering_hierarchical_modular (fm_train=traindat,merges=3):
 	from modshogun import EuclideanDistance, RealFeatures, Hierarchical, CSVFile
 
-	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_train=RealFeatures(CSVFile(fm_train))
 	distance=EuclideanDistance(feats_train, feats_train)
 
 	hierarchical=Hierarchical(merges, distance)

examples/undocumented/python_modular/clustering_kmeans_modular.py
@@ -7,7 +7,7 @@ def clustering_kmeans_modular (fm_train=traindat,k=3):
 	from modshogun import EuclideanDistance, RealFeatures, KMeans, Math_init_random, CSVFile
 	Math_init_random(17)
 
-	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_train=RealFeatures(CSVFile(fm_train))
 	distance=EuclideanDistance(feats_train, feats_train)
 
 	kmeans=KMeans(k, distance)

examples/undocumented/python_modular/distance_euclidean_modular.py
@@ -6,7 +6,7 @@ parameter_list = [[traindat,traindat],[traindat,testdat]]
 
 def distance_euclidean_modular(train_fname=traindat,test_fname=testdat):
 
-	from modshogun import RealFeatures, EuclideanDistance
+	from modshogun import RealFeatures, EuclideanDistance, CSVFile
 
 	feats_train=RealFeatures(CSVFile(train_fname))
 	feats_test=RealFeatures(CSVFile(test_fname))