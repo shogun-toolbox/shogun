6197bb4ab279aec6cb3d84c6c5d6fbe87ccf22a8
Soeren Sonnenburg <sonne@debian.org>
2013-09-19 23:20:33 +0200

    convert more examples to CSVFile

--- examples/undocumented/python_modular/kernel_sigmoid_modular.py
+++ examples/undocumented/python_modular/kernel_sigmoid_modular.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
-from tools.load import LoadMatrix
-lm=LoadMatrix()
-
-traindat = lm.load_numbers('../data/fm_train_real.dat')
-testdat = lm.load_numbers('../data/fm_test_real.dat')
+traindat = '../data/fm_train_real.dat'
+testdat = '../data/fm_test_real.dat'
 
 parameter_list = [[traindat,testdat,10,1.2,1.3],[traindat,testdat,10,1.2,1.3]]
 
-def kernel_sigmoid_modular (fm_train_real=traindat,fm_test_real=testdat,size_cache=10,gamma=1.2,coef0=1.3):
+def kernel_sigmoid_modular (train_fname=traindat,test_fname=testdat,size_cache=10,gamma=1.2,coef0=1.3):
+	from modshogun import RealFeatures, SigmoidKernel, CSVFile
 
-	from modshogun import RealFeatures
-	from modshogun import SigmoidKernel
-
-	feats_train=RealFeatures(fm_train_real)
-	feats_test=RealFeatures(fm_test_real)
-	
+	feats_train=RealFeatures(CSVFile(train_fname))
+	feats_test=RealFeatures(CSVFile(test_fname))
 
 	kernel=SigmoidKernel(feats_train, feats_train, size_cache, gamma, coef0)
 	km_train=kernel.get_kernel_matrix()
