6f750b8ff57ac9fa49e28ceb324a3c75926d06e6
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2013-09-22 01:07:56 +0400

    Proper gdb path in gdb checker

--- cmake/checkgdb.cmake
+++ cmake/checkgdb.cmake
@@ -1,6 +1,9 @@
 execute_process(COMMAND ${EXECUTABLE} ${FILE} ${ARGS} RESULT_VARIABLE RESULT)
 if (RESULT)
-	if (GDBSCRIPT)
-		execute_process(COMMAND gdb --command=${GDBSCRIPT} --args ${EXECUTABLE} ${FILE} ${ARGS} RESULT_VARIABLE GDB_RESULT)
+	if (GDB_COMMAND AND GDB_SCRIPT)
+		execute_process(COMMAND ${GDB_COMMAND} 
+			--command=${GDB_SCRIPT} 
+			--args ${EXECUTABLE} ${FILE} ${ARGS} 
+			RESULT_VARIABLE GDB_RESULT)
 	endif()
 endif()
