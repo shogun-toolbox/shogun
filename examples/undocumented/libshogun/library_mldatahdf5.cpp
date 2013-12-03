f53fc96fd40b04221fa14c8c104ff4e55dcd402b
Soeren Sonnenburg <sonne@debian.org>
2013-12-03 08:34:12 +0100

    when mldata.org cannot be accessed w/o error - don't let the example fail

--- examples/undocumented/libshogun/library_mldatahdf5.cpp
+++ examples/undocumented/libshogun/library_mldatahdf5.cpp
@@ -3,6 +3,7 @@
 #include <shogun/lib/common.h>
 #include <shogun/lib/SGMatrix.h>
 #include <shogun/io/MLDataHDF5File.h>
+#include <shogun/io/SGIO.h>
 #include <shogun/mathematics/Math.h>
 
 using namespace shogun;
@@ -12,12 +13,19 @@ int main(int argc, char** argv)
 	init_shogun_with_defaults();
 #if defined(HAVE_HDF5) && defined( HAVE_CURL)
 	CMLDataHDF5File* hdf = new CMLDataHDF5File((char *)"australian", "/data/data");
-	float64_t* mat;
+	float64_t* mat=NULL;
 	int32_t num_feat;
 	int32_t num_vec;
-	hdf->get_matrix(mat, num_feat, num_vec);
+	try
+	{
+		hdf->get_matrix(mat, num_feat, num_vec);
+		SGMatrix<float64_t>::display_matrix(mat, num_feat, num_vec);
+	}
+	catch (ShogunException& e)
+	{
+		SG_SWARNING("%s", e.get_exception_string());
+	}
 
-	SGMatrix<float64_t>::display_matrix(mat, num_feat, num_vec);
 	SG_FREE(mat);
 	SG_UNREF(hdf);
 #endif // HAVE_CURL && HAVE_HDF5
