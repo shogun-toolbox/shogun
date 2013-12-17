32725f50f23ece9771b7fce70821f2007da99034
Saurabh7 <saurabh.mahindre@gmail.com>
2013-12-17 12:28:51 +0530

    remove file

--- tests/unit/evaluation/CrossValidation_unittest.cc
+++ tests/unit/evaluation/CrossValidation_unittest.cc
@@ -14,16 +14,16 @@
 
 using namespace shogun;
 
-TEST(standardCrossvalidationIndices,one)
+TEST(Crossvalidation,standard)
 {	
-	index_t num_indices;
+	index_t fold_sizes;
 	index_t num_labels;
 	index_t num_subsets;
 	index_t runs=100;
 
 	while (runs-->0)
 	{	
-		num_indices=0;
+		fold_sizes=0;
 		num_labels=CMath::random(10, 150);
 		num_subsets=CMath::random(1, 5);
 		index_t desired_size=CMath::round(
@@ -44,8 +44,6 @@ TEST(standardCrossvalidationIndices,one)
 		CCrossValidationSplitting* splitting=
 				new CCrossValidationSplitting(labels, num_subsets);
 
-		/* build index sets (twice to ensure memory is not leaking) */
-		splitting->build_subsets();
 		splitting->build_subsets();
 
 		for (index_t i=0; i<num_subsets; ++i)
@@ -53,13 +51,13 @@ TEST(standardCrossvalidationIndices,one)
 			SGVector<index_t> subset=splitting->generate_subset_indices(i);
 			SGVector<index_t> inverse=splitting->generate_subset_inverse(i);
 
-			num_indices = num_indices + subset.vlen;
+			fold_sizes+=subset.vlen;
 
 			EXPECT_LE(CMath::abs(subset.vlen-desired_size),1);
 			EXPECT_EQ(subset.vlen+inverse.vlen,num_labels);
 		}
 
-		EXPECT_EQ(num_indices,num_labels);
+		EXPECT_EQ(fold_sizes,num_labels);
 
 		/* clean up */
 		SG_UNREF(splitting);
@@ -67,3 +65,96 @@ TEST(standardCrossvalidationIndices,one)
 
 }
 
+TEST(Crossvalidation,stratified)
+{
+	index_t num_labels, num_classes, num_subsets, fold_sizes;
+	index_t runs=50;
+
+	while (runs-->0)
+	{	
+		fold_sizes=0;
+		num_labels=CMath::random(5, 100);
+		num_classes=CMath::random(2, 10);
+		num_subsets=CMath::random(1, 10);
+
+		/* this will throw an error */
+		if (num_labels<num_subsets)
+			continue;
+
+
+		/* build labels */
+		CMulticlassLabels* labels=new CMulticlassLabels(num_labels);
+		for (index_t i=0; i<num_labels; ++i)
+		{
+			labels->set_label(i, CMath::random()%num_classes);
+		}
+
+		SGVector<float64_t> classes=labels->get_unique_labels();
+		
+		/*No. of labels belonging to one class*/
+		SGVector<index_t> classLabels(num_classes);
+		SGVector<index_t>::fill_vector(classLabels.vector, classLabels.vlen, 0);
+			
+		/*check total no. of class labels*/
+		for (index_t i=0; i<num_clases; ++i)
+		{
+			for(index_t j=0, j<num_labels, ++j)
+			{
+			       if ((int32_t)labels->get_label(j)==i)
+			       ++classLabels.vector[i];
+			}
+		}		
+
+
+		/* build splitting strategy */
+		CStratifiedCrossValidationSplitting* splitting=
+				new CStratifiedCrossValidationSplitting(labels, num_subsets);
+
+		splitting->build_subsets();
+
+		for (index_t i=0; i<num_subsets; ++i)
+		{
+			SGVector<index_t> subset=splitting->generate_subset_indices(i);
+			SGVector<index_t> inverse=splitting->generate_subset_inverse(i);
+			
+			EXPECT_EQ(subset.vlen+inverse.vlen, num_labels);
+			fold_sizes+=subset.vlen;
+		}
+
+		EXPECT_EQ(fold_sizes, num_labels);
+
+		/* check whether number of labels in every subset is nearly equal */
+		for (index_t i=0; i<num_classes; ++i)
+		{
+			/* count number of elements for this class */
+			SGVector<index_t> temp=splitting->generate_subset_indices(0);
+			int32_t count=0;
+			int32_t total_count=0;
+			for (index_t j=0; j<temp.vlen; ++j)
+			{
+				if ((int32_t)labels->get_label(temp.vector[j])==i)
+					++count;
+			}
+
+			/* check all subsets for same ratio */
+			for (index_t j=0; j<num_subsets; ++j)
+			{
+				SGVector<index_t> subset=splitting->generate_subset_indices(j);
+				int32_t temp_count=0;
+				for (index_t k=0; k<subset.vlen; ++k)
+				{
+					if ((int32_t)labels->get_label(subset.vector[k])==i)
+						++temp_count;
+				}
+				
+				total_count+=temp_count;
+				/* at most one difference */
+				EXPECT_LE(CMath::abs(temp_count-count),1);
+			}
+			EXPECT_EQ(total_count,classLabels.vector[i]);
+		}
+
+		/* clean up */
+		SG_UNREF(splitting);
+	}
+}
