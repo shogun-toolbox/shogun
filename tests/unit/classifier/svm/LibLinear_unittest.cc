35cfec9db5c541b59db2f308f730d041cc64af97
pl8787 <pl8787@gmail.com>
2014-03-06 17:21:27 +0800

    Merge the common code into a function.

--- tests/unit/classifier/svm/LibLinear_unittest.cc
+++ tests/unit/classifier/svm/LibLinear_unittest.cc
@@ -8,9 +8,11 @@
 using namespace shogun;
 
 #ifdef HAVE_LAPACK
-TEST(LibLinearTest,train_L2R_LR)
+
+void GenerateData_L1(CDenseFeatures<float64_t>* &train_feats,
+					 CDenseFeatures<float64_t>* &test_feats,
+					 CBinaryLabels* &ground_truth)
 {
-	LIBLINEAR_SOLVER_TYPE liblinear_solver_type = L2R_LR;
 	index_t num_samples = 50;
 	CMath::init_random(5);
 	SGMatrix<float64_t> data =
@@ -29,9 +31,52 @@ TEST(LibLinearTest,train_L2R_LR)
 		labels[i/2] = (i < data.num_cols/2) ? 1.0 : -1.0;
 	}
 
-	CDenseFeatures<float64_t>* train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
-	CDenseFeatures<float64_t>* test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
-	CBinaryLabels* ground_truth = new CBinaryLabels(labels);
+	train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
+	test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
+
+	SGMatrix<float64_t> train_matrix = train_feats->get_feature_matrix();
+	SGMatrix<float64_t>::transpose_matrix(train_matrix.matrix, train_matrix.num_rows, train_matrix.num_cols);
+	train_feats = new CDenseFeatures<float64_t>(train_matrix);
+
+	ground_truth = new CBinaryLabels(labels);
+}
+
+void GenerateData_L2(CDenseFeatures<float64_t>* &train_feats,
+					 CDenseFeatures<float64_t>* &test_feats,
+					 CBinaryLabels* &ground_truth)
+{
+	index_t num_samples = 50;
+	CMath::init_random(5);
+	SGMatrix<float64_t> data =
+		CDataGenerator::generate_gaussians(num_samples, 2, 2);
+	CDenseFeatures<float64_t> features(data);
+
+	SGVector<index_t> train_idx(num_samples), test_idx(num_samples);
+	SGVector<float64_t> labels(num_samples);
+	for (index_t i = 0, j = 0; i < data.num_cols; ++i)
+	{
+		if (i % 2 == 0)
+			train_idx[j] = i;
+		else
+			test_idx[j++] = i;
+
+		labels[i/2] = (i < data.num_cols/2) ? 1.0 : -1.0;
+	}
+
+	train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
+	test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
+	ground_truth = new CBinaryLabels(labels);
+}
+
+TEST(LibLinearTest,train_L2R_LR)
+{
+	LIBLINEAR_SOLVER_TYPE liblinear_solver_type = L2R_LR;
+
+	CDenseFeatures<float64_t>* train_feats = NULL;
+	CDenseFeatures<float64_t>* test_feats = NULL;
+	CBinaryLabels* ground_truth = NULL;
+
+	GenerateData_L2(train_feats, test_feats, ground_truth);
 
 	CLibLinear* ll = new CLibLinear();
 
@@ -58,27 +103,12 @@ TEST(LibLinearTest,train_L2R_LR)
 TEST(LibLinearTest,train_L2R_L2LOSS_SVC_DUAL)
 {
 	LIBLINEAR_SOLVER_TYPE liblinear_solver_type = L2R_L2LOSS_SVC_DUAL;
-	index_t num_samples = 50;
-	CMath::init_random(5);
-	SGMatrix<float64_t> data =
-		CDataGenerator::generate_gaussians(num_samples, 2, 2);
-	CDenseFeatures<float64_t> features(data);
 
-	SGVector<index_t> train_idx(num_samples), test_idx(num_samples);
-	SGVector<float64_t> labels(num_samples);
-	for (index_t i = 0, j = 0; i < data.num_cols; ++i)
-	{
-		if (i % 2 == 0)
-			train_idx[j] = i;
-		else
-			test_idx[j++] = i;
+	CDenseFeatures<float64_t>* train_feats = NULL;
+	CDenseFeatures<float64_t>* test_feats = NULL;
+	CBinaryLabels* ground_truth = NULL;
 
-		labels[i/2] = (i < data.num_cols/2) ? 1.0 : -1.0;
-	}
-
-	CDenseFeatures<float64_t>* train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
-	CDenseFeatures<float64_t>* test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
-	CBinaryLabels* ground_truth = new CBinaryLabels(labels);
+	GenerateData_L2(train_feats, test_feats, ground_truth);
 
 	CLibLinear* ll = new CLibLinear();
 
@@ -105,27 +135,12 @@ TEST(LibLinearTest,train_L2R_L2LOSS_SVC_DUAL)
 TEST(LibLinearTest,train_L2R_L2LOSS_SVC)
 {
 	LIBLINEAR_SOLVER_TYPE liblinear_solver_type = L2R_L2LOSS_SVC;
-	index_t num_samples = 50;
-	CMath::init_random(5);
-	SGMatrix<float64_t> data =
-		CDataGenerator::generate_gaussians(num_samples, 2, 2);
-	CDenseFeatures<float64_t> features(data);
 
-	SGVector<index_t> train_idx(num_samples), test_idx(num_samples);
-	SGVector<float64_t> labels(num_samples);
-	for (index_t i = 0, j = 0; i < data.num_cols; ++i)
-	{
-		if (i % 2 == 0)
-			train_idx[j] = i;
-		else
-			test_idx[j++] = i;
+	CDenseFeatures<float64_t>* train_feats = NULL;
+	CDenseFeatures<float64_t>* test_feats = NULL;
+	CBinaryLabels* ground_truth = NULL;
 
-		labels[i/2] = (i < data.num_cols/2) ? 1.0 : -1.0;
-	}
-
-	CDenseFeatures<float64_t>* train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
-	CDenseFeatures<float64_t>* test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
-	CBinaryLabels* ground_truth = new CBinaryLabels(labels);
+	GenerateData_L2(train_feats, test_feats, ground_truth);
 
 	CLibLinear* ll = new CLibLinear();
 
@@ -152,27 +167,12 @@ TEST(LibLinearTest,train_L2R_L2LOSS_SVC)
 TEST(LibLinearTest,train_L2R_L1LOSS_SVC_DUAL)
 {
 	LIBLINEAR_SOLVER_TYPE liblinear_solver_type = L2R_L1LOSS_SVC_DUAL;
-	index_t num_samples = 50;
-	CMath::init_random(5);
-	SGMatrix<float64_t> data =
-		CDataGenerator::generate_gaussians(num_samples, 2, 2);
-	CDenseFeatures<float64_t> features(data);
 
-	SGVector<index_t> train_idx(num_samples), test_idx(num_samples);
-	SGVector<float64_t> labels(num_samples);
-	for (index_t i = 0, j = 0; i < data.num_cols; ++i)
-	{
-		if (i % 2 == 0)
-			train_idx[j] = i;
-		else
-			test_idx[j++] = i;
+	CDenseFeatures<float64_t>* train_feats = NULL;
+	CDenseFeatures<float64_t>* test_feats = NULL;
+	CBinaryLabels* ground_truth = NULL;
 
-		labels[i/2] = (i < data.num_cols/2) ? 1.0 : -1.0;
-	}
-
-	CDenseFeatures<float64_t>* train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
-	CDenseFeatures<float64_t>* test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
-	CBinaryLabels* ground_truth = new CBinaryLabels(labels);
+	GenerateData_L2(train_feats, test_feats, ground_truth);
 
 	CLibLinear* ll = new CLibLinear();
 
@@ -199,131 +199,76 @@ TEST(LibLinearTest,train_L2R_L1LOSS_SVC_DUAL)
 TEST(LibLinearTest,train_L1R_L2LOSS_SVC)
 {
 	LIBLINEAR_SOLVER_TYPE liblinear_solver_type = L1R_L2LOSS_SVC;
-		index_t num_samples = 50;
-		CMath::init_random(5);
-		SGMatrix<float64_t> data =
-			CDataGenerator::generate_gaussians(num_samples, 2, 2);
-		CDenseFeatures<float64_t> features(data);
 
-		SGVector<index_t> train_idx(num_samples), test_idx(num_samples);
-		SGVector<float64_t> labels(num_samples);
-		for (index_t i = 0, j = 0; i < data.num_cols; ++i)
-		{
-			if (i % 2 == 0)
-				train_idx[j] = i;
-			else
-				test_idx[j++] = i;
+	CDenseFeatures<float64_t>* train_feats = NULL;
+	CDenseFeatures<float64_t>* test_feats = NULL;
+	CBinaryLabels* ground_truth = NULL;
 
-			labels[i/2] = (i < data.num_cols/2) ? 1.0 : -1.0;
-		}
+	GenerateData_L1(train_feats, test_feats, ground_truth);
 
-		CDenseFeatures<float64_t>* train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
-		CDenseFeatures<float64_t>* test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
+	CLibLinear* ll = new CLibLinear();
 
-		SGMatrix<float64_t> train_matrix = train_feats->get_feature_matrix();
-		SGMatrix<float64_t>::transpose_matrix(train_matrix.matrix, train_matrix.num_rows, train_matrix.num_cols);
-		train_feats = new CDenseFeatures<float64_t>(train_matrix);
+	CBinaryLabels* pred = NULL;
+	float64_t liblin_accuracy;
+	CContingencyTableEvaluation* eval = new CContingencyTableEvaluation();
 
-		CBinaryLabels* ground_truth = new CBinaryLabels(labels);
+	ll->set_bias_enabled(true);
+	ll->set_features((CDenseFeatures<float64_t> *)(train_feats->clone()));
+	ll->set_labels(ground_truth);
 
-		CLibLinear* ll = new CLibLinear();
+	ll->set_liblinear_solver_type(liblinear_solver_type);
+	ll->train();
+	pred = ll->apply_binary(test_feats);
+	liblin_accuracy = eval->evaluate(pred, ground_truth);
 
-		CBinaryLabels* pred = NULL;
-		float64_t liblin_accuracy;
-		CContingencyTableEvaluation* eval = new CContingencyTableEvaluation();
+	EXPECT_NEAR(liblin_accuracy, 1.0, 1e-6);
 
-		ll->set_bias_enabled(true);
-		ll->set_features((CDenseFeatures<float64_t> *)(train_feats->clone()));
-		ll->set_labels(ground_truth);
-
-		ll->set_liblinear_solver_type(liblinear_solver_type);
-		ll->train();
-		pred = ll->apply_binary(test_feats);
-		liblin_accuracy = eval->evaluate(pred, ground_truth);
-
-		EXPECT_NEAR(liblin_accuracy, 1.0, 1e-6);
-
-		SG_UNREF(train_feats);
-		SG_UNREF(test_feats);
-		SG_UNREF(pred);
+	SG_UNREF(train_feats);
+	SG_UNREF(test_feats);
+	SG_UNREF(pred);
 }
 
 TEST(LibLinearTest,train_L1R_LR)
 {
 	LIBLINEAR_SOLVER_TYPE liblinear_solver_type = L1R_LR;
-		index_t num_samples = 50;
-		CMath::init_random(5);
-		SGMatrix<float64_t> data =
-			CDataGenerator::generate_gaussians(num_samples, 2, 2);
-		CDenseFeatures<float64_t> features(data);
 
-		SGVector<index_t> train_idx(num_samples), test_idx(num_samples);
-		SGVector<float64_t> labels(num_samples);
-		for (index_t i = 0, j = 0; i < data.num_cols; ++i)
-		{
-			if (i % 2 == 0)
-				train_idx[j] = i;
-			else
-				test_idx[j++] = i;
+	CDenseFeatures<float64_t>* train_feats = NULL;
+	CDenseFeatures<float64_t>* test_feats = NULL;
+	CBinaryLabels* ground_truth = NULL;
 
-			labels[i/2] = (i < data.num_cols/2) ? 1.0 : -1.0;
-		}
+	GenerateData_L1(train_feats, test_feats, ground_truth);
 
-		CDenseFeatures<float64_t>* train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
-		CDenseFeatures<float64_t>* test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
+	CLibLinear* ll = new CLibLinear();
 
-		SGMatrix<float64_t> train_matrix = train_feats->get_feature_matrix();
-		SGMatrix<float64_t>::transpose_matrix(train_matrix.matrix, train_matrix.num_rows, train_matrix.num_cols);
-		train_feats = new CDenseFeatures<float64_t>(train_matrix);
+	CBinaryLabels* pred = NULL;
+	float64_t liblin_accuracy;
+	CContingencyTableEvaluation* eval = new CContingencyTableEvaluation();
 
-		CBinaryLabels* ground_truth = new CBinaryLabels(labels);
+	ll->set_bias_enabled(true);
+	ll->set_features((CDenseFeatures<float64_t> *)(train_feats->clone()));
+	ll->set_labels(ground_truth);
 
-		CLibLinear* ll = new CLibLinear();
+	ll->set_liblinear_solver_type(liblinear_solver_type);
+	ll->train();
+	pred = ll->apply_binary(test_feats);
+	liblin_accuracy = eval->evaluate(pred, ground_truth);
 
-		CBinaryLabels* pred = NULL;
-		float64_t liblin_accuracy;
-		CContingencyTableEvaluation* eval = new CContingencyTableEvaluation();
+	EXPECT_NEAR(liblin_accuracy, 1.0, 1e-6);
 
-		ll->set_bias_enabled(true);
-		ll->set_features((CDenseFeatures<float64_t> *)(train_feats->clone()));
-		ll->set_labels(ground_truth);
-
-		ll->set_liblinear_solver_type(liblinear_solver_type);
-		ll->train();
-		pred = ll->apply_binary(test_feats);
-		liblin_accuracy = eval->evaluate(pred, ground_truth);
-
-		EXPECT_NEAR(liblin_accuracy, 1.0, 1e-6);
-
-		SG_UNREF(train_feats);
-		SG_UNREF(test_feats);
-		SG_UNREF(pred);
+	SG_UNREF(train_feats);
+	SG_UNREF(test_feats);
+	SG_UNREF(pred);
 }
 
 TEST(LibLinearTest,train_L2R_LR_DUAL)
 {
 	LIBLINEAR_SOLVER_TYPE liblinear_solver_type = L2R_LR_DUAL;
-	index_t num_samples = 50;
-	CMath::init_random(5);
-	SGMatrix<float64_t> data =
-		CDataGenerator::generate_gaussians(num_samples, 2, 2);
-	CDenseFeatures<float64_t> features(data);
 
-	SGVector<index_t> train_idx(num_samples), test_idx(num_samples);
-	SGVector<float64_t> labels(num_samples);
-	for (index_t i = 0, j = 0; i < data.num_cols; ++i)
-	{
-		if (i % 2 == 0)
-			train_idx[j] = i;
-		else
-			test_idx[j++] = i;
+	CDenseFeatures<float64_t>* train_feats = NULL;
+	CDenseFeatures<float64_t>* test_feats = NULL;
+	CBinaryLabels* ground_truth = NULL;
 
-		labels[i/2] = (i < data.num_cols/2) ? 1.0 : -1.0;
-	}
-
-	CDenseFeatures<float64_t>* train_feats = (CDenseFeatures<float64_t>*)features.copy_subset(train_idx);
-	CDenseFeatures<float64_t>* test_feats =  (CDenseFeatures<float64_t>*)features.copy_subset(test_idx);
-	CBinaryLabels* ground_truth = new CBinaryLabels(labels);
+	GenerateData_L2(train_feats, test_feats, ground_truth);
 
 	CLibLinear* ll = new CLibLinear();
 
