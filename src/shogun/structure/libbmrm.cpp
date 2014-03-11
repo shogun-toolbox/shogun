bce7c517c3c4c26b61dd5a24eb7cbf9f983f6c85
akpraharaj <adityakumarpraharaj@gmail.com>
2014-03-11 23:52:18 +0530

    removed silly mistake.

--- src/shogun/structure/libbmrm.cpp
+++ src/shogun/structure/libbmrm.cpp
@@ -49,7 +49,7 @@ void add_cutting_plane(
 		return;
 	}
 
-	cp->bmrm_ll_init(*tail, NULL, A+(free_idx*dim));
+	cp->bmrm_ll_init(*tail, NULL, A+(free_idx*dim)); 
 	(*tail)->next=cp;
 	*tail=cp;
 }
@@ -331,4 +331,280 @@ BmrmStatistics svm_bmrm_solver(
 		goto cleanup;
 	}
 
-	memset( (bool*) map, 
+	memset( (bool*) map, true, BufSize);
+
+	cp_list= (bmrm_ll*) LIBBMRM_CALLOC(1, bmrm_ll);
+
+	if (cp_list==NULL)
+	{
+		bmrm.exitflag=-2;
+		goto cleanup;
+	}
+
+	prevW= (float64_t*) LIBBMRM_CALLOC(nDim, float64_t);
+
+	if (prevW==NULL)
+	{
+		bmrm.exitflag=-2;
+		goto cleanup;
+	}
+
+	bmrm.hist_Fp = SGVector< float64_t >(histSize);
+	bmrm.hist_Fd = SGVector< float64_t >(histSize);
+	bmrm.hist_wdist = SGVector< float64_t >(histSize);
+
+	/* Iinitial solution */
+	R=machine->risk(subgrad, W);
+
+	bmrm.nCP=0;
+	bmrm.nIter=0;
+	bmrm.exitflag=0;
+
+	b[0]=-R;
+
+	/* Cutting plane auxiliary double linked list */
+
+	LIBBMRM_MEMCPY(A, subgrad, nDim*sizeof(float64_t));
+	map[0]=false;
+	cp_list->address=&A[0];
+	cp_list->idx=0;
+	cp_list->prev=NULL;
+	cp_list->next=NULL;
+	CPList_head=cp_list;
+	CPList_tail=cp_list;
+
+	/* Compute initial value of Fp, Fd, assuming that W is zero vector */
+
+	sq_norm_W=0;
+	bmrm.Fp=R+0.5*_lambda*sq_norm_W;
+	bmrm.Fd=-LIBBMRM_PLUS_INF;
+
+	tstop=ttime.cur_time_diff(false);
+
+	/* Verbose output */
+
+	if (verbose)
+		SG_SPRINT("%4d: tim=%.3lf, Fp=%lf, Fd=%lf, R=%lf\n",
+				bmrm.nIter, tstop-tstart, bmrm.Fp, bmrm.Fd, R);
+
+	/* store Fp, Fd and wdist history */
+	bmrm.hist_Fp[0]=bmrm.Fp;
+	bmrm.hist_Fd[0]=bmrm.Fd;
+	bmrm.hist_wdist[0]=0.0;
+
+	if (verbose)
+		helper = machine->get_helper();
+
+	/* main loop */
+	ASSERT(bmrm.nCP<BufSize);
+	while (bmrm.exitflag==0)
+	{
+		tstart=ttime.cur_time_diff(false);
+		bmrm.nIter++;
+
+		/* Update H */
+
+		if (bmrm.nCP>0)
+		{
+			A_2=get_cutting_plane(CPList_tail);
+			cp_ptr=CPList_head;
+
+			for (uint32_t i=0; i<bmrm.nCP; ++i)
+			{
+				A_1=get_cutting_plane(cp_ptr);
+				cp_ptr=cp_ptr->next;
+				rsum= SGVector<float64_t>::dot(A_1, A_2, nDim);
+
+				H[LIBBMRM_INDEX(bmrm.nCP, i, BufSize)]
+					= H[LIBBMRM_INDEX(i, bmrm.nCP, BufSize)]
+					= rsum/_lambda;
+			}
+		}
+
+		A_2=get_cutting_plane(CPList_tail);
+		rsum = SGVector<float64_t>::dot(A_2, A_2, nDim);
+
+		H[LIBBMRM_INDEX(bmrm.nCP, bmrm.nCP, BufSize)]=rsum/_lambda;
+
+		diag_H[bmrm.nCP]=H[LIBBMRM_INDEX(bmrm.nCP, bmrm.nCP, BufSize)];
+		I[bmrm.nCP]=1;
+
+		beta[bmrm.nCP]=0.0; // [beta; 0]
+		bmrm.nCP++;
+		ASSERT(bmrm.nCP<BufSize);
+
+#if 0
+		/* TODO: scaling...*/
+		float64_t scale = SGVector<float64_t>::max(diag_H, BufSize)/(1000.0*_lambda);
+		SGVector<float64_t> sb(bmrm.nCP);
+		sb.zero();
+		sb.vec1_plus_scalar_times_vec2(sb.vector, 1/scale, b, bmrm.nCP);
+
+		SGVector<float64_t> sh(bmrm.nCP);
+		sh.zero();
+		sb.vec1_plus_scalar_times_vec2(sh.vector, 1/scale, diag_H, bmrm.nCP);
+
+		qp_exitflag =
+			libqp_splx_solver(&get_col, sh.vector, sb.vector, &C, I, &S, beta,
+				bmrm.nCP, QPSolverMaxIter, 0.0, QPSolverTolRel, -LIBBMRM_PLUS_INF, 0);
+#else
+		/* call QP solver */
+		qp_exitflag=libqp_splx_solver(&get_col, diag_H, b, &C, I, &S, beta,
+				bmrm.nCP, QPSolverMaxIter, 0.0, QPSolverTolRel, -LIBBMRM_PLUS_INF, 0);
+#endif
+
+		bmrm.qp_exitflag=qp_exitflag.exitflag;
+
+		/* Update ICPcounter (add one to unused and reset used)
+		 * + compute number of active CPs */
+		bmrm.nzA=0;
+
+		for (uint32_t aaa=0; aaa<bmrm.nCP; ++aaa)
+		{
+			if (beta[aaa]>epsilon)
+			{
+				++bmrm.nzA;
+				icp_stats.ICPcounter[aaa]=0;
+			}
+			else
+			{
+				icp_stats.ICPcounter[aaa]+=1;
+			}
+		}
+
+		/* W update */
+		memset(W, 0, sizeof(float64_t)*nDim);
+		cp_ptr=CPList_head;
+		for (uint32_t j=0; j<bmrm.nCP; ++j)
+		{
+			A_1=get_cutting_plane(cp_ptr);
+			cp_ptr=cp_ptr->next;
+			SGVector<float64_t>::vec1_plus_scalar_times_vec2(W, -beta[j]/_lambda, A_1, nDim);
+		}
+
+		/* risk and subgradient computation */
+		R = machine->risk(subgrad, W);
+		add_cutting_plane(&CPList_tail, map, A,
+				find_free_idx(map, BufSize), subgrad, nDim);
+
+		sq_norm_W=SGVector<float64_t>::dot(W, W, nDim);
+		b[bmrm.nCP]=SGVector<float64_t>::dot(subgrad, W, nDim) - R;
+
+		sq_norm_Wdiff=0.0;
+		for (uint32_t j=0; j<nDim; ++j)
+		{
+			sq_norm_Wdiff+=(W[j]-prevW[j])*(W[j]-prevW[j]);
+		}
+
+		bmrm.Fp=R+0.5*_lambda*sq_norm_W;
+		bmrm.Fd=-qp_exitflag.QP;
+		wdist=CMath::sqrt(sq_norm_Wdiff);
+
+		/* Stopping conditions */
+		if (bmrm.Fp - bmrm.Fd <= TolRel*LIBBMRM_ABS(bmrm.Fp))
+			bmrm.exitflag=1;
+
+		if (bmrm.Fp - bmrm.Fd <= TolAbs)
+			bmrm.exitflag=2;
+
+		tstop=ttime.cur_time_diff(false);
+
+		/* Verbose output */
+		if (verbose)
+			SG_SPRINT("%4d: tim=%.3lf, Fp=%lf, Fd=%lf, (Fp-Fd)=%lf, (Fp-Fd)/Fp=%lf, R=%lf, nCP=%d, nzA=%d, QPexitflag=%d\n",
+					bmrm.nIter, tstop-tstart, bmrm.Fp, bmrm.Fd, bmrm.Fp-bmrm.Fd,
+					(bmrm.Fp-bmrm.Fd)/bmrm.Fp, R, bmrm.nCP, bmrm.nzA, qp_exitflag.exitflag);
+
+		// iteration exceeds histSize
+		if (bmrm.nIter >= histSize)
+		{
+			histSize += BufSize;
+			bmrm.hist_Fp.resize_vector(histSize);
+			bmrm.hist_Fd.resize_vector(histSize);
+			bmrm.hist_wdist.resize_vector(histSize);
+		}
+
+		/* Keep Fp, Fd and w_dist history */
+		ASSERT(bmrm.nIter < histSize);
+		bmrm.hist_Fp[bmrm.nIter]=bmrm.Fp;
+		bmrm.hist_Fd[bmrm.nIter]=bmrm.Fd;
+		bmrm.hist_wdist[bmrm.nIter]=wdist;
+
+		/* keep W (for wdist history track) */
+		LIBBMRM_MEMCPY(prevW, W, nDim*sizeof(float64_t));
+
+		/* Inactive Cutting Planes (ICP) removal */
+		if (cleanICP)
+		{
+			clean_icp(&icp_stats, bmrm, &CPList_head, &CPList_tail, H, diag_H, beta, map, cleanAfter, b, I);
+			ASSERT(bmrm.nCP<BufSize);
+		}
+
+		// next CP would exceed BufSize
+		if (bmrm.nCP+1 >= BufSize)
+			bmrm.exitflag=-1;
+
+		/* Debug: compute objective and training error */
+		if (verbose && SG_UNLIKELY(sg_io->loglevel_above(MSG_DEBUG)))
+		{
+			float64_t debug_tstart=ttime.cur_time_diff(false);
+
+			SGVector<float64_t> w_debug(W, nDim, false);
+			float64_t primal = CSOSVMHelper::primal_objective(w_debug, model, _lambda);
+			float64_t train_error = CSOSVMHelper::average_loss(w_debug, model);
+			helper->add_debug_info(primal, bmrm.nIter, train_error);
+
+			float64_t debug_tstop=ttime.cur_time_diff(false);
+			SG_SPRINT("%4d: add_debug_info: tim=%.3lf, primal=%.3lf, train_error=%lf\n",
+				bmrm.nIter, debug_tstop-debug_tstart, primal, train_error);
+		}
+
+	} /* end of main loop */
+
+	if (verbose)
+	{
+		helper->terminate();
+		SG_UNREF(helper);
+	}
+
+	ASSERT(bmrm.nIter+1 <= histSize);
+	bmrm.hist_Fp.resize_vector(bmrm.nIter+1);
+	bmrm.hist_Fd.resize_vector(bmrm.nIter+1);
+	bmrm.hist_wdist.resize_vector(bmrm.nIter+1);
+
+	cp_ptr=CPList_head;
+
+	while(cp_ptr!=NULL)
+	{
+		cp_ptr2=cp_ptr;
+		cp_ptr=cp_ptr->next;
+		LIBBMRM_FREE(cp_ptr2);
+		cp_ptr2=NULL;
+	}
+
+	cp_list=NULL;
+
+cleanup:
+
+	LIBBMRM_FREE(H);
+	LIBBMRM_FREE(b);
+	LIBBMRM_FREE(beta);
+	LIBBMRM_FREE(A);
+	LIBBMRM_FREE(subgrad);
+	LIBBMRM_FREE(diag_H);
+	LIBBMRM_FREE(I);
+	LIBBMRM_FREE(icp_stats.ICPcounter);
+	LIBBMRM_FREE(icp_stats.ICPs);
+	LIBBMRM_FREE(icp_stats.ACPs);
+	LIBBMRM_FREE(icp_stats.H_buff);
+	LIBBMRM_FREE(map);
+	LIBBMRM_FREE(prevW);
+
+	if (cp_list)
+		LIBBMRM_FREE(cp_list);
+
+	SG_UNREF(model);
+
+	return(bmrm);
+}
+}
