4f613fdbbae184ec7083cdcdd3e3e3bb6d393d32
frank0523 <zhangyuyu2008@gmail.com>
2014-03-19 23:04:10 +0800

    Add code on BP implementation

---
+++ src/shogun/deeplearning/DataAdapter.h
@@ -0,0 +1,49 @@
+#ifndef DATA_ADAPTER
+#define DATA_ADAPTER
+
+#include "TypeDefine.h"
+#include "NNConfig.h"
+#include <vector>
+
+using namespace shogun::deeplearning::typedefine;
+
+struct FeatLabelPair
+{
+	EigenDenseMat feats;
+	std::vector<int32_t> labels;
+
+	FeatLabelPair()
+	{
+		labels.clear();
+	}
+};
+
+class CDataAdapter
+{
+public:
+	CDataAdapter();
+
+	//Call before training
+	void Init(const char* _feature_file, const char* _label_file);
+	//Call before each epoch training begins
+	void Open();
+	//Call before each batch training begins
+	int32_t GetBatchSamples(int32_t batch_size, void*& samples);
+	//Call after each epoch training ends (prepare for the next epoch)
+	void Close();
+	//Call after all training done
+	void Destroy();
+
+protected:
+	int32_t LoadBuffer();
+	int32_t LoadMatrix(EigenDenseMat &m, int32_t rows, int32_t cols, FILE* fid);
+
+	int32_t feat_cnt, label_cnt;
+	EigenDenseMat feat_buffer, label_buffer;
+
+	FILE* fid_feat, *fid_label;
+	const char* feature_file;
+	const char* label_file;
+};
+
+#endif
---
+++ src/shogun/deeplearning/NNConfig.cpp
@@ -0,0 +1,43 @@
+#include "NNConfig.h"
+
+//Basic parameters of NN
+int32_t CNNConfig::epoch = 0;
+int32_t CNNConfig::batchsize = 0;
+int32_t CNNConfig::layers = 0;
+int32_t* CNNConfig::length = NULL;
+RuntimeConfig CNNConfig::rtc;
+TrainingOptions CNNConfig::opt;
+
+//Datasets for train/test
+const char* CNNConfig::trainx = NULL;
+const char* CNNConfig::trainy = NULL;
+const char* CNNConfig::testx = NULL;
+const char* CNNConfig::testy = NULL;
+
+TrainingOptions::TrainingOptions()
+{
+	act_type = CFuncType::RECTIFIED;
+	out_type = CFuncType::SOFTMAX;
+	weightPenaltyL2 = 0.001;
+	isGradChecking = false; //Flag for gradient checking
+}
+
+void CNNConfig::Initialize()
+{
+	//To be further updated as parameters setting interfaces
+	epoch = 1;
+	batchsize = 100;
+
+	std::stringstream layer_config;
+	//config string for layer setting: layer#, followed by unit# in each layer
+	layer_config << "4 784 1024 1024 10";
+	layer_config >> layers;
+	length = (int32_t*)malloc(sizeof(int32_t) * layers);
+	for (int32_t i = 0; i < layers; ++i)
+		layer_config >> length[i];
+
+	trainx = "Dataset/MNIST_TrainX.dat";
+	trainy = "Dataset/MNIST_TrainY.dat";
+	testx = "Dataset/MNIST_TestX.dat";
+	testy = "Dataset/MNIST_TestY.dat";
+}
