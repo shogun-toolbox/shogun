9ec2a1b4a43ceb56686d533b52ebf650ae5f082c
grecocd <cassiodgreco@gmail.com>
2014-03-22 14:30:53 -0400

    KDE Iteration 2 - Need Feedback

--- src/shogun/distributions/KDE.cpp
+++ src/shogun/distributions/KDE.cpp
@@ -4,44 +4,72 @@ Created by Cassio Greco <cassiodgreco@gmail.com>
 
 #include "KDE.h"
 #include <shogun/multiclass/KNN.cpp>
-#include <shogun/lib/SGMatrix.cpp> 
-#include <shogun/distributions/Gaussian.cpp> //Kernel
+#include <shogun/lib/SGMatrix.cpp>
+#include <shogun/distributions/GaussianKernel.cpp> //Kernel
+#include <shogun/features/Features.cpp>
+//#include <shogun/features/DenseFeatures.h> Does it need dense features?
 
 using namespace shogun;
 
 //Constructor of the class. All predefined values are assinged
-KDE::KDE(rows,columns){ 
-    	this->algorithm = "knn";
-        //this->bandwidth = 1.0;
-        this->kernel = "Gaussian"; //CKernel* when the user will be able to choose the kernel
-        //this->metric = metric;
-        this->points = this->points.matrix(rows,columns); //Creates a SGMatrix. Contains the data
-        this->log_density = 0;
-
+CKDE::CKDE(int32_t rows,int32_t columns,CFeatures* datas,bool origin)
+{
+    	this->algorithm="knn";
+    	this->origin=origin;
+    	this->num_rows=rows;
+    	this->num_cols=cols;
+        //this->bandwidth=1.0;
+        this->data=datas;
+        this->kernel="Gaussian"; //CKernel* when the user will be able to choose the kernel
+        this->points(rows,columns,true); //Creates a SGMatrix. Contains the data
+        this->points=this->setData(origin,this->points,this->data);
+        this->log_density=0;
 }
 
+//Method that takes the SGMatrix points, the data that will be used in the algorithm
+//and the boolean value origin.
+//If origin is true, it means that the data is loaded from a file.
+//Is called by the constructor.
+SGMatrix CKDE::set_data(bool origin,SGMatrix points,CFeatures data)
+{
+	if origin
+		points.load();
+	else
+		points=data;
+	return points;
+}
 
 //Method that calls the KNN algorithm and the score method
-KDE& KDE::compute(){
+CKDE& CKDE::compute()
+{
 	//Calls the KNN algorithm with the points in the SGMatrix
-	this->points = this->nearest_neighbors(); 
-	this->log_density = this->score();
-	return *this;
+	this->points=this->points.nearest_neighbors();
+	//Calls the method that calculates the kernel weight function
+	this->log_density=this->kernel_weight();
+	return this->log_density;
 }
 
-//Method that computes the Gaussian log_PDF
-float64_t KDE::score(){
+//Method that computes the Gaussian Kernel(?) weight function
+//Returns an SGVector with the value of each kernel function in each set of points in points
+SGVector<float64_t> CKDE::kernel_weight()
+{
 	//Gaussian PDF calculation . Uses SGVector as parameter, not SGMatrix -> needs to be fixed
-	float64_t density = compute_log_PDF(this->points); 
+	SGVector<float64_t> density(this->num_rows*this->num_cols,true);
+	int i=0,j=0;
+	for (i=0;i<this->num_rows;i++)
+		for (j=0;j<this->num_cols;j++)
+			density.set_element(this->points.compute(i,j),i+j);
 	return density;
 }
 
+/*
 //Function that calcuates the samples needed for the Gaussian kernel
-SGVector<float64_t> KDE::sample_values(){
+SGVector<float64_t> CKDE::sample_values(){
 	//sample() is a method from Gaussian. Used to calculate the samples needed for the kernel
 	SGVector<float64_t> samples = sample(); 
 	return samples;
 }
+*/
 
 //Function for the user to choose which kernel to use. There are so many in the kernel and 
 /*
@@ -50,7 +78,7 @@ KDE& KDE::choose_kernel(*Ckernel){
 }
 */
 
-KDE::~KDE()
+CKDE::~CKDE()
 {
     cleanup();
 }
--- src/shogun/distributions/KDE.h
+++ src/shogun/distributions/KDE.h
@@ -7,25 +7,27 @@ Created by Cassio Greco <cassiodgreco@gmail.com>
 
 #include <shogun/multiclass/KNN.h>
 #include <shogun/lib/SGMatrix.cpp>
+#include <shogun/features/Features.cpp>
 
 using namespace shogun;
 
-
-
-class KDE: public CKNN{
+class CKDE{
     public:
-        KDE(*); // Constructor
-        virtual ~KDE(); // Destructor
-        KDE compute();
-        float64_t score();
-        SGVector<float64_t> sample_values();
+        CKDE(int32_t,int32_t,CFeatures*,bool);//Constructor
+        virtual ~CKDE();//Destructor
+        CKDE compute();
+        SGMatrix set_data(bool, SGMatrix, CFeatures);
+        SGVector<float64_t> kernel_weight();
+        //SGVector<float64_t> sample_values();
     protected:
     	//float bandwidth;
     	char *algorithm;
     	char *kernel
-    	//char metric;
     	SGMatrix points; //Do I specify the size?
-    	float64_t log_density;
+    	SGVector<float64_t> log_density;
+        CFeatures data;
+        bool origin; //True if matrix comes from a file. False if it is not.
+        int32_t num_rows,num_cols;
     private: 
 };
 
