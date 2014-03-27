75163caaf20732f52f4d7f2ff2293bf25ae68054
Wu Lin <yorker.lin@gmail.com>
2014-03-27 01:29:44 -0400

    update the header file

--- src/shogun/machine/gp/LogitPiecewiseBoundLikelihood.h
+++ src/shogun/machine/gp/LogitPiecewiseBoundLikelihood.h
@@ -27,8 +27,6 @@
  * of the authors and should not be interpreted as representing official policies,
  * either expressed or implied, of the Shogun Development Team.
  *
- * Code adapted from Gaussian Process Machine Learning Toolbox
- * http://www.gaussianprocess.org/gpml/code/matlab/doc/
  */
 
 #ifndef _LOGITPIECEWISEBOUNDLIKELIHOOD_H_
@@ -39,15 +37,26 @@
 #ifdef HAVE_EIGEN3
 
 #include <shogun/machine/gp/LogitLikelihood.h>
+#include <shogun/mathematics/eigen3.h>
+#include <shogun/mathematics/Statistics.h>
+#include <shogun/mathematics/Math.h>
+#include <shogun/labels/BinaryLabels.h>
 
 namespace shogun
 {
+
+
 
 /** @brief Class that models Logit likelihood with variational piecewise bound
  *
  */
 class CLogitPiecewiseBoundLikelihood : public CLogitLikelihood
 {
+typedef enum _my_bsxfunOp
+{
+	plus,
+	times
+} MyBsxfunOp;
 public:
 	CLogitPiecewiseBoundLikelihood();
 
@@ -60,47 +69,92 @@ public:
 	virtual const char* get_name() const { return "LogitPiecewiseBoundLikelihood"; }
 
 
+
+
 	/** set the variational piecewise bound for logit likelihood
 	 *
 	 *  @param bound variational piecewise bound
 	 */
-	virtual void setbound(const SGMatrix<float64_t> & bound);
+	virtual void set_bound(SGMatrix<float64_t> bound);
 
-	/** get model type
+
+	/** set the variational normal distribution given data and parameters
 	 *
-	 * @return model type Logit
+	 * @param mu mean of the variational normal distribution
+	 * @param s2 variance of the variational normal distribution
+	 * @param lab labels/data used
+	 *
 	 */
-	virtual ELikelihoodModelType get_model_type() const { return LT_LOGIT; }
+	virtual void set_distribution(SGVector<float64_t> mu, SGVector<float64_t> s2, const CLabels* lab);
 
 	/** returns the expection of the logarithm of a logit distribution 
 	 * wrt the variational distribution using piecewise bound
 	 *
-	 * @param lab labels used
-	 * @param mu mean of the variational normal distribution
-	 * @param s2 variance of the variational normal distribution
-	 *
 	 * @return expection
 	 */
-	virtual SGVector<float64_t> get_variational_expection(SGVector<float64_t> mu,
-			SGVector<float64_t> s2, const CLabels* lab);
+	virtual SGVector<float64_t> get_variational_expection();
 
 	/** get derivative of the variational expection of log LogitLikelihood
 	 * using the piecewise bound with respect to given parameter
 	 *
-	 * @param lab labels used
-	 * @param mu mean of the variational normal distribution
-	 * @param s2 variance of the variational normal distribution
 	 * @param param parameter
 	 *
 	 * @return derivative
 	 */
-	virtual SGVector<float64_t> get_variational_first_derivative(const CLabels* lab,
-			SGVector<float64_t> mu, SGVector<float64_t> s2, 
-			const TParameter* param) const;
+	virtual SGVector<float64_t> get_variational_first_derivative(const TParameter* param) const;
 private:
 	void init();
 
-	SGMatrix<float64_t>  m_bound;
+	void clean();
+
+	void precompute();
+
+	void init_matrix(SGMatrix<float64_t> ** ptr, index_t num_rows, index_t num_cols);
+
+	SGMatrix<float64_t> * m_bound;
+
+	SGVector<float64_t> * m_mu;
+
+	SGVector<float64_t> * m_s2;
+
+	SGVector<float64_t> * m_lab;
+
+	//rows = l.size   cols = m.size
+	SGMatrix<float64_t> * m_pl;
+
+	SGMatrix<float64_t> * m_ph;
+
+	SGMatrix<float64_t> * m_cdf_diff;
+
+	SGMatrix<float64_t> * m_l2_plus_s2;
+
+	SGMatrix<float64_t> * m_h2_plus_s2;
+
+	SGMatrix<float64_t> * m_weighted_pdf_diff;
+
+
+	// It seems that pl8787 tries to include it into the cade base
+	// Temporarily add here for unit test
+	static Eigen::MatrixXd my_bsxfun_vec(MyBsxfunOp op, const Eigen::MatrixXd & x,
+		const Eigen::VectorXd & y, bool is_col_vec);
+
+	template<typename M1, typename M2>
+	static Eigen::MatrixXd my_bsxfun(MyBsxfunOp op, const Eigen::MatrixBase<M1> & x,
+		const Eigen::MatrixBase<M2> & y);
+
+	static float64_t _standard_norm_pdf(float64_t x);
+
+	template<typename M1>
+	static Eigen::MatrixXd standard_norm_pdf(const Eigen::MatrixBase<M1> &x);
+
+	static float64_t _norm_cdf_minus_const(float64_t x);
+
+	template<typename M1>
+	static Eigen::MatrixXd normal_cdf_minus_const(const Eigen::MatrixBase<M1> &x);
+
+	static float64_t _check_variance(float64_t x);
+	static float64_t _convert_label(float64_t x);
+	// end for "temporarily add for unit test"
 };
 }
 #endif /* HAVE_EIGEN3 */
