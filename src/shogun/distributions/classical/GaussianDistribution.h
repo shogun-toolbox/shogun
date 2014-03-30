143a7ba7d3c9a15850bab2abcff5cc033ad06093
Wu Lin <yorker.lin@gmail.com>
2014-03-29 23:34:31 -0400

    update the GaussianDistribution class

--- src/shogun/distributions/classical/GaussianDistribution.h
+++ src/shogun/distributions/classical/GaussianDistribution.h
@@ -1,10 +1,33 @@
 /*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
+ * Copyright (c) The Shogun Machine Learning Toolbox
+ * Written (w) 2014 Wu Lin
  * Written (W) 2013 Heiko Strathmann
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Shogun Development Team.
+ *
  */
 #ifdef HAVE_EIGEN3
 
@@ -13,6 +36,7 @@
 
 #include <shogun/distributions/classical/ProbabilityDistribution.h>
 #include <shogun/lib/SGVector.h>
+#include <shogun/mathematics/Math.h>
 
 namespace shogun
 {
@@ -87,6 +111,24 @@ public:
 		return "GaussianDistribution";
 	}
 
+
+	/** Computes the univariate pdf for a given samples.
+	 *
+	 * @param sample is a given sample 
+	 * @param mu is the mean of univariate Normal distribution (defaul value is 0.0)
+	 * @param sigma2 is the variance of univariate Normal distribution (defaul value is 1.0)
+	 * @return the pdf of the distribution given the sample
+	 */
+	static float64_t get_univariate_pdf(float64_t sample, float64_t mu = 0.0, float64_t sigma2 = 1.0)
+	{
+		REQUIRE(sigma2 > 0, "Variance should be positive\n");
+
+		if(sample == CMath::INFTY || sample == -CMath::INFTY)
+			return 0.0;
+		else
+			return CMath::exp(-0.5*(CMath::pow(sample-mu, 2)/sigma2
+					+ CMath::log(2.0 * CMath::PI) + CMath::log(sigma2)));
+	}
 private:
 
 	/** Initialses and registers parameters */
