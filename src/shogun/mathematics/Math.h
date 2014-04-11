517a5f97c2a49693447235ef3c7a8ab19f2e7d7e
Wu Lin <yorker.lin@gmail.com>
2014-04-11 17:57:46 -0400

    add doc

--- src/shogun/mathematics/Math.h
+++ src/shogun/mathematics/Math.h
@@ -269,20 +269,20 @@ class CMath : public CSGObject
 		 *
 		 * Note that a unit test will be passed only when 
 		 * \f[
-		 * |v_{true} - v_{predict}| <= tol_{relative} * |v_{true}|
+		 * |v_\text{true} - v_\text{predict}| \leq tol_\text{relative} * |v_\text{true}|
 		 * \f] which is equivalent to 
 		 * \f[
-		 * |v_{true} - v_{predict}| <= tol_{absolute}
+		 * |v_\text{true} - v_\text{predict}| \leq tol_\text{absolute}
 		 * \f] where
 		 * \f[
-		 * tol_{absolute} = tol_{relative} * |v_{true}|
+		 * tol_\text{absolute} = tol_\text{relative} * |v_\text{true}|
 		 * \f]
 		 *
-		 * @param true_value true value should be finite (Neither NAN nor INF)
+		 * @param true_value true value should be finite (neither NAN nor INF)
 		 * @param rel_tolorance relative tolerance should be positive and less than 1.0
 		 *
 		 * @return the corresponding absolute tolerance
-		 * */
+		 */
 		static float64_t get_abs_tolorance(float64_t true_value, float64_t rel_tolorance);
 		
 		static inline float64_t round(float64_t d)
