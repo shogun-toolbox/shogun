381ba2f535d6b5aa213add697353cb7a7c6d4789
Thoralf Klein <thoralf@fischlustig.de>
2014-04-20 00:01:50 +0200

    NULLing weights when copying the pointer to prevent leaks/double-frees.

--- src/shogun/machine/OnlineLinearMachine.cpp
+++ src/shogun/machine/OnlineLinearMachine.cpp
@@ -17,7 +17,7 @@ using namespace shogun;
 COnlineLinearMachine::COnlineLinearMachine()
 : CMachine(), w_dim(0), w(NULL), bias(0), features(NULL)
 {
-	// m_parameters->add_vector(&w, &w_dim, "w", "Parameter vector w.");
+	m_parameters->add_vector(&w, &w_dim, "w", "Parameter vector w.");
 	SG_ADD(&bias, "bias", "Bias b.", MS_NOT_AVAILABLE);
 	SG_ADD((CSGObject**) &features, "features",
 	    "Feature object.", MS_NOT_AVAILABLE);
