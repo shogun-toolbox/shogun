121036515a5224644a90b5cda88563da3d424006
Abhijeet <abhijeetkislay@gmail.com>
2014-05-14 22:38:42 +0530

    opencv integration test cleaned_.

--- tests/integration/opencv/opencv_test.cpp
+++ tests/integration/opencv/opencv_test.cpp
@@ -1,30 +1,18 @@
+#include <gtest/gtest.h>
 
- 
+#ifdef HAVE_OPENCV
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
- 
-#include <shogun/base/init.h>
-#include <shogun/lib/common.h>
-#include <shogun/lib/SGMatrix.h>
-#include <shogun/features/DenseFeatures.h>
- 
 #include <iostream>
  
 using namespace cv;
-using namespace shogun;
  
-int main()
+TEST(OpenCV, OpenCV_Integration_Test)
 {
-	init_shogun_with_defaults();
-		
-	Mat cvMat = Mat::eye(3,3,CV_8UC1);
-	cvMat.at<unsigned char>(0,1) = 3;
-	SGMatrix<unsigned char> sgMat(cvMat.data,3,3,false);
-	
-	std::cout << cvMat << std::endl ;
-	sgMat.display_matrix();
-	
-	
-	return 0;
-}
-	
+	Mat cvMat = Mat::eye(3,3,CV_64FC1);
+	EXPECT_EQ (cvMat.at<double>(0,0),1);
+	EXPECT_EQ (cvMat.at<double>(1,0),0);
+}	
+#endif 
+
