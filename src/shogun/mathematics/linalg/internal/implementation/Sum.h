115cc9998f55d8856ac9eb61b778a386924bf101
lambday <heavensdevil6909@gmail.com>
2014-05-19 09:15:38 +0530

    added rowwise and colwise sum methods in linalg

--- src/shogun/mathematics/linalg/internal/implementation/Sum.h
+++ src/shogun/mathematics/linalg/internal/implementation/Sum.h
@@ -33,6 +33,7 @@
 
 #include <shogun/lib/config.h>
 #include <shogun/lib/SGMatrix.h>
+#include <shogun/lib/SGVector.h>
 #include <shogun/io/SGIO.h>
 #include <shogun/mathematics/linalg/internal/Block.h>
 
@@ -108,6 +109,66 @@ struct sum_symmetric
 	 * @return the sum of co-efficients computed as \f$\sum_{i,j}b_{i,j}\f$
 	 */
 	static T compute(Block<Info,Matrix,T,I...> b, bool no_diag);
+};
+
+/**
+ * @brief Generic class colwise_sum which provides a static compute method. This class
+ * is specialized for different types of matrices and backend, providing a mean
+ * to deal with various matrices directly without having to convert
+ */
+template <class Info,enum Backend,template<class,Info...>class Matrix,class T,Info... I>
+struct colwise_sum
+{
+	typedef Matrix<T,I...> matrix_type;
+
+	/**
+	 * Method that computes column wise sum of co-efficients of a dense matrix
+	 *
+	 * @param m the matrix whose colwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the colwise sum of co-efficients computed as \f$s_j=\sum_{i}m_{i,j}\f$
+	 */
+	static SGVector<T> compute(matrix_type m, bool no_diag);
+
+	/**
+	 * Method that computes column wise sum of co-efficients of a symmetric dense
+	 * matrix blocks
+	 *
+	 * @param b the matrix-block whose colwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the colwise sum of co-efficients computed as \f$s_j=\sum_{i}b_{i,j}\f$
+	 */
+	static SGVector<T> compute(Block<Info,Matrix,T,I...> b, bool no_diag);
+};
+
+/**
+ * @brief Generic class rowwise_sum which provides a static compute method. This class
+ * is specialized for different types of matrices and backend, providing a mean
+ * to deal with various matrices directly without having to convert
+ */
+template <class Info,enum Backend,template<class,Info...>class Matrix,class T,Info... I>
+struct rowwise_sum
+{
+	typedef Matrix<T,I...> matrix_type;
+
+	/**
+	 * Method that computes row wise sum of co-efficients of a dense matrix
+	 *
+	 * @param m the matrix whose rowwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the rowwise sum of co-efficients computed as \f$s_i=\sum_{j}m_{i,j}\f$
+	 */
+	static SGVector<T> compute(matrix_type m, bool no_diag);
+
+	/**
+	 * Method that computes row wise sum of co-efficients of a symmetric dense
+	 * matrix blocks
+	 *
+	 * @param b the matrix-block whose rowwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the rowwise sum of co-efficients computed as \f$s_i=\sum_{j}b_{i,j}\f$
+	 */
+	static SGVector<T> compute(Block<Info,Matrix,T,I...> b, bool no_diag);
 };
 
 #ifdef HAVE_EIGEN3
@@ -296,6 +357,203 @@ struct sum_symmetric<int,Backend::EIGEN3,Eigen::Matrix,T,Info...>
 		return compute(block, no_diag);
 	}
 };
+
+/**
+ * @brief Specialization of generic colwise_sum which works with SGMatrix and uses Eigen3
+ * as backend for computing sum.
+ */
+template <> template <class T>
+struct colwise_sum<int,Backend::EIGEN3,shogun::SGMatrix,T>
+{
+	typedef shogun::SGMatrix<T> matrix_type;
+
+	/**
+	 * Method that computes the column wise sum of co-efficients of SGMatrix using Eigen3
+	 *
+	 * @param m the matrix whose colwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the colwise sum of co-efficients computed as \f$s_j=\sum_{i}m_{i,j}\f$
+	 */
+	static SGVector<T> compute(matrix_type m, bool no_diag)
+	{
+		typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> MatrixXt;
+		Eigen::Map<MatrixXt> eig_m(m.matrix, m.num_rows, m.num_cols);
+
+		return colwise_sum<int,Backend::EIGEN3,Eigen::Matrix,T,Eigen::Dynamic,Eigen::Dynamic>
+			::compute(eig_m, no_diag);
+	}
+
+	/**
+	 * Method that computes the column wise sum of co-efficients of SGMatrix blocks
+	 * using Eigen3
+	 *
+	 * @param b the matrix-block whose colwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the colwise sum of co-efficients computed as \f$s_j=\sum_{i}b_{i,j}\f$
+	 */
+	static SGVector<T> compute(Block<int,shogun::SGMatrix,T> b, bool no_diag)
+	{
+		typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> MatrixXt;
+		Eigen::Map<MatrixXt> eig_m(b.m_matrix.matrix, b.m_matrix.num_rows,
+				b.m_matrix.num_cols);
+
+		const MatrixXt& block=eig_m.template block(b.m_row_begin, b.m_col_begin,
+				b.m_row_size, b.m_col_size);
+
+		return colwise_sum<int,Backend::EIGEN3,Eigen::Matrix,T,Eigen::Dynamic,Eigen::Dynamic>
+			::compute(block, no_diag);
+	}
+};
+
+/**
+ * @brief Specialization of generic colwise_sum which works with Eigen3 and uses Eigen3
+ * as backend for computing sum.
+ */
+template <> template <class T,int...Info>
+struct colwise_sum<int,Backend::EIGEN3,Eigen::Matrix,T,Info...>
+{
+	typedef Eigen::Matrix<T,Info...> matrix_type;
+
+	/**
+	 * Method that computes the column wise sum of co-efficients of Eigen3 Matrix
+	 * using Eigen3
+	 *
+	 * @param m the matrix whose colwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the colwise sum of co-efficients computed as \f$s_j=\sum_{i}m_{i,j}\f$
+	 */
+	static SGVector<T> compute(matrix_type m, bool no_diag)
+	{
+		Eigen::Matrix<T,Eigen::Dynamic,1> sum=m.colwise().sum();
+
+		// remove the main diagonal elements if required
+		if (no_diag)
+		{
+			index_t len_major_diag=m.rows() < m.cols() ? m.rows() : m.cols();
+			for (index_t i=0; i<len_major_diag; ++i)
+				sum[i]-=m(i,i);
+		}
+
+		SGVector<T> s(sum.rows());
+		std::template copy(sum.data(), sum.data()+sum.size(), s.vector);
+
+		return s;
+	}
+
+	/**
+	 * Method that computes the sum of co-efficients of Eigen3 Matrix blocks using Eigen3
+	 *
+	 * @param b the matrix-block whose sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the colwise sum of co-efficients computed as \f$s_j=\sum_{i}b_{i,j}\f$
+	 */
+	static SGVector<T> compute(Block<int,Eigen::Matrix,T,Info...> b, bool no_diag)
+	{
+		const matrix_type& block=b.m_matrix.template block(b.m_row_begin, b.m_col_begin,
+				b.m_row_size, b.m_col_size);
+
+		return compute(block, no_diag);
+	}
+};
+
+/**
+ * @brief Specialization of generic rowwise_sum which works with SGMatrix and uses Eigen3
+ * as backend for computing sum.
+ */
+template <> template <class T>
+struct rowwise_sum<int,Backend::EIGEN3,shogun::SGMatrix,T>
+{
+	typedef shogun::SGMatrix<T> matrix_type;
+
+	/**
+	 * Method that computes the row wise sum of co-efficients of SGMatrix using Eigen3
+	 *
+	 * @param m the matrix whose rowwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the rowwise sum of co-efficients computed as \f$s_i=\sum_{j}m_{i,j}\f$
+	 */
+	static SGVector<T> compute(matrix_type m, bool no_diag)
+	{
+		typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> MatrixXt;
+		Eigen::Map<MatrixXt> eig_m(m.matrix, m.num_rows, m.num_cols);
+
+		return rowwise_sum<int,Backend::EIGEN3,Eigen::Matrix,T,Eigen::Dynamic,Eigen::Dynamic>
+			::compute(eig_m, no_diag);
+	}
+
+	/**
+	 * Method that computes the row wise sum of co-efficients of SGMatrix blocks
+	 * using Eigen3
+	 *
+	 * @param b the matrix-block whose rowwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the rowwise sum of co-efficients computed as \f$s_i=\sum_{j}b_{i,j}\f$
+	 */
+	static SGVector<T> compute(Block<int,shogun::SGMatrix,T> b, bool no_diag)
+	{
+		typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> MatrixXt;
+		Eigen::Map<MatrixXt> eig_m(b.m_matrix.matrix, b.m_matrix.num_rows,
+				b.m_matrix.num_cols);
+
+		const MatrixXt& block=eig_m.template block(b.m_row_begin, b.m_col_begin,
+				b.m_row_size, b.m_col_size);
+
+		return rowwise_sum<int,Backend::EIGEN3,Eigen::Matrix,T,Eigen::Dynamic,Eigen::Dynamic>
+			::compute(block, no_diag);
+	}
+};
+
+/**
+ * @brief Specialization of generic rowwise_sum which works with Eigen3 and uses Eigen3
+ * as backend for computing sum.
+ */
+template <> template <class T,int...Info>
+struct rowwise_sum<int,Backend::EIGEN3,Eigen::Matrix,T,Info...>
+{
+	typedef Eigen::Matrix<T,Info...> matrix_type;
+
+	/**
+	 * Method that computes the row wise sum of co-efficients of Eigen3 Matrix
+	 * using Eigen3
+	 *
+	 * @param m the matrix whose rowwise sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the rowwise sum of co-efficients computed as \f$s_j=\sum_{i}m_{i,j}\f$
+	 */
+	static SGVector<T> compute(matrix_type m, bool no_diag)
+	{
+		Eigen::Matrix<T,Eigen::Dynamic,1> sum=m.rowwise().sum();
+
+		// remove the main diagonal elements if required
+		if (no_diag)
+		{
+			index_t len_major_diag=m.rows() < m.cols() ? m.rows() : m.cols();
+			for (index_t i=0; i<len_major_diag; ++i)
+				sum[i]-=m(i,i);
+		}
+
+		SGVector<T> s(sum.rows());
+		std::template copy(sum.data(), sum.data()+sum.size(), s.vector);
+
+		return s;
+	}
+
+	/**
+	 * Method that computes the sum of co-efficients of Eigen3 Matrix blocks using Eigen3
+	 *
+	 * @param b the matrix-block whose sum of co-efficients has to be computed
+	 * @param no_diag if true, diagonal entries are excluded from the sum
+	 * @return the rowwise sum of co-efficients computed as \f$s_j=\sum_{i}b_{i,j}\f$
+	 */
+	static SGVector<T> compute(Block<int,Eigen::Matrix,T,Info...> b, bool no_diag)
+	{
+		const matrix_type& block=b.m_matrix.template block(b.m_row_begin, b.m_col_begin,
+				b.m_row_size, b.m_col_size);
+
+		return compute(block, no_diag);
+	}
+};
+
 #endif // HAVE_EIGEN3
 
 }
