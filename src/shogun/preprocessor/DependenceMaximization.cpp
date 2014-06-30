c180e5f583bdc71c44b0f7335c118d4e88427df6
lambday <heavensdevil6909@gmail.com>
2014-07-01 04:42:44 +0530

    Added class CDependenceMaximization for feature selection

---
+++ src/shogun/preprocessor/DependenceMaximization.cpp
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) The Shogun Machine Learning Toolbox
+ * Written (w) 2014 Soumyajit De
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Shogun Development Team.
+ */
+
+#include <shogun/lib/SGMatrix.h>
+#include <shogun/labels/Labels.h>
+#include <shogun/features/DenseFeatures.h>
+#include <shogun/statistics/IndependenceTest.h>
+#include <shogun/preprocessor/DependenceMaximization.h>
+
+using namespace shogun;
+
+CDependenceMaximization::CDependenceMaximization()
+	: CFeatureSelection<float64_t>()
+{
+	init();
+}
+
+void CDependenceMaximization::init()
+{
+	SG_ADD((CSGObject**)&m_estimator, "estimator",
+			"the estimator for computing measures", MS_NOT_AVAILABLE);
+	SG_ADD((CSGObject**)&m_labels_feats, "labels_feats",
+			"the features based on labels", MS_NOT_AVAILABLE);
+
+	m_estimator=NULL;
+	m_labels_feats=NULL;
+}
+
+CDependenceMaximization::~CDependenceMaximization()
+{
+	SG_UNREF(m_estimator);
+	SG_UNREF(m_labels_feats);
+}
+
+bool CDependenceMaximization::init(CFeatures* features)
+{
+	REQUIRE(features, "Features are not initialized!\n");
+	REQUIRE(features->get_feature_class()==C_DENSE ||
+			features->get_feature_class()==C_SPARSE,
+			"Only allowed for dense/sparse features! Provided an instance of "
+			"%s which is of class %d!\n",
+			features->get_name(), features->get_feature_class());
+	REQUIRE(features->get_feature_type()==F_DREAL, "Only allowed for "
+			"features of double type! Provided %d!\n",
+			features->get_feature_type());
+
+	return true;
+}
+
+void CDependenceMaximization::precompute()
+{
+	SG_DEBUG("Entering!\n");
+
+	REQUIRE(m_labels, "Labels are not initialized!\n");
+	REQUIRE(m_estimator, "Estimator is not initialized!\n");
+
+	// convert the CLabels object to CDenseFeatures
+	SG_UNREF(m_labels_feats);
+
+	SGMatrix<float64_t> labels_matrix(1, m_labels->get_num_labels());
+	for (index_t i=0; i<labels_matrix.num_cols; ++i)
+		labels_matrix.matrix[i]=m_labels->get_value(i);
+
+	m_labels_feats=new CDenseFeatures<float64_t>(labels_matrix);
+	SG_REF(m_labels_feats);
+
+	// and set this to the estimator
+	m_estimator->set_q(m_labels_feats);
+
+	SG_DEBUG("Leaving!\n");
+}
+
+CFeatures* CDependenceMaximization::create_transformed_copy(CFeatures* features,
+		index_t idx)
+{
+	SG_DEBUG("Entering!\n");
+
+	// remove the dimension specified by the index, i.e. get X\X_i
+	index_t num_features=get_num_features(features);
+	SGVector<index_t> dims(num_features-1);
+	index_t n_dims=0;
+	for (index_t i=0; i<num_features; ++i)
+	{
+		if (i!=idx)
+			dims[n_dims++]=i;
+	}
+
+	if (io->get_loglevel()==MSG_DEBUG || io->get_loglevel()==MSG_GCDEBUG)
+		dims.display_vector("dims");
+
+	// the following already does a SG_REF on the newly created feature
+	SG_DEBUG("Leaving!\n");
+	return features->copy_dimension_subset(dims);
+}
+
+float64_t CDependenceMaximization::compute_measures(CFeatures* features,
+		index_t idx)
+{
+	SG_DEBUG("Entering!\n");
+
+	// remove the dimension (feat) specified by the index idx
+	CFeatures* reduced_feats=create_transformed_copy(features, idx);
+
+	// perform an independence test for X\X_i ~ p and Y ~ q with
+	// H_0: P(X\X_i, Y) = P(X\X_i) * P(Y)
+	// the test statistic can then be used as a measure of dependence
+	// See CIndependenceTest class documentation for details
+	m_estimator->set_p(reduced_feats);
+	float64_t statistic=m_estimator->compute_statistic();
+
+	SG_DEBUG("statistic = %f!\n", statistic);
+
+	SG_UNREF(reduced_feats);
+
+	SG_DEBUG("Leaving!\n");
+	return statistic;
+}
+
+CFeatures* CDependenceMaximization::remove_feats(CFeatures* features,
+		SGVector<index_t> argsorted)
+{
+	SG_DEBUG("Entering!\n");
+
+	REQUIRE(m_num_remove>0, "Number or percentage of features to be removed is "
+			"not set! Please use set_num_remove() to set this!\n");
+	REQUIRE(m_policy==N_SMALLEST || m_policy==PERCENTILE_SMALLEST,
+			"Only N_SMALLEST and PERCENTILE_SMALLEST removal policy can work "
+			"with %s!\n", get_name());
+
+	// compute a threshold to remove for both the policies
+	index_t threshold=m_num_remove;
+	if (m_policy==PERCENTILE_SMALLEST)
+		threshold*=argsorted.vlen*0.01;
+
+	// make sure that the threshold is valid given the current number of feats
+	ASSERT(threshold<argsorted.vlen)
+
+	// remove the lowest threshold rank holders by storing indices
+	SGVector<index_t> inds(argsorted.vlen-threshold);
+	memcpy(inds.vector, argsorted.vector+threshold, sizeof(index_t)*inds.vlen);
+
+	// sorting the indices to get the original order
+	inds.qsort();
+	if (io->get_loglevel()==MSG_DEBUG || io->get_loglevel()==MSG_GCDEBUG)
+		inds.display_vector("selected feats");
+
+	// copy rest of the features and SG_UNREF the original feat obj
+	CFeatures* reduced_feats=features->copy_dimension_subset(inds);
+	SG_UNREF(features);
+
+	SG_DEBUG("Leaving!\n");
+	return reduced_feats;
+}
+
+void CDependenceMaximization::set_policy(EFeatureRemovalPolicy policy)
+{
+	REQUIRE(policy==N_SMALLEST || policy==PERCENTILE_SMALLEST,
+			"Only N_SMALLEST and PERCENTILE_SMALLEST removal policy can work "
+			"with %s!\n", get_name());
+	m_policy=policy;
+}
---
+++ src/shogun/preprocessor/DependenceMaximization.h
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) The Shogun Machine Learning Toolbox
+ * Written (w) 2014 Soumyajit De
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Shogun Development Team.
+ */
+
+#ifndef DEPENDENCE_MAXIMIZATION_H__
+#define DEPENDENCE_MAXIMIZATION_H__
+
+#include <shogun/lib/config.h>
+#include <shogun/preprocessor/FeatureSelection.h>
+
+namespace shogun
+{
+
+class CFeatures;
+class CIndependenceTest;
+
+/** @brief Class CDependenceMaximization, base class for all feature selection
+ * preprocessors which select a subset of features that shows maximum dependence
+ * between the features and the labels. This is done via an implementation of
+ * CIndependenceTest, #m_estimator inside compute_measures() (see class
+ * documentation of CFeatureSelection), which performs a statistical test for a
+ * given feature \f$\mathbf{X}_i\f$ from the set of features \f$\mathbf{X}\f$,
+ * and the labels \f$\mathbf{Y}\f$. The test checks
+ * \f[
+ * 	\textbf{H}_0 : P\left(\mathbf{X}\setminus \mathbf{X}_i, \mathbf{Y}\right)
+ *	=P\left(\mathbf{X}\setminus \mathbf{X}_i\right)P\left(\mathbf{Y}\right)
+ * \f]
+ * The test statistic is then used as a measure. Lowest measure signifies
+ * lowest dependence. Therefore, lowest scoring features are removed. The
+ * removal policy thus can only be ::N_SMALLEST and ::PERCENTILE_SMALLEST and
+ * it can be set via set_policy() call. remove_feats() method handles the
+ * removal of features based on the specified policy.
+ *
+ * The estimator cannot be set via user interface, rather its subclasses
+ * initialize this estimator with appropriate instances internally.
+ *
+ * This class also overrides precompute() method to create a feature object from
+ * the labels and sets this as features \f$\mathbf{Y}\sim q\f$ to the estimator
+ * which is required to compute the measure.
+ */
+class CDependenceMaximization : public CFeatureSelection<float64_t>
+{
+public:
+	/** Default constructor */
+	CDependenceMaximization();
+
+	/** Destructor */
+	virtual ~CDependenceMaximization();
+
+	/**
+	 * Method that computes the measures using test statistic computed by
+	 * an instance of CIndependenceTest wiht the provided features and
+	 * the labels
+	 *
+	 * @param features the features on which the measure has to be computed
+	 * @param idx the index that decides which features should we compute
+	 * the measure on
+	 * @return the measure based on which features are selected
+	 */
+	virtual float64_t compute_measures(CFeatures* features, index_t idx);
+
+	/**
+	 * Method which handles the removal of features based on removal policy.
+	 * see documentation of CFeatureSelection. For dependence maximization
+	 * approach, the lowest scoring features are removed. Therefore, only
+	 * #m_policy can only be N_SMALLEST, PERCENTILE_SMALLEST. see set_policy()
+	 * method for specifying the exact policy
+	 *
+	 * @param features the features object from which specific features has
+	 * to be removed
+	 * @param ranks the ranks of the features based on their measures, 0 being
+	 * the lowest rank which corresponds to smallest measure.
+	 * @return the feature object after removal of features based on the policy
+	 */
+	virtual CFeatures* remove_feats(CFeatures* features, SGVector<index_t> ranks);
+
+	/** @param policy feature removal policy */
+	virtual void set_policy(EFeatureRemovalPolicy policy);
+
+	/**
+	 * Abstract method which is overridden in the subclasses to set accepted
+	 * feature selection algorithm
+	 *
+	 * @param algorithm the feature selection algorithm to use
+	 */
+	virtual void set_algorithm(EFeatureSelectionAlgorithm algorithm)=0;
+
+	/**
+	 * Initialize preprocessor from features
+	 *
+	 * @param features the features
+	 * @return true if init was successful
+	 */
+	virtual bool init(CFeatures* features);
+
+	/** @return the class name */
+	virtual const char* get_name() const
+	{
+		return "DependenceMaximization";
+	}
+
+protected:
+	/**
+	 * Helper method which removes the dimension specified via the index. It
+	 * copies the rest of the features into a separate object via
+	 * copy_dimension_subset() call.
+	 *
+	 * @param features the features
+	 * @param idx index of the dimension which is required to be removed
+	 * @return a new feature object with the specified dimension removed
+	 */
+	virtual CFeatures* create_transformed_copy(CFeatures* features, index_t idx);
+
+	/**
+	 * Creates a dense feature object from the labels provided, #m_labels and
+	 * sets this feature in the independence test estimator. These labels serve
+	 * as samples \f$\mathbf{Y}\sim q\f$ in the independence test
+	 */
+	virtual void precompute();
+
+	/**
+	 * The estimator for performing statistical tests for independence which
+	 * is used for computing measures
+	 */
+	CIndependenceTest* m_estimator;
+
+	/** The feature for the labels */
+	CFeatures* m_labels_feats;
+
+private:
+	/** Register params and initialize with default values */
+	void init();
+
+};
+
+}
+#endif // DEPENDENCE_MAXIMIZATION_H__
