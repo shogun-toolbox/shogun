b2a84dac3ad029a4093d36de1eb80fef4562c884
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2014-08-17 11:55:36 +0200

    Fix failure of KNN with cross-validation and memory leak on Features copy constructor.

--- src/shogun/multiclass/KNN.cpp
+++ src/shogun/multiclass/KNN.cpp
@@ -455,8 +455,7 @@ void CKNN::store_model_features()
 	/* copy lhs of underlying distance */
 	distance->init(d_lhs->duplicate(), d_rhs);
 
-	SG_UNREF(d_lhs); // because of d_lhs->duplicate()
-	SG_UNREF(d_lhs); // because of distance->get_lhs()
+	SG_UNREF(d_lhs);
 	SG_UNREF(d_rhs);
 }
 
