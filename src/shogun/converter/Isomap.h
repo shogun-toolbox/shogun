8b438dc7c10dbe36475c828f663c6fbfea994035
Kunal Arora <kunalarora.135@gmail.com>
2015-01-30 15:13:38 +0530

    adjusted the width

--- src/shogun/converter/Isomap.h
+++ src/shogun/converter/Isomap.h
@@ -36,11 +36,13 @@ class CDistance;
  * classic Multidimensional Scaling algorithm. The algorithm itself
  * consists of the following steps:
  * - For each feature vector \f$x\in X\f$ find its \f$k\f$ nearest
- * neighbours and construct the sparse neighbourhood graph.
+ *   neighbours and construct the sparse neighbourhood graph.
  * - Compute squared distances matrix \f$D\f$ such as \f$D\{i,j\} = d^2(x_i,x_j)\f$.
  * - Relax distances with shortest(so-called geodesic) distances on the sparse neighbourhood graph (e.g. with sparse Dijkstra algorithm).
  * - Center the matrix \f$D\f$ with subtracting row mean, column mean and adding to the grand mean. Multiply \f$D\f$ element-wise with \f$-0.5\f$.
- * - Compute embedding with the \f$t\f$ eigenvectors that correspond to the largest eigenvalues of the matrix \f$D\f$; normalize these vectors dividing each eigenvector by the square root of its corresponding eigenvalue. Form the final embedding with eigenvectors as rows and projected feature vectors as columns.
+ * - Compute embedding with the \f$t\f$ eigenvectors that correspond to the largest eigenvalues of the matrix \f$D\f$; normalize these vectors
+ *   dividing each eigenvector by the square root of its corresponding eigenvalue. Form the final embedding with eigenvectors as rows and projected
+ *   feature vectors as columns.
 
  * It is possible to apply preprocessor to specified distance using
  * apply_to_distance.
