f238c7e947adf38c53da84e500d254ed62baa777
Wu Lin <yorker.lin@gmail.com>
2015-04-01 17:25:18 -0400

    add comments

--- src/shogun/machine/gp/FITCInferenceMethod.h
+++ src/shogun/machine/gp/FITCInferenceMethod.h
@@ -44,7 +44,8 @@ namespace shogun
  *
  * Warning: the time complexity of method,
  * CSingleFITCLaplacianBase::get_derivative_wrt_kernel(const TParameter* param),
- * depends on the implementation of kernel method, get_parameter_gradient_diagonal(param, i).
+ * depends on the implementation of virtual kernel method,
+ * CKernel::get_parameter_gradient_diagonal(param, i).
  * The default time complexity of the kernel method can be O(n^2)
  *
  * Warning: the the time complexity increases from O(m^2*n) to O(n^2*m) if method 
