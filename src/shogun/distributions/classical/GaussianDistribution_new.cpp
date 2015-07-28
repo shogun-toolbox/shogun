82b5c396eddf30ee86d478f75422de80cadd1fdf
Alessandro Ialongo <alex.ialongo@gmail.com>
2015-07-28 21:24:22 +0100

    added overloaded constructors and getter/setter methods to GaussianDistribution_new

---
+++ src/shogun/distributions/classical/GaussianDistribution_new.cpp
@@ -0,0 +1,244 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2013 Heiko Strathmann
+ */
+
+#include <shogun/distributions/classical/GaussianDistribution.h>
+
+#ifdef HAVE_EIGEN3
+
+#include <shogun/base/Parameter.h>
+#include <shogun/mathematics/eigen3.h>
+
+using namespace shogun;
+using namespace Eigen;
+
+CGaussianDistribution::CGaussianDistribution() : CProbabilityDistribution()
+{
+	init();
+}
+
+CGaussianDistribution::CGaussianDistribution(SGVector<float64_t> mean,
+		float64_t cov, bool cov_is_cholesky) :
+				CProbabilityDistribution(mean.vlen)
+{
+	init();
+
+	m_mean=mean;
+
+	m_cov=SGMatrix<float64_t>(1,1);
+	m_cov(1,1)=cov;
+}
+
+CGaussianDistribution::CGaussianDistribution(SGVector<float64_t> mean,
+		SGVector<float64_t> cov, bool cov_is_cholesky) :
+				CProbabilityDistribution(mean.vlen)
+{
+	REQUIRE(mean.vlen==cov.vlen, "Mean must have same dimension as "
+			"covariance, which is %dx%d, but is %d\n",
+			cov.vlen, cov.vlen, mean.vlen);
+
+	init();
+
+	m_mean=mean;
+
+	m_cov=convert_to_matrix(cov,1,cov.vlen,false);
+}
+
+CGaussianDistribution::CGaussianDistribution(SGVector<float64_t> mean,
+		SGMatrix<float64_t> cov, bool cov_is_cholesky) :
+				CProbabilityDistribution(mean.vlen)
+{
+	REQUIRE(cov.num_rows==cov.num_cols, "Covariance must be square but is "
+			"%dx%d\n", cov.num_rows, cov.num_cols);
+	REQUIRE(mean.vlen==cov.num_cols, "Mean must have same dimension as "
+			"covariance, which is %dx%d, but is %d\n",
+			cov.num_rows, cov.num_cols, mean.vlen);
+
+	init();
+
+	m_mean=mean;
+
+	if (!cov_is_cholesky)
+	{
+		Map<MatrixXd> eigen_cov(cov.matrix, cov.num_rows, cov.num_cols);
+		m_cov=SGMatrix<float64_t>(cov.num_rows, cov.num_cols);
+		Map<MatrixXd> eigen_L(m_cov.matrix, m_cov.num_rows, m_cov.num_cols);
+
+		/* compute cholesky */
+		LLT<MatrixXd> llt(eigen_cov);
+		if (llt.info()==NumericalIssue)
+		{
+			/* try to compute smalles eigenvalue for information */
+			SelfAdjointEigenSolver<MatrixXd> solver(eigen_cov);
+			if (solver.info() == Success)
+			{
+				VectorXd ev=solver.eigenvalues();
+				SG_ERROR("Error computing Cholesky of Gaussian's covariance. "
+						"Smallest Eigenvalue is %f.\n", ev[0]);
+			}
+		}
+
+		eigen_L=llt.matrixL();
+	}
+	else
+		m_cov=cov;
+}
+
+CGaussianDistribution::~CGaussianDistribution()
+{
+
+}
+
+CGaussianDistribution::set_mean(SGVector<float64_t> mean)
+{
+	m_mean=mean;
+}
+
+CGaussianDistribution::set_cov(float64_t cov)
+{
+	m_cov=SGMatrix<float64_t>(1,1);
+	m_cov(1,1)=cov;
+}
+
+CGaussianDistribution::set_cov(SGVector<float64_t> cov)
+{
+	m_cov=convert_to_matrix(cov,1,cov.vlen,false);
+}
+
+CGaussianDistribution::set_cov(SGMatrix<float64_t> cov)
+{
+	m_cov=cov;
+}
+
+SGVector<float64_t> CGaussianDistribution::get_mean() const
+{
+	return m_mean;
+}
+
+float64_t CGaussianDistribution::get_cov_spherical() const
+{
+	return *m_cov.data();
+}
+
+SGVector<float64_t> CGaussianDistribution::get_cov_diag() const
+{
+	return m_cov.get_row_vector(0);
+}
+
+SGMatrix<float64_t> CGaussianDistribution::get_cov_full() const
+{
+	return m_cov;
+}
+
+SGMatrix<float64_t> CGaussianDistribution::sample(int32_t num_samples,
+		SGMatrix<float64_t> pre_samples) const
+{
+	REQUIRE(num_samples>0, "Number of samples (%d) must be positive\n",
+			num_samples);
+
+	/* use pre-allocated samples? */
+	SGMatrix<float64_t> samples;
+	if (pre_samples.matrix)
+	{
+		REQUIRE(pre_samples.num_rows==m_dimension, "Dimension of pre-samples"
+				" (%d) does not match dimension of Gaussian (%d)\n",
+				pre_samples.num_rows, m_dimension);
+
+		REQUIRE(pre_samples.num_cols==num_samples, "Number of pre-samples"
+				" (%d) does not desired number of samples (%d)\n",
+				pre_samples.num_cols, num_samples);
+
+		samples=pre_samples;
+	}
+	else
+	{
+		/* allocate memory and sample from std normal */
+		samples=SGMatrix<float64_t>(m_dimension, num_samples);
+		for (index_t i=0; i<m_dimension*num_samples; ++i)
+			samples.matrix[i]=sg_rand->std_normal_distrib();
+	}
+
+	/* map into desired Gaussian covariance */
+	Map<MatrixXd> eigen_samples(samples.matrix, samples.num_rows,
+			samples.num_cols);
+	Map<MatrixXd> eigen_L(m_cov.matrix, m_cov.num_rows, m_cov.num_cols);
+	eigen_samples=eigen_L*eigen_samples;
+
+	/* add mean */
+	Map<VectorXd> eigen_mean(m_mean.vector, m_mean.vlen);
+	eigen_samples.colwise()+=eigen_mean;
+
+	return samples;
+}
+
+SGVector<float64_t> CGaussianDistribution::log_pdf_multiple(SGMatrix<float64_t> samples) const
+{
+	REQUIRE(samples.num_cols>0, "Number of samples must be positive, but is %d\n",
+			samples.num_cols);
+	REQUIRE(samples.num_rows==m_dimension, "Sample dimension (%d) does not match"
+			"Gaussian dimension (%d)\n", samples.num_rows, m_dimension);
+
+	/* for easier to read code */
+	index_t num_samples=samples.num_cols;
+
+	float64_t const_part=-0.5 * m_dimension * CMath::log(2 * CMath::PI);
+
+	/* determinant is product of diagonal elements of triangular matrix */
+	float64_t log_det_part=0;
+	Map<MatrixXd> eigen_L(m_cov.matrix, m_cov.num_rows, m_cov.num_cols);
+	VectorXd diag=eigen_L.diagonal();
+	log_det_part=-diag.array().log().sum();
+
+	/* sample based part */
+	Map<MatrixXd> eigen_samples(samples.matrix, samples.num_rows,
+			samples.num_cols);
+	Map<VectorXd> eigen_mean(m_mean.vector, m_mean.vlen);
+
+	/* substract mean from samples (create copy) */
+	SGMatrix<float64_t> centred(m_dimension, num_samples);
+	Map<MatrixXd> eigen_centred(centred.matrix, centred.num_rows,
+			centred.num_cols);
+	for (index_t dim=0; dim<m_dimension; ++dim)
+	{
+		for (index_t sample_idx=0; sample_idx<num_samples; ++sample_idx)
+			centred(dim,sample_idx)=samples(dim,sample_idx)-m_mean[dim];
+	}
+
+	/* solve the linear system based on factorization */
+	MatrixXd solved=eigen_L.triangularView<Lower>().solve(eigen_centred);
+	solved=eigen_L.transpose().triangularView<Upper>().solve(solved);
+
+	/* one quadratic part x^T C^-1 x for each sample x */
+	SGVector<float64_t> result(num_samples);
+	Map<VectorXd> eigen_result(result.vector, result.vlen);
+	for (index_t i=0; i<num_samples; ++i)
+	{
+		/* i-th centred sample */
+		VectorXd left=eigen_centred.block(0, i, m_dimension, 1);
+
+		/* inverted covariance times i-th centred sample */
+		VectorXd right=solved.block(0,i,m_dimension,1);
+		result[i]=-0.5*left.dot(right);
+	}
+
+	/* combine and return */
+	eigen_result=eigen_result.array()+(log_det_part+const_part);
+
+	/* contains everything */
+	return result;
+
+}
+
+void CGaussianDistribution::init()
+{
+	SG_ADD(&m_mean, "mean", "Mean of the Gaussian.", MS_NOT_AVAILABLE);
+	SG_ADD(&m_cov, "L", "Lower factor of covariance matrix, "
+			"depending on the factorization type.", MS_NOT_AVAILABLE);
+}
+
+#endif // HAVE_EIGEN3
---
+++ src/shogun/distributions/classical/GaussianDistribution_new.h
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) The Shogun Machine Learning Toolbox
+ * Written (w) 2014 Wu Lin
+ * Written (W) 2013 Heiko Strathmann
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the Shogun Development Team.
+ *
+ */
+
+#ifndef GAUSSIANDISTRIBUTION_H
+#define GAUSSIANDISTRIBUTION_H
+
+#include <shogun/lib/config.h>
+
+#ifdef HAVE_EIGEN3
+
+#include <shogun/distributions/classical/ProbabilityDistribution.h>
+#include <shogun/lib/SGVector.h>
+#include <shogun/mathematics/Math.h>
+
+namespace shogun
+{
+/** TODO commenting properly the new fields
+ */
+
+/** @brief Dense version of the well-known Gaussian probability distribution,
+ * defined as
+ * \f[
+ * \mathcal{N}_x(\mu,\Sigma)=
+ * \frac{1}{\sqrt{|2\pi\Sigma|}}
+ * \exp\left(-\frac{1}{2}(x-\mu)^T\Sigma^{-1}(x-\mu)\right)
+ * \f]
+ *
+ * The implementation represents the covariance matrix \f$\Sigma \f$, as
+ * Cholesky factorisation, such that the covariance can be computed as
+ * \f$\Sigma=LL^T\f$.
+ */
+
+class CGaussianDistribution: public CProbabilityDistribution
+{
+public:
+	/** Default constructor */
+	CGaussianDistribution();
+
+	/** Constructor for which takes Gaussian mean and its covariance matrix.
+	 * It is also possible to pass a precomputed matrix factor of the specified
+	 * form. In this case, the factorization is not explicitly computed.
+	 *
+	 * @param mean mean of the Gaussian
+	 * @param cov covariance of the Gaussian, or covariance factor
+	 * @param cov_is_cholesky whether cov is a Cholesky factorization of the covariance or not
+	 * (default is false). If false, the Cholesky factorization is explicitly computed
+	 */
+	CGaussianDistribution(SGVector<float64_t> mean, float64_t cov,
+			bool cov_is_cholesky=false);
+
+	CGaussianDistribution(SGVector<float64_t> mean, SGVector<float64_t> cov,
+			bool cov_is_cholesky=false);
+
+	CGaussianDistribution(SGVector<float64_t> mean, SGMatrix<float64_t> cov,
+			bool cov_is_cholesky=false);
+
+	/** Destructor */
+	virtual ~CGaussianDistribution();
+
+	void set_mean(SGVector<float64_t> cov);
+	void set_cov(float64_t cov);
+	void set_cov(SGVector<float64_t> cov);
+	void set_cov(SGMatrix<float64_t> cov);
+
+	SGVector<float64_t> get_mean() const;
+	float64_t get_cov_spherical() const;
+	SGVector<float64_t> get_cov_diag() const;
+	SGMatrix<float64_t> get_cov_full() const;
+
+	/** Samples from the distribution multiple times
+	 *
+	 * @param num_samples number of samples to generate
+	 * @param pre_samples a matrix of standard normal samples that might be used
+	 * for sampling the Gaussian. Ignored by default. If passed, the pre-samples
+	 * will be modified.
+	 * @return matrix with samples (column vectors)
+	 */
+	virtual SGMatrix<float64_t> sample(int32_t num_samples,
+			SGMatrix<float64_t> pre_samples=SGMatrix<float64_t>()) const;
+
+	/** Computes the log-pdf for all provided samples. That is
+	 *
+	 * \f[
+	 * \log(\mathcal{N}_x(\mu,\Sigma))=
+	 * - \frac{d}{2}  \log(2\pi)
+	 * -\frac{1}{2}\log(\det(\Sigma))
+	 * -\frac{1}{2}(x-\mu)^T\Sigma^{-1}(x-\mu),
+	 * \f]
+	 *
+	 * where \f$d\f$ is the dimension of the Gaussian.
+	 * The method to compute the log-determinant is based on the factorization
+	 * of the covariance matrix.
+	 *
+	 * @param samples samples to compute log-pdf of (column vectors)
+	 * @return vector with log-pdfs of given samples
+	 */
+
+	virtual SGVector<float64_t> log_pdf_multiple(SGMatrix<float64_t> samples) const;
+
+	/** @return name of the SGSerializable */
+	virtual const char* get_name() const
+	{
+		return "GaussianDistribution";
+	}
+
+
+	/** Computes the univariate pdf for one given sample.
+	 *
+	 * @param sample is a given sample
+	 * @param mu is the mean of univariate Normal distribution (default value is 0.0)
+	 * @param sigma2 is the variance of univariate Normal distribution (default value is 1.0)
+	 * @return the pdf of the distribution given the sample
+	 */
+	static float64_t univariate_log_pdf(float64_t sample, float64_t mu = 0.0, float64_t sigma2 = 1.0)
+	{
+		REQUIRE(sigma2 > 0, "Variance should be positive\n");
+		return -0.5 * (CMath::pow(sample - mu, 2) / sigma2
+			+ CMath::log(2.0 * CMath::PI) + CMath::log(sigma2));
+	}
+
+	static float64_t multivariate_log_pdf(SGVector<float64_t> sample, SGVector<float64_t> mu, SGVector<float64_t> cov)
+	{
+		// TODO:
+		REQUIRE(sigma2 > 0, "Variance should be positive\n");
+		return -0.5 * (CMath::pow(sample - mu, 2) / sigma2
+			+ CMath::log(2.0 * CMath::PI) + CMath::log(sigma2));
+	}
+
+private:
+
+	/** Initialses and registers parameters */
+	void init();
+
+protected:
+	/** Mean */
+	SGVector<float64_t> m_mean;
+
+	/** Lower factor of covariance matrix (depends on factorization type).
+	 * Covariance (approximation) is given by \f$\Sigma=LL^T\f$ */
+	SGMatrix<float64_t> m_cov;
+};
+
+}
+
+#endif // HAVE_EIGEN3
+#endif // GAUSSIANDISTRIBUTION_H
