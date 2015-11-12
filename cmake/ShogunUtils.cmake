0efe434563fd4c9fa8016e5aef556b9fca9caac3
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2015-11-12 11:21:13 +0300

    Use function to detect system name

--- cmake/ShogunUtils.cmake
+++ cmake/ShogunUtils.cmake
@@ -33,6 +33,36 @@ MACRO(GetCompilers)
 	ENDIF()
 ENDMACRO()
 
+macro(DetectSystemName)
+	IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+		SET(DARWIN 1)
+		set(CMAKE_MACOSX_RPATH TRUE)
+
+		# use, i.e. don't skip the full RPATH for the build tree
+		set(CMAKE_SKIP_BUILD_RPATH FALSE)
+
+		# when building, don't use the install RPATH already
+		# (but later on when installing)
+		set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+
+		set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+
+		# add the automatically determined parts of the RPATH
+		# which point to directories outside the build tree to the install RPATH
+		set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+		# the RPATH to be used when installing, but only if it's not a system directory
+		list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+		if(${isSystemDir} STREQUAL "-1")
+			set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+		endif(${isSystemDir} STREQUAL "-1")		
+	ELSEIF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+		SET(LINUX 1)
+	ELSEIF(${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
+		SET(FREEBSD 1)
+	ENDIF()
+endmacro()
+
 MACRO(PrintInterfaceStatus INTERFACE_NAME INTERFACE_FLAG)
 	IF ( ${INTERFACE_FLAG} )
 		message(STATUS "  ${INTERFACE_NAME} is ON")
