e99ef4eeb237308ee53ba8df222666ea5619b971
Wu Lin <yorker.lin@gmail.com>
2015-12-19 12:52:49 -0500

    added comments for the AdaDelta method

--- src/shogun/optimization/AdaDeltaUpdater.h
+++ src/shogun/optimization/AdaDeltaUpdater.h
@@ -35,7 +35,26 @@
 #include <shogun/optimization/LearningRate.h>
 namespace shogun
 {
-/** @brief The class implements the AdaDelta method. */
+/** @brief The class implements the AdaDelta method.
+ *	\f[
+ *	\begin{array}{l}
+ *	g_\theta=(1-\lambda){(\frac{ \partial f(\cdot) }{\partial \theta })}^2+\lambda g_\theta\\
+ *	d_\theta=\alpha\frac{\sqrt{s_\theta+\epsilon}}{\sqrt{g_t+\epsilon}}\frac{ \partial f(\cdot) }{\partial \theta }\\
+ *	s_\theta=(1-\lambda){(d_\theta)}^2+\lambda s_\theta
+ *	\end{array}
+ *	\f]
+ *
+ * where
+ * \f$ \frac{ \partial f(\cdot) }{\partial \theta } \f$ is a negative descend direction (eg, gradient) wrt \f$\theta$\f,
+ * \f$\lambda\f$ is a decay factor,
+ * \f$\epsilon$\f is used to avoid dividing by 0,
+ * \f$ \alpha \f$ is a build-in learning rate
+ * \f$d_\theta$\f is a corrected negative descend direction.
+ * 
+ * Reference:
+ * Matthew D. Zeiler, ADADELTA: An Adaptive Learning Rate Method, arXiv:1212.5701
+ *
+ * */
 class AdaDeltaUpdater: public DescendUpdaterWithCorrection
 {
 public:
@@ -95,30 +114,34 @@ public:
 		SGVector<float64_t> raw_negative_descend_direction, float64_t learning_rate);
 
 protected:
-	/** Get the negative descend direction given current variable and gradient
+	/** Get the negative descend direction given current variable  and gradient 
 	 *
 	 * It will be called at update_variable()
 	 *
-	 * @param variable current variable
-	 * @param gradient current gradient
+	 * @param variable current variable (eg, \f$\theta$\f)
+	 * @param gradient current gradient (eg, \f$ \frac{ \partial f(\cdot) }{\partial \theta }$\f)
 	 * @param idx the index of the variable
-	 * @param learning_rate learning rate
+	 * @param learning_rate learning rate (for AdaDelta, learning_rate is NOT used because there is a build-in
+	 * learning_rate)
 	 * 
-	 * @return negative descend direction (that is, the given gradient in the class)
+	 * @return negative descend direction (that is, \f$\d_\theta$\f)
 	 */
 	virtual float64_t get_negative_descend_direction(float64_t variable,
 		float64_t gradient, index_t idx, float64_t learning_rate);
 
-	/** learning_rate at iteration */
+	/** learning_rate \f$\alpha$\f at iteration */
 	float64_t m_build_in_learning_rate;
 
-	/** epsilon */
+	/** \f$epsilon$\f */
 	float64_t m_epsilon;
 
-	/** decay term */
+	/** decay term \f$\lambda$\f */
 	float64_t m_decay_factor;
 
+	/** \f$g_\theta$\f */
 	SGVector<float64_t> m_gradient_accuracy;
+
+	/** \f$s_\theta$\f */
 	SGVector<float64_t> m_gradient_delta_accuracy;
 private:
 	/**  Init */
