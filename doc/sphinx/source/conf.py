7d943252e9d7040e36a80bad197a918325c744c3
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2015-12-27 17:41:17 +0300

    Copy sphinx custom templates to build directory

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -15,14 +15,10 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath('extensions'))
-sys.path.append(os.path.abspath('.'))
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.abspath('.'))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
