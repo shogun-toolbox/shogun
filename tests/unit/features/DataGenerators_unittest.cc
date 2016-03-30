9c80edff7a2a5f30f9363e40715f84623adc6467
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-03-30 19:37:40 +0100

    fix GaussianBlob unit test broken due to wrong covariance matrix interpretation

--- tests/unit/features/DataGenerators_unittest.cc
+++ tests/unit/features/DataGenerators_unittest.cc
@@ -14,7 +14,7 @@
 
 using namespace shogun;
 
-TEST(GaussianBlobsDataGenerator,get_next_example)
+TEST(GaussianBlobsDataGenerator,get_next_example1)
 {
 	index_t num_blobs=1;
 	float64_t distance=3;
@@ -38,33 +38,37 @@ TEST(GaussianBlobsDataGenerator,get_next_example)
 	}
 
 	SGVector<float64_t> mean=CStatistics::matrix_mean(samples, false);
-	SGMatrix<float64_t>::transpose_matrix(samples.matrix, samples.num_rows,
-			samples.num_cols);
-#ifdef HAVE_LAPACK
 	SGMatrix<float64_t> cov=CStatistics::covariance_matrix(samples);
-#endif // HAVE_LAPACK
-    //mean.display_vector("mean");
-	//cov.display_matrix("cov");
 
-    /* rougly ensures right results, set to 0.3 for now, if test fails, set a
-     * bit larger */
-    float64_t accuracy=0.5;
+    /* rougly ensures right results, if test fails, set a bit larger */
+    float64_t accuracy=1e-1;
 
 	/* matrix is expected to look like [1.5, 0.5; 0.5, 1.5] */
-#ifdef HAVE_LAPACK
-	EXPECT_LE(CMath::abs(cov(0,0)-1.5), accuracy);
-	EXPECT_LE(CMath::abs(cov(0,1)-0.5), accuracy);
-	EXPECT_LE(CMath::abs(cov(1,0)-0.5), accuracy);
-	EXPECT_LE(CMath::abs(cov(1,1)-1.5), accuracy);
-#endif // HAVE_LAPACK
-
+	EXPECT_NEAR(cov(0,0), 1.5, accuracy);
+	EXPECT_NEAR(cov(0,1), 0.5, accuracy);
+	EXPECT_NEAR(cov(1,0), 0.5, accuracy);
+	EXPECT_NEAR(cov(1,1), 1.5, accuracy);
+	
 	/* mean is supposed to do [0, 0] */
 	EXPECT_LE(CMath::abs(mean[0]-0), 0.1);
 	EXPECT_LE(CMath::abs(mean[1]-0), 0.1);
+	
+	SG_UNREF(gen);
+}
+
+TEST(GaussianBlobsDataGenerator,get_next_example2)
+{
+	index_t num_blobs=3;
+	float64_t distance=3;
+	float64_t epsilon=2;
+	float64_t angle=CMath::PI/4;
+	index_t num_samples=50000;
+
+	CGaussianBlobsDataGenerator* gen=new CGaussianBlobsDataGenerator(num_blobs,
+			distance, epsilon, angle);
 
 	/* and another one */
 	SGMatrix<float64_t> samples2(2, num_samples);
-	num_blobs=3;
 	gen->set_blobs_model(num_blobs, distance, epsilon, angle);
 
 	for (index_t i=0; i<num_samples; ++i)
@@ -77,22 +81,16 @@ TEST(GaussianBlobsDataGenerator,get_next_example)
 	}
 
 	SGVector<float64_t> mean2=CStatistics::matrix_mean(samples2, false);
-	SGMatrix<float64_t>::transpose_matrix(samples2.matrix, samples2.num_rows,
-			samples2.num_cols);
-#ifdef HAVE_LAPACK
 	SGMatrix<float64_t> cov2=CStatistics::covariance_matrix(samples2);
-#endif // HAVE_LAPACK
-    //mean2.display_vector("mean2");
-	//cov2.display_matrix("cov2");
 
+    /* rougly ensures right results, if test fails, set a bit larger */
+    float64_t accuracy=1e-1;
 
 	/* matrix is expected to look like [7.55, 0.55; 0.55, 7.55] */
-#ifdef HAVE_LAPACK
-	EXPECT_LE(CMath::abs(cov2(0,0)-7.55), accuracy);
-	EXPECT_LE(CMath::abs(cov2(0,1)-0.55), accuracy);
-	EXPECT_LE(CMath::abs(cov2(1,0)-0.55), accuracy);
-	EXPECT_LE(CMath::abs(cov2(1,1)-7.55), accuracy);
-#endif // HAVE_LAPACK
+	EXPECT_NEAR(cov2(0,0), 7.55, accuracy);
+	EXPECT_NEAR(cov2(0,1), 0.55, accuracy);
+	EXPECT_NEAR(cov2(1,0), 0.55, accuracy);
+	EXPECT_NEAR(cov2(1,1), 7.55, accuracy);
 
 	/* mean is supposed to do [3, 3] */
 	EXPECT_LE(CMath::abs(mean2[0]-3), accuracy);
