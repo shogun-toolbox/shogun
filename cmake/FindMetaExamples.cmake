70f921a894eeae979e849a37334d3ede1ef3abc9
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-04-03 16:34:58 +0100

    cmake guard for nlopt dependency of gaussian process regression

--- cmake/FindMetaExamples.cmake
+++ cmake/FindMetaExamples.cmake
@@ -1,9 +1,13 @@
 function(find_meta_examples)
 	FILE(GLOB_RECURSE META_EXAMPLE_LISTINGS ${CMAKE_SOURCE_DIR}/examples/meta/src/*.sg)
 	
-	# temporary hack to exclude certain meta examples that have dependencies
+	# temporary hacks to exclude certain meta examples that have dependencies
 	IF(NOT HAVE_NLOPT)
 		LIST(REMOVE_ITEM META_EXAMPLE_LISTINGS ${CMAKE_SOURCE_DIR}/examples/meta/src/gaussian_processes/gaussian_process_regression.sg)
+	ENDIF()
+	
+	IF(NOT USE_GPL_SHOGUN)
+		LIST(REMOVE_ITEM META_EXAMPLE_LISTINGS ${CMAKE_CURRENT_SOURCE_DIR}/src/gaussian_processes/gaussian_process_regression.sg)
 	ENDIF()
 
 	SET(META_EXAMPLES ${META_EXAMPLE_LISTINGS} PARENT_SCOPE)
