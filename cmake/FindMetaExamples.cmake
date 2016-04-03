a18a6dc49541eee4cc726a06329dfea05cdc58e2
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-04-03 16:30:11 +0100

    meta example fake dependency guard works locally

--- cmake/FindMetaExamples.cmake
+++ cmake/FindMetaExamples.cmake
@@ -1,11 +1,10 @@
 function(find_meta_examples)
 	FILE(GLOB_RECURSE META_EXAMPLE_LISTINGS ${CMAKE_SOURCE_DIR}/examples/meta/src/*.sg)
-	SET(META_EXAMPLES ${META_EXAMPLE_LISTINGS} PARENT_SCOPE)
 	
 	# temporary hack to exclude certain meta examples that have dependencies
-	IF(DEFINED META_EXAMPLES)
-		IF(NOT HAVE_NLOPT)
-			LIST(REMOVE_ITEM META_EXAMPLES ${CMAKE_SOURCE_DIR}/examples/meta/src/gaussian_processes/gaussian_process_regression.sg)
-		ENDIF()
+	IF(NOT HAVE_NLOPT)
+		LIST(REMOVE_ITEM META_EXAMPLE_LISTINGS ${CMAKE_SOURCE_DIR}/examples/meta/src/gaussian_processes/gaussian_process_regression.sg)
 	ENDIF()
+
+	SET(META_EXAMPLES ${META_EXAMPLE_LISTINGS} PARENT_SCOPE)
 endfunction()
