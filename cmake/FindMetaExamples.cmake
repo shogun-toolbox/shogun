cc987d05f109648a25097b839b8190757b79e36d
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-04-03 16:23:00 +0100

    attempt to fix buildbot

--- cmake/FindMetaExamples.cmake
+++ cmake/FindMetaExamples.cmake
@@ -3,7 +3,9 @@ function(find_meta_examples)
 	SET(META_EXAMPLES ${META_EXAMPLE_LISTINGS} PARENT_SCOPE)
 	
 	# temporary hack to exclude certain meta examples that have dependencies
-	IF(NOT HAVE_NLOPT)
-		LIST(REMOVE_ITEM META_EXAMPLES ${CMAKE_SOURCE_DIR}/examples/meta/src/gaussian_processes/gaussian_process_regression.sg)
+	IF(DEFINED META_EXAMPLES)
+		IF(NOT HAVE_NLOPT)
+			LIST(REMOVE_ITEM META_EXAMPLES ${CMAKE_SOURCE_DIR}/examples/meta/src/gaussian_processes/gaussian_process_regression.sg)
+		ENDIF()
 	ENDIF()
 endfunction()
