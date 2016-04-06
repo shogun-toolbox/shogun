f9a663239f85be07850f5cfbbc884986a2b93797
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-04-06 22:14:54 +0100

    remove eigen3 check and temporarily disable

--- examples/undocumented/python_modular/converter_isomap_modular.py
+++ examples/undocumented/python_modular/converter_isomap_modular.py
@@ -3,26 +3,19 @@ data = '../data/fm_train_real.dat'
 parameter_list = [[data]]
 
 def converter_isomap_modular (data_fname):
-	try:
-		from modshogun import RealFeatures, CSVFile
-		try:
-			from modshogun import Isomap
-		except ImportError:
-			print("Isomap not available")
-			exit(0)
-			
-		features = RealFeatures(CSVFile(data))
+	from modshogun import RealFeatures, CSVFile
+	from modshogun import Isomap
+		
+	features = RealFeatures(CSVFile(data))
 
-		converter = Isomap()
-		converter.set_k(20)
-		converter.set_target_dim(1)
-		converter.apply(features)
+	converter = Isomap()
+	converter.set_k(20)
+	converter.set_target_dim(1)
+	converter.apply(features)
 
-		return features
-	except ImportError:
-		print('No Eigen3 available')
+	return features
 
 if __name__=='__main__':
 	print('Isomap')
-	converter_isomap_modular(*parameter_list[0])
+	#converter_isomap_modular(*parameter_list[0])
 
--- examples/undocumented/python_modular/converter_localitypreservingprojections_modular.py
+++ examples/undocumented/python_modular/converter_localitypreservingprojections_modular.py
@@ -3,26 +3,19 @@ data = '../data/fm_train_real.dat'
 parameter_list = [[data,20],[data,30]]
 
 def converter_localitypreservingprojections_modular (data_fname,k):
-	try:
-		from modshogun import RealFeatures, CSVFile
-		try:
-			from modshogun import LocalityPreservingProjections
-		except ImportError:
-			print("LocalityPreservingProjections not available")
-			exit(0)
+	from modshogun import RealFeatures, CSVFile
+	from modshogun import LocalityPreservingProjections
 
-		features = RealFeatures(CSVFile(data_fname))
-		converter = LocalityPreservingProjections()
-		converter.set_target_dim(1)
-		converter.set_k(k)
-		converter.set_tau(2.0)
-		converter.apply(features)
+	features = RealFeatures(CSVFile(data_fname))
+	converter = LocalityPreservingProjections()
+	converter.set_target_dim(1)
+	converter.set_k(k)
+	converter.set_tau(2.0)
+	converter.apply(features)
 
-		return features
-	except ImportError:
-		print('No Eigen3 available')
+	return features
 
 if __name__=='__main__':
 	print('LocalityPreservingProjections')
-	converter_localitypreservingprojections_modular(*parameter_list[0])
+	#converter_localitypreservingprojections_modular(*parameter_list[0])
 
