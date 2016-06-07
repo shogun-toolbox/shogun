a7d6258ae6a8bc36d0ed28448976cbd52a7e7ad4
OXPHOS <engelzora@gmail.com>
2016-06-07 12:23:50 -0400

    set default cpubackend

--- src/shogun/mathematics/linalgrefactor/linalgRefactor.cpp
+++ src/shogun/mathematics/linalgrefactor/linalgRefactor.cpp
@@ -3,37 +3,37 @@
 namespace shogun
 {
 
-Linalg::Linalg():cpubackend(nullptr), gpubackend(nullptr){}
+Linalg::Linalg():m_cpubackend(&eigenBackend), m_gpubackend(nullptr){}
 
-Linalg::Linalg(CPUBackend* cpu_backend):cpubackend(cpu_backend), gpubackend(nullptr){}
+Linalg::Linalg(CPUBackend* cpubackend):m_cpubackend(cpubackend), m_gpubackend(nullptr){}
 
-Linalg::Linalg(GPUBackend* gpu_backend):cpubackend(nullptr), gpubackend(gpu_backend){}
+Linalg::Linalg(GPUBackend* gpubackend):m_cpubackend(&eigenBackend), m_gpubackend(gpubackend){}
 
-Linalg::Linalg(CPUBackend* cpu_backend, GPUBackend* gpu_backend)
-:cpubackend(cpu_backend), gpubackend(gpu_backend){}
+Linalg::Linalg(CPUBackend* cpubackend, GPUBackend* gpubackend)
+:m_cpubackend(cpubackend), m_gpubackend(gpubackend){}
 
-void Linalg::set_cpu_backend(const CPUBackend* cpu_backend)
+void Linalg::set_cpu_backend(CPUBackend* cpubackend)
 {
-    this->cpubackend = const_cast<CPUBackend*>(cpu_backend);
+    m_cpubackend = cpubackend;//const_cast<CPUBackend*>(cpubackend);
 }
 
 CPUBackend* Linalg::get_cpu_backend()
 {
-    return cpubackend;
+    return m_cpubackend;
 }
 
-void Linalg::set_gpu_backend(const GPUBackend* gpu_backend)
+void Linalg::set_gpu_backend(GPUBackend* gpubackend)
 {
-    this->gpubackend = const_cast<GPUBackend*>(gpu_backend);
+    m_gpubackend = gpubackend;
 }
 
 GPUBackend* Linalg::get_gpu_backend()
 {
-    return gpubackend;
+    return m_gpubackend;
 }
 
 template <class T>
-T Linalg::dot(BaseVector<T>* a, BaseVector<T>* b)
+T Linalg::dot(BaseVector<T> *a, BaseVector<T> *b)
 {
     if (a->onGPU() && b->onGPU())
     {
@@ -41,32 +41,35 @@ T Linalg::dot(BaseVector<T>* a, BaseVector<T>* b)
         {
             // do the gpu backend dot product
             // you shouldn't care whether it's viennacl or some other GPU backend.
-            return this->gpubackend->dot<T>(static_cast<GPU_Vector<T>&>(*a),
+            return m_gpubackend->dot<T>(static_cast<GPU_Vector<T>&>(*a),
                                             static_cast<GPU_Vector<T>&>(*b));
         } else {
             SG_SERROR("User did not register GPU backend. \n");
+            return -1;
         }
     }
     else {
         // take care that the matricies are on the same backend
         if (a->onGPU()){
             SG_SERROR("User did not register GPU backend. \n");
+            return -1;
         }
         else if (b->onGPU()) {
             SG_SERROR("User did not register GPU backend. \n");
+            return -1;
         }
 
         // do the non-gpu based default backend:
         // this should be actually as well implemented in a separate class's function and just that being called here:
         // like:
-        return this->cpubackend->dot<T>(static_cast<CPUVector<T>&>(*a),
+        return m_cpubackend->dot<T>(static_cast<CPUVector<T>&>(*a),
                                         static_cast<CPUVector<T>&>(*b));
     }
 }
 
 bool Linalg::hasGPUBackend()
 {
-    return gpubackend != nullptr;
+    return m_gpubackend != nullptr;
 }
 
 template int32_t Linalg::dot<int32_t>(BaseVector<int32_t>* a, BaseVector<int32_t>* b);
