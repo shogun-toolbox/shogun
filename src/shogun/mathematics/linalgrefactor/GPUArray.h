b15fdb54026d21cea87e3600e0f35c7151434c7c
OXPHOS <engelzora@gmail.com>
2016-06-08 12:05:59 -0400

    header

--- src/shogun/mathematics/linalgrefactor/GPUArray.h
+++ src/shogun/mathematics/linalgrefactor/GPUArray.h
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2016, Shogun-Toolbox e.V. <shogun-team@shogun-toolbox.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: 2016 Pan Deng, Soumyajit De, Viktor Gal
+ */
+
 #include <shogun/lib/config.h>
 #include <shogun/io/SGIO.h>
 #include <shogun/lib/SGVector.h>
@@ -21,6 +54,7 @@ struct GPUVector<T>::GPUArray
 #ifdef HAVE_VIENNACL
 	typedef viennacl::backend::mem_handle VCLMemoryArray;
 	typedef viennacl::vector_base<T, std::size_t, std::ptrdiff_t> VCLVectorBase;
+	typedef viennacl::vector<T> VCLVector;
 
 	std::shared_ptr<VCLMemoryArray> GPUptr;
 	index_t vlen;
@@ -28,9 +62,9 @@ struct GPUVector<T>::GPUArray
 
 	GPUArray(const SGVector<T> &vector);
 	GPUArray(const GPUVector<T>::GPUArray &array);
+
 	VCLVectorBase GPUvec();
-	viennacl::const_entry_proxy<T> operator[](index_t index) const;
-	viennacl::entry_proxy<T> operator[](index_t index);
+	VCLVector vector();
 
 #else //HAVE_VIENNACL
 	GPUArray(const SGVector<T> &vector);
@@ -41,4 +75,3 @@ struct GPUVector<T>::GPUArray
 }
 
 #endif
-
