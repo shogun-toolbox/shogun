1272d6ebf9731ade5fd781ddf5aef92bf54a71d5
OXPHOS <engelzora@gmail.com>
2016-06-08 18:58:15 -0400

    update

--- benchmarks/linalg_benchmark.cpp
+++ benchmarks/linalg_benchmark.cpp
@@ -1,6 +1,38 @@
+/*
+ * Copyright (c) 2016, Shogun-Toolbox e.V. <shogun-team@shogun-toolbox.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: 2016 Pan Deng, Soumyajit De */
+
 #include <shogun/base/init.h>
 #include <shogun/lib/SGVector.h>
-#include <shogun/mathematics/linalgrefactor/SGLinalg.h>
+#include <shogun/mathematics/linalg/SGLinalg.h>
 
 #include <shogun/mathematics/eigen3.h>
 #include <viennacl/linalg/inner_prod.hpp>
@@ -62,9 +94,9 @@ struct Data
 
 		init_shogun_with_defaults();
 
-		std::unique_ptr<GPUBackend> ViennaCLBackend;
-	    ViennaCLBackend = std::unique_ptr<GPUBackend>(new GPUBackend);
-	    sg_linalg->set_gpu_backend(std::move(ViennaCLBackend));
+		std::shared_ptr<GPUBackend> ViennaCLBackend;
+	    ViennaCLBackend = std::shared_ptr<GPUBackend>(new GPUBackend);
+	    sg_linalg->set_gpu_backend(ViennaCLBackend);
 	}
 
 	/** SGVector **/
