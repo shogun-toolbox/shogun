12318a148184e353091457b198affae30770e51c
OXPHOS <engelzora@gmail.com>
2016-06-08 12:05:59 -0400

    header

--- tests/unit/mathematics/LinalgRefactor_unittest.cc
+++ tests/unit/mathematics/LinalgRefactor_unittest.cc
@@ -1,3 +1,12 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2016 Pan Deng
+ */
+
 #include <shogun/lib/config.h>
 #include <shogun/mathematics/linalgrefactor/SGLinalg.h>
 #include <shogun/lib/SGVector.h>
