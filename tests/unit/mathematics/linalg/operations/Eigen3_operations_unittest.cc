7494ad2f81d23146725ecc9ae2228d989660e677
OXPHOS <engelzora@gmail.com>
2016-07-04 22:26:40 +0800

    Linalg-SGVector-add

--- tests/unit/mathematics/linalg/operations/Eigen3_operations_unittest.cc
+++ tests/unit/mathematics/linalg/operations/Eigen3_operations_unittest.cc
@@ -6,6 +6,26 @@
 using namespace shogun;
 using namespace linalg;
 
+TEST(LinalgBackendEigen, add)
+{
+	const float64_t alpha = 0.3;
+	const float64_t beta = -1.5;
+
+	SGVector<float64_t> A(9);
+	SGVector<float64_t> B(9);
+
+	for (index_t i = 0; i < 9; ++i)
+	{
+		A[i] = i;
+		B[i] = 0.5*i;
+	}
+
+	auto result = add(A, B, alpha, beta);
+
+	for (index_t i = 0; i < 9; ++i)
+		EXPECT_NEAR(alpha*A[i]+beta*B[i], result[i], 1e-15);
+}
+
 TEST(LinalgBackendEigen, dot)
 {
 	const index_t size = 3;
