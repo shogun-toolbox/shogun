e8f1719aa3f7ab9bc8154bb8c5b24915879a8503
siebenHeaven <anup.mahindre.7@gmail.com>
2016-07-12 13:46:26 +0530

    minor change

--- tests/unit/multiclass/KNN_unittest.cc
+++ tests/unit/multiclass/KNN_unittest.cc
@@ -8,16 +8,16 @@
 using namespace shogun;
 
 TEST (KNN, simple_classification)
-{   
-    /*create a rectangle with four points as (5,5), (-5,5), (-5,-5), (5,-5)*/
-    SGMatrix<float64_t> rect(2, 4);
-    rect(0,0)=5;
-	rect(0,1)=5;
+{
+	/*create a rectangle with four points as (5,5), (-5,5), (-5,-5), (5,-5)*/
+	SGMatrix<float64_t> rect(2, 4);
+	rect(0,0)=5;
+	rect(0,1)=-5;
 	rect(0,2)=-5;
 	rect(0,3)=5;
-	rect(1,0)=-5;
-	rect(1,1)=-5;
-	rect(1,2)=5;
+	rect(1,0)=5;
+	rect(1,1)=5;
+	rect(1,2)=-5;
 	rect(1,3)=-5;
 	
 	
