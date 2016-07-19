b9db0cb847f2562c66b378b927e7179c7c96cac6
lambday <heavensdevil6909@gmail.com>
2016-07-19 20:21:07 +0100

    prepare variance h1 for multikernel processing

--- src/shogun/statistical_testing/internals/mmd/VarianceH1.h
+++ src/shogun/statistical_testing/internals/mmd/VarianceH1.h
@@ -48,111 +48,145 @@ namespace mmd
 
 struct VarianceH1
 {
+	VarianceH1() : m_should_free_terms(true)
+	{
+	}
+
+	void init_terms()
+	{
+		m_sum_colwise_x.resize(m_n_x, 0);
+		m_sum_colwise_y.resize(m_n_y, 0);
+		m_sum_rowwise_xy.resize(m_n_y, 0);
+		m_sum_colwise_xy.resize(m_n_x, 0);
+		if (m_second_order_terms.rows()==m_n_x && m_second_order_terms.cols()==m_n_y)
+			m_second_order_terms.setZero();
+		else
+			m_second_order_terms=Eigen::MatrixXd::Zero(m_n_x, m_n_x);
+
+		m_sum_x=0;
+		m_sum_y=0;
+		m_sum_xy=0;
+		m_sum_sq_x=0;
+		m_sum_sq_y=0;
+		m_sum_sq_xy=0;
+	}
+
+	void free_terms()
+	{
+		if (m_should_free_terms)
+		{
+			m_sum_colwise_x.resize(0);
+			m_sum_colwise_y.resize(0);
+			m_sum_rowwise_xy.resize(0);
+			m_sum_colwise_xy.resize(0);
+			m_second_order_terms=Eigen::MatrixXd::Zero(0, 0);
+		}
+	}
+
+	template <typename T>
+	void add_terms(T kernel_value, index_t i, index_t j)
+	{
+		if (i<m_n_x && j<m_n_x)
+		{
+			m_sum_x+=2*kernel_value;
+			m_sum_sq_x+=2*kernel_value*kernel_value;
+			m_sum_colwise_x[i]+=kernel_value;
+			m_sum_colwise_x[j]+=kernel_value;
+			m_second_order_terms(i, j)+=kernel_value;
+			m_second_order_terms(j, i)+=kernel_value;
+		}
+		else if (i>=m_n_x && j>=m_n_x)
+		{
+			m_sum_y+=2*kernel_value;
+			m_sum_sq_y+=2*kernel_value*kernel_value;
+			m_sum_colwise_y[i-m_n_x]+=kernel_value;
+			m_sum_colwise_y[j-m_n_x]+=kernel_value;
+			m_second_order_terms(i-m_n_x, j-m_n_x)+=kernel_value;
+			m_second_order_terms(j-m_n_x, i-m_n_x)+=kernel_value;
+		}
+		else if (i<m_n_x && j>=m_n_x)
+		{
+			m_sum_xy+=kernel_value;
+			m_sum_sq_xy+=kernel_value*kernel_value;
+			if (j-i!=m_n_x)
+			{
+				m_second_order_terms(i, j-m_n_x)-=kernel_value;
+				m_second_order_terms(j-m_n_x, i)-=kernel_value;
+			}
+			m_sum_rowwise_xy[i]+=kernel_value;
+			m_sum_colwise_xy[j-m_n_x]+=kernel_value;
+		}
+	}
+
+	float64_t compute_variance_estimate()
+	{
+		Eigen::Map<Eigen::VectorXd> map_sum_colwise_x(m_sum_colwise_x.data(), m_sum_colwise_x.size());
+		Eigen::Map<Eigen::VectorXd> map_sum_colwise_y(m_sum_colwise_y.data(), m_sum_colwise_y.size());
+		Eigen::Map<Eigen::VectorXd> map_sum_rowwise_xy(m_sum_rowwise_xy.data(), m_sum_rowwise_xy.size());
+		Eigen::Map<Eigen::VectorXd> map_sum_colwise_xy(m_sum_colwise_xy.data(), m_sum_colwise_xy.size());
+
+		auto t_0=(map_sum_colwise_x.dot(map_sum_colwise_x)-m_sum_sq_x)/m_n_x/(m_n_x-1)/(m_n_x-2);
+		auto t_1=CMath::sq(m_sum_x/m_n_x/(m_n_x-1));
+
+		auto t_2=map_sum_colwise_x.dot(map_sum_rowwise_xy)*2/m_n_x/(m_n_x-1)/m_n_y;
+		auto t_3=m_sum_x*m_sum_xy*2/m_n_x/m_n_x/(m_n_x-1)/m_n_y;
+
+		auto t_4=(map_sum_colwise_y.dot(map_sum_colwise_y)-m_sum_sq_y)/m_n_y/(m_n_y-1)/(m_n_y-2);
+		auto t_5=CMath::sq(m_sum_y/m_n_y/(m_n_y-1));
+
+		auto t_6=map_sum_colwise_y.dot(map_sum_colwise_xy)*2/m_n_y/(m_n_y-1)/m_n_x;
+		auto t_7=m_sum_y*m_sum_xy*2/m_n_y/m_n_y/(m_n_y-1)/m_n_x;
+
+		auto t_8=(map_sum_rowwise_xy.dot(map_sum_rowwise_xy)-m_sum_sq_xy)/m_n_y/(m_n_y-1)/m_n_x;
+		auto t_9=2*CMath::sq(m_sum_xy/m_n_x/m_n_y);
+		auto t_10=(map_sum_colwise_xy.dot(map_sum_colwise_xy)-m_sum_sq_xy)/m_n_x/(m_n_x-1)/m_n_y;
+
+		auto var_first=(t_0-t_1)-t_2+t_3+(t_4-t_5)-t_6+t_7+(t_8-t_9+t_10);
+		var_first*=4.0*(m_n_x-2)/m_n_x/(m_n_x-1);
+
+		auto var_second=2.0/m_n_x/m_n_y/(m_n_x-1)/(m_n_y-1)*m_second_order_terms.array().square().sum();
+
+		auto variance_estimate=var_first+var_second;
+		if (variance_estimate<0)
+			variance_estimate=var_second;
+
+		return variance_estimate;
+	}
+
 	template <class Kernel>
 	float64_t operator()(const Kernel& kernel)
 	{
 		ASSERT(m_n_x>0 && m_n_y>0);
 		ASSERT(m_n_x==m_n_y);
 		const index_t size=m_n_x+m_n_y;
-
-		sum_colwise_x.resize(m_n_x, 0);
-		sum_colwise_y.resize(m_n_y, 0);
-		sum_rowwise_xy.resize(m_n_y, 0);
-		sum_colwise_xy.resize(m_n_x, 0);
-
-		float64_t sum_x=0;
-		float64_t sum_y=0;
-		float64_t sum_xy=0;
-		float64_t sum_sq_x=0;
-		float64_t sum_sq_y=0;
-		float64_t sum_sq_xy=0;
-
-		Eigen::MatrixXd second_order_terms=Eigen::MatrixXd::Zero(m_n_x, m_n_x);
-
+		init_terms();
 		for (auto j=0; j<size; ++j)
 		{
 			for (auto i=0; i<j; ++i)
-			{
-				auto kernel_value=kernel(i, j);
-				if (i<m_n_x && j<m_n_x)
-				{
-					sum_x+=2*kernel_value;
-					sum_sq_x+=2*kernel_value*kernel_value;
-					sum_colwise_x[i]+=kernel_value;
-					sum_colwise_x[j]+=kernel_value;
-					second_order_terms(i, j)+=kernel_value;
-					second_order_terms(j, i)+=kernel_value;
-				}
-				else if (i>=m_n_x && j>=m_n_x)
-				{
-					sum_y+=2*kernel_value;
-					sum_sq_y+=2*kernel_value*kernel_value;
-					sum_colwise_y[i-m_n_x]+=kernel_value;
-					sum_colwise_y[j-m_n_x]+=kernel_value;
-					second_order_terms(i-m_n_x, j-m_n_x)+=kernel_value;
-					second_order_terms(j-m_n_x, i-m_n_x)+=kernel_value;
-				}
-				else if (i<m_n_x && j>=m_n_x)
-				{
-					sum_xy+=kernel_value;
-					sum_sq_xy+=kernel_value*kernel_value;
-					if (j-i!=m_n_x)
-					{
-						second_order_terms(i, j-m_n_x)-=kernel_value;
-						second_order_terms(j-m_n_x, i)-=kernel_value;
-					}
-					sum_rowwise_xy[i]+=kernel_value;
-					sum_colwise_xy[j-m_n_x]+=kernel_value;
-				}
-			}
+				add_terms(kernel(i, j), i, j);
 		}
-
-		typedef Eigen::Matrix<float64_t, Eigen::Dynamic, 1> VectorXt;
-		Eigen::Map<const VectorXt> map_sum_colwise_x(sum_colwise_x.data(), sum_colwise_x.size());
-		Eigen::Map<const VectorXt> map_sum_colwise_y(sum_colwise_y.data(), sum_colwise_y.size());
-		Eigen::Map<const VectorXt> map_sum_rowwise_xy(sum_rowwise_xy.data(), sum_rowwise_xy.size());
-		Eigen::Map<const VectorXt> map_sum_colwise_xy(sum_colwise_xy.data(), sum_colwise_xy.size());
-
-		auto t_0=(map_sum_colwise_x.dot(map_sum_colwise_x)-sum_sq_x)/m_n_x/(m_n_x-1)/(m_n_x-2);
-		auto t_1=CMath::sq(sum_x/m_n_x/(m_n_x-1));
-
-		auto t_2=map_sum_colwise_x.dot(map_sum_rowwise_xy)*2/m_n_x/(m_n_x-1)/m_n_y;
-		auto t_3=sum_x*sum_xy*2/m_n_x/m_n_x/(m_n_x-1)/m_n_y;
-
-		auto t_4=(map_sum_colwise_y.dot(map_sum_colwise_y)-sum_sq_y)/m_n_y/(m_n_y-1)/(m_n_y-2);
-		auto t_5=CMath::sq(sum_y/m_n_y/(m_n_y-1));
-
-		auto t_6=map_sum_colwise_y.dot(map_sum_colwise_xy)*2/m_n_y/(m_n_y-1)/m_n_x;
-		auto t_7=sum_y*sum_xy*2/m_n_y/m_n_y/(m_n_y-1)/m_n_x;
-
-		auto t_8=(map_sum_rowwise_xy.dot(map_sum_rowwise_xy)-sum_sq_xy)/m_n_y/(m_n_y-1)/m_n_x;
-		auto t_9=2*CMath::sq(sum_xy/m_n_x/m_n_y);
-		auto t_10=(map_sum_colwise_xy.dot(map_sum_colwise_xy)-sum_sq_xy)/m_n_x/(m_n_x-1)/m_n_y;
-
-		auto var_first=(t_0-t_1)-t_2+t_3+(t_4-t_5)-t_6+t_7+(t_8-t_9+t_10);
-		var_first*=4.0*(m_n_x-2)/m_n_x/(m_n_x-1);
-
-		auto var_second=2.0/m_n_x/m_n_y/(m_n_x-1)/(m_n_y-1)*second_order_terms.array().square().sum();
-
-		auto variance_estimate=var_first+var_second;
-		if (variance_estimate<0)
-			variance_estimate=var_second;
-
-		sum_colwise_x.resize(0);
-		sum_colwise_y.resize(0);
-		sum_rowwise_xy.resize(0);
-		sum_colwise_xy.resize(0);
-
+		auto variance_estimate=compute_variance_estimate();
+		free_terms();
 		return variance_estimate;
 	}
 
 	index_t m_n_x;
 	index_t m_n_y;
 
-	vector<float64_t> sum_colwise_x;
-	vector<float64_t> sum_colwise_y;
-	vector<float64_t> sum_rowwise_xy;
-	vector<float64_t> sum_colwise_xy;
+	float64_t m_sum_x;
+	float64_t m_sum_y;
+	float64_t m_sum_xy;
+	float64_t m_sum_sq_x;
+	float64_t m_sum_sq_y;
+	float64_t m_sum_sq_xy;
+
+	vector<float64_t> m_sum_colwise_x;
+	vector<float64_t> m_sum_colwise_y;
+	vector<float64_t> m_sum_rowwise_xy;
+	vector<float64_t> m_sum_colwise_xy;
+	Eigen::MatrixXd m_second_order_terms;
+
+	bool m_should_free_terms;
 };
 
 }
