5389481fbc710b14542ad8888f52d803ea8845ff
Viktor Gal <viktor.gal@maeth.com>
2016-08-16 21:25:32 +0800

    fix unit test discovery on windows

--- tests/unit/discover_gtest_tests.cpp
+++ tests/unit/discover_gtest_tests.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #ifndef _WIN32
 #include <libgen.h>
+#else
+#include <regex>
 #endif
 
 #if _MSC_VER >= 1600
@@ -29,7 +31,7 @@ int main (int argc, char **argv)
 
 	if (argc < 2)
 	{
-		cout << "Usage: PATH_TO_TEST_BINARY --gtest_list_tests | ./build_test_cases PATH_TO_TEST_BINARY";
+		cout << "Usage: PATH_TO_TEST_BINARY --gtest_list_tests | ./discover_gtest_tests PATH_TO_TEST_BINARY";
 		return 1;
 	}
 
@@ -40,7 +42,7 @@ int main (int argc, char **argv)
 	while (getline (cin, line))
 	{
 		if ((line.find (" ") != 0) && (line[0] != '0'))
-			testCases.push_back(line.substr(0, line.size()-1));
+			testCases.push_back(line.substr(0, line.rfind('.')));
 	}
 
 	ofstream testfilecmake;
@@ -61,6 +63,11 @@ int main (int argc, char **argv)
 			{
 				string addTest ("ADD_TEST (unit-");
 				string testExec (" \"" + string (argv[1]) + "\"");
+#ifdef _WIN32
+				// escape backslash in path
+				std::regex e ("(\\\\)");
+				testExec = std::regex_replace (testExec, e, "\\\\");
+#endif
 				string gTestFilter (" \"--gtest_filter=");
 				string endParen (".*\")");
 
