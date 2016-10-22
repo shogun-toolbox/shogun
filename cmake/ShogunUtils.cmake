76ed307546c8507efc1d1f2c229a23d01995f36c
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-10-22 10:59:48 +0100

    cherry picked meta integration patch in feature branch

--- cmake/ShogunUtils.cmake
+++ cmake/ShogunUtils.cmake
@@ -51,3 +51,16 @@ function (shogun_discover_tests EXECUTABLE)
             DEPENDS
             VERBATIM)
 endfunction ()
+
+MACRO(AddMetaIntegrationTest META_TARGET CONDITION)
+    IF (${CONDITION})
+        add_test(NAME integration_meta_${META_TARGET}-${NAME_WITH_DIR}
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                        COMMAND meta_example_integration_tester ${REL_DIR} ${NAME}.dat ${META_TARGET} generated_results reference_results)
+                    set_tests_properties(
+                        integration_meta_${META_TARGET}-${NAME_WITH_DIR}
+	                        PROPERTIES
+	                        DEPENDS generated_${META_TARGET}-${NAME}
+                    )
+    ENDIF()
+ENDMACRO()
