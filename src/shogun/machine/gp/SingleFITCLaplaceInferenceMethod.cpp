13d7eeb1563fee713fbe9f1d21194edbbbc71c09
Viktor Gal <viktor.gal@maeth.com>
2016-10-30 13:33:44 -0700

    Fix reference counting in SingleFITCLaplaceInferenceMethod

--- src/shogun/machine/gp/SingleFITCLaplaceInferenceMethod.cpp
+++ src/shogun/machine/gp/SingleFITCLaplaceInferenceMethod.cpp
@@ -563,8 +563,10 @@ void CSingleFITCLaplaceInferenceMethod::update_alpha()
 	if (opt)
 	{
 		opt->set_target(this);
+#ifdef USE_REFERENCE_COUNTING
 		if(this->ref_count()>1)
 			cleanup=true;
+#endif
 		opt->minimize();
 		opt->unset_target(cleanup);
 	}
@@ -575,8 +577,10 @@ void CSingleFITCLaplaceInferenceMethod::update_alpha()
 
 		SingleFITCLaplaceInferenceMethodCostFunction *cost_fun=new SingleFITCLaplaceInferenceMethodCostFunction();
 		cost_fun->set_target(this);
+#ifdef USE_REFERENCE_COUNTING
 		if(this->ref_count()>1)
 			cleanup=true;
+#endif
 		minimizer->set_cost_function(cost_fun);
 		minimizer->minimize();
 		minimizer->unset_cost_function(false);
