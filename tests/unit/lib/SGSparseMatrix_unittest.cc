66bc053297afb357474e01258f7e7de46ed8c4a5
sudk1896 <sudk1896@gmail.com>
2016-11-15 00:29:36 +0530

    Adding a test for transposing a Square matrix.

--- tests/unit/lib/SGSparseMatrix_unittest.cc
+++ tests/unit/lib/SGSparseMatrix_unittest.cc
@@ -265,3 +265,25 @@ TEST(SGSparseMatrix, from_dense)
 		for (index_t vecIndex=0; vecIndex<numberOfVectors; ++vecIndex)
 			EXPECT_EQ(sparseMatrix(featIndex,vecIndex), denseMatrix(featIndex,vecIndex));
 }
+
+TEST(SGSparseMatrix, test_transposed_square_matrix)
+{
+	//Test for transposing Sparse Matrix.
+	//number_of_features = number_of_vectors.
+	const float64_t sparseLevel=0.1;
+	const index_t numberOfFeatures=100;
+	const index_t numberOfVectors=100;
+	const index_t randSeed=0;
+
+	SGSparseMatrix<float64_t> sparseMatrix(numberOfFeatures, numberOfVectors);
+	GenerateMatrix<SGSparseMatrix<float64_t> >(sparseLevel, numberOfFeatures, numberOfVectors, randSeed, &sparseMatrix);
+
+	SGSparseMatrix<float64_t> sparseMatrixT=sparseMatrix.get_transposed();
+
+	EXPECT_EQ(sparseMatrix.num_features, sparseMatrixT.num_vectors);
+	EXPECT_EQ(sparseMatrix.num_vectors, sparseMatrixT.num_features);
+
+	for (index_t featIndex=0; featIndex<numberOfFeatures; ++featIndex)
+		for (index_t vecIndex=0; vecIndex<numberOfVectors; ++vecIndex)
+			EXPECT_EQ(sparseMatrix(featIndex,vecIndex), sparseMatrixT(vecIndex,featIndex));
+}
