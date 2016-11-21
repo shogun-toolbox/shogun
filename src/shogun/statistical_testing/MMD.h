1f1520c9e93b71f5df247e5af30d50846888c844
lambday <heavensdevil6909@gmail.com>
2016-11-21 21:37:16 +0530

    some more doxygen additions for bigtesting

--- src/shogun/statistical_testing/MMD.h
+++ src/shogun/statistical_testing/MMD.h
@@ -45,35 +45,148 @@ class CKernel;
 class CKernelSelectionStrategy;
 template <typename> class SGVector;
 
+/** @brief Abstract base class that provides an interface for performing kernel
+ * two-sample test using Maximum Mean Discrepancy (MMD) as the test statistic.
+ * The MMD is the distance of two probability distributions \f$p\f$ and \f$q\f$
+ * in a RKHS (see [1] for formal description).
+ *
+ * \f[
+ * \text{MMD}[\mathcal{F},p,q]^2=||\mu_p - \mu_q||^2_\mathcal{F}=
+ * \textbf{E}_{x,x'}\left[ k(x,x')\right]
+ * -2\textbf{E}_{x,y}\left[ k(x,y)\right]
+ * +\textbf{E}_{y,y'}\left[ k(y,y')\right]
+ * \f]
+ *
+ * where \f$x,x'\sim p\f$ and \f$y,y'\sim q\f$. Subclasses implement various
+ * estimators for this expression, and therefore in this class compute_statistic()
+ * method is still  undefined.
+ *
+ * This class provides an interface for adding multiple kernels and then
+ * selecting the best kernel based on specified strategies. To know more in details
+ * about various learning algorithms for optimal kernel selection, please refer to [2].
+ *
+ * [1]: Gretton, A., Borgwardt, K. M., Rasch, M. J., Schoelkopf, B., &
+ * Smola, A. (2012). A Kernel Two-Sample Test. Journal of Machine Learning
+ * Research, 13, 671-721.
+ * [2] Arthur Gretton, Bharath K. Sriperumbudur, Dino Sejdinovic, Heiko Strathmann,
+ * Sivaraman Balakrishnan, Massimiliano Pontil, Kenji Fukumizu: Optimal kernel choice
+ * for large-scale two-sample tests. NIPS 2012: 1214-1222.
+ */
 class CMMD : public CTwoSampleTest
 {
 public:
+	/** Default constructor */
 	CMMD();
+
+	/**
+	 * Convenience constructor that initializes the samples from two distributions.
+	 *
+	 * @param samples_from_p Samples from \f$p\f$
+	 * @param samples_from_q Samples from \f$q\f$
+	 */
 	CMMD(CFeatures* samples_from_p, CFeatures* samples_from_q);
+
+	/** Destructor */
 	virtual ~CMMD();
 
+	/**
+	 * Method that sets the specific kernel selection strategy based on the
+	 * specific parameters provided. Please see class documentation for details.
+	 * Use this method for every other strategy other than KSM_CROSS_VALIDATION.
+	 *
+	 * @param method The kernel selection method as specified in EKernelSelectionMethod.
+	 * @param weighted If true, then an weighted combination of the kernel is used after
+	 * solving an optimization. If false, only a single kernel is selected among the
+	 * provided ones.
+	 */
 	void set_kernel_selection_strategy(EKernelSelectionMethod method, bool weighted = false);
+
+	/**
+	 * Method that sets the specific kernel selection strategy based on the
+	 * specific parameters provided. Please see class documentation for details.
+	 * Use this method for KSM_CROSS_VALIDATION.
+	 *
+	 * @param method The kernel selection method as specified in EKernelSelectionMethod.
+	 * @param num_runs The number of total runs of the cross-validation algorithm.
+	 * @param num_folds The number of folds (k) to be used in k-fold stratified cross-validation.
+	 * @param alpha The threshold to be used while performing test for the test-folds.
+	 */
 	void set_kernel_selection_strategy(EKernelSelectionMethod method, index_t num_runs, index_t num_folds, float64_t alpha);
 
+	/**
+	 * Method that adds a kernel instance to be used for kernel selection. Please
+	 * note that the kernels added by this method are NOT set as the main test kernel
+	 * unless select_kernel() method is executed.
+	 *
+	 * This method is NOT thread safe. Please DO NOT use this method from multiple threads.
+	 *
+	 * @param kernel One of the kernel instances with which learning algorithm will work.
+	 */
 	void add_kernel(CKernel *kernel);
+
+	/**
+	 * Method that selects/learns the kernel based on the defined kernel selection strategy.
+	 * If no explicit kernel selection strategy was set using set_kernel_selection_strategy()
+	 * method, then a default strategy is used. Please see EKernelSelectionMethod for the
+	 * default strategy.
+	 *
+	 * This method is NOT thread safe. It replaces the internel kernel set by set_kernel()
+	 * method, if there was any. Please DO NOT use this method from multiple threads.
+	 *
+	 * The learned/selected kernel can be obtained from a subsequent get_kernel() call.
+	 *
+	 * This method expects train-test mode to be turned on at the time of invocation. Please
+	 * see the class documentation of CHypothesisTest.
+	 */
 	virtual void select_kernel();
 
+	/**
+	 * Method that returns the kernel selection strategy wrapper object that will be/
+	 * was used in the last kernel learning algorithm. Use this method when results of
+	 * intermediate steps taken by the kernel selection algorithms are of interest.
+	 *
+	 * @return The internal instance of CKernelSelectionStrategy that holds intermediate
+	 * measures computed at the time of the last kernel selection algorithm invocation.
+	 */
 	CKernelSelectionStrategy const * get_kernel_selection_strategy() const;
 
 	virtual float64_t compute_statistic() = 0;
 	virtual SGVector<float64_t> sample_null() = 0;
 
+	/** Method that releases the pre-computed kernel that is used in the computation. */
 	void cleanup();
 
+	/**
+	 * Method that sets the number of null-samples used for computing p-value.
+	 *
+	 * @param null_samples Number of null-samples.
+	 */
 	void set_num_null_samples(index_t null_samples);
+
+	/** @return Number of null-samples */
 	index_t get_num_null_samples() const;
 
+	/**
+	 * Method that sets the type of the estimator for MMD^2
+	 *
+	 * @param stype The type of the estimator for MMD^2
+	 */
 	void set_statistic_type(EStatisticType stype);
+
+	/** @return The type of the estimator for MMD^2 */
 	EStatisticType get_statistic_type() const;
 
+	/**
+	 * Method that sets the approach to be taken while approximating the null-samples.
+	 *
+	 * @nmethod The null-approximation method
+	 */
 	void set_null_approximation_method(ENullApproximationMethod nmethod);
+
+	/** @return The null-approximation method */
 	ENullApproximationMethod get_null_approximation_method() const;
 
+	/** @return The name of this class */
 	virtual const char* get_name() const;
 protected:
 	virtual float64_t normalize_statistic(float64_t statistic) const = 0;
--- src/shogun/statistical_testing/TwoSampleTest.h
+++ src/shogun/statistical_testing/TwoSampleTest.h
@@ -33,19 +33,59 @@ namespace internal
 	class KernelManager;
 }
 
+/** @brief Kernel two sample test base class. Provides an interface for
+ * performing a two-sample test using a kernel, i.e. Given samples from two
+ * distributions \f$p\f$ and \f$q\f$, the null-hypothesis is: \f$H_0: p=q\f$,
+ * the alternative hypothesis: \f$H_1: p\neq q\f$.
+ *
+ * In this class, this is done using a single kernel for the data.
+ *
+ * Abstract base class.
+ */
 class CTwoSampleTest : public CTwoDistributionTest
 {
 public:
+	/** Default constructor */
 	CTwoSampleTest();
+
+	/**
+	 * Convenience constructor that initializes the samples from two distributions.
+	 *
+	 * @param samples_from_p Samples from \f$p\f$
+	 * @param samples_from_q Samples from \f$q\f$
+	 */
 	CTwoSampleTest(CFeatures* samples_from_p, CFeatures* samples_from_q);
+
+	/** Destructor */
 	virtual ~CTwoSampleTest();
 
+	/**
+	 * Method that sets the kernel that is used for performing the two-sample test.
+	 * It is kept virtual so that sub-classes can perform other initialization tasks
+	 * that has to be trigger every time a kernel is set/updated.
+	 *
+	 * @param kernel The kernel instance.
+	 */
 	virtual void set_kernel(CKernel* kernel);
+
+	/** @return The kernel instance that is presently being used for performing the test */
 	CKernel* get_kernel() const;
 
+	/**
+	 * Interface for computing the test-statistic for the hypothesis test.
+	 *
+	 * @return test statistic for the given data/parameters/methods
+	 */
 	virtual float64_t compute_statistic()=0;
+
+	/**
+	 * Interface for computing the samples under the null-hypothesis.
+	 *
+	 * @return vector of all statistics
+	 */
 	virtual SGVector<float64_t> sample_null()=0;
 
+	/** @return The name of the class */
 	virtual const char* get_name() const;
 protected:
 	internal::KernelManager& get_kernel_mgr();
