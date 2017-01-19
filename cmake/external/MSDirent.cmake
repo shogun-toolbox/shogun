19916d3a7910040f7fa26061eadc196012a218c7
Viktor Gal <viktor.gal@maeth.com>
2017-01-19 10:50:54 +0800

    Update MS Dirent implementation

--- cmake/external/MSDirent.cmake
+++ cmake/external/MSDirent.cmake
@@ -4,8 +4,8 @@ ExternalProject_Add(
 	MSDirent
 	PREFIX ${CMAKE_BINARY_DIR}/MSDirent
 	DOWNLOAD_DIR ${THIRD_PARTY_DIR}/MSDirent
-	URL http://www.softagalleria.net/download/dirent/dirent-1.13.zip
-	URL_MD5 4a4fdd27848dde028bd5e9712e2636bc
+	GIT_REPOSITORY https://github.com/tronkko/dirent.git
+	GIT_TAG 8b1db5092479a73d47eafd3de739b27e876e6bf3
 	INSTALL_COMMAND ""
 	BUILD_COMMAND ""
 	CONFIGURE_COMMAND ""
