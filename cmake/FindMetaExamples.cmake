20dda194042159dd04239621deb55c6569196a5e
Giovanni De Toni <giovanni.det@gmail.com>
2017-01-22 21:04:00 +0100

    Remove Mahalanobis example if HAVE_LAPACK is not available

--- cmake/FindMetaExamples.cmake
+++ cmake/FindMetaExamples.cmake
@@ -1,22 +1,23 @@
 function(find_meta_examples)
 	FILE(GLOB_RECURSE META_EXAMPLE_LISTINGS ${CMAKE_SOURCE_DIR}/examples/meta/src/*.sg)
-	
+
 	# temporary hacks to exclude certain meta examples that have dependencies
 	IF(NOT HAVE_NLOPT)
 		LIST(REMOVE_ITEM META_EXAMPLE_LISTINGS ${CMAKE_SOURCE_DIR}/examples/meta/src/gaussian_processes/gaussian_process_regression.sg)
 	ENDIF()
-	
+
 	IF(NOT USE_GPL_SHOGUN)
 		LIST(REMOVE_ITEM META_EXAMPLE_LISTINGS
 			${CMAKE_SOURCE_DIR}/examples/meta/src/gaussian_processes/gaussian_process_regression.sg
 			${CMAKE_SOURCE_DIR}/examples/meta/src/multiclass_classifier/multiclass_logisticregression.sg
 			)
 	ENDIF()
-	
+
 	IF(NOT HAVE_LAPACK)
 		LIST(REMOVE_ITEM META_EXAMPLE_LISTINGS
 			${CMAKE_SOURCE_DIR}/examples/meta/src/regression/linear_ridge_regression.sg
 			${CMAKE_SOURCE_DIR}/examples/meta/src/clustering/gmm.sg
+			${CMAKE_SOURCE_DIR}/examples/meta/src/distance/mahalanobis.sg
 			)
 	ENDIF()
 
