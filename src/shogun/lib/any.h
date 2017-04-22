93e50126048f4d149eb44a039d177e3e3d574d45
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2017-04-22 21:52:05 +0200

    Implement non-owning any

--- src/shogun/lib/any.h
+++ src/shogun/lib/any.h
@@ -63,6 +63,11 @@ namespace shogun
         return demangled_string;
     }
 
+    enum class PolicyType {
+        OWNING,
+        NON_OWNING
+    };
+
     /** @brief An interface for a policy to store a value.
      * Value can be any data like primitive data-types, shogun objects, etc.
      * Policy defines how to handle this data. It works with a
@@ -100,6 +105,16 @@ namespace shogun
          * @return true if both storages have same value
          */
         virtual bool equals(void** storage, void** other_storage) const = 0;
+
+        /** Returns the name of policy.
+         * @return name of policy
+         */
+        virtual std::string policy_name() const = 0;
+
+        /** Returns the type of policy.
+         * @return type of policy
+         */
+        virtual PolicyType policy_type() const = 0;
     };
 
     /** @brief This is one concrete implementation of policy that
@@ -154,7 +169,89 @@ namespace shogun
             T typed_other_storage = *(reinterpret_cast<T*>(*other_storage));
             return typed_storage == typed_other_storage;
         }
+
+        virtual std::string policy_name() const {
+            return "owning";
+        }
+
+        virtual PolicyType policy_type() const {
+            return PolicyType::OWNING;
+        }
     };
+
+    template <typename T>
+    class NonOwningAnyPolicy : public BaseAnyPolicy
+    {
+    public:
+        /** Puts provided value pointed by v (untyped to be generic) to storage.
+         * @param storage pointer to a pointer to storage
+         * @param v pointer to value
+         */
+        virtual void set(void** storage, const void* v) const
+        {
+            *(storage) = const_cast<void*>(v);
+        }
+
+        /** Clears storage.
+         * @param storage pointer to a pointer to storage
+         */
+        virtual void clear(void** storage) const
+        {
+        }
+
+        /** Returns type-name as string.
+         * @return name of type class
+         */
+        virtual std::string type() const
+        {
+            return demangledType<T>();
+        }
+
+        /** Compares type.
+         * @param ti type information
+         * @return true if type matches
+         */
+        virtual bool matches(const std::type_info& ti) const
+        {
+            return typeid(T) == ti;
+        }
+
+        /** Compares two storages.
+         * @param storage pointer to a pointer to storage
+         * @param other_storage pointer to a pointer to another storage
+         * @return true if both storages have same value
+         */
+        bool equals(void** storage, void** other_storage) const
+        {
+            T typed_storage = *(reinterpret_cast<T*>(*storage));
+            T typed_other_storage = *(reinterpret_cast<T*>(*other_storage));
+            return typed_storage == typed_other_storage;
+        }
+
+        virtual std::string policy_name() const {
+            return "non owning";
+        }
+
+        virtual PolicyType policy_type() const {
+            return PolicyType::NON_OWNING;
+        }
+    };
+
+    template <typename T>
+    static BaseAnyPolicy* owning_policy()
+    {
+        typedef PointerValueAnyPolicy<T> Policy;
+        static Policy policy;
+        return &policy;
+    }
+
+    template <typename T>
+    static BaseAnyPolicy* non_owning_policy()
+    {
+        typedef NonOwningAnyPolicy<T> Policy;
+        static Policy policy;
+        return &policy;
+    }
 
     /** @brief Allows to store objects of arbitrary types
      * by using a BaseAnyPolicy and provides a type agnostic API.
@@ -168,21 +265,27 @@ namespace shogun
         /** Used to denote an empty Any object */
         struct Empty;
 
-        /** Constructor */
-        Any() : policy(select_policy<Empty>()), storage(nullptr)
+        /** Empty value constructor */
+        Any() : Any(owning_policy<Empty>(), nullptr)
         {
         }
 
         /** Constructor to copy value */
         template <typename T>
-        explicit Any(const T& v) : policy(select_policy<T>()), storage(nullptr)
+        explicit Any(const T& v) : Any(owning_policy<T>(), nullptr)
         {
             policy->set(&storage, &v);
         }
 
-        /** Copy constructor */
-        Any(const Any& other) : policy(other.policy), storage(nullptr)
+        /** Base constructor */
+        Any(BaseAnyPolicy* the_policy, void* the_storage) : policy(the_policy), storage(the_storage)
         {
+        }
+
+        /** Copy constructor */
+        Any(const Any& other) : Any(other.policy, nullptr)
+        {
+            assert_same_policy_type(other.policy);
             policy->set(&storage, other.storage);
         }
 
@@ -192,6 +295,7 @@ namespace shogun
          */
         Any& operator=(const Any& other)
         {
+            assert_same_policy_type(other.policy);
             policy->clear(&storage);
             policy = other.policy;
             policy->set(&storage, other.storage);
@@ -239,7 +343,7 @@ namespace shogun
         template <typename T>
         inline bool same_type() const
         {
-            return (policy == select_policy<T>()) || same_type_fallback<T>();
+            return (policy == owning_policy<T>()) || (policy == non_owning_policy<T>()) || same_type_fallback<T>();
         }
 
         /** @return true if type-id is same. */
@@ -254,15 +358,18 @@ namespace shogun
         {
             return same_type<Empty>();
         }
+
     private:
-        template <typename T>
-        static BaseAnyPolicy* select_policy()
-        {
-            typedef PointerValueAnyPolicy<T> Policy;
-            static Policy policy;
-            return &policy;
+
+        void assert_same_policy_type(BaseAnyPolicy* other_policy) {
+            if (policy->policy_type() != other_policy->policy_type()) {
+                throw std::logic_error("The policies are different: " +
+                                       policy->policy_name() + " and " +
+                                       other_policy->policy_name());
+            }
         }
 
+    private:
         BaseAnyPolicy* policy;
         void* storage;
     };
@@ -302,6 +409,12 @@ namespace shogun
     inline Any erase_type(const T& v)
     {
         return Any(v);
+    }
+
+    template <typename T>
+    inline Any erase_type_non_owning(T* v)
+    {
+        return Any(non_owning_policy<T>(), v);
     }
 
     /** Tries to recall Any type, fails when type is wrong.
