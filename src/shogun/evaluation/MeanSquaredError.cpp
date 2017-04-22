26d7312011d54e5eed104bf4c27538ecfc93ef94
Fernando Iglesias Garcia <fernando.iglesiasg@gmail.com>
2017-04-22 16:19:09 +0200

    From from ASSERT to REQUIRE in MeanSquaredError.

--- src/shogun/evaluation/MeanSquaredError.cpp
+++ src/shogun/evaluation/MeanSquaredError.cpp
@@ -17,10 +17,11 @@ using namespace shogun;
 
 float64_t CMeanSquaredError::evaluate(CLabels* predicted, CLabels* ground_truth)
 {
-	ASSERT(predicted && ground_truth)
-	ASSERT(predicted->get_num_labels() == ground_truth->get_num_labels())
-	ASSERT(predicted->get_label_type() == LT_REGRESSION)
-	ASSERT(ground_truth->get_label_type() == LT_REGRESSION)
+	REQUIRE(predicted, "Predicted labels must be not null.\n")
+	REQUIRE(ground_truth, "Ground truth labels must be not null.\n")
+	REQUIRE(predicted->get_num_labels() == ground_truth->get_num_labels(), "The number of predicted labels (%d) must be equal to the number of ground truth labels (%d).\n")
+	REQUIRE(predicted->get_label_type() == LT_REGRESSION, "Predicted label type (%d) must be regression (%d).\n", predicted->get_label_type(), LT_REGRESSION)
+	REQUIRE(ground_truth->get_label_type() == LT_REGRESSION, "Ground truth label type (%d) must be regression (%d).\n", ground_truth->get_label_type(), LT_REGRESSION)
 	int32_t length = predicted->get_num_labels();
 	float64_t mse = 0.0;
 	for (int32_t i=0; i<length; i++)
