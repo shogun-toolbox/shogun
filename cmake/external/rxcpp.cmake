f7fe02f60483cec2165384560c8592e86cd4349d
Giovanni De Toni <giovanni.det@gmail.com>
2017-06-12 14:53:50 +0200

    [PrematureStopping] Add CMake support to search or install RxCpp.

---
+++ cmake/external/rxcpp.cmake
@@ -0,0 +1,23 @@
+GetCompilers()
+
+include(ExternalProject)
+ExternalProject_Add(
+        rxcpp
+        PREFIX ${CMAKE_BINARY_DIR}/rxcpp
+        DOWNLOAD_DIR ${THIRD_PARTY_DIR}/rxcpp
+        URL https://github.com/Reactive-Extensions/RxCpp/archive/v4.0.0.tar.gz
+        URL_MD5 feb89934f465bb5ac513c9adce8d3b1b
+        CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo
+        -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_BINARY_DIR}/src/shogun/lib/external
+        -DCMAKE_C_COMPILER:STRING=${C_COMPILER}
+        -DCMAKE_CXX_COMPILER:STRING=${CXX_COMPILER}
+        BUILD_COMMAND ""
+        )
+
+add_dependencies(libshogun rxcpp)
+
+set(rxcpp_INCLUDE_DIR ${THIRD_PARTY_INCLUDE_DIR})
+
+UNSET(C_COMPILER)
+UNSET(CXX_COMPILER)
