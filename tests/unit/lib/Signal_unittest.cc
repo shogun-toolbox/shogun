378e9d0196ab8d89a5d4f8b1cd6b6ff7b3c056cd
Giovanni De Toni <giovanni.det@gmail.com>
2017-06-19 10:27:20 +0200

    [PrematureStopping] Removed Windows code and polish code.

--- tests/unit/lib/Signal_unittest.cc
+++ tests/unit/lib/Signal_unittest.cc
@@ -9,33 +9,32 @@ using namespace rxcpp;
 
 TEST(Signal, SIGINT_test)
 {
-
 	CSignal tmp;
+	tmp.enable_handler();
 	int on_next_v = 0;
 	int on_complete_v = 0;
 	auto sub = rxcpp::make_subscriber<int>(
 	    [&on_next_v](int v) { on_next_v = 1; }, [&]() { on_complete_v = 1; });
 
 	tmp.get_SIGINT_observable().subscribe(sub);
+	tmp.get_SIGINT_observable().connect();
 
-	tmp.handler(SIGINT);
-
-	EXPECT_TRUE(on_next_v == 0);
 	EXPECT_TRUE(on_complete_v == 1);
+	EXPECT_TRUE(on_next_v == 0);
 }
 
 TEST(Signal, SIGURG_test)
 {
 
 	CSignal tmp;
+	tmp.enable_handler();
 	int on_next_v = 0;
 	int on_complete_v = 0;
 	auto sub = rxcpp::make_subscriber<int>(
 	    [&](int v) { on_next_v++; }, [&]() { on_complete_v++; });
 
 	tmp.get_SIGURG_observable().subscribe(sub);
-
-	tmp.handler(SIGURG);
+	tmp.get_SIGURG_observable().connect();
 
 	EXPECT_TRUE(on_next_v == 1);
 	EXPECT_TRUE(on_complete_v == 0);
