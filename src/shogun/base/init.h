07335b1195e4a16e59633c889db8a70d66f1e764
Giovanni De Toni <giovanni.det@gmail.com>
2017-06-23 09:16:32 +0200

    [PrematureStopping] Register SIGINT signal handler.
    The signal handler is set up when calling init_shogun(). It is
    disabled by default (sg_signal->enable_handler() is needed).
    Applied also style fixes.

--- src/shogun/base/init.h
+++ src/shogun/base/init.h
@@ -23,91 +23,96 @@ namespace shogun
 	class Parallel;
 	class CRandom;
 	class SGLinalg;
+	class CSignal;
 
-/** This function must be called before libshogun is used. Usually shogun does
- * not provide any output messages (neither debugging nor error; apart from
- * exceptions). This function allows one to specify customized output
- * callback functions and a callback function to check for exceptions:
- *
- * @param print_message function pointer to print a message
- * @param print_warning function pointer to print a warning message
- * @param print_error function pointer to print an error message (this will be
- *                                  printed before shogun throws an exception)
- *
- * @param cancel_computations function pointer to check for exception
- *
- */
-void init_shogun(void (*print_message)(FILE* target, const char* str) = NULL,
-		void (*print_warning)(FILE* target, const char* str) = NULL,
-		void (*print_error)(FILE* target, const char* str) = NULL,
-		void (*cancel_computations)(bool &delayed, bool &immediately)=NULL);
+	/** This function must be called before libshogun is used. Usually shogun
+	 * does
+	 * not provide any output messages (neither debugging nor error; apart from
+	 * exceptions). This function allows one to specify customized output
+	 * callback functions and a callback function to check for exceptions:
+	 *
+	 * @param print_message function pointer to print a message
+	 * @param print_warning function pointer to print a warning message
+	 * @param print_error function pointer to print an error message (this will
+	 * be
+	 *                                  printed before shogun throws an
+	 * exception)
+	 *
+	 * @param cancel_computations function pointer to check for exception
+	 *
+	 */
+	void init_shogun(
+	    void (*print_message)(FILE* target, const char* str) = NULL,
+	    void (*print_warning)(FILE* target, const char* str) = NULL,
+	    void (*print_error)(FILE* target, const char* str) = NULL,
+	    void (*cancel_computations)(bool& delayed, bool& immediately) = NULL);
 
-/** init shogun with defaults */
-void init_shogun_with_defaults();
+	/** init shogun with defaults */
+	void init_shogun_with_defaults();
 
-/** This function must be called when one stops using libshogun. It will
- * perform a number of cleanups */
-void exit_shogun();
+	/** This function must be called when one stops using libshogun. It will
+	 * perform a number of cleanups */
+	void exit_shogun();
 
-/** set the global io object
- *
- * @param io io object to use
- */
-void set_global_io(SGIO* io);
+	/** set the global io object
+	 *
+	 * @param io io object to use
+	 */
+	void set_global_io(SGIO* io);
 
-/** get the global io object
- *
- * @return io object
- */
-SGIO* get_global_io();
+	/** get the global io object
+	 *
+	 * @return io object
+	 */
+	SGIO* get_global_io();
 
-/** set the global parallel object
- *
- * @param parallel parallel object to use
- */
-void set_global_parallel(Parallel* parallel);
+	/** set the global parallel object
+	 *
+	 * @param parallel parallel object to use
+	 */
+	void set_global_parallel(Parallel* parallel);
 
-/** get the global parallel object
- *
- * @return parallel object
- */
-Parallel* get_global_parallel();
+	/** get the global parallel object
+	 *
+	 * @return parallel object
+	 */
+	Parallel* get_global_parallel();
 
-/** set the global version object
- *
- * @param version version object to use
- */
-void set_global_version(Version* version);
+	/** set the global version object
+	 *
+	 * @param version version object to use
+	 */
+	void set_global_version(Version* version);
 
-/** get the global version object
- *
- * @return version object
- */
-Version* get_global_version();
+	/** get the global version object
+	 *
+	 * @return version object
+	 */
+	Version* get_global_version();
 
-/** set the global math object
- *
- * @param math math object to use
- */
-void set_global_math(CMath* math);
+	/** set the global math object
+	 *
+	 * @param math math object to use
+	 */
+	void set_global_math(CMath* math);
 
-/** get the global math object
- *
- * @return math object
- */
-CMath* get_global_math();
+	/** get the global math object
+	 *
+	 * @return math object
+	 */
+	CMath* get_global_math();
 
-/** set the global random object
- *
- * @param rand random object to use
- */
-void set_global_rand(CRandom* rand);
+	/** set the global random object
+	 *
+	 * @param rand random object to use
+	 */
+	void set_global_rand(CRandom* rand);
 
-/** get the global random object
- *
- * @return random object
- */
-CRandom* get_global_rand();
+	/** get the global random object
+	 *
+	 * @return random object
+	 */
+	CRandom* get_global_rand();
 
 #ifndef SWIG // SWIG should skip this part
 /** get the global linalg library object
@@ -116,6 +121,12 @@ CRandom* get_global_rand();
  */
 SGLinalg* get_global_linalg();
 #endif
+
+/** get the global singnal handler object
+ *
+ * @return linalg object
+ */
+CSignal* get_global_signal();
 
 /** Checks environment variables and modifies global objects
  */
