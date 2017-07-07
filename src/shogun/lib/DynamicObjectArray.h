1b55e385bb2326f54394e7a6cabab1f376247448
Viktor Gal <viktor.gal@maeth.com>
2017-07-07 22:54:34 +0800

    Add Serializable object and drop Wrapped classes

--- src/shogun/lib/DynamicObjectArray.h
+++ src/shogun/lib/DynamicObjectArray.h
@@ -12,11 +12,14 @@
 #ifndef _DYNAMIC_OBJECT_ARRAY_H_
 #define _DYNAMIC_OBJECT_ARRAY_H_
 
+#include <type_traits>
+
 #include <shogun/lib/config.h>
 
 #include <shogun/base/SGObject.h>
 #include <shogun/base/DynArray.h>
 #include <shogun/base/Parameter.h>
+#include <shogun/io/Serializable.h>
 
 namespace shogun
 {
@@ -277,6 +280,17 @@ class CDynamicObjectArray : public CSGObject
 			return success;
 		}
 
+		template <typename T, typename T2 = typename std::enable_if<!std::is_base_of<CSGObject, typename std::remove_pointer<T>::type>::value, T>::type>
+		inline bool append_element(T e, const char* name="")
+		{
+			auto serialized_element = new CSerializable<T>(e, name);
+			bool success = m_array.append_element(serialized_element);
+			if (success)
+				SG_REF(serialized_element);
+
+			return success;
+		}
+
 		/** append array element to the end of array
 		 *
 		 * @param e element to append
@@ -396,6 +410,10 @@ class CDynamicObjectArray : public CSGObject
 		/** @return object name */
 		virtual const char* get_name() const
 		{ return "DynamicObjectArray"; }
+
+		// without this definition R interface is missing these inherited functions
+		using CSGObject::save_serializable;
+		using CSGObject::load_serializable;
 
 		/** Can (optionally) be overridden to pre-initialize some member
 		 *  variables which are not PARAMETER::ADD'ed.  Make sure that at
