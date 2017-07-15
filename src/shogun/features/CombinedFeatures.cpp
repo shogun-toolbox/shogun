c52acaf9bdc7492801642d76f5e4e2d61b73673a
DavidH <HeyItsDavid@users.noreply.github.com>
2017-07-15 14:32:36 +0100

    Because i apparently cant configure my idents properly

--- src/shogun/features/CombinedFeatures.cpp
+++ src/shogun/features/CombinedFeatures.cpp
@@ -50,9 +50,7 @@ CFeatures* CCombinedFeatures::get_feature_obj(int32_t idx)
 {
 	if (idx >= get_num_feature_obj())
 	{
-		SG_SERROR(
-				"feature index out of bounds (%d >= %d)\n", idx,
-				get_num_feature_obj());
+		SG_SERROR("feature index out of bounds (%d >= %d)\n", idx, get_num_feature_obj());
 	}
 	return (CFeatures*) feature_array->get_element(idx);
 }
