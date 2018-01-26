74869b0f501527bcdbd913fe7a37b58f5f7bf0db
Heiko Strathmann <heiko.strathmann@gmail.com>
2018-01-26 13:30:01 +0000

    add clone_basic and clone_equals tests for all SGObjects

--- tests/unit/base/SGObject_unittest.cc
+++ tests/unit/base/SGObject_unittest.cc
@@ -10,6 +10,7 @@
  */
 
 #include "MockObject.h"
+#include <iterator>
 #include <shogun/base/class_list.h>
 #include <shogun/base/some.h>
 #include <shogun/features/DenseFeatures.h>
@@ -35,10 +36,11 @@ class SGObjectEquals : public ::testing::Test
 {
 };
 // TODO: SGString doesn't support complex128_t, so omitted here
-typedef ::testing::Types<bool, char, int8_t, uint8_t, int16_t, int32_t, int64_t,
+typedef ::testing::Types<bool, char, int8_t, int16_t, int32_t, int64_t,
                          float32_t, float64_t, floatmax_t>
-    CloneEqualsTypes;
-TYPED_TEST_CASE(SGObjectEquals, CloneEqualsTypes);
+    SGBasicTypes;
+
+TYPED_TEST_CASE(SGObjectEquals, SGBasicTypes);
 
 TYPED_TEST(SGObjectEquals, same)
 {
@@ -584,5 +586,198 @@ TEST(SGObjectAll, DISABLED_tag_coverage)
 		EXPECT_EQ(tag_names, old_names);
 
 		SG_UNREF(obj);
+	}
+}
+
+template <typename T>
+class SGObjectAll : public ::testing::Test
+{
+};
+
+struct untemplated_sgobject
+{
+};
+typedef ::testing::Types<bool, char, int8_t, int16_t, int32_t, int64_t,
+                         float32_t, float64_t, floatmax_t, untemplated_sgobject>
+    SGPrimitiveTypes;
+TYPED_TEST_CASE(SGObjectAll, SGPrimitiveTypes);
+
+/** Returns primitive type value for template parameter. Used in tests that
+ * cover
+ * all available classes. */
+template <class T>
+EPrimitiveType sg_primitive_type();
+
+#define SG_PRIMITIVE_TYPE(T, ptype)                                            \
+	template <>                                                                \
+	EPrimitiveType sg_primitive_type<T>()                                      \
+	{                                                                          \
+		return ptype;                                                          \
+	}
+
+SG_PRIMITIVE_TYPE(bool, PT_BOOL);
+SG_PRIMITIVE_TYPE(char, PT_CHAR);
+SG_PRIMITIVE_TYPE(int8_t, PT_INT8);
+SG_PRIMITIVE_TYPE(uint8_t, PT_UINT8);
+SG_PRIMITIVE_TYPE(int16_t, PT_INT16);
+SG_PRIMITIVE_TYPE(int32_t, PT_INT32);
+SG_PRIMITIVE_TYPE(int64_t, PT_INT64);
+SG_PRIMITIVE_TYPE(float32_t, PT_FLOAT32);
+SG_PRIMITIVE_TYPE(float64_t, PT_FLOAT64);
+SG_PRIMITIVE_TYPE(floatmax_t, PT_FLOATMAX);
+SG_PRIMITIVE_TYPE(untemplated_sgobject, PT_NOT_GENERIC);
+
+/** Helper to write c++11 style loops in tests that cover all Shogun classes */
+template <typename T>
+class sg_object_iterator
+{
+public:
+	sg_object_iterator()
+	{
+		m_class_names = available_objects();
+	}
+
+	sg_object_iterator(std::set<std::string> ignores)
+	{
+		m_class_names = available_objects();
+
+		std::set<std::string> diff;
+
+		std::set_difference(
+		    m_class_names.begin(), m_class_names.end(), ignores.begin(),
+		    ignores.end(), std::inserter(diff, diff.begin()));
+		m_class_names = diff;
+	}
+
+	class Iterator : public std::iterator<std::input_iterator_tag, T>
+	{
+	public:
+		Iterator(
+		    std::set<std::string>::iterator it,
+		    std::set<std::string>::iterator end)
+		    : m_it(it), m_end(end), m_obj(nullptr)
+		{
+			create_and_forward_if_necessary();
+		}
+		Iterator(const Iterator& other)
+		    : m_it(other.m_it), m_end(other.m_end), m_obj(other.m_obj)
+		{
+			create_and_forward_if_necessary();
+		}
+		Iterator(Iterator&& other)
+		    : m_it(other.m_it), m_end(other.m_end), m_obj(other.m_obj)
+		{
+			create_and_forward_if_necessary();
+		}
+		~Iterator()
+		{
+			SG_UNREF(m_obj);
+		}
+
+		Iterator& operator=(const Iterator&) = delete;
+		Iterator& operator++()
+		{
+			SG_UNREF(m_obj);
+			do
+			{
+				++m_it;
+
+				if (m_it == m_end)
+					return *this;
+
+				m_obj = create_obj();
+			} while (!m_obj);
+
+			return *this;
+		}
+		CSGObject* operator*()
+		{
+			return m_obj;
+		}
+		bool operator!=(const Iterator& other) const
+		{
+			return this->m_it != other.m_it;
+		}
+
+	private:
+		CSGObject* create_obj()
+		{
+			auto obj = create((*m_it).c_str(), sg_primitive_type<T>());
+			SG_REF(obj);
+			return obj;
+		}
+
+		void create_and_forward_if_necessary()
+		{
+			if (m_it == m_end)
+				return;
+
+			m_obj = create_obj();
+			while (!m_obj && m_it != m_end)
+			{
+				this->operator++();
+			}
+		}
+
+		std::set<std::string>::iterator m_it;
+		std::set<std::string>::iterator m_end;
+		CSGObject* m_obj;
+	};
+
+	Iterator begin() const
+	{
+		return Iterator(m_class_names.begin(), m_class_names.end());
+	}
+	Iterator end() const
+	{
+		return Iterator(m_class_names.end(), m_class_names.end());
+	}
+
+	std::set<std::string> m_class_names;
+};
+
+TYPED_TEST(SGObjectAll, sg_object_iterator)
+{
+	for (auto obj : sg_object_iterator<TypeParam>())
+	{
+		SCOPED_TRACE(obj->get_name());
+		ASSERT_NE(obj, nullptr);
+		ASSERT_EQ(obj->ref_count(), 1);
+	}
+}
+
+std::set<std::string> sg_object_all_ignores = {"Set", "ParseBuffer",
+                                               "TreeMachine"};
+
+TYPED_TEST(SGObjectAll, clone_basic)
+{
+	for (auto obj : sg_object_iterator<TypeParam>(sg_object_all_ignores))
+	{
+		CSGObject* clone = nullptr;
+		try
+		{
+			clone = obj->clone();
+		}
+		catch (...)
+		{
+		}
+
+		ASSERT_NE(clone, nullptr);
+		EXPECT_NE(clone, obj);
+		EXPECT_EQ(clone->ref_count(), 1);
+		EXPECT_EQ(std::string(clone->get_name()), std::string(obj->get_name()));
+
+		SG_UNREF(clone);
+	}
+}
+
+TYPED_TEST(SGObjectAll, clone_equals)
+{
+	for (auto obj : sg_object_iterator<TypeParam>(sg_object_all_ignores))
+	{
+		CSGObject* clone = obj->clone();
+		EXPECT_TRUE(clone->equals(obj));
+
+		SG_UNREF(clone);
 	}
 }
