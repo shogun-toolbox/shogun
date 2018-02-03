9c3dc95ba8967612b5cfd3d5b0c957bb0a90208d
Heiko Strathmann <heiko.strathmann@gmail.com>
2018-02-03 11:05:47 +0000

    completely refactor tests due to failing travis

---
+++ tests/unit/base/SGObjectAll_unittest.cc
@@ -0,0 +1,315 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Written (W) 2013-2018 Heiko Strathmann
+ * Written (W) 2014 Thoralf Klein
+ * Written (W) 2015 Wu Lin
+ */
+
+#include "utils/Utils.h"
+#include <gtest/gtest.h>
+#include <iterator>
+#include <shogun/base/Parameter.h>
+#include <shogun/base/SGObject.h>
+#include <shogun/base/class_list.h>
+#include <shogun/base/range.h>
+#include <shogun/base/some.h>
+#include <shogun/io/SerializableAsciiFile.h>
+
+using namespace shogun;
+
+// to have a type for non-template SGObject classes
+struct untemplated_sgobject
+{
+};
+
+/** Returns primitive type value for template parameter */
+template <class T>
+EPrimitiveType sg_primitive_type();
+template <class T>
+std::string sg_primitive_type_string();
+
+// stringizing the result of expansion of a macro argument used below
+// https://gcc.gnu.org/onlinedocs/cpp/Stringizing.html
+#define STRING__(s) #s
+#define XSTRING__(s) STRING__(s)
+
+#define SG_PRIMITIVE_TYPE(T, ptype)                                            \
+	template <>                                                                \
+	EPrimitiveType sg_primitive_type<T>()                                      \
+	{                                                                          \
+		return ptype;                                                          \
+	}                                                                          \
+                                                                               \
+	template <>                                                                \
+	std::string sg_primitive_type_string<T>()                                  \
+	{                                                                          \
+		return std::string(XSTRING__(ptype));                                  \
+	}
+
+SG_PRIMITIVE_TYPE(bool, PT_BOOL);
+SG_PRIMITIVE_TYPE(char, PT_CHAR);
+SG_PRIMITIVE_TYPE(int8_t, PT_INT8);
+SG_PRIMITIVE_TYPE(uint8_t, PT_UINT8);
+SG_PRIMITIVE_TYPE(int16_t, PT_INT16);
+SG_PRIMITIVE_TYPE(int32_t, PT_INT32);
+SG_PRIMITIVE_TYPE(int64_t, PT_INT64);
+SG_PRIMITIVE_TYPE(float32_t, PT_FLOAT32);
+SG_PRIMITIVE_TYPE(float64_t, PT_FLOAT64);
+SG_PRIMITIVE_TYPE(floatmax_t, PT_FLOATMAX);
+SG_PRIMITIVE_TYPE(untemplated_sgobject, PT_NOT_GENERIC);
+#undef SG_PRIMITIVE_TYPE
+#undef XSTRING__
+#undef STRING__
+
+/** Helper to write c++11 style loops in tests that cover all Shogun classes */
+template <typename T>
+class sg_object_iterator
+{
+public:
+	sg_object_iterator()
+	{
+		m_class_names = available_objects();
+	}
+
+	sg_object_iterator(std::set<std::string> ignores)
+	{
+		m_class_names = available_objects();
+
+		std::set<std::string> diff;
+
+		std::set_difference(
+		    m_class_names.begin(), m_class_names.end(), ignores.begin(),
+		    ignores.end(), std::inserter(diff, diff.begin()));
+		m_class_names = diff;
+	}
+
+	class Iterator : public std::iterator<std::input_iterator_tag, T>
+	{
+	public:
+		Iterator(
+		    std::set<std::string>::iterator it,
+		    std::set<std::string>::iterator end)
+		    : m_it(it), m_end(end), m_obj(nullptr)
+		{
+			create_and_forward_if_necessary();
+		}
+		Iterator(const Iterator& other)
+		    : m_it(other.m_it), m_end(other.m_end), m_obj(other.m_obj)
+		{
+			create_and_forward_if_necessary();
+		}
+		Iterator(Iterator&& other)
+		    : m_it(other.m_it), m_end(other.m_end), m_obj(other.m_obj)
+		{
+			create_and_forward_if_necessary();
+		}
+		~Iterator()
+		{
+			SG_UNREF(m_obj);
+		}
+
+		Iterator& operator=(const Iterator&) = delete;
+		Iterator& operator++()
+		{
+			SG_UNREF(m_obj);
+			do
+			{
+				++m_it;
+
+				if (m_it == m_end)
+					return *this;
+
+				m_obj = create_obj();
+			} while (!m_obj);
+
+			return *this;
+		}
+		CSGObject* operator*()
+		{
+			return m_obj;
+		}
+		bool operator!=(const Iterator& other) const
+		{
+			return this->m_it != other.m_it;
+		}
+
+	private:
+		CSGObject* create_obj()
+		{
+			auto obj = create((*m_it).c_str(), sg_primitive_type<T>());
+			SG_REF(obj);
+			return obj;
+		}
+
+		void create_and_forward_if_necessary()
+		{
+			if (m_it == m_end)
+				return;
+
+			m_obj = create_obj();
+			while (!m_obj && m_it != m_end)
+			{
+				this->operator++();
+			}
+		}
+
+		std::set<std::string>::iterator m_it;
+		std::set<std::string>::iterator m_end;
+		CSGObject* m_obj;
+	};
+
+	Iterator begin() const
+	{
+		return Iterator(m_class_names.begin(), m_class_names.end());
+	}
+	Iterator end() const
+	{
+		return Iterator(m_class_names.end(), m_class_names.end());
+	}
+
+	std::set<std::string> m_class_names;
+};
+
+// list of classes that (currently) cannot be instantiated
+std::set<std::string> sg_object_all_ignores = {"ParseBuffer", "Set",
+                                               "TreeMachine"};
+
+// template arguments for SGObject
+// TODO: SGString doesn't support complex128_t, so omitted here
+typedef ::testing::Types<bool, char, int8_t, int16_t, int32_t, int64_t,
+                         float32_t, float64_t, floatmax_t, untemplated_sgobject>
+    SGPrimitiveTypes;
+
+template <typename T>
+class SGObjectAll : public ::testing::Test
+{
+};
+
+TYPED_TEST_CASE(SGObjectAll, SGPrimitiveTypes);
+
+TYPED_TEST(SGObjectAll, sg_object_iterator)
+{
+	for (auto obj : sg_object_iterator<TypeParam>())
+	{
+		ASSERT_NE(obj, nullptr);
+		SCOPED_TRACE(obj->get_name());
+		ASSERT_EQ(obj->ref_count(), 1);
+	}
+}
+
+TYPED_TEST(SGObjectAll, clone_basic)
+{
+	for (auto obj : sg_object_iterator<TypeParam>(sg_object_all_ignores))
+	{
+		SCOPED_TRACE(obj->get_name());
+		CSGObject* clone = nullptr;
+		try
+		{
+			clone = obj->clone();
+		}
+		catch (...)
+		{
+		}
+
+		ASSERT_NE(clone, nullptr);
+		EXPECT_NE(clone, obj);
+		EXPECT_EQ(clone->ref_count(), 1);
+		EXPECT_EQ(std::string(clone->get_name()), std::string(obj->get_name()));
+
+		SG_UNREF(clone);
+	}
+}
+
+TYPED_TEST(SGObjectAll, clone_equals_empty)
+{
+	for (auto obj : sg_object_iterator<TypeParam>(sg_object_all_ignores))
+	{
+		SCOPED_TRACE(obj->get_name());
+
+		CSGObject* clone = obj->clone();
+		EXPECT_TRUE(clone->equals(obj));
+
+		SG_UNREF(clone);
+	}
+}
+
+TYPED_TEST(SGObjectAll, serialization_empty_ascii)
+{
+	for (auto obj : sg_object_iterator<TypeParam>(sg_object_all_ignores))
+	{
+		SCOPED_TRACE(obj->get_name());
+
+		std::string filename = "/tmp/shogun-unittest-serialization-ascii-" +
+		                       std::string(obj->get_name()) + "_" +
+		                       sg_primitive_type_string<TypeParam>() +
+		                       ".XXXXXX";
+
+		generate_temp_filename(const_cast<char*>(filename.c_str()));
+
+		auto file_save = some<CSerializableAsciiFile>(filename.c_str(), 'w');
+		ASSERT_TRUE(obj->save_serializable(file_save));
+		file_save->close();
+
+		CSGObject* loaded = create(obj->get_name(), obj->get_generic());
+		ASSERT_NE(loaded, nullptr);
+		auto file_load = some<CSerializableAsciiFile>(filename.c_str(), 'r');
+		ASSERT_TRUE(loaded->load_serializable(file_load));
+		file_load->close();
+
+		// set accuracy to tolerate lossy formats
+		set_global_fequals_epsilon(1e-14);
+		ASSERT_TRUE(obj->equals(loaded));
+		set_global_fequals_epsilon(0);
+
+		SG_UNREF(loaded);
+
+		ASSERT_EQ(unlink(filename.c_str()), 0);
+	}
+}
+
+// temporary test until old parameter framework is gone
+// enable test to hunt for parameters not registered in tags
+// see https://github.com/shogun-toolbox/shogun/issues/4117
+// not typed as all template instantiations will have the same tags
+TEST(SGObjectAll, DISABLED_tag_coverage)
+{
+	auto class_names = available_objects();
+
+	for (auto class_name : class_names)
+	{
+		auto obj = create(class_name.c_str(), PT_NOT_GENERIC);
+
+		// templated classes cannot be created in the above way
+		if (!obj)
+		{
+			// only test single generic type here: all types have the same
+			// parameter names
+			obj = create(class_name.c_str(), PT_FLOAT64);
+		}
+
+		// obj must exist now, whether templated or not
+		ASSERT_NE(obj, nullptr);
+
+		// old parameter framework names
+		std::vector<std::string> old_names;
+		for (auto i : range(obj->m_parameters->get_num_parameters()))
+			old_names.push_back(obj->m_parameters->get_parameter(i)->m_name);
+		auto tag_names = obj->parameter_names();
+
+		// hack to increase readability of error messages
+		old_names.push_back("_Shogun class: " + class_name);
+		tag_names.push_back("_Shogun class: " + class_name);
+
+		// comparing std::vector depends on order
+		std::sort(old_names.begin(), old_names.end());
+		std::sort(tag_names.begin(), tag_names.end());
+
+		EXPECT_EQ(tag_names, old_names);
+
+		SG_UNREF(obj);
+	}
+}
--- tests/unit/base/SGObject_unittest.cc
+++ tests/unit/base/SGObject_unittest.cc
@@ -11,7 +11,6 @@
 
 #include "MockObject.h"
 #include "utils/Utils.h"
-#include <iterator>
 #include <shogun/base/class_list.h>
 #include <shogun/base/some.h>
 #include <shogun/features/DenseFeatures.h>
@@ -45,6 +44,11 @@ typedef ::testing::Types<bool, char, int8_t, int16_t, int32_t, int64_t,
 
 TYPED_TEST_CASE(SGObjectEquals, SGBasicTypes);
 
+TYPED_TEST(SGObjectEquals, mock_allocate_delete)
+{
+	auto obj = some<CCloneEqualsMock<int32_t>>();
+}
+
 TYPED_TEST(SGObjectEquals, same)
 {
 	auto obj1 = some<CCloneEqualsMock<TypeParam>>();
@@ -53,11 +57,6 @@ TYPED_TEST(SGObjectEquals, same)
 	EXPECT_TRUE(obj1->equals(obj1));
 	EXPECT_TRUE(obj1->equals(obj2));
 	EXPECT_TRUE(obj2->equals(obj1));
-}
-
-TYPED_TEST(SGObjectEquals, mock_allocate_delete)
-{
-	auto obj = some<CCloneEqualsMock<int32_t>>();
 }
 
 TYPED_TEST(SGObjectEquals, different_null)
@@ -186,14 +185,6 @@ TEST(SGObject, equals_different_type)
 
 	EXPECT_FALSE(obj1->equals(obj2));
 	EXPECT_FALSE(obj2->equals(obj1));
-}
-
-TEST(SGObject, clone_not_null_and_correct_type)
-{
-	auto object = some<CGaussianKernel>();
-	auto clone = object->clone();
-	ASSERT_NE(clone, nullptr);
-	ASSERT_TRUE(!strcmp(object->get_name(), clone->get_name()));
 }
 
 TEST(SGObject, clone_basic_parameter)
@@ -553,292 +544,3 @@ TEST(SGObject, watched_parameter_object)
 	obj = nullptr;
 	EXPECT_EQ(other_obj->ref_count(), 1);
 }
-
-// temporary test until old parameter framework is gone
-// enable test to hunt for parameters not registered in tags
-// see https://github.com/shogun-toolbox/shogun/issues/4117
-// not typed as all template instantiations will have the same tags
-TEST(SGObjectAll, DISABLED_tag_coverage)
-{
-	auto class_names = available_objects();
-
-	for (auto class_name : class_names)
-	{
-		auto obj = create(class_name.c_str(), PT_NOT_GENERIC);
-
-		// templated classes cannot be created in the above way
-		if (!obj)
-		{
-			// only test single generic type here: all types have the same
-			// parameter names
-			obj = create(class_name.c_str(), PT_FLOAT64);
-		}
-
-		// whether templated or not
-		ASSERT_NE(obj, nullptr);
-
-		// old parameter framework names
-		std::vector<std::string> old_names;
-		for (auto i : range(obj->m_parameters->get_num_parameters()))
-			old_names.push_back(obj->m_parameters->get_parameter(i)->m_name);
-		auto tag_names = obj->parameter_names();
-
-		// hack to increase readability of error messages
-		old_names.push_back("Class: " + class_name);
-		tag_names.push_back("Class: " + class_name);
-
-		EXPECT_EQ(tag_names, old_names);
-
-		SG_UNREF(obj);
-	}
-}
-
-// fixture for tests that cover all SGObject classes
-template <typename T>
-class SGObjectAll : public ::testing::Test
-{
-};
-
-// to have a type for non-template SGObject classes
-struct untemplated_sgobject
-{
-};
-
-//// template arguments for SGObject
-//// TODO: SGString doesn't support complex128_t, so omitted here
-//typedef ::testing::Types<bool, char, int8_t, int16_t, int32_t, int64_t,
-//                         float32_t, float64_t, floatmax_t, untemplated_sgobject>
-//    SGPrimitiveTypes;
-//TYPED_TEST_CASE(SGObjectAll, SGPrimitiveTypes);
-//
-///** Returns primitive type value for template parameter. Used in tests that
-// * cover all available classes. */
-//template <class T>
-//EPrimitiveType sg_primitive_type();
-//template <class T>
-//std::string sg_primitive_type_string();
-//
-//// stringizing the result of expansion of a macro argument used below
-//// https://gcc.gnu.org/onlinedocs/cpp/Stringizing.html
-//#define STRING__(s) #s
-//#define XSTRING__(s) STRING__(s)
-//
-//#define SG_PRIMITIVE_TYPE(T, ptype)                                            \
-//	template <>                                                                \
-//	EPrimitiveType sg_primitive_type<T>()                                      \
-//	{                                                                          \
-//		return ptype;                                                          \
-//	}                                                                          \
-//                                                                               \
-//	template <>                                                                \
-//	std::string sg_primitive_type_string<T>()                                  \
-//	{                                                                          \
-//		return std::string(XSTRING__(ptype));                                  \
-//	}
-//
-//SG_PRIMITIVE_TYPE(bool, PT_BOOL);
-//SG_PRIMITIVE_TYPE(char, PT_CHAR);
-//SG_PRIMITIVE_TYPE(int8_t, PT_INT8);
-//SG_PRIMITIVE_TYPE(uint8_t, PT_UINT8);
-//SG_PRIMITIVE_TYPE(int16_t, PT_INT16);
-//SG_PRIMITIVE_TYPE(int32_t, PT_INT32);
-//SG_PRIMITIVE_TYPE(int64_t, PT_INT64);
-//SG_PRIMITIVE_TYPE(float32_t, PT_FLOAT32);
-//SG_PRIMITIVE_TYPE(float64_t, PT_FLOAT64);
-//SG_PRIMITIVE_TYPE(floatmax_t, PT_FLOATMAX);
-//SG_PRIMITIVE_TYPE(untemplated_sgobject, PT_NOT_GENERIC);
-//#undef SG_PRIMITIVE_TYPE
-//#undef XSTRING__
-//#undef STRING__
-//
-///** Helper to write c++11 style loops in tests that cover all Shogun classes */
-//template <typename T>
-//class sg_object_iterator
-//{
-//public:
-//	sg_object_iterator()
-//	{
-//		m_class_names = available_objects();
-//	}
-//
-//	sg_object_iterator(std::set<std::string> ignores)
-//	{
-//		m_class_names = available_objects();
-//
-//		std::set<std::string> diff;
-//
-//		std::set_difference(
-//		    m_class_names.begin(), m_class_names.end(), ignores.begin(),
-//		    ignores.end(), std::inserter(diff, diff.begin()));
-//		m_class_names = diff;
-//	}
-//
-//	class Iterator : public std::iterator<std::input_iterator_tag, T>
-//	{
-//	public:
-//		Iterator(
-//		    std::set<std::string>::iterator it,
-//		    std::set<std::string>::iterator end)
-//		    : m_it(it), m_end(end), m_obj(nullptr)
-//		{
-//			create_and_forward_if_necessary();
-//		}
-//		Iterator(const Iterator& other)
-//		    : m_it(other.m_it), m_end(other.m_end), m_obj(other.m_obj)
-//		{
-//			create_and_forward_if_necessary();
-//		}
-//		Iterator(Iterator&& other)
-//		    : m_it(other.m_it), m_end(other.m_end), m_obj(other.m_obj)
-//		{
-//			create_and_forward_if_necessary();
-//		}
-//		~Iterator()
-//		{
-//			SG_UNREF(m_obj);
-//		}
-//
-//		Iterator& operator=(const Iterator&) = delete;
-//		Iterator& operator++()
-//		{
-//			SG_UNREF(m_obj);
-//			do
-//			{
-//				++m_it;
-//
-//				if (m_it == m_end)
-//					return *this;
-//
-//				m_obj = create_obj();
-//			} while (!m_obj);
-//
-//			return *this;
-//		}
-//		CSGObject* operator*()
-//		{
-//			return m_obj;
-//		}
-//		bool operator!=(const Iterator& other) const
-//		{
-//			return this->m_it != other.m_it;
-//		}
-//
-//	private:
-//		CSGObject* create_obj()
-//		{
-//			auto obj = create((*m_it).c_str(), sg_primitive_type<T>());
-//			SG_REF(obj);
-//			return obj;
-//		}
-//
-//		void create_and_forward_if_necessary()
-//		{
-//			if (m_it == m_end)
-//				return;
-//
-//			m_obj = create_obj();
-//			while (!m_obj && m_it != m_end)
-//			{
-//				this->operator++();
-//			}
-//		}
-//
-//		std::set<std::string>::iterator m_it;
-//		std::set<std::string>::iterator m_end;
-//		CSGObject* m_obj;
-//	};
-//
-//	Iterator begin() const
-//	{
-//		return Iterator(m_class_names.begin(), m_class_names.end());
-//	}
-//	Iterator end() const
-//	{
-//		return Iterator(m_class_names.end(), m_class_names.end());
-//	}
-//
-//	std::set<std::string> m_class_names;
-//};
-//
-//TYPED_TEST(SGObjectAll, sg_object_iterator)
-//{
-//	for (auto obj : sg_object_iterator<TypeParam>())
-//	{
-//		ASSERT_NE(obj, nullptr);
-//		SCOPED_TRACE(obj->get_name());
-//		ASSERT_EQ(obj->ref_count(), 1);
-//	}
-//}
-//
-//std::set<std::string> sg_object_all_ignores = {"Set", "ParseBuffer",
-//                                               "TreeMachine"};
-//
-//TYPED_TEST(SGObjectAll, clone_basic)
-//{
-//	for (auto obj : sg_object_iterator<TypeParam>(sg_object_all_ignores))
-//	{
-//		SCOPED_TRACE(obj->get_name());
-//		CSGObject* clone = nullptr;
-//		try
-//		{
-//			clone = obj->clone();
-//		}
-//		catch (...)
-//		{
-//		}
-//
-//		ASSERT_NE(clone, nullptr);
-//		EXPECT_NE(clone, obj);
-//		EXPECT_EQ(clone->ref_count(), 1);
-//		EXPECT_EQ(std::string(clone->get_name()), std::string(obj->get_name()));
-//
-//		SG_UNREF(clone);
-//	}
-//}
-//
-//TYPED_TEST(SGObjectAll, clone_equals_empty)
-//{
-//	for (auto obj : sg_object_iterator<TypeParam>(sg_object_all_ignores))
-//	{
-//		SCOPED_TRACE(obj->get_name());
-//
-//		CSGObject* clone = obj->clone();
-//		EXPECT_TRUE(clone->equals(obj));
-//
-//		SG_UNREF(clone);
-//	}
-//}
-//
-//TYPED_TEST(SGObjectAll, serialization_empty_ascii)
-//{
-//	for (auto obj : sg_object_iterator<TypeParam>(sg_object_all_ignores))
-//	{
-//		SCOPED_TRACE(obj->get_name());
-//
-//		std::string filename = "/tmp/shogun-unittest-serialization-ascii-" +
-//		                       std::string(obj->get_name()) + "_" +
-//		                       sg_primitive_type_string<TypeParam>() +
-//		                       ".XXXXXX";
-//
-//		generate_temp_filename(const_cast<char*>(filename.c_str()));
-//
-//		auto file = some<CSerializableAsciiFile>(filename.c_str(), 'w');
-//		ASSERT_TRUE(obj->save_serializable(file));
-//		file->close();
-//
-//		CSGObject* loaded = create(obj->get_name(), obj->get_generic());
-//		ASSERT_NE(loaded, nullptr);
-//		file = some<CSerializableAsciiFile>(filename.c_str(), 'r');
-//		ASSERT_TRUE(loaded->load_serializable(file));
-//		file->close();
-//
-//		// set accuracy to tolerate lossy formats
-//		set_global_fequals_epsilon(1e-14);
-//		ASSERT_TRUE(obj->equals(loaded));
-//		set_global_fequals_epsilon(0);
-//
-//		SG_UNREF(loaded);
-//
-//		ASSERT_EQ(unlink(filename.c_str()), 0);
-//	}
-//}
