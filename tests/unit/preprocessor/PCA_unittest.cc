a87600050457c6dcc1eb552cc47128490c41ea1a
Heiko Strathmann <heiko.strathmann@gmail.com>
2018-02-03 11:26:47 +0000

    move linalg-dependent function into PCA unittest

--- tests/unit/preprocessor/PCA_unittest.cc
+++ tests/unit/preprocessor/PCA_unittest.cc
@@ -1,32 +1,8 @@
 /*
-* Copyright (c) The Shogun Machine Learning Toolbox
-* Written (w) 2014 Parijat Mazumdar
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice, this
-* list of conditions and the following disclaimer.
-* 2. Redistributions in binary form must reproduce the above copyright notice,
-* this list of conditions and the following disclaimer in the documentation
-* and/or other materials provided with the distribution.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* The views and conclusions contained in the software and documentation are those
-* of the authors and should not be interpreted as representing official policies,
-* either expressed or implied, of the Shogun Development Team.
-*/
+ * This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Parijat Mazumdar, Heiko Strathmann
+ */
 
 #include <gtest/gtest.h>
 #include <shogun/mathematics/Math.h>
@@ -37,9 +13,23 @@
 
 #include <shogun/preprocessor/PCA.h>
 
-#include "utils/Utils.h"
-
 using namespace shogun;
+
+/** Check eigenvector equality
+ * This expects that the input vectors are normalised
+ *
+ * @param a first vector
+ * @param b second vector
+ * @return 1.0 if the eigen vectors are pointing to the same director, -1.0
+ * if pointing to the opposite direction.
+ */
+template<class T>
+inline T check_eigenvector_eq(const SGVector<T>& a, const SGVector<T>& b, float64_t epsilon = 10E-8)
+{
+	T sign = linalg::dot(a, b);
+	EXPECT_NEAR(1.0, CMath::abs(sign), epsilon);
+	return (sign < 0.0) ? -1.0 : 1.0;
+}
 
 TEST(PCA, PCA_N_greater_D_EVD)
 {
