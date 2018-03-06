ba8a32d9da6e0f644de96b3b63860e750426bca2
Viktor Gal <viktor.gal@maeth.com>
2018-03-06 09:32:11 +0100

    upgrade bundled eigen to 3.3.4

--- cmake/external/Eigen3.cmake
+++ cmake/external/Eigen3.cmake
@@ -1,17 +1,14 @@
 GetCompilers()
 
 set(EIGEN_INCLUDE_DIR ${THIRD_PARTY_INCLUDE_DIR}/eigen)
-set(EIGEN_VERSION 3.2.8)
+set(EIGEN_VERSION 3.3.4)
 include(ExternalProject)
 ExternalProject_Add(
 	Eigen3
 	PREFIX ${CMAKE_BINARY_DIR}/Eigen3
 	DOWNLOAD_DIR ${THIRD_PARTY_DIR}/Eigen3
 	URL https://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.tar.bz2
-	URL_MD5 9e3bfaaab3db18253cfd87ea697b3ab1
-#	FIXME: http://eigen.tuxfamily.org/bz/show_bug.cgi?id=1445
-#	URL https://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2
-#	URL_MD5 a7aab9f758249b86c93221ad417fbe18
+	URL_MD5 a7aab9f758249b86c93221ad417fbe18
 	CMAKE_ARGS -DEIGEN_INCLUDE_INSTALL_DIR:PATH=${EIGEN_INCLUDE_DIR}
 		-DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}${CMAKE_DEFINITIONS}
 		-DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}${CMAKE_DEFINITIONS}
