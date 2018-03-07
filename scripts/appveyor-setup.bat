d789b17cf889245fbe293c82e81f978134653288
Viktor Gal <viktor.gal@maeth.com>
2018-03-07 14:44:29 +0100

    fix MSVC env vars

--- scripts/appveyor-setup.bat
+++ scripts/appveyor-setup.bat
@@ -13,6 +13,18 @@ conda config --add channels https://repo.continuum.io/pkgs/free
 conda config --add channels conda-forge
 conda info -a
 
+if "%GENERATOR%"=="NMake Makefiles" set need_vcvarsall=1
+if "%GENERATOR%"=="Ninja" set need_vcvarsall=1
+
+if defined need_vcvarsall (
+    @rem Select desired compiler version
+    if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+    ) else (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+    )
+)
+
 @rem Use clcache for faster builds
 pip install -q git+https://github.com/frerich/clcache.git
 clcache -s
