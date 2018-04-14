1ff35858756a05d45e0775eec278c9e6b3739c45
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2018-04-14 12:29:42 +0200

    Draft of IO streams and (De)serializers

---
+++ src/shogun/io/stream/InputStream.h
@@ -0,0 +1,22 @@
+/** This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Sergey Lisitsyn
+ */
+#ifndef __INPUT_STREAM_H__
+#define __INPUT_STREAM_H__
+
+#include <shogun/base/SGObject.h>
+
+namespace shogun
+{
+	class CInputStream : public CSGObject
+	{
+	public:
+		CInputStream();
+		virtual ~CInputStream();
+
+		virtual void read(void* buffer, size_t size) = 0;
+	};
+}
+
+#endif
