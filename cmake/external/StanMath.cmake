b6362302ebfc258376a1c572f02cdd5e081744ab
root <af.y.f@hotmail.com>
2018-04-16 07:31:15 +0000

    Added installation requirements

--- cmake/external/StanMath.cmake
+++ cmake/external/StanMath.cmake
@@ -19,3 +19,8 @@ add_dependencies(libshogun StanMath)
 set(STAN_INCLUDE_DIR_STAN_MATH ${CMAKE_BINARY_DIR}/StanMath/src/StanMath)
 set(STAN_INCLUDE_DIR_BOOST ${CMAKE_BINARY_DIR}/StanMath/src/StanMath/lib/boost_1.64.0)
 set(STAN_INCLUDE_DIR_CVODES ${CMAKE_BINARY_DIR}/StanMath/src/StanMath/lib/cvodes_2.9.0/include)
+
+
+INSTALL( DIRECTORY ${STAN_INCLUDE_DIR_STAN_MATH} DESTINATION include/shogun/lib/external/Stan )
+INSTALL( DIRECTORY ${STAN_INCLUDE_DIR_BOOST} DESTINATION include/shogun/lib/external/Stan_Boost )
+INSTALL( DIRECTORY ${STAN_INCLUDE_DIR_CVODES} DESTINATION include/shogun/lib/external/Stan_Cvodes )
