c4299e5e253695bc9f4c7c455741f245de82a129
Wuwei Lin <vincentl13x@gmail.com>
2018-05-14 19:28:46 +0800

    Add transformer base class

---
+++ src/shogun/transformer/Transformer.h
@@ -0,0 +1,47 @@
+/*
+ * This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Wuwei Lin
+ */
+
+#ifndef TRANSFORMER_H_
+#define TRANSFORMER_H_
+
+#include <shogun/lib/config.h>
+
+#include <shogun/base/SGObject.h>
+#include <shogun/features/Features.h>
+#include <shogun/lib/common.h>
+
+namespace shogun
+{
+
+	/** @brief class Transformer used to transform data
+	 *
+	 */
+	class CTransformer : public CSGObject
+	{
+	public:
+		/** constructor */
+		CTransformer() : CSGObject()
+		{
+		}
+
+		/** destructor */
+		virtual ~CTransformer()
+		{
+		}
+
+		/** get name */
+		virtual const char* get_name() const
+		{
+			return "Transformer";
+		}
+
+		/** Fit transformer to features */
+		virtual void fit(CFeatures* features)
+		{
+		}
+	};
+}
+#endif /* TRANSFORMER_H_ */
