eca71c628e197da7308087fd2d270c023527afeb
Viktor Gal <viktor.gal@maeth.com>
2018-05-18 15:26:17 +0200

    Always use Stan's eigen fix #4288 remove optional system dependency to eigen as we anyways have eigen internally available due to Stan dependency

--- cmake/external/StanMath.cmake
+++ cmake/external/StanMath.cmake
@@ -3,6 +3,7 @@ set(STAN_STABLE_RELEASE 4b1a10bc877d941bbe0a12c198526807be27167a)
 set(STAN_INCLUDE_DIR_STAN_MATH ${CMAKE_BINARY_DIR}/StanMath/src/StanMath)
 set(STAN_INCLUDE_DIR_BOOST ${CMAKE_BINARY_DIR}/StanMath/src/StanMath/lib/boost_1.64.0)
 set(STAN_INCLUDE_DIR_CVODES ${CMAKE_BINARY_DIR}/StanMath/src/StanMath/lib/cvodes_2.9.0/include)
+set(STAN_INCLUDE_DIR_EIGEN ${CMAKE_BINARY_DIR}/StanMath/src/StanMath/lib/eigen_3.3.3)
 
 include(ExternalProject)
 ExternalProject_Add(
@@ -13,10 +14,7 @@ ExternalProject_Add(
 	GIT_TAG ${STAN_STABLE_RELEASE}
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND ""
-	INSTALL_COMMAND
-		${CMAKE_COMMAND} -E copy_if_different ${STAN_INCLUDE_DIR_STAN_MATH} include/shogun/third_party/Stan
-		&& ${CMAKE_COMMAND} -E copy_if_different ${STAN_INCLUDE_DIR_BOOST} include/shogun/third_party/Stan_Boost
-		&& ${CMAKE_COMMAND} -E copy_if_different ${STAN_INCLUDE_DIR_CVODES} include/shogun/third_party/Stan_Cvodes
+	INSTALL_COMMAND ""
 	LOG_DOWNLOAD ON
 	)
 add_dependencies(libshogun StanMath)
