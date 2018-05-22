2f211c9a9a27d3b2b70b4ae2d0dc999478eea3b9
Wuwei Lin <vincentl13x@gmail.com>
2018-05-22 18:34:03 +0800

    Fix variant download and macro guard

--- cmake/external/variant.cmake
+++ cmake/external/variant.cmake
@@ -7,8 +7,7 @@ ExternalProject_Add(
 		PREFIX ${CMAKE_BINARY_DIR}/variant
 		SOURCE_DIR ${VARIANT_SOURCE_DIR}
 		GIT_REPOSITORY https://github.com/mpark/variant.git
-		GIT_SHALLOW 1
-		GIT_TAG single-header
+		GIT_TAG 37c7b8c163c482358afa7daf4c6ee74fe55a6d91
 		CONFIGURE_COMMAND ""
 		BUILD_COMMAND ""
 		INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${VARIANT_SOURCE_DIR}/${VARIANT_RELEASE_VERSION}/variant.hpp ${VARIANT_INCLUDE_DIR}/variant.hpp
