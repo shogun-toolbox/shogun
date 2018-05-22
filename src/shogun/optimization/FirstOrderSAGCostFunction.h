220b11d8f0acf69ae26efc7cdf2d31612053ae73
root <af.y.f@hotmail.com>
2018-05-22 06:02:20 +0000

    Changed ordering of includes to avoid boost issues

--- src/shogun/optimization/FirstOrderSAGCostFunction.h
+++ src/shogun/optimization/FirstOrderSAGCostFunction.h
@@ -31,20 +31,20 @@
 
 #ifndef FIRSTORDERSAGCOSTFUNCTION_H
 #define FIRSTORDERSAGCOSTFUNCTION_H
-#include <shogun/lib/config.h>
 #include <shogun/optimization/FirstOrderStochasticCostFunction.h>
+#include <shogun/lib/config.h>
 namespace shogun
 {
 /** @brief The class is about a stochastic cost function for stochastic average minimizers.
  *
- * The cost function must be written as a finite sample-specific sum of cost.  
+ * The cost function must be written as a finite sample-specific sum of cost.
  * For example, least squares cost function,
  * \f[
  * f(w)=\frac{ \sum_i^n{ (y_i-w^T x_i)^2 } }{2}
  * \f]
  * where \f$n\f$ is the sample size,
  * \f$(y_i,x_i)\f$ is the i-th sample,
- * \f$y_i\f$ is the label and \f$x_i\f$ is the features 
+ * \f$y_i\f$ is the label and \f$x_i\f$ is the features
  *
  * A stochastic average minimizer uses average sample gradients ( get_average_gradient() )
  * to reduce variance related to stochastic gradients.
@@ -73,13 +73,13 @@ class FirstOrderSAGCostFunction
 public:
 	virtual ~FirstOrderSAGCostFunction() {};
 
-	/** Get the sample size 
+	/** Get the sample size
 	 *
 	 * @return the sample size
 	 */
 	virtual int32_t get_sample_size()=0;
 
-	/** Get the AVERAGE gradient value wrt target variables 
+	/** Get the AVERAGE gradient value wrt target variables
 	 *
 	 * Note that the average gradient is the mean of sample gradient from get_gradient()
 	 * if samples are generated (uniformly) at random.
@@ -96,23 +96,23 @@ public:
 	 */
 	virtual SGVector<float64_t> get_average_gradient()=0;
 
-	/** Get the SAMPLE gradient value wrt target variables 
+	/** Get the SAMPLE gradient value wrt target variables
 	 *
 	 * WARNING
-	 * This method does return 
+	 * This method does return
 	 * \f$ \frac{\partial f_i(w) }{\partial w} \f$
 	 * instead of
 	 * \f$\sum_i^n{ \frac{\partial f_i(w) }{\partial w} }\f$
 	 *
 	 * For least squares cost function, that is the value of
 	 * \f$\frac{\partial f_i(w) }{\partial w}\f$ given \f$w\f$ is known
-	 * where the index \f$i\f$ is obtained by next_sample() 
+	 * where the index \f$i\f$ is obtained by next_sample()
 	 *
 	 * @return sample gradient of target variables
 	 */
 	virtual SGVector<float64_t> get_gradient()=0;
 
-	/** Get the cost given current target variables 
+	/** Get the cost given current target variables
 	 *
 	 * For least squares cost function, that is the value of \f$f(w)\f$.
 	 *
