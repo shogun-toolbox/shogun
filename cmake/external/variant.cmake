80c4e738eb009fd6e413de90b03a087686c0cd49
Viktor Gal <viktor.gal@maeth.com>
2018-05-23 10:08:29 +0200

    fix installation of variant replacement

--- cmake/external/variant.cmake
+++ cmake/external/variant.cmake
@@ -13,4 +13,4 @@ ExternalProject_Add(
 		INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${VARIANT_SOURCE_DIR}/${VARIANT_RELEASE_VERSION}/variant.hpp ${VARIANT_INCLUDE_DIR}/variant.hpp
 )
 
-LIST(APPEND SHOGUN_DEPENDS variant)
+add_dependencies(libshogun variant)
