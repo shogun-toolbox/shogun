2a9f348c9b84ddebc04e65ad2e8840747a6ee09d
Viktor Gal <viktor.gal@maeth.com>
2018-05-25 19:26:19 +0200

    linalg::zero benchmark has been moved to Misc_benchmark

--- src/shogun/mathematics/linalg/backend/eigen/BasicOps_benchmark.cc
+++ src/shogun/mathematics/linalg/backend/eigen/BasicOps_benchmark.cc
@@ -22,32 +22,12 @@ void BM_LinAlg_SGVector_scale(benchmark::State& state)
 }
 
 template<typename T>
-void BM_LinAlg_SGVector_zero(benchmark::State& state)
-{
-	for (auto _ : state)
-	{
-		SGVector<T> v(state.range(0));
-		linalg::zero(v);
-	}
-}
-
-template<typename T>
 void BM_SGVector_scale(benchmark::State& state)
 {
 	for (auto _ : state)
 	{
 		SGVector<T> v(state.range(0));
 		v.scale(T(12.3));
-	}
-}
-
-template<typename T>
-void BM_SGVector_zero(benchmark::State& state)
-{
-	for (auto _ : state)
-	{
-		SGVector<T> v(state.range(0));
-		v.zero();
 	}
 }
 
@@ -60,16 +40,5 @@ BENCHMARK_TEMPLATE(BM_SGVector_scale, int32_t)->Range(8, 8<<10);
 BENCHMARK_TEMPLATE(BM_SGVector_scale, int64_t)->Range(8, 8<<10);
 BENCHMARK_TEMPLATE(BM_SGVector_scale, float32_t)->Range(8, 8<<10);
 BENCHMARK_TEMPLATE(BM_SGVector_scale, float64_t)->Range(8, 8<<10);
-
-BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_zero, int32_t)->Range(8, 8<<10);
-BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_zero, int64_t)->Range(8, 8<<10);
-BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_zero, float32_t)->Range(8, 8<<10);
-BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_zero, float64_t)->Range(8, 8<<10);
-
-BENCHMARK_TEMPLATE(BM_SGVector_zero, int32_t)->Range(8, 8<<10);
-BENCHMARK_TEMPLATE(BM_SGVector_zero, int64_t)->Range(8, 8<<10);
-BENCHMARK_TEMPLATE(BM_SGVector_zero, float32_t)->Range(8, 8<<10);
-BENCHMARK_TEMPLATE(BM_SGVector_zero, float64_t)->Range(8, 8<<10);
-
 
 }
