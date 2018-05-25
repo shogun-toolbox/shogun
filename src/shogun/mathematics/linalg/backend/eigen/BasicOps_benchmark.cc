af2ea4c54309abbde8b85cdd563e864a5c33ec2b
Viktor Gal <viktor.gal@maeth.com>
2018-05-25 19:14:20 +0200

    add some basic linalg benchmarks fix stan installation as a third party lib

---
+++ src/shogun/mathematics/linalg/backend/eigen/BasicOps_benchmark.cc
@@ -0,0 +1,75 @@
+/*
+ * This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Viktor Gal
+ */
+
+#include <benchmark/benchmark.h>
+
+#include "shogun/mathematics/linalg/LinalgNamespace.h"
+
+namespace shogun
+{
+
+template<typename T>
+void BM_LinAlg_SGVector_scale(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SGVector<T> v(state.range(0));
+		linalg::scale(v, T(12.3));
+	}
+}
+
+template<typename T>
+void BM_LinAlg_SGVector_zero(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SGVector<T> v(state.range(0));
+		linalg::zero(v);
+	}
+}
+
+template<typename T>
+void BM_SGVector_scale(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SGVector<T> v(state.range(0));
+		v.scale(T(12.3));
+	}
+}
+
+template<typename T>
+void BM_SGVector_zero(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SGVector<T> v(state.range(0));
+		v.zero();
+	}
+}
+
+BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_scale, int32_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_scale, int64_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_scale, float32_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_scale, float64_t)->Range(8, 8<<10);
+
+BENCHMARK_TEMPLATE(BM_SGVector_scale, int32_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_SGVector_scale, int64_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_SGVector_scale, float32_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_SGVector_scale, float64_t)->Range(8, 8<<10);
+
+BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_zero, int32_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_zero, int64_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_zero, float32_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_LinAlg_SGVector_zero, float64_t)->Range(8, 8<<10);
+
+BENCHMARK_TEMPLATE(BM_SGVector_zero, int32_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_SGVector_zero, int64_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_SGVector_zero, float32_t)->Range(8, 8<<10);
+BENCHMARK_TEMPLATE(BM_SGVector_zero, float64_t)->Range(8, 8<<10);
+
+
+}
