20a188ed27349af8ee865d12fb3d38a8f9846b3f
Viktor Gal <viktor.gal@maeth.com>
2018-05-25 19:14:20 +0200

    add some basic linalg benchmarks fix stan installation as a third party lib

--- cmake/external/StanMath.cmake
+++ cmake/external/StanMath.cmake
@@ -1,9 +1,9 @@
 set(STAN_STABLE_RELEASE 4b1a10bc877d941bbe0a12c198526807be27167a)
 
 set(STAN_INCLUDE_DIR_STAN_MATH ${CMAKE_BINARY_DIR}/StanMath/src/StanMath)
-set(STAN_INCLUDE_DIR_BOOST ${CMAKE_BINARY_DIR}/StanMath/src/StanMath/lib/boost_1.64.0)
-set(STAN_INCLUDE_DIR_CVODES ${CMAKE_BINARY_DIR}/StanMath/src/StanMath/lib/cvodes_2.9.0/include)
-set(STAN_INCLUDE_DIR_EIGEN ${CMAKE_BINARY_DIR}/StanMath/src/StanMath/lib/eigen_3.3.3)
+set(STAN_INCLUDE_DIR_BOOST ${STAN_INCLUDE_DIR_STAN_MATH}/lib/boost_1.64.0)
+set(STAN_INCLUDE_DIR_CVODES ${STAN_INCLUDE_DIR_STAN_MATH}/lib/cvodes_2.9.0/include)
+set(STAN_INCLUDE_DIR_EIGEN ${STAN_INCLUDE_DIR_STAN_MATH}/lib/eigen_3.3.3)
 
 include(ExternalProject)
 ExternalProject_Add(
@@ -14,7 +14,10 @@ ExternalProject_Add(
 	GIT_TAG ${STAN_STABLE_RELEASE}
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND ""
-	INSTALL_COMMAND ""
+	INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory ${STAN_INCLUDE_DIR_STAN_MATH}/stan ${THIRD_PARTY_INCLUDE_DIR}/stan
+		&& ${CMAKE_COMMAND} -E copy_directory ${STAN_INCLUDE_DIR_BOOST} ${THIRD_PARTY_INCLUDE_DIR}/stan_boost
+		&& ${CMAKE_COMMAND} -E copy_directory ${STAN_INCLUDE_DIR_CVODES} ${THIRD_PARTY_INCLUDE_DIR}/stan_cvodes
+		&& ${CMAKE_COMMAND} -E copy_directory ${STAN_INCLUDE_DIR_EIGEN} ${THIRD_PARTY_INCLUDE_DIR}/eigen
 	LOG_DOWNLOAD ON
 	)
 add_dependencies(libshogun StanMath)
