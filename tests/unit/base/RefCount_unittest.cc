cbaa656def33c048f633b37ec9433d0c07e824d3
Viktor Gal <viktor.gal@maeth.com>
2018-07-20 19:40:20 +0200

    remove none c++11 code from RefCount unit test

--- tests/unit/base/RefCount_unittest.cc
+++ tests/unit/base/RefCount_unittest.cc
@@ -3,23 +3,15 @@
  *
  * Authors: Thoralf Klein, Sanuj Sharma, Viktor Gal
  */
-
-#include <shogun/lib/RefCount.h>
-#if defined(HAVE_PTHREAD) || defined(HAVE_CXX11)
-
-#ifdef HAVE_CXX11
-#include <thread>
-#elif HAVE_PTHREAD
-#include <pthread.h>
-#endif
 #include <gtest/gtest.h>
+
+#include <thread>
+#include <shogun/lib/RefCount.h>
 
 using namespace shogun;
 
-void * stress_test_helper(void * args)
+void stress_test_helper(RefCount* rc)
 {
-	RefCount * rc = (RefCount *) args;
-
 	for (index_t i = 0; i < 10; i++)
 	{
 		rc->ref();
@@ -27,50 +19,24 @@ void * stress_test_helper(void * args)
 		rc->unref();
 		rc->unref();
 	}
-
-#if !defined(HAVE_CXX11) && defined(HAVE_PTHREAD)
-	pthread_exit(0);
-#endif
-	return NULL;
 }
 
 TEST(RefCount, stress_test)
 {
-	RefCount * rc = new RefCount(0);
+	auto rc = new RefCount(0);
 	EXPECT_EQ(rc->ref_count(), 0);
 	rc->ref();
 	EXPECT_EQ(rc->ref_count(), 1);
 
-#ifdef HAVE_CXX11
 	std::thread threads[5];
-#elif HAVE_PTHREAD
-	pthread_t * threads = new pthread_t[5];
-#endif
 	for (index_t i = 0; i < 5; i++)
-	{
-#ifdef HAVE_CXX11
-		threads[i] = std::thread(&stress_test_helper, static_cast<void *>(rc));
-#elif HAVE_PTHREAD
-		pthread_create(&threads[i], NULL, stress_test_helper, static_cast<void *>(rc));
-#endif
-	}
+		threads[i] = std::thread(stress_test_helper, rc);
 
 	for (index_t i = 0; i < 5; i++)
-	{
-#ifdef HAVE_CXX11
 		threads[i].join();
-#elif HAVE_PTHREAD
-		pthread_join(threads[i], NULL);
-#endif
-	}
 
 	EXPECT_EQ(rc->ref_count(), 1);
 	rc->unref();
 	EXPECT_EQ(rc->ref_count(), 0);
-#if !defined(HAVE_CXX11) && defined(HAVE_PTHREAD)
-	delete [] threads;
-#endif
 	delete rc;
 }
-
-#endif //  defined(HAVE_PTHREAD) || defined(HAVE_CXX11)
