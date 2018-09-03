7c7693f81974191b377245ea4f5ba94b1dadd095
Viktor Gal <viktor.gal@maeth.com>
2018-09-03 16:40:26 +0200

    upgrade rxcpp to 4.1.0

--- cmake/external/rxcpp.cmake
+++ cmake/external/rxcpp.cmake
@@ -3,8 +3,8 @@ ExternalProject_Add(
         rxcpp
         PREFIX ${CMAKE_BINARY_DIR}/rxcpp
         DOWNLOAD_DIR ${THIRD_PARTY_DIR}/rxcpp
-        URL https://github.com/Reactive-Extensions/RxCpp/archive/v4.0.0.tar.gz
-        URL_MD5 feb89934f465bb5ac513c9adce8d3b1b
+        URL https://github.com/ReactiveX/RxCpp/archive/4.1.0.tar.gz
+        URL_MD5 6c283f36ce251f45146f7099aa9ef19a
         CMAKE_ARGS
         -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo
         -DCMAKE_INSTALL_PREFIX:STRING=${THIRD_PARTY_INCLUDE_DIR}
@@ -15,7 +15,7 @@ ExternalProject_Add(
 
 add_dependencies(libshogun rxcpp)
 
-set(rxcpp_INCLUDE_DIR ${THIRD_PARTY_INCLUDE_DIR})
+set(rxcpp_INCLUDE_DIR ${THIRD_PARTY_INCLUDE_DIR}/include)
 
 UNSET(C_COMPILER)
 UNSET(CXX_COMPILER)
