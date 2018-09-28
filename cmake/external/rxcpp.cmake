02e7de5fdef6d193130f93ce62a04e10c20437b4
Viktor Gal <viktor.gal@maeth.com>
2018-09-28 15:12:35 +0200

    fix rxcpp bundling

--- cmake/external/rxcpp.cmake
+++ cmake/external/rxcpp.cmake
@@ -1,21 +1,16 @@
 include(ExternalProject)
+SET(RXCPP_SOURCE_DIR ${THIRD_PARTY_DIR}/rxcpp)
 ExternalProject_Add(
         rxcpp
         PREFIX ${CMAKE_BINARY_DIR}/rxcpp
-        DOWNLOAD_DIR ${THIRD_PARTY_DIR}/rxcpp
+        DOWNLOAD_DIR ${RXCPP_SOURCE_DIR}
+        SOURCE_DIR ${RXCPP_SOURCE_DIR}/src
         URL https://github.com/ReactiveX/RxCpp/archive/4.1.0.tar.gz
         URL_MD5 6c283f36ce251f45146f7099aa9ef19a
-        CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo
-        -DCMAKE_INSTALL_PREFIX:STRING=${THIRD_PARTY_INCLUDE_DIR}
-        -DCMAKE_C_COMPILER:STRING=${CMAKE_C_COMPILER}
-        -DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}
         BUILD_COMMAND ""
+        INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory ${RXCPP_SOURCE_DIR}/src/Rx/v2/src/ ${THIRD_PARTY_INCLUDE_DIR}/
         )
 
 add_dependencies(libshogun rxcpp)
 
-set(rxcpp_INCLUDE_DIR ${THIRD_PARTY_INCLUDE_DIR}/include)
-
-UNSET(C_COMPILER)
-UNSET(CXX_COMPILER)
+set(rxcpp_INCLUDE_DIR ${THIRD_PARTY_INCLUDE_DIR})
