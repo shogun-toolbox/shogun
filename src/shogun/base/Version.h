46f7838f3c0815e821a3bd0b8805e152e26e9da7
Gil <g.f.hoben@gmail.com>
2019-01-24 19:04:08 +0000

    constexpr version

--- src/shogun/base/Version.h
+++ src/shogun/base/Version.h
@@ -12,6 +12,7 @@
 #define VERSION_H__
 
 #include <shogun/lib/config.h>
+#include <shogun/lib/versionstring.h>
 
 namespace shogun
 {
@@ -34,35 +35,60 @@ public:
 	/** print version */
 	static void print_version();
 
+	/** get main version */
+    static constexpr const char* get_version_main() {
+        return version_main;
+    }
+
 	/** get version extra */
-	static const char* get_version_extra();
+	static constexpr const char* get_version_extra() {
+	    return version_extra;
+	}
 
 	/** get version release */
-	static const char* get_version_release();
+	static constexpr const char* get_version_release() {
+	    return version_release;
+	}
 
 	/** get version revision */
-	static int64_t get_version_revision();
+    static constexpr int64_t get_version_revision() {
+        return version_revision;
+    }
 
 	/** get version year */
-	static int32_t get_version_year();
+    static constexpr int32_t get_version_year() {
+        return version_year;
+    }
 
 	/** get version month */
-	static int32_t get_version_month();
+    static constexpr int32_t get_version_month() {
+        return version_month;
+    }
 
 	/** get version day */
-	static int32_t get_version_day();
+    static constexpr int32_t get_version_day() {
+        return version_day;
+    }
 
 	/** get version hour */
-	static int32_t get_version_hour();
+    static constexpr int32_t get_version_hour() {
+        return version_hour;
+    }
 
 	/** get version minute */
-	static int32_t get_version_minute();
+    static constexpr int32_t get_version_minute() {
+        return version_minute;
+    }
 
 	/** get parameter serialization version */
-	static int32_t get_version_parameter();
+    static constexpr int32_t get_version_parameter() {
+        return version_parameter;
+    }
 
 	/** get version in minutes */
-	static int64_t get_version_in_minutes();
+    static constexpr int64_t get_version_in_minutes() {
+        return version_minute;
+    }
 
 	/** ref object
 	 * @return ref count
@@ -81,24 +107,26 @@ public:
 
 protected:
 	/** version release */
-	static const char version_release[128];
+	static constexpr char version_release[128] = VERSION_RELEASE;
 	/** version extra */
-	static const char version_extra[128];
+	static constexpr char version_extra[128] = VERSION_EXTRA;
 
 	/** version revision */
-	static const int64_t version_revision;
+	static constexpr int64_t version_revision = VERSION_REVISION;
 	/** version year */
-	static const int32_t version_year;
+	static constexpr int32_t version_year = VERSION_YEAR;
 	/** version month */
-	static const int32_t version_month;
+	static constexpr int32_t version_month = VERSION_MONTH;
 	/** version day */
-	static const int32_t version_day;
+	static constexpr int32_t version_day = VERSION_DAY;
 	/** version hour */
-	static const int32_t version_hour;
+	static constexpr int32_t version_hour = VERSION_HOUR;
 	/** version minute */
-	static const int32_t version_minute;
+	static constexpr int32_t version_minute = VERSION_MINUTE;
 	/** version parameter */
-	static const int32_t version_parameter;
+	static constexpr int32_t version_parameter = VERSION_PARAMETER;
+    /** version main */
+	static constexpr char version_main[32] = MAINVERSION;
 
 private:
 	RefCount* m_refcount;
