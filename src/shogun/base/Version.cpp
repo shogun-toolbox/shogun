b91af0cee632f66a316e06ad0f6f084c968d2264
Viktor Gal <viktor.gal@maeth.com>
2019-01-30 14:07:23 +0100

    add Version::version_main

--- src/shogun/base/Version.cpp
+++ src/shogun/base/Version.cpp
@@ -1,7 +1,7 @@
 /*
  * This software is distributed under BSD 3-clause license (see LICENSE file).
  *
- * Authors: Soeren Sonnenburg, Viktor Gal, Heiko Strathmann, Thoralf Klein, 
+ * Authors: Soeren Sonnenburg, Viktor Gal, Heiko Strathmann, Thoralf Klein,
  *          Evan Shelhamer, Bjoern Esser, Evangelos Anagnostopoulos
  */
 
@@ -23,6 +23,7 @@ const int32_t Version::version_day = VERSION_DAY;
 const int32_t Version::version_hour = VERSION_HOUR;
 const int32_t Version::version_minute = VERSION_MINUTE;
 const int32_t Version::version_parameter=VERSION_PARAMETER;
+const char Version::version_main[32] = MAINVERSION;
 const char Version::version_extra[128] = VERSION_EXTRA;
 const char Version::version_release[128] = VERSION_RELEASE;
 }
@@ -50,6 +51,11 @@ void Version::print_version()
 	SG_SPRINT("This is free software; see the source for copying conditions.  There is NO\n")
 	SG_SPRINT("warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\n")
 #endif
+}
+
+const char* Version::get_version_main()
+{
+	return version_main;
 }
 
 const char* Version::get_version_extra()
