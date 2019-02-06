shogun-toolbox/shogun

commit 21d55d24675b99b72c1acd88a161ba522e2e2e49

Made LinearKernels clear_normal method implementation corrrrrect

src/shogun/kernel/LinearKernel.h
@@ -100,7 +100,9 @@ class CLinearKernel: public CDotKernel
 
 		virtual void clear_normal()
 		{
-			normal = SGVector<float64_t>();
+			normal = SGVector<float64_t>(((CDotFeatures*)lhs)->get_dim_feature_space());
+			normal.zero();
+			set_is_initialized(true);
 		}
 
 		/** add to normal vector

commit e4099536e11e4531ac9b35ca8c417c49abe62b26

fix bug in LinearKernel introduced in 21d55d2467

src/shogun/kernel/LinearKernel.h
@@ -99,7 +99,7 @@ class CLinearKernel: public CDotKernel
 		{
 			normal = SGVector<float64_t>(((CDotFeatures*)lhs)->get_dim_feature_space());
 			normal.zero();
-			set_is_initialized(true);
+			set_is_initialized(false);
 		}
 
 		/** add to normal vector