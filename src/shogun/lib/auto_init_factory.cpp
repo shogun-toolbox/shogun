561a1de1c0660a76db4f84417e5099a3515e8041
gf712 <gil_f.hoben@hotmail.com>
2019-02-20 13:50:21 +0000

    changed API to use AutoInit class

--- src/shogun/lib/auto_init_factory.cpp
+++ src/shogun/lib/auto_init_factory.cpp
@@ -10,7 +10,12 @@ namespace shogun
 {
 	namespace factory
 	{
-        const char* const GammaFeatureNumberInit::kName = "GammaFeatureNumberInit";
-        const char* const GammaFeatureNumberInit::kDescription = "GammaFeatureNumberInit";
+		const char* const GammaFeatureNumberInit::kName =
+		    "GammaFeatureNumberInit";
+		const char* const GammaFeatureNumberInit::kDescription =
+		    "Automatic initialisation of the gamma dot product scaling "
+		    "parameter. If the standard deviation of the features can be "
+		    "calculated then gamma = 1 / (n_features * std(features)), else "
+		    "gamma = 1 / n_features.";
 	} // namespace factory
 } // namespace shogun
