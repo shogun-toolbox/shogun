b0c6941476c70d1718714b770a187ddd1569bfa5
gf712 <gil_f.hoben@hotmail.com>
2019-02-20 12:26:24 +0000

    AutoInit class for parameter instantiation at runtime

---
+++ src/shogun/lib/abstract_auto_init.h
@@ -0,0 +1,31 @@
+/*
+ * This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Gil Hoben
+ */
+
+#ifndef SHOGUN_ABSTRACT_AUTO_INIT_H
+#define SHOGUN_ABSTRACT_AUTO_INIT_H
+
+namespace shogun
+{
+    class Any;
+
+    namespace factory {
+        class AutoInit {
+        public:
+            AutoInit(const std::string &name, const std::string &description)
+                    : m_name(name), m_description(description) {
+            }
+
+			virtual Any operator()() = 0;
+            virtual ~AutoInit() = default;
+
+        protected:
+            const std::string m_name;
+            const std::string m_description;
+        };
+    }
+}
+
+#endif //SHOGUN_ABSTRACT_AUTO_INIT_H
---
+++ src/shogun/lib/auto_init_factory.cpp
@@ -0,0 +1,16 @@
+/*
+ * This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Gil Hoben
+ */
+
+#include <shogun/lib/auto_init_factory.h>
+
+namespace shogun
+{
+	namespace factory
+	{
+        const char* const GammaFeatureNumberInit::kName = "GammaFeatureNumberInit";
+        const char* const GammaFeatureNumberInit::kDescription = "GammaFeatureNumberInit";
+	} // namespace factory
+} // namespace shogun
