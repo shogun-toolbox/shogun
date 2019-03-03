fa82eb3e18811940e2987323f576f55a9c577bcb
gf712 <gil_f.hoben@hotmail.com>
2019-03-03 11:51:13 +0000

    replaced put with init in kernel
    example implementation
    
    
    renamed const to readonly
    
    
    t
    
    t

--- src/shogun/kernel/Kernel.cpp
+++ src/shogun/kernel/Kernel.cpp
@@ -924,9 +924,10 @@ void CKernel::register_params()
 		&lhs, "lhs", "Feature vectors to occur on left hand side.",
 		ParameterProperties::READONLY);
 	SG_ADD(
-	    &rhs, "rhs", "Feature vectors to occur on right hand side.");
+		&rhs, "rhs", "Feature vectors to occur on right hand side.",
+		ParameterProperties::READONLY);
 	SG_ADD(&lhs_equals_rhs, "lhs_equals_rhs",
-	    "If features on lhs are the same as on rhs.");
+		"If features on lhs are the same as on rhs.");
 	SG_ADD(&num_lhs, "num_lhs", "Number of feature vectors on left hand side.");
 	SG_ADD(
 	    &num_rhs, "num_rhs", "Number of feature vectors on right hand side.");
