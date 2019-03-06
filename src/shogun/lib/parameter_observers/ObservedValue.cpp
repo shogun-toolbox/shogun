d2e86d1bcbece5ccfa79af4d80e919ca6d7b25c9
Giovanni De Toni <giovanni.det@gmail.com>
2019-03-06 11:08:13 +0100

    Fix errors.

--- src/shogun/lib/parameter_observers/ObservedValue.cpp
+++ src/shogun/lib/parameter_observers/ObservedValue.cpp
@@ -2,12 +2,9 @@
 
 using namespace shogun;
 
-ObservedValue::ObservedValue(int64_t step, std::string& name, Any value)
+ObservedValue::ObservedValue(int64_t step, std::string name, Any value)
 		    : m_step(step), m_name(name), m_value(value)
 {
-	SG_ADD(&m_step, "step", "Observation step", ParameterProperties::NONE);
-	SG_ADD(&m_name, "name", "Observed value name", ParameterProperties::NONE);
-	SG_ADD(&m_value, "value", "Observed value", ParameterProperties::NONE);
 }
 
 ObservedValue::~ObservedValue() {}
