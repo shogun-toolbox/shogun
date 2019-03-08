046e9e384b4164b984c912ce9a7bf7882bb61ab7
Ahmed Essam <theartful.ae@gmail.com>
2019-03-08 09:59:33 +0200

    Use iterative machine mixin in AveragedPerceptron

--- src/shogun/classifier/AveragedPerceptron.cpp
+++ src/shogun/classifier/AveragedPerceptron.cpp
@@ -1,31 +1,19 @@
 /*
  * This software is distributed under BSD 3-clause license (see LICENSE file).
  *
- * Authors: Soeren Sonnenburg, Sergey Lisitsyn, Giovanni De Toni, 
+ * Authors: Soeren Sonnenburg, Sergey Lisitsyn, Giovanni De Toni,
  *          Michele Mazzoni
  */
 
-#include <shogun/base/progress.h>
 #include <shogun/classifier/AveragedPerceptron.h>
-#include <shogun/labels/BinaryLabels.h>
+#include <shogun/features/iterators/DotIterator.h>
 #include <shogun/labels/Labels.h>
-#include <shogun/lib/Signal.h>
-#include <shogun/mathematics/Math.h>
+#include <shogun/mathematics/linalg/LinalgNamespace.h>
 
 using namespace shogun;
 
-CAveragedPerceptron::CAveragedPerceptron()
-: CLinearMachine()
+CAveragedPerceptron::CAveragedPerceptron() : CIterativeMachine<CLinearMachine>()
 {
-	init();
-
-}
-
-CAveragedPerceptron::CAveragedPerceptron(CDotFeatures* traindat, CLabels* trainlab)
-: CLinearMachine()
-{
-	set_features(traindat);
-	set_labels(trainlab);
 	init();
 }
 
@@ -35,84 +23,74 @@ CAveragedPerceptron::~CAveragedPerceptron()
 
 void CAveragedPerceptron::init()
 {
-	max_iter = 1000;
-	learn_rate = 0.1;
-	SG_ADD(&max_iter, "max_iter", "Maximum number of iterations.", ParameterProperties::HYPER);
-	SG_ADD(&learn_rate, "learn_rate", "Learning rate.", ParameterProperties::HYPER);
+	set_max_iter(1000);
+	set_learn_rate(0.1);
+
+	SG_ADD(
+	    &learn_rate, "learn_rate", "Learning rate.",
+	    ParameterProperties::HYPER);
+	SG_ADD(
+	    &cached_w, "cached_w",
+	    "Cached weights that contribute to the average.");
+	SG_ADD(
+	    &cached_bias, "cached_bias",
+	    "Cached bias that contribute to the average.");
 }
 
-bool CAveragedPerceptron::train_machine(CFeatures* data)
+void CAveragedPerceptron::init_model(CFeatures* data)
 {
 	ASSERT(m_labels)
-
 	if (data)
 	{
 		if (!data->has_property(FP_DOT))
 			SG_ERROR("Specified features are not of type CDotFeatures\n")
 		set_features((CDotFeatures*) data);
+		set_continue_features(data);
 	}
 	ASSERT(features)
-	bool converged=false;
-	int32_t iter=0;
+
 	SGVector<int32_t> train_labels = binary_labels(m_labels)->get_int_labels();
-	int32_t num_feat=features->get_dim_feature_space();
-	int32_t num_vec=features->get_num_vectors();
+	int32_t num_feat = features->get_dim_feature_space();
+	int32_t num_vec = features->get_num_vectors();
+	ASSERT(num_vec == train_labels.vlen)
 
-	ASSERT(num_vec==train_labels.vlen)
 	SGVector<float64_t> w(num_feat);
-	float64_t* tmp_w=SG_MALLOC(float64_t, num_feat);
-	memset(tmp_w, 0, sizeof(float64_t)*num_feat);
-	float64_t* output=SG_MALLOC(float64_t, num_vec);
-
-	//start with uniform w, bias=0, tmp_bias=0
-	bias=0;
-	float64_t tmp_bias=0;
-	for (int32_t i=0; i<num_feat; i++)
-		w[i]=1.0/num_feat;
-
-	auto pb = SG_PROGRESS(range(max_iter));
-	// loop till we either get everything classified right or reach max_iter
-	while (!converged && iter < max_iter)
-	{
-		COMPUTATION_CONTROLLERS
-		converged=true;
-		SG_INFO("Iteration Number : %d of max %d\n", iter, max_iter);
-
-		for (int32_t i=0; i<num_vec; i++)
-		{
-			output[i] = features->dense_dot(i, w.vector, w.vlen) + bias;
-
-			if (CMath::sign<float64_t>(output[i]) != train_labels.vector[i])
-			{
-				converged=false;
-				bias+=learn_rate*train_labels.vector[i];
-				features->add_to_dense_vec(learn_rate*train_labels.vector[i], i, w.vector, w.vlen);
-			}
-
-			// Add current w to tmp_w, and current bias to tmp_bias
-			// To calculate the sum of each iteration's w, bias
-			for (int32_t j=0; j<num_feat; j++)
-				tmp_w[j]+=w[j];
-			tmp_bias+=bias;
-		}
-		iter++;
-		pb.print_progress();
-	}
-	pb.complete();
-	if (converged)
-		SG_INFO("Averaged Perceptron algorithm converged after %d iterations.\n", iter)
-	else
-		SG_WARNING("Averaged Perceptron algorithm did not converge after %d iterations.\n", max_iter)
-
-	// calculate and set the average paramter of w, bias
-	for (int32_t i=0; i<num_feat; i++)
-		w[i]=tmp_w[i]/(num_vec*iter);
-	bias=tmp_bias/(num_vec*iter);
-
-	SG_FREE(output);
-	SG_FREE(tmp_w);
-
+	cached_w = SGVector<float64_t>(num_feat);
+	// start with uniform w, bias=0, tmp_bias=0
+	bias = 0;
+	cached_bias = 0;
+	cached_w.set_const(1.0 / num_feat);
+	w.set_const(0);
 	set_w(w);
+}
 
-	return converged;
+void CAveragedPerceptron::iteration()
+{
+	bool converged = true;
+	
+	SGVector<float64_t> w = get_w();
+	auto labels = binary_labels(m_labels)->get_int_labels();
+	auto iter_train_labels = labels.begin();
+	
+	int32_t num_vec = features->get_num_vectors();
+	// this assumes that m_current_iteration starts at 0
+	int32_t num_prev_weights = num_vec * m_current_iteration + 1;
+	
+	for (const auto& feature : DotIterator(features))
+	{
+		const auto true_label = *(iter_train_labels++);
+		auto predicted_label = feature.dot(cached_w) + cached_bias;
+
+		if (CMath::sign<float64_t>(predicted_label) != true_label)
+		{
+			converged = false;
+			const auto gradient = learn_rate * true_label;
+			cached_bias += gradient;
+			feature.add(gradient, cached_w);
+		}
+		linalg::update_mean(w, cached_w, num_prev_weights);
+		linalg::update_mean(bias, cached_bias, num_prev_weights);
+		num_prev_weights++;
+	}
+	m_complete = converged;
 }
