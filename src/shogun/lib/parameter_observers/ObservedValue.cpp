68f8c4b139ebbe98ad5b0018929fe128b4e739b9
Giovanni De Toni <giovanni.det@gmail.com>
2019-03-12 17:05:22 +0100

    Add more documentation.

--- src/shogun/lib/parameter_observers/ObservedValue.cpp
+++ src/shogun/lib/parameter_observers/ObservedValue.cpp
@@ -1,6 +1,9 @@
 /*
-* Written (W) 2019 Giovanni De Toni
-*/
+ * This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Giovanni De Toni
+ *
+ */
 
 #include <shogun/base/Parameter.h>
 #include <shogun/lib/parameter_observers/ObservedValue.h>
