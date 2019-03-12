68590be7d353df4c8a3654cb6473e35cd12eb6ed
Giovanni De Toni <giovanni.det@gmail.com>
2019-03-12 13:28:19 +0100

    Add unit test for ObservedValue.

---
+++ tests/unit/lib/parameter_observers/ObservedValue_unittest.cc
@@ -0,0 +1,17 @@
+/*
+* Written (W) 2019 Giovanni De Toni
+*/
+
+#include "sg_gtest_utilities.h"
+#include <shogun/lib/parameter_observers/ObservedValueTemplated.h>
+#include <shogun/lib/config.h>
+
+using namespace shogun;
+
+TEST(ObservedValue, set_correct)
+{
+	auto obs = ObservedValue::make_observation(1, "test", 42);
+	EXPECT_EQ(obs->get<int64_t>("step"), 1)
+	EXPECT_EQ(obs->get<std::string>("name"), "test");
+	EXPECT_EQ(obs->get<int64_t>("value"), 42);
+};
