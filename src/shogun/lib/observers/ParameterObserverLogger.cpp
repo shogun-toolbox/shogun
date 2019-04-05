a79531896e92de3e54b38cd001f248a64d9e7cb8
Giovanni De Toni <giovanni.det@gmail.com>
2019-04-05 15:11:01 +0200

    Fix ParameterObserverLogger when it needs to print the received values.
    * Fix comparison warning;

--- src/shogun/lib/observers/ParameterObserverLogger.cpp
+++ src/shogun/lib/observers/ParameterObserverLogger.cpp
@@ -31,11 +31,22 @@ void ParameterObserverLogger::on_next_impl(const TimedObservedValue &value) {
 
 	auto name = value.first->get<std::string>("name");
 	auto any_val = value.first->get_any();
-	auto print_func = [&](auto v){
+
+	auto pf_n = [&](auto v){
 		SG_PRINT("[%l] Received a value called %s which contains: %s", convert_to_millis(value.second),
-				 name, v);
+				 name, std::to_string(v));
 	};
 
-	sg_any_dispatch(any_val, sg_all_typemap, print_func, print_func, print_func);
+	auto pf_sgvector = [&](auto v){
+		//SG_PRINT("[%l] Received a value called %s which contains: %s", convert_to_millis(value.second),
+		//		 name, v);
+	};
+
+	auto pf_sgmatrix = [&](auto v){
+		//SG_PRINT("[%l] Received a value called %s which contains: %s", convert_to_millis(value.second),
+		//		 name, v);
+	};
+
+	sg_any_dispatch(any_val, sg_all_typemap, pf_n, pf_sgvector, pf_sgmatrix);
 
 }
