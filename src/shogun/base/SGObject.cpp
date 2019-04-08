3e47f2347336e13fbbb3c99f29651ec32053489a
Giovanni De Toni <giovanni.det@gmail.com>
2019-04-08 13:19:45 +0200

    Refactor CrossValidationStorage/FoldStorage such to be gettable.
    * Fix memory error related to crossvalidation's fold results;

--- src/shogun/base/SGObject.cpp
+++ src/shogun/base/SGObject.cpp
@@ -39,6 +39,7 @@
 #include <utility>
 
 #include <shogun/distance/Distance.h>
+#include <shogun/evaluation/EvaluationResult.h>
 #include <shogun/features/DotFeatures.h>
 #include <shogun/features/Features.h>
 #include <shogun/kernel/Kernel.h>
