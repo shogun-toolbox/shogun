bb53aefb749dc41027af5eeb43cb0b7a82c1e229
Viktor Gal <viktor.gal@maeth.com>
2019-04-10 11:11:29 +0200

    port meta integration tester to use new serialization API

--- tests/meta/tester.cpp
+++ tests/meta/tester.cpp
@@ -1,49 +1,49 @@
 #include <shogun/base/init.h>
-#include <shogun/io/SerializableAsciiFile.h>
+#include <shogun/base/some.h>
 #include <shogun/lib/DynamicObjectArray.h>
+#include <shogun/io/fs/FileSystem.h>
+#include <shogun/io/serialization/JsonDeserializer.h>
+#include <shogun/io/stream/FileInputStream.h>
+#include <shogun/io/ShogunErrc.h>
+#include <shogun/io/fs/Path.h>
 
 #include <iostream>
-#include <sstream>
 
 using namespace shogun;
 using namespace std;
 
+static Some<CSGObject> load_object(const string& fname)
+{
+	auto fs = io::FileSystemRegistry::instance();
+	auto ec = fs->file_exists(fname);
+	if (ec)
+		throw io::to_system_error(ec);
+	std::unique_ptr<io::RandomAccessFile> f;
+	ec = fs->new_random_access_file(fname, &f);
+	if (ec)
+		throw io::to_system_error(ec);
+	auto fis = some<io::CFileInputStream>(f.get());
+	auto deserializer = some<io::CJsonDeserializer>();
+	deserializer->attach(fis);
+	return deserializer->read();
+}
+
 int main(int argc, const char *argv[])
 {
-    if (argc != 6)
-        cout << "Usage: tester REL_DIR NAME TARGET GENERATED_RESULTS_DIR REFERENCE_RESULTS_DIR" << endl;
+	if (argc != 6)
+		cout << "Usage: tester REL_DIR NAME TARGET GENERATED_RESULTS_DIR REFERENCE_RESULTS_DIR" << endl;
 
-    string rel_dir = argv[1];
-    string name = argv[2];
-    string target = argv[3];
-    string generated_results_dir = argv[4];
-    string reference_results_dir = argv[5];
+	string rel_dir = argv[1];
+	string name = argv[2];
+	string target = argv[3];
+	string generated_results_dir = argv[4];
+	string reference_results_dir = argv[5];
 
-    stringstream os;
-    os << generated_results_dir << "/" << target << "/" << rel_dir << "/" << name;
-    string fname_full_generated = os.str();
-    os.str("");
-    os << reference_results_dir << "/" << rel_dir << "/" << name;
-    string fname_full_reference = os.str();
-
-    init_shogun_with_defaults();
-
-    CSerializableAsciiFile* f = new CSerializableAsciiFile(fname_full_generated.c_str(), 'r');
-    CSerializableAsciiFile* f_ref = new CSerializableAsciiFile(fname_full_reference.c_str(), 'r');
-    SG_REF(f);
-    SG_REF(f_ref);
-
-    auto a = some<CDynamicObjectArray>();
-    auto a_ref = some<CDynamicObjectArray>();
-
-    if (!a->load_serializable(f))
-		SG_SERROR(
-		    "Error while loading the test input from %s\n",
-		    fname_full_generated.c_str());
-	if (!a_ref->load_serializable(f_ref))
-		SG_SERROR(
-		    "Error while loading the reference input from %s\n",
-		    fname_full_reference.c_str());
+	init_shogun_with_defaults();
+	auto fname_full_generated = io::join_path(generated_results_dir, target, rel_dir, name);
+	auto fname_full_reference = io::join_path(reference_results_dir, rel_dir, name);
+	auto a = load_object(fname_full_generated);
+	auto a_ref = load_object(fname_full_reference);
 
 	// allow for lossy serialization format
 	set_global_fequals_epsilon(1e-6);
@@ -60,24 +60,21 @@ int main(int argc, const char *argv[])
 		if (!loaded_equals_ref)
 		{
 			SG_SDEBUG(
-			    "Test input different from reference input for %s\n",
-			    a->get_name());
+				"Test input different from reference input for %s\n",
+				a->get_name());
 			a->equals(a_ref);
 		}
 		else
 		{
 			SG_SDEBUG(
-			    "Reference input different from test input for %s.\n",
-			    a->get_name());
+				"Reference input different from test input for %s.\n",
+				a->get_name());
 			a_ref->equals(a);
 		}
 		SG_SDEBUG(
-		    "For details, run: diff %s %s\n", fname_full_generated.c_str(),
-		    fname_full_reference.c_str());
+			"For details, run: diff %s %s\n", fname_full_generated.c_str(),
+			fname_full_reference.c_str());
 	}
-
-	SG_UNREF(f);
-	SG_UNREF(f_ref);
 
 	exit_shogun();
 	return !(loaded_equals_ref && ref_equals_loaded);
