e62683249e1dbbb773115bb226f505c74b49d118
Giovanni De Toni <giovanni.det@gmail.com>
2019-04-10 16:32:57 +0200

    Removed obtain_from_generic() from GradientResult and CrossValidationResult.

--- src/shogun/modelselection/RandomSearchModelSelection.cpp
+++ src/shogun/modelselection/RandomSearchModelSelection.cpp
@@ -83,11 +83,7 @@ CParameterCombination* CRandomSearchModelSelection::select_model(bool print_stat
 				machine->m_model_selection_parameters);
 
 		/* note that this may implicitly lock and unlockthe machine */
-		CCrossValidationResult* result =
-				(CCrossValidationResult*)(m_machine_eval->evaluate());
-
-		if (result->get_result_type() != CROSSVALIDATION_RESULT)
-			SG_ERROR("Evaluation result is not of type CCrossValidationResult!")
+		CCrossValidationResult* result = m_machine_eval->evaluate()->as<CCrossValidationResult>();
 
 		if (print_state)
 			result->print_result();
