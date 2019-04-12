c75799ca0f7013a6c60a0f27bccbf11e99de359e
gf712 <gil_f.hoben@hotmail.com>
2019-04-12 13:50:35 +0100

    fix recursive calls

--- examples/meta/generator/translate.py
+++ examples/meta/generator/translate.py
@@ -19,7 +19,7 @@ def find(key, dictionary):
     for k, v in dictionary.items():
         if k == key:
             yield v
-        elif isinstance(v, dict):
+        if isinstance(v, dict):
             for result in find(key, v):
                 yield result
         elif isinstance(v, list):
