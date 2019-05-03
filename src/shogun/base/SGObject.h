19219195abec87c4a44e025761c889bc6613dec4
Giovanni De Toni <giovanni.det@gmail.com>
2019-05-03 11:32:53 +0200

    Add correct type traits for SGVector<T> and SGMatrix<T>.

--- src/shogun/base/SGObject.h
+++ src/shogun/base/SGObject.h
@@ -30,7 +30,6 @@
 #include <unordered_map>
 #include <utility>
 #include <vector>
-#include <shogun/lib/type_case.h>
 
 /** \namespace shogun
  * @brief all of classes and functions are contained in the shogun namespace
@@ -1014,14 +1013,14 @@ protected:
 	 */
 	template<class T,
 			typename std::enable_if_t<
-				!type_internal::is_sg_vector<T>::value &&
-				!type_internal::is_sg_matrix<T>::value>* = nullptr>
+				!is_sg_vector<T>::value &&
+				!is_sg_matrix<T>::value>* = nullptr>
 	void observe(const int64_t step, const std::string& name) const;
 
 	template<class T,
 			typename std::enable_if_t<
-					type_internal::is_sg_vector<T>::value ||
-					type_internal::is_sg_matrix<T>::value>* = nullptr>
+					is_sg_vector<T>::value ||
+					is_sg_matrix<T>::value>* = nullptr>
 	void observe(const int64_t step, const std::string& name) const;
 
 	/**
@@ -1323,10 +1322,10 @@ void CSGObject::observe(
 	this->observe(obs);
 }
 
-	template<class T,
-			typename std::enable_if_t<
-					!type_internal::is_sg_vector<T>::value &&
-					!type_internal::is_sg_matrix<T>::value>* = nullptr>
+template<class T,
+		typename std::enable_if_t<
+				!is_sg_vector<T>::value &&
+				!is_sg_matrix<T>::value>*>
 void CSGObject::observe(const int64_t step, const std::string& name) const
 {
 	auto param = this->get_parameter(BaseTag(name));
@@ -1337,8 +1336,8 @@ void CSGObject::observe(const int64_t step, const std::string& name) const
 
 template<class T,
 		typename std::enable_if_t<
-				type_internal::is_sg_vector<T>::value ||
-				type_internal::is_sg_matrix<T>::value>* = nullptr>
+				is_sg_vector<T>::value ||
+				is_sg_matrix<T>::value>*>
 void CSGObject::observe(const int64_t step, const std::string& name) const
 {
 	auto param = this->get_parameter(BaseTag(name));
