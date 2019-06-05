8c74516e61e95fb3e4c52382a71cd3e741468384
Giovanni De Toni <giovanni.det@gmail.com>
2019-06-05 10:51:40 +0200

    Refactor ToStringVisitor to its own file.
    This will reduce the LoC of SGObject.cpp and it will also
    enable developers to use the visitor in other locations.

---
+++ src/shogun/io/visitors/ToStringVisitor.cpp
@@ -0,0 +1,65 @@
+/*
+ * This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Viktor Gal, Giovanni De Toni
+ */
+
+#include <shogun/base/SGObject.h>
+#include <shogun/io/visitors/ToStringVisitor.h>
+
+using namespace shogun;
+
+virtual void ToStringVisitor::on(bool *v) {
+	stream() << (*v ? "true" : "false");
+}
+
+virtual void ToStringVisitor::on(int32_t *v) {
+	stream() << *v;
+}
+
+virtual void ToStringVisitor::on(int64_t *v) {
+	stream() << *v;
+}
+
+virtual void ToStringVisitor::on(float *v) {
+	stream() << *v;
+}
+
+virtual void ToStringVisitor::on(double *v) {
+	stream() << *v;
+}
+
+virtual void ToStringVisitor::on(long double *v) {
+	stream() << *v;
+}
+
+virtual void ToStringVisitor::on(CSGObject **v) {
+	if (*v) {
+		stream() << (*v)->get_name() << "(...)";
+	} else {
+		stream() << "null";
+	}
+}
+virtual void ToStringVisitor::on(SGVector<int> *v) {
+	to_string(v);
+}
+
+virtual void ToStringVisitor::on(SGVector<float> *v) {
+	to_string(v);
+}
+
+virtual void ToStringVisitor::on(SGVector<double> *v) {
+	to_string(v);
+}
+
+virtual void ToStringVisitor::on(SGMatrix<int> *mat) {
+	to_string(mat);
+}
+
+virtual void ToStringVisitor::on(SGMatrix<float> *mat) {
+	to_string(mat);
+}
+
+virtual void ToStringVisitor::on(SGMatrix<double> *mat) {
+	to_string(mat);
+}
