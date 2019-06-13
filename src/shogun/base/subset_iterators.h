df723488a0030e0c4403d320dc0a39c2a40f40c5
gf712 <gil_f.hoben@hotmail.com>
2019-06-13 16:03:13 +0100

    added operator++(int)

--- src/shogun/base/subset_iterators.h
+++ src/shogun/base/subset_iterators.h
@@ -8,56 +8,104 @@
 #define SHOGUN_SUBSET_ITERATORS_H
 
 #include <shogun/features/Features.h>
+#include <shogun/labels/Labels.h>
 
-namespace shogun {
-    template<typename T, std::enable_if<
-            (std::is_base_of<T, CFeatures>::value) || (std::is_base_of<T, CLabels>::value)> * = nullptr>
-    class SubsetIterator {
-    public:
-        SubsetIterator(T *pointer) {
-            if (pointer->get_subset_stack()->get_last_subset() != nullptr)
-                m_subset = pointer->get_subset_stack()->get_last_subset()->get_subset_idx();
-            m_idx = 0;
-            m_argsorted_subset = CMath::argsort(m_subset);
-            m_argsorted_subset_iter = m_argsorted_subset.begin();
-            m_ptr = pointer;
-            SG_REF(m_ptr)
-        }
+namespace shogun
+{
+	template <typename T>
+	class CDenseFeatures;
 
-        ~SubsetIterator() {
-            SG_UNREF(m_ptr)
-        }
+	//    template <typename T>
+	//    class CDenseLabels;
 
-        SubsetIterator operator++(int) {
-            m_argsorted_subset_iter++;
-            m_idx++;
-            return *this;
-        }
+	template <typename T>
+	struct return_type_subset_iterator
+	{
+	};
 
-        SubsetIterator& operator++() {
-            m_argsorted_subset_iter++;
-            m_idx++;
-            return *this;
-        }
+	template <typename T>
+	struct return_type_subset_iterator<CDenseFeatures<T>>
+	{
+		using type = SGVector<T>;
+	};
 
-        const auto operator*() {
-            index_t idx;
-            if (m_ptr->get_subset_stack()->get_last_subset() != nullptr)
-                idx = m_subset[*m_argsorted_subset_iter];
-            else
-                idx = m_idx;
-            if constexpr (std::is_base_of<CFeatures, T>::value)
-                return m_ptr->get_feature_vector(idx);
-            if constexpr (std::is_base_of<CLabels, T>::value)
-                return m_ptr->get_label(idx);
-        }
+	//    template <typename T>
+	//    struct return_type_subset_iterator<CDenseLabels<T>>
+	//    {
+	//        using type = T;
+	//    };
 
-    private:
-        index_t m_idx;
-        T *m_ptr;
-        SGVector<index_t> m_subset;
-        SGVector<index_t> m_argsorted_subset;
-        SGVector<index_t>::iterator m_argsorted_subset_iter;
-    };
-}
-#endif //SHOGUN_SUBSET_ITERATORS_H
+	template <
+	    typename T, std::enable_if<
+	                    (std::is_base_of<T, CFeatures>::value) ||
+	                    (std::is_base_of<T, CLabels>::value)>* = nullptr>
+	class SubsetIterator
+	{
+	public:
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = typename return_type_subset_iterator<T>::type;
+		using difference_type = index_t;
+		// not a reference but is used by stl algorithms to check type
+		using reference = typename return_type_subset_iterator<T>::type;
+		using pointer = typename return_type_subset_iterator<T>::type*;
+
+		SubsetIterator(T* pointer, index_t idx = 0)
+		{
+			if (pointer->get_subset_stack()->get_last_subset() != nullptr)
+				m_subset = pointer->get_subset_stack()
+				               ->get_last_subset()
+				               ->get_subset_idx();
+			m_idx = idx;
+			// need to keep this variable so that the vector destructor isn't
+			// called
+			m_argsorted_subset = CMath::argsort(m_subset);
+			m_argsorted_subset_iter = m_argsorted_subset.begin() + idx;
+			m_ptr = pointer;
+		}
+
+		SubsetIterator& operator++()
+		{
+			m_argsorted_subset_iter++;
+			m_idx++;
+			return *this;
+		}
+
+		SubsetIterator operator++(int)
+		{
+			SubsetIterator retval(*this);
+			++(*this);
+			return retval;
+		}
+
+		bool operator==(const SubsetIterator<T>& other)
+		{
+			return m_ptr == other.m_ptr && m_idx == other.m_idx;
+		}
+
+		bool operator!=(const SubsetIterator<T>& other)
+		{
+			return !(*this == other);
+		}
+
+		const reference operator*()
+		{
+			index_t idx;
+			if (m_ptr->get_subset_stack()->get_last_subset() != nullptr)
+				idx = *m_argsorted_subset_iter;
+			else
+				idx = m_idx;
+			if constexpr (std::is_base_of<CFeatures, T>::value)
+				return m_ptr->get_feature_vector(idx);
+			if constexpr (std::is_base_of<CLabels, T>::value)
+				return m_ptr->get_label(idx);
+		}
+
+	private:
+		index_t m_idx;
+		T* m_ptr;
+		SGVector<index_t> m_subset;
+		SGVector<index_t> m_argsorted_subset;
+		SGVector<index_t>::iterator m_argsorted_subset_iter;
+	};
+} // namespace shogun
+#endif // SHOGUN_SUBSET_ITERATORS_H
