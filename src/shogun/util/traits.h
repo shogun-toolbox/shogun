f19f2df6fd658b29c2912cdd82ceeebf02238a18
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2019-06-16 10:44:20 +0300

    Constexpr-based things in Any (#4677)
    * Constexpr-based hashing of any
    * Use constexpr based compare

--- src/shogun/util/traits.h
+++ src/shogun/util/traits.h
@@ -5,34 +5,105 @@
 
 namespace shogun
 {
-	namespace utils
+	namespace traits
 	{
-		template<typename T, typename _ = void>
-		struct is_container : std::false_type {};
+		template<typename... Ts>
+		struct try_to_declare {};
 
 		template<typename... Ts>
-		struct is_container_helper {};
+		using when_exists = std::conditional_t<false, try_to_declare<Ts...>, void>;
+
+		template<typename T, typename _ = void>
+		struct is_container : std::false_type {};
 
 		template<typename T>
 		struct is_container<
 			T,
-			std::conditional_t<
-				false,
-				is_container_helper<
-					typename T::value_type,
-					typename T::size_type,
-					typename T::allocator_type,
-					typename T::iterator,
-					typename T::const_iterator,
-					decltype(std::declval<T>().size()),
-					decltype(std::declval<T>().begin()),
-					decltype(std::declval<T>().end()),
-					decltype(std::declval<T>().cbegin()),
-					decltype(std::declval<T>().cend())
-					>,
-				void
-				>
-			> : public std::true_type {};
-	} // namespace utils
+			when_exists<
+				typename T::value_type,
+				typename T::size_type,
+				typename T::allocator_type,
+				typename T::iterator,
+				typename T::const_iterator,
+				decltype(std::declval<T>().size()),
+				decltype(std::declval<T>().begin()),
+				decltype(std::declval<T>().end()),
+				decltype(std::declval<T>().cbegin()),
+				decltype(std::declval<T>().cend())
+			>
+		> : public std::true_type {};
+
+		template<typename T, typename _ = void>
+		struct is_hashable : std::false_type {};
+
+		template<typename T>
+		struct is_hashable<
+			T,
+			when_exists<
+				decltype(std::hash<T>{}(std::declval<T>()))
+			>
+		> : public std::true_type {};
+
+		template<typename T, typename _ = void>
+		struct is_pair : std::false_type {};
+
+		template<typename T>
+		struct is_pair<
+			T,
+			when_exists<
+				decltype(std::declval<T>().first),
+				decltype(std::declval<T>().second)
+			>
+		> : public std::true_type {};
+
+		template<typename T, typename _ = void>
+		struct has_equals : std::false_type {};
+
+		template<typename T>
+		struct has_equals<
+			T,
+			when_exists<
+				decltype(std::declval<T>().equals(std::declval<T>()))
+			>
+		> : public std::true_type {};
+
+		template<typename T, typename _ = void>
+		struct has_equals_ptr : std::false_type {};
+
+		template<typename T>
+		struct has_equals_ptr<
+			T,
+			when_exists<
+				decltype(std::declval<T>()->equals(std::declval<T>()))
+			>
+		> : public std::true_type {};
+
+		template<typename T, typename _ = void>
+		struct is_comparable : std::false_type {};
+
+		template<typename T>
+		struct is_comparable<
+			T,
+			when_exists<
+				decltype(std::declval<T>() == std::declval<T>())
+			>
+		> : public std::true_type {};
+
+		template<typename T, typename _ = void>
+		struct is_functional : std::false_type {};
+
+		template<typename T>
+		struct is_functional<
+			T,
+			when_exists<
+				decltype(std::declval<T>()()),
+				typename T::result_type
+			>
+		> : public std::true_type {};
+
+		template<typename T>
+		using returns_void = std::is_same<typename T::result_type, void>;
+
+	} // namespace traits
 } // namespace shogun
 #endif
