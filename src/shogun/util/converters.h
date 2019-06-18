a5935d25502bf66f05850a11bf52feed950f98e4
Viktor Gal <viktor.gal@maeth.com>
2019-06-18 13:57:11 +0200

    undefine isfinite macro in ruby interface

--- src/shogun/util/converters.h
+++ src/shogun/util/converters.h
@@ -18,6 +18,10 @@
 // TODO: microsoft should really start supporting c++11
 #define IS_FINITE(x) _finite(x)
 #else
+// TODO: there must be a better way to fix this
+#ifdef SWIGRUBY
+#undef isfinite
+#endif
 #include <cmath>
 #define IS_FINITE(x) std::isfinite(x)
 #endif
