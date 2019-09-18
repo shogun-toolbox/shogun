1a4a8877f34df735113ca5cbd472d9ffc83d998d
gf712 <gil_f.hoben@hotmail.com>
2019-09-18 08:18:13 +0100

    fixes container representation

--- src/shogun/io/visitors/ToStringVisitor.cpp
+++ src/shogun/io/visitors/ToStringVisitor.cpp
@@ -10,103 +10,114 @@
 using namespace shogun;
 
 void ToStringVisitor::on(bool *v) {
-	stream() << (*v ? "true" : "false");
+	stream() << (*v ? "true" : "false") << m_buffer;
 }
 
 void ToStringVisitor::on(int32_t *v) {
-	stream() << *v;
+	stream() << *v << m_buffer;
 }
 
 void ToStringVisitor::on(int64_t *v) {
-	stream() << *v;
+	stream() << *v << m_buffer;
 }
 
 void ToStringVisitor::on(int8_t *v) {
-	stream() << *v;
+	stream() << *v << m_buffer;
 }
 
 void ToStringVisitor::on(int16_t *v) {
-	stream() << *v;
+	stream() << *v << m_buffer;
 }
 
 void ToStringVisitor::on(std::string *v) {
-	stream() << *v;
+	stream() << *v << m_buffer;
 }
 
 void ToStringVisitor::on(CSGObject **v) {
 	if (*v) {
-		stream() << (*v)->get_name() << "(...)";
+		stream() << (*v)->get_name() << "(...)" << m_buffer;
 	} else {
-		stream() << "null";
+		stream() << "null" << m_buffer;
 	}
 }
 
 void ToStringVisitor::on(char *string) {
-	stream() << *string;
+	stream() << *string << m_buffer;
 }
 
 void ToStringVisitor::on(uint8_t *uint8) {
-	stream() << *uint8;
+	stream() << *uint8 << m_buffer;
 }
 
 void ToStringVisitor::on(uint16_t *uint16) {
-	stream() << *uint16;
+	stream() << *uint16 << m_buffer;
 }
 
 void ToStringVisitor::on(uint32_t *uint32) {
-	stream() << *uint32;
+	stream() << *uint32 << m_buffer;
 }
 
 void ToStringVisitor::on(uint64_t *uint64) {
-	stream() << *uint64;
+	stream() << *uint64 << m_buffer;
 }
 
 void ToStringVisitor::on(complex128_t *complex128) {
-	stream() << *complex128;
+	stream() << *complex128 << m_buffer;
 }
 
 void ToStringVisitor::enter_matrix(index_t *rows, index_t *cols) {
 	stream() << "Matrix<"<< *rows << "x" << *cols << ">( ";
+	m_buffer = ", ";
 }
 
 void ToStringVisitor::enter_vector(index_t *size) {
 	stream() << "Vector<" << *size << ">( ";
+	m_buffer = ", ";
 }
 
 void ToStringVisitor::enter_std_vector(size_t *size) {
 	stream() << "Vector<" << *size << ">( ";
+	m_buffer = ", ";
 }
 
 void ToStringVisitor::enter_map(size_t *size) {
 	stream() << "Map<" << *size << ">( ";
+	m_buffer = ", ";
 }
 
 void ToStringVisitor::on(float32_t *v) {
-	stream() << *v;
+	stream() << *v << m_buffer;
 }
 
 void ToStringVisitor::on(float64_t *v) {
-	stream() << *v;
+	stream() << *v << m_buffer;
 }
 
 void ToStringVisitor::on(floatmax_t *v) {
-	stream() << *v;
+	stream() << *v << m_buffer;
 }
 
 void ToStringVisitor::exit_matrix(index_t *rows, index_t *cols) {
 	stream() << ")";
+	m_buffer.clear();
 }
 
 void ToStringVisitor::exit_vector(index_t *size) {
-	stream() << ")";
+	stream().seekp(-2, std::ios_base::end);
+	stream() << " )";
+	m_buffer.clear();
 }
 
 void ToStringVisitor::exit_std_vector(size_t *size) {
-	stream() << ")";
+	stream().seekp(-2, std::ios_base::end);
+	stream() << " )";
+	m_buffer.clear();
 }
 
 void ToStringVisitor::exit_map(size_t *size) {
-	stream() << ")";
+	stream().seekp(-2, std::ios_base::end);
+	stream() << " )";
+	m_buffer.clear();
 }
 
 void ToStringVisitor::enter_matrix_row(index_t *rows, index_t *cols)
