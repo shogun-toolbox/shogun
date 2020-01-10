f1991ad27663ff64e8fe0ebfd3ed5a6f1c5cbfdf
thebhatman <manjunathbhat9920@gmail.com>
2020-01-10 15:35:25 +0530

    Gradients added

--- src/shogun/neuralnets/NeuralMishLayer.cpp
+++ src/shogun/neuralnets/NeuralMishLayer.cpp
@@ -28,3 +28,27 @@ void NeuralMishLayer::compute_activations(SGVector<float64_t> parameters,
 		activation *= std::tanh(std::log(1 + std::exp(activation)));
 	}
 }
+
+void NeuralRectifiedLinearLayer::compute_local_gradients(
+		SGMatrix<float64_t> targets)
+{
+	if (targets.num_rows != 0)
+	{
+		int32_t length = m_num_neurons*m_batch_size;
+		for (int32_t i=0; i<length; i++)
+		{
+			m_local_gradients[i] = (m_activations[i]-targets[i])/m_batch_size;
+		}
+	}
+	else
+	{
+		int32_t len = m_num_neurons*m_batch_size;
+		for (int32_t i=0; i< len; i++)
+		{
+			auto e = std::exp(m_activations[i]);
+			auto omega = 4 * (m_activations[i] + 1) + 4*pow(e, 2) + pow(e, 3) + e*(4*m_activations[i] + 6);
+			auto delta = 2*e + pow(e, 2) + 2;
+			m_local_gradients[i] = m_activation_gradients[i] * e * omega/pow(delta, 2);
+		}
+	}
+}
