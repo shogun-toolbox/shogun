dcbd29641ed71fd65178e502c848ee33e03a1032
Viktor Gal <viktor.gal@maeth.com>
2020-01-25 13:18:48 +0100

    fix benchmark main and add PutPerceptron benchmark

--- src/shogun/util/benchmark_main.cc
+++ src/shogun/util/benchmark_main.cc
@@ -1,12 +1,9 @@
 #include <benchmark/benchmark.h>
 
-using namespace shogun;
-
 int main(int argc, char** argv)
 {
 	benchmark::Initialize(&argc, argv);
 	benchmark::RunSpecifiedBenchmarks();
 
-	exit_shogun();
 	return 0;
 }
