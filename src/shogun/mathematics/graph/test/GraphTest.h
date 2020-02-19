b8f6eaa80ebbe99dad06271f3f10c74483ca6403
Viktor Gal <viktor.gal@maeth.com>
2020-02-19 11:59:42 +0100

    add helper function for binary operator testing

--- src/shogun/mathematics/graph/test/GraphTest.h
+++ src/shogun/mathematics/graph/test/GraphTest.h
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <shogun/mathematics/graph/GraphExecutor.h>
+#include <shogun/util/zip_iterator.h>
 
 #include <tuple>
 
@@ -10,6 +11,37 @@ class GraphTest : public ::testing::Test
 protected:
 	GraphTest() : m_backends(shogun::graph::available_backends())
 	{
+	}
+
+	void test_binary_op_results(const std::shared_ptr<shogun::graph::Graph>& graph,
+		const shogun::SGVector<T>& X1,
+		const shogun::SGVector<T>& X2,
+		const shogun::SGVector<T>& expected_result1,
+		const shogun::SGVector<T>& expected_result2)
+	{
+		for (auto&& backend : this->m_backends)
+		{
+			graph->build(backend);
+
+			std::vector<std::shared_ptr<shogun::graph::Tensor>> result = graph->evaluate(
+				std::vector{std::make_shared<shogun::graph::Tensor>(X1),
+					std::make_shared<shogun::graph::Tensor>(X2)});
+
+			auto result1 = result[0]->template as<shogun::SGVector<T>>();
+			auto result2 = result[1]->template as<shogun::SGVector<T>>();
+
+			for (const auto& [expected_i, result_i] :
+				shogun::zip_iterator(expected_result1, result1))
+			{
+				EXPECT_EQ(expected_i, result_i);
+			}
+
+			for (const auto& [expected_i, result_i] :
+				shogun::zip_iterator(expected_result2, result2))
+			{
+				EXPECT_EQ(expected_i, result_i);
+			}
+		}
 	}
 
 	std::set<GRAPH_BACKEND> m_backends;
