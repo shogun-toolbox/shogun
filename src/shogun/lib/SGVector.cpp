8a7c9b80a857d43f5a81bf05585b45f812f69a54
Viktor Gal <viktor.gal@maeth.com>
2020-03-02 12:57:15 +0100

    add SG_ALIGNED_FREE in order to handle aligned free in MSVC

--- src/shogun/lib/SGVector.cpp
+++ src/shogun/lib/SGVector.cpp
@@ -399,7 +399,7 @@ void SGVector<T>::init_data()
 template<class T>
 void SGVector<T>::free_data()
 {
-	SG_FREE(vector);
+	SG_ALIGNED_FREE(vector);
 	vector=NULL;
 	vlen=0;
 	gpu_ptr=NULL;
@@ -885,7 +885,7 @@ void SGVector<T>::convert_to_matrix(T*& matrix, index_t nrows, index_t ncols, co
 		error("SGVector::convert_to_matrix():: Dimensions mismatch");
 
 	if (matrix!=NULL)
-		SG_FREE(matrix);
+		SG_ALIGNED_FREE(matrix);
 	matrix=SG_ALIGNED_MALLOC(T, nrows*ncols, alignment::container_alignment);
 
 	if (fortran_order)
