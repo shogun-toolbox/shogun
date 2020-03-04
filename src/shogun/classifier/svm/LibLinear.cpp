92d6d67d657ca3721420253ce1b3e08ba48122ab
Khalifa1997 <ahmed70000@gmail.com>
2020-03-04 20:56:40 +0200

    Using casting to send index_t type to SG_ALIGNED_MALLOC

--- src/shogun/classifier/svm/LibLinear.cpp
+++ src/shogun/classifier/svm/LibLinear.cpp
@@ -117,9 +117,8 @@ bool LibLinear::train_machine(std::shared_ptr<Features> data)
 		}
 	}
 	SGVector<float64_t> w;
-	index_t len =num_feat + 1;
 	if (get_bias_enabled())
-		w = SGVector<float64_t>(SG_ALIGNED_MALLOC(float64_t,len,alignment::container_alignment), num_feat);
+		w = SGVector<float64_t>(SG_ALIGNED_MALLOC(float64_t,index_t(num_feat + 1),alignment::container_alignment), num_feat);
 	else
 		w = SGVector<float64_t>(num_feat);
 
