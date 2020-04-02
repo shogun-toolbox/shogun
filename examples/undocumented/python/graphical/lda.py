56a8f97d259f8d6b4a11885c9c8760432e029a92
Dmitry <vinnik.dmitry07@gmail.com>
2020-04-02 18:13:13 +0300

    Minor changes to lda.py: reformat.
    (cherry picked from commit 1814ebdac9f079ccee9424935786f523724d511c)

--- examples/undocumented/python/graphical/lda.py
+++ examples/undocumented/python/graphical/lda.py
@@ -5,24 +5,24 @@ import util
 plt.title('LDA')
 util.DISTANCE = 0.5
 
-gamma=0.1
+gamma = 0.1
 
 # positive examples
-pos=util.get_realdata(True)
-plot(pos[0,:], pos[1,:], "r.")
+pos = util.get_realdata(True)
+plt.plot(pos[0, :], pos[1, :], "r.")
 
 # negative examples
-neg=util.get_realdata(False)
-plot(neg[0,:], neg[1,:], "b.")
+neg = util.get_realdata(False)
+plt.plot(neg[0, :], neg[1, :], "b.")
 
 # train lda
-labels=util.get_labels()
-features=util.get_realfeatures(pos, neg)
-lda=LDA(gamma, features, labels)
+labels = util.get_labels()
+features = util.get_realfeatures(pos, neg)
+lda = LDA(gamma, features, labels)
 lda.train()
 
 # compute output plot iso-lines
-x, y, z=util.compute_output_plot_isolines(lda)
+x, y, z = util.compute_output_plot_isolines(lda)
 
 c = plt.pcolor(x, y, z)
 plt.contour(x, y, z, linewidths=1, colors='black', hold=True)
