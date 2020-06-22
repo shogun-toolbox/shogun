aea04e761fe7c63c129c76664f07fc17bf71be67
LiuYuHui <liuyuhui002@foxmail.com>
2020-06-22 11:15:10 +0800

    refine function name

--- src/shogun/labels/BinaryLabelEncoder.h
+++ src/shogun/labels/BinaryLabelEncoder.h
@@ -33,7 +33,7 @@ namespace shogun
 		{
 			const auto result_vector = labs->as<DenseLabels>()->get_labels();
 			check_is_valid(result_vector);
-			if (is_convert_float_to_int(result_vector))
+			if (!can_convert_float_to_int(result_vector))
 			{
 				io::warn(
 				    "({}, {}) have been converted to (-1, 1).",
@@ -118,12 +118,11 @@ namespace shogun
 			    std::unordered_set<float64_t>(vec.begin(), vec.end());
 			require(
 			    unique_set.size() == 2,
-			    "Binary labels should contain only two elements, ({}) have "
-			    "been detected.",
+			    "Binary labels should contain only two elements, can not interpret ({}) as binary labels",
 			    fmt::join(unique_set, ", "));
 		}
 
-		bool is_convert_float_to_int(const SGVector<float64_t>& vec) const
+		bool can_convert_float_to_int(const SGVector<float64_t>& vec) const
 		{
 			SGVector<int32_t> converted(vec.vlen);
 			std::transform(
@@ -132,7 +131,7 @@ namespace shogun
 			return std::equal(
 			    vec.begin(), vec.end(), converted.begin(),
 			    [](auto&& e1, auto&& e2) {
-				    return std::abs(e1 - e2) >
+				    return std::abs(e1 - e2) <
 				           std::numeric_limits<float64_t>::epsilon();
 			    });
 		}
