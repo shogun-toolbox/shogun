f7492f2ca3117c1c8873781da30f80bb0bd95bf4
JasonSurendran <surendranjason@gmail.com>
2020-07-11 22:13:05 -0400

    removing from sigmoid_as only

--- src/shogun/evaluation/SigmoidCalibration.h
+++ src/shogun/evaluation/SigmoidCalibration.h
@@ -142,11 +142,11 @@ namespace shogun
 	#ifndef SWIG
 	public:
 		static constexpr std::string_view kSigmoidAs = "sigmoid_as";
-		static constexpr std::string_view kSigmoidBs = "sigmoid_bs";
-		static constexpr std::string_view kMaxiter = "maxiter";
-		static constexpr std::string_view kMinstep = "minstep";
-		static constexpr std::string_view kSigma = "sigma";
-		static constexpr std::string_view kEpsilon = "epsilon";
+		static constexpr std::string_view kSigmoidBs = "m_sigmoid_bs";
+		static constexpr std::string_view kMaxiter = "m_maxiter";
+		static constexpr std::string_view kMinstep = "m_minstep";
+		static constexpr std::string_view kSigma = "m_sigma";
+		static constexpr std::string_view kEpsilon = "m_epsilon";
 	#endif
 	};
 }
