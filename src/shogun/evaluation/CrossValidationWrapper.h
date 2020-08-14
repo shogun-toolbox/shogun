a330f537f240c0ad857b89afb570359f14c12339
LiuYuHui <liuyuhui002@foxmail.com>
2020-08-14 21:57:28 +0800

    add unit test

--- src/shogun/evaluation/CrossValidationWrapper.h
+++ src/shogun/evaluation/CrossValidationWrapper.h
@@ -12,19 +12,21 @@
 #include <shogun/machine/Machine.h>
 #include <type_traits>
 #include <string_view>
+#include <memory>
+#include <queue>
 #include <shogun/evaluation/CrossValidation.h>
 
 #define IGNORE_IN_CLASSLIST
 
 namespace shogun
 {
-    template<typename MachineType, std::enable_if_t<
-                    std::is_base_of_v<Machine, MachineType>>>
+    template<typename MachineType>
     class CrossValidationWrapper : public SGObject
     {   
-        template<typename T, typename U>
-        CrossValidationWrapper(T&& params, U&& cross_validater): 
-            m_params(std::forward<T>(params), m_cross_validater(std::forward<U>(cross_validater)))
+    public:
+        CrossValidationWrapper(std::pair<std::string_view, std::vector<float64_t>> params, 
+            std::shared_ptr<CrossValidation> cross_validater): 
+            m_params(params), m_cross_validater(cross_validater)
         {
         }
 
@@ -36,7 +38,8 @@ namespace shogun
             for(const float64_t& param : m_params.second)
             {
                 m_machine->put(param_name, param);
-                auto result = m_cross_validater->evaluate(data, labs)->get_mean();
+                auto result = m_cross_validater->evaluate(data, labs)
+                            ->as<CrossValidationResult>()->get_mean();
                 best_param_candidates.push(std::make_pair(result, param));
             }
             const auto best_param_pair = best_param_candidates.top();
@@ -46,7 +49,11 @@ namespace shogun
             m_machine->train(data);
         }
 
-        std::shared_ptr<Labels> apply(std::shared<Features> data)
+        const char* get_name() const override
+        {
+            return "CrossValidationWrapper";
+        }
+        std::shared_ptr<Labels> apply(std::shared_ptr<Features> data)
         {
             return m_machine ->apply(data);
         }
@@ -56,8 +63,8 @@ namespace shogun
         }
     private:
         std::pair<std::string_view, std::vector<float64_t>> m_params;
-        std::shared_ptr<CrossValidation> m_cross_validater;
-        std::shard_ptr<MachineType> m_machine;
+        const std::shared_ptr<CrossValidation> m_cross_validater;
+        std::shared_ptr<Machine> m_machine;
         float64_t m_best_param;
     };
 }
---
+++ tests/unit/evaluation/CrossValidationWrapper_unittest.cc
@@ -0,0 +1,41 @@
+/*
+ * This software is distributed under BSD 3-clause license (see LICENSE file).
+ *
+ * Authors: Yuhui Liu
+ */
+
+#include <shogun/evaluation/CrossValidationWrapper.h>
+#include <shogun/evaluation/CrossValidation.h>
+#include <shogun/regression/LinearRidgeRegression.h>
+#include <shogun/evaluation/ContingencyTableEvaluation.h>
+#include <shogun/evaluation/CrossValidationSplitting.h>
+#include <shogun/evaluation/MeanAbsoluteError.h>
+#include <shogun/evaluation/MeanSquaredError.h>
+#include <gtest/gtest.h>
+#include "environments/RegressionTestEnvironment.h"
+extern RegressionTestEnvironment* regression_test_env;
+
+
+using namespace shogun;
+
+TEST(CrossValidation_Wrapper, fit)
+{
+
+	auto train_feats = regression_test_env->get_features_train();
+	auto test_feats = regression_test_env->get_features_test();
+
+	auto labels_test = regression_test_env->get_labels_test();
+	auto labels_train = regression_test_env->get_labels_train();
+    auto strategy = std::make_shared<CrossValidationSplitting>(labels_train, 2);
+    strategy->put("seed", 2);
+    auto machine = std::make_shared<LinearRidgeRegression>();
+    auto evaluation_criterion = std::make_shared<MeanSquaredError>();
+    auto cv = std::make_shared<CrossValidation>(machine, strategy, evaluation_criterion);
+    std::pair<std::string_view, std::vector<double>> params{"tau", {0.1, 0.2, 0.5, 0.8, 2}};
+    auto cv_wrapper = std::make_shared<CrossValidationWrapper<LinearRidgeRegression>>(params, cv);
+    cv_wrapper->fit(train_feats, labels_train);
+    auto pred = machine->apply(test_feats);
+	MeanSquaredError evaluate;
+	float64_t result = evaluate.evaluate(pred, labels_test);
+	EXPECT_NEAR(result, 0.035017321025255156,  std::numeric_limits<float64_t>::epsilon());
+}
