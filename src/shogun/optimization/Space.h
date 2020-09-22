641018b207d717decd04dd93379f1ba038e91874
LiuYuHui <liuyuhui002@foxmail.com>
2020-09-22 11:14:05 +0800

    refine space

--- src/shogun/optimization/Space.h
+++ src/shogun/optimization/Space.h
@@ -3,11 +3,12 @@
  *
  * Authors: Yuhui Liu
  */
-#include<string_view>
+#include<string>
 #include<variant>
 #include<shogun/mathematics/RandomMixin.h>
 #include<shogun/mathematics/UniformRealDistribution.h>
 #include<shogun/base/SGObject.h>
+#include<shogun/util/traits.h>
 namespace shogun
 {
     /**
@@ -16,13 +17,13 @@ namespace shogun
      */
     class Dimension : public RandomMixin<Dimension> {
     public:
-        Dimension(std::pair<float64_t, float64_t> bound, std::string_view param_name,
-            std::string_view prior = "uniform", std::string_view param_type = "Real") :
+        Dimension(std::string param_name, std::pair<float64_t, float64_t> bound, 
+            std::string prior = "uniform", std::string param_type = "Real") :
             m_bound(bound), m_prior(prior),
             m_param_type(param_type) {}
 
-        Dimension(std::vector<std::shared_ptr<SGObject>> objects, std::string_view param_name,
-            std::string_view prior = "uniform", std::string_view param_type = "Categority") :
+        Dimension(std::string param_name, std::vector<std::shared_ptr<SGObject>> objects, 
+            std::string prior = "uniform", std::string param_type = "Categority") :
              m_bound(bound), m_prior(prior), m_param_type(param_type) {}
 
          //unnormalize the value in [0, 1] to origianl space [lower, high]
@@ -41,7 +42,7 @@ namespace shogun
         }
         
         //this function aims to select the initial point
-        std::pair<string_view, float64_t> random_samples()
+        std::pair<string, float64_t> random_samples()                                 
         {
             if(m_prior == "uniform") {
                 auto uniform = UniformRealDistribution<>(0.0, 1.0);
@@ -50,33 +51,33 @@ namespace shogun
             //Todo log-uniform
         }
 
-    private:
+    public:
         using sgobjects_t = std::vector<std::shared_ptr<SGObject>>;
         //float64_t m_lower_bound, m_upper_bound;
         //{ConstKernel, GaussianKernel}
         std::variant<std::pair<float64_t, float64_t>, sgobjects_t> m_bound;
-        std::string_view m_prior, m_param_name, m_param_type;
+        std::string m_prior, m_param_name, m_param_type;
     };
 
-    using param_type1 = std::tuple<float64_t, float64_t>;
-    using param_type2 = std::tuple<float64_t, float64_t, std::string_view>;
-    using param_type3 = std::tuple<float64_t, float64_t, std::string_view, 
-        std::string_view>;
-    using param_type4 = std::tuple<float64_t, float64_t, std::string_view, 
-        std::string_view, std::string_view>;
-    using param_type5 = std::tuple<std::shared_ptr<SGObject>, std::string_view, 
-        std::variant<param_type1, param_type2, param_type3, param_type4>>;
-    using param = std::pair<std::string_view, std::variant<param_type1, 
-        param_type2, param_type3, param_type4>>>;
+    using sgobject_t = std::shared_ptr<SGObject>;
+    struct DimensionWithSGObject : public SGObject 
+    {
+        DimensionWithSGObject(std::pair<std::string, std::vector<std::pair<sgobject_t , Dimension>>> params)
+            :sgobject_name(params.first), m_params(params.second)
+            {}
+        
+        std::vector<std::pair<sgobject_t , Dimension>> m_params;
+        std::string sgobject_name;
+    }; 
     class Space {
         
     public:
-        Space(std::vector<std::pair<string_view, >> s) {
-        m_dimensions = convert(s);   
+        Space(std::vector<std::variant<Dimension, DimensionWithSGObject>> s) {
+            m_dimensions = convert(s);   
         }
 
-        std::tuple<std::vector<std::string_view>, SGVector<float64_t>>random_samples() {
-            std::vector<std::string_view> param_names;
+        std::tuple<std::vector<std::string>, SGVector<float64_t>>random_samples() {
+            std::vector<std::string> param_names;
             SGVector<float64_t> param_values(m_dimensions.size());
             int idx = 0;
             for(auto dim: m_dimensions) {
@@ -91,17 +92,32 @@ namespace shogun
         // a list of parameters, and one of the parameters is kernel, which have 
         // other parameters, we should flatten those parameters to a linear structure,
         // so we can do bayesian optimization.
-        vector<Dimension> convert(vector<_> ) {
-        /*convert {("C1", (1.0, 4.0)),
+         /*convert {("C1", (1.0, 4.0)),
                 ("C2", (1.0, 4.0)),
-                ("kernel", ("GaussianKernel", "width" (1.0, 2.0),
-                            "ConstKernel", "const_value" (1.0, 2.0)))}
-        to
+                ("kernel", ("GaussianKernel", ("width" (1.0, 2.0)),
+                            ("ConstKernel", "const_value" (1.0, 2.0))))}
+        to 
         {("C1", (1.0, 4.0)),
         ("C2", (1.0, 4.0)),
         ("GaussianKernel::width", (1.0, 2.0)), 
         ("ConstKernel::const_value", (1.0, 2.0)),
         ("kernel": ("GaussianKernel", "ConstKernel"))}*/
+        void convert(std::vector<std::variant<Dimension, DimensionWithSGObject>> s ) {
+            for(auto v : s) {
+                std::visit(overloaded {
+                    [&](Dimension dim) {m_dimensions.push_back(dim);}
+                    [&](DimensionWithSGObject dim) {
+                        std::vector<SGObject> sgobjects;
+                        string name = dim.sgobject_name;
+                        auto params = std::transform(dim.m_params.begin(), dim.m_params.end(),
+                         std::back_inserter(sgobjects), [](std::pair<sgobject_t , Dimension> p) {
+                             return p.first;
+                         };
+                         
+                    }
+                }, v)
+            }
+            
         }
         vector<Dimension> m_dimensions; //X^m,where m represents the numbers of parameters, each dimension contains the constrict on this dimension.
     };
