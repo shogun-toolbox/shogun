006666894dcfabf8dc22fc6931fff5463ebd9359
AllenRLopez <39066932+AllenRLopez@users.noreply.github.com>
2020-12-08 08:35:27 -0500

    Fixed shogun import issue (#5126)

--- examples/undocumented/python/kernel_distantsegments.py
+++ examples/undocumented/python/kernel_distantsegments.py
@@ -7,12 +7,12 @@ testdat = lm.load_dna('../data/fm_test_dna.dat')
 parameter_list = [[traindat,testdat,5,5],[traindat,testdat,6,6]]
 
 def kernel_distantsegments (fm_train_dna=traindat,fm_test_dna=testdat,delta=5, theta=5):
-	from shogun import DistantSegmentsKernel
+	import shogun as sg
 
 	feats_train=sg.create_string_features(fm_train_dna, sg.DNA)
 	feats_test=sg.create_string_features(fm_test_dna, sg.DNA)
 
-	kernel=DistantSegmentsKernel(feats_train, feats_train, 10, delta, theta)
+	kernel=sg.DistantSegmentsKernel(feats_train, feats_train, 10, delta, theta)
 
 	km_train=kernel.get_kernel_matrix()
 	kernel.init(feats_train, feats_test)
