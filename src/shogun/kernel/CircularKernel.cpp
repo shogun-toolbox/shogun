0efcc6922ceb4e0960844e9b69b43df5780951d1
NanuSai <gnanasai.n18@iiits.in>
2021-01-19 21:27:25 +0530

    - use L1 distance

--- src/shogun/kernel/CircularKernel.cpp
+++ src/shogun/kernel/CircularKernel.cpp
@@ -6,22 +6,23 @@
 
 #include <shogun/kernel/CircularKernel.h>
 #include <shogun/mathematics/Math.h>
+#include <shogun/distance/ManhattanMetric.h>
 
 #include <utility>
 
 using namespace shogun;
 
-CircularKernel::CircularKernel(): ShiftInvariantKernel(), distance(NULL)
+CircularKernel::CircularKernel(): ShiftInvariantKernel()
 {
 	init();
 	set_cache_size(0);
 	set_sigma(1.0);
 }
 
-CircularKernel::CircularKernel(int32_t size, float64_t sig, std::shared_ptr<Distance> dist)
-: ShiftInvariantKernel(), distance(std::move(dist))
+CircularKernel::CircularKernel(int32_t size, float64_t sig)
+: ShiftInvariantKernel()
 {
-	ASSERT(distance)
+	ASSERT(m_distance)
 	
 	set_cache_size(size);
 	set_sigma(sig);
@@ -29,13 +30,13 @@ CircularKernel::CircularKernel(int32_t size, float64_t sig, std::shared_ptr<Dist
 }
 
 CircularKernel::CircularKernel(
-	std::shared_ptr<Features >l, std::shared_ptr<Features >r, float64_t sig, std::shared_ptr<Distance> dist)
-: ShiftInvariantKernel(), distance(std::move(dist))
+	std::shared_ptr<Features >l, std::shared_ptr<Features >r, float64_t sig)
+: ShiftInvariantKernel()
 {
-	ASSERT(distance)
+	ASSERT(m_distance)
 	
 	set_sigma(sig);
-	set_cache_size(10)
+	set_cache_size(10);
 	init();
 	init(std::move(l), std::move(r));
 }
@@ -48,9 +49,9 @@ CircularKernel::~CircularKernel()
 
 bool CircularKernel::init(std::shared_ptr<Features> l, std::shared_ptr<Features> r)
 {
-	ASSERT(distance)
+	ASSERT(m_distance);
 	Kernel::init(l,r);
-	distance->init(l,r);
+	m_distance->init(l,r);
 	return init_normalizer();
 }
 
@@ -61,13 +62,15 @@ void CircularKernel::load_serializable_post() noexcept(false)
 
 void CircularKernel::init()
 {
-	SG_ADD(&distance, "distance", "Distance to be used.", ParameterProperties::HYPER);
+	auto dist = std::make_shared<ManhattanMetric>();
+	m_distance = dist;
+
 	SG_ADD(&sigma, "sigma", "Sigma kernel parameter.", ParameterProperties::HYPER);
 }
 
 float64_t CircularKernel::compute(int32_t idx_a, int32_t idx_b)
 {
-	float64_t dist=distance->distance(idx_a, idx_b);
+	float64_t dist=m_distance->distance(idx_a, idx_b);
 	float64_t ds_ratio=dist/sigma;
 
 	if (dist < sigma)
